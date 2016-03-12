ca6dba53159090271d74196f1582d2711d207761
xieguigang <xie.guigang@gmail.com>
2016-03-13 00:56:06 +0800

    build syntax parser

---
+++ Dev.Shl/TokenTag/OokTokenTagger.vb
@@ -0,0 +1,50 @@
+﻿Imports Microsoft.VisualBasic.Scripting.ShoalShell.Interpreter.LDM.Expressions
+Imports Microsoft.VisualStudio.Text
+Imports Microsoft.VisualStudio.Text.Tagging
+Imports Microsoft.VisualBasic
+
+Namespace OokLanguage
+
+    Public NotInheritable Class OokTokenTagger : Implements ITagger(Of OokTokenTag)
+
+        ReadOnly _buffer As ITextBuffer
+        ReadOnly _ookTypes As IDictionary(Of String, ExpressionTypes)
+
+        Public Sub New(ByVal buffer As ITextBuffer)
+            _buffer = buffer
+            _ookTypes = New Dictionary(Of String, ExpressionTypes)
+            _ookTypes("die") = ExpressionTypes.Die
+            _ookTypes("call") = ExpressionTypes.FunctionCalls
+            _ookTypes("goto") = ExpressionTypes.GoTo
+            _ookTypes("var") = ExpressionTypes.VariableDeclaration
+            _ookTypes("dim") = ExpressionTypes.VariableDeclaration
+            _ookTypes("if") = ExpressionTypes.If
+        End Sub
+
+        Public Function GetTags(ByVal spans As NormalizedSnapshotSpanCollection) As IEnumerable(Of ITagSpan(Of OokTokenTag)) Implements ITagger(Of OokTokenTag).GetTags
+            Dim tags As New List(Of TagSpan(Of OokTokenTag))
+
+            For Each curSpan As SnapshotSpan In spans
+                Dim containingLine As ITextSnapshotLine = curSpan.Start.GetContainingLine()
+                Dim curLoc As Integer = containingLine.Start.Position
+                Dim tokens() As String = containingLine.GetText().ToLower().Split(" "c)
+
+                For Each ookToken As String In tokens
+                    If _ookTypes.ContainsKey(ookToken) Then
+                        Dim tokenSpan = New SnapshotSpan(curSpan.Snapshot, New Span(curLoc, ookToken.Length))
+                        If tokenSpan.IntersectsWith(curSpan) Then
+                            tags.Add(New TagSpan(Of OokTokenTag)(tokenSpan, New OokTokenTag(_ookTypes(ookToken))))
+                        End If
+                    End If
+                    'add an extra char location because of the space
+                    curLoc += ookToken.Length + 1
+                Next ookToken
+            Next curSpan
+
+            Return tags
+        End Function
+
+        Public Event TagsChanged(ByVal sender As Object, ByVal e As SnapshotSpanEventArgs) Implements ITagger(Of OokTokenTag).TagsChanged
+
+    End Class
+End Namespace
