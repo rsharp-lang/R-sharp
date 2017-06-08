320d45abc0158eacd9363161064631c066010037
谢桂纲 <xie.guigang@gcmodeller.org>
2017-06-08 16:58:37 +0800

    re-design variable model

---
+++ R/runtime/BindVariable.vb
@@ -0,0 +1,46 @@
+﻿Imports System.Reflection
+Imports Microsoft.VisualBasic.Emit.Delegates
+
+''' <summary>
+''' ``R#``脚本会通过这个变量对象将包之中的模块变量或者Class之中的共享变量作为一个R的内部变量
+''' </summary>
+Public Class BindVariable : Inherits Variable
+
+    ''' <summary>
+    ''' 编译之后所产生的变量值的设置或者获取的过程
+    ''' </summary>
+    ReadOnly getValue As Func(Of Object), setValue As Action(Of Object)
+
+    ''' <summary>
+    ''' 这个属性获取的是包之中的模块变量的值
+    ''' </summary>
+    ''' <returns></returns>
+    Public Overrides Property Value As Object
+        Get
+            Return getValue()
+        End Get
+        Set(value As Object)
+            Call setValue(value)
+            MyBase.Value = value
+        End Set
+    End Property
+
+    Sub New(field As FieldInfo)
+        With field
+            getValue = StaticFieldGet(.DeclaringType, .Name)
+            setValue = StaticFieldSet(.DeclaringType, .Name)
+        End With
+
+        Name = field.Name
+    End Sub
+
+    Sub New([property] As PropertyInfo)
+        With [property]
+            getValue = StaticPropertyGet(.DeclaringType, .Name)
+            setValue = StaticPropertySet(.DeclaringType, .Name)
+        End With
+
+        Name = [property].Name
+    End Sub
+
+End Class
---
+++ R/runtime/Variable.vb
@@ -0,0 +1,31 @@
+﻿Imports Microsoft.VisualBasic.ComponentModel.Collection.Generic
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel.Repository
+Imports Microsoft.VisualBasic.Language
+Imports Microsoft.VisualBasic.Scripting
+
+Public Class Variable
+
+    Implements INamedValue
+    Implements Value(Of Object).IValueOf
+
+    Public Property Name As String Implements IKeyedEntity(Of String).Key
+    Public Overridable Property Value As Object Implements Value(Of Object).IValueOf.value
+
+    ''' <summary>
+    ''' Get the type of the current object <see cref="Value"/>.
+    ''' </summary>
+    ''' <returns></returns>
+    Public Overloads ReadOnly Property [TypeOf] As Type
+        Get
+            If Value Is Nothing Then
+                Return GetType(Object)
+            Else
+                Return Value.GetType
+            End If
+        End Get
+    End Property
+
+    Public Overrides Function ToString() As String
+        Return $"Dim {Name} As {Me.TypeOf.FullName} = {CStrSafe(Value)}"
+    End Function
+End Class
