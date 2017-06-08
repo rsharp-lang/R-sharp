86b0f622660b4ad66a99a3635c448c7087dd0756
xieguigang <xie.guigang@gmail.com>
2017-06-08 19:19:17 +0800

    required improvements on the source tree

---
+++ R/Interpreter/SyntaxParser.vb
@@ -0,0 +1,62 @@
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Scripting.TokenIcer
+
+Public Module SyntaxParser
+
+    ''' <summary>
+    ''' Convert the string token model as the language runtime model
+    ''' </summary>
+    ''' <param name="statement"></param>
+    ''' <returns></returns>
+    <Extension> Public Function Parse(statement As Statement(Of LanguageTokens)) As PrimitiveExpression
+        Dim expression As PrimitiveExpression = Nothing
+
+        If TryParseObjectDeclare(statement, expression) Then
+            Return expression
+        ElseIf TryParseTypedObjectDeclare(statement, expression) Then
+            Return expression
+        End If
+
+        Throw New SyntaxErrorException("The syntax is currently not support yet!")
+    End Function
+
+    ''' <summary>
+    ''' ```R
+    ''' var x &lt;- "string";
+    ''' ```
+    ''' </summary>
+    ''' <param name="statement"></param>
+    ''' <returns></returns>
+    Public Function TryParseObjectDeclare(statement As Statement(Of LanguageTokens), ByRef out As PrimitiveExpression) As Boolean
+        Dim tokens = statement.tokens
+
+        If Not tokens.First.name = LanguageTokens.var Then
+            Return False
+        ElseIf Not tokens(2).name = LanguageTokens.LeftAssign Then
+            Return False
+        End If
+
+        Dim var$ = tokens(1).Text
+
+    End Function
+
+    ''' <summary>
+    ''' ```R
+    ''' var x as string &lt;- "string"; 
+    ''' ```
+    ''' </summary>
+    ''' <param name="statement"></param>
+    ''' <returns></returns>
+    Public Function TryParseTypedObjectDeclare(statement As Statement(Of LanguageTokens), ByRef out As PrimitiveExpression) As Boolean
+        Dim tokens = statement.tokens
+
+        If Not tokens.First.name = LanguageTokens.var Then
+            Return False
+        ElseIf Not tokens(2).Text = "as" Then
+            Return False
+        End If
+
+        Dim var$ = tokens(1).Text
+
+    End Function
+End Module
--- R/runtime/Expression/PrimitiveExpression.vb
+++ R/runtime/Expression/PrimitiveExpression.vb
@@ -1,3 +1,6 @@
-﻿Public Class PrimitiveExpression
+﻿''' <summary>
+''' The very base expression in the R# language
+''' </summary>
+Public Class PrimitiveExpression
 
 End Class
