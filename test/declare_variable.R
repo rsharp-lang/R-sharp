b45803364ecca262d28c707c2026feb86d43b71f
Master <xie.guigang@gcmodeller.org>
2017-06-08 11:56:36 +0800

    Create declare_variable.R

---
+++ test/declare_variable.R
@@ -0,0 +1,22 @@
+var x <- 123;         # integer vector only have one element
+var y <- {1, 2, 3};   # integer vector have 3 elements
+
+print(x);
+
+# [1] 123
+
+print(y);
+
+# [1] 1 2 3
+
+typeof(x);
+
+# [1] integer
+
+typeof(y);
+
+# [1] integer
+
+typeof(x) is typeof(y);
+
+# [1] TRUE
