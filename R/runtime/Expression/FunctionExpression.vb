475f02f424989aa0fba09909b3431649e33ef991
xieguigang <xie.guigang@gmail.com>
2017-06-10 22:21:57 +0800

    expressions

--- R/runtime/Expression/FunctionExpression.vb
+++ R/runtime/Expression/FunctionExpression.vb
@@ -1,3 +1,19 @@
-﻿Public Class FunctionExpression
+﻿''' <summary>
+''' Declare a function
+''' 
+''' ```
+''' func &lt;- function(a,b, c as integer = a - 99 + b / 2) {
+'''    return [a * 2, b ^ 3, mean({a, b, c})];
+''' }
+''' ```
+''' </summary>
+Public Class FunctionExpression
+
+    ''' <summary>
+    ''' The function name
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property Name As String
+
 
 End Class
