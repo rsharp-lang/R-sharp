402647ae52d456d0d67561047ae989369ca7d48d
xieguigang <xie.guigang@gmail.com>
2017-06-11 11:31:30 +0800

    add if statement description

--- R#/Test/SourceTrees.vb
+++ R#/Test/SourceTrees.vb
@@ -11,6 +11,22 @@ var x <- 123;").GetSourceTree)
 # Type constraint variable
 var x as integer <- {1, 2, 3};").GetSourceTree)
 
+        Console.WriteLine(TokenIcer.Parse("
+if(TRUE) {
+    # blablabla
+    println(""%s is TRUE"", TRUE);
+}").GetSourceTree)
+
+        Console.WriteLine(TokenIcer.Parse("
+if(a == b) {
+    println(""%s is equals to %s"", a, b);
+} else if(a <= b) {
+    println(""%s is less than or equals to %s"", a, b);
+} else {
+    println(""Not sure about this."");
+}
+").GetSourceTree)
+
         Pause()
     End Sub
 End Module
