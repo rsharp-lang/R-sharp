9119cf58d324a2c5614c95639fcc722f3981d958
谢桂纲 <xie.guigang@gcmodeller.org>
2017-06-25 22:57:49 +0800

    using custom type model?

---
+++ R#/runtime/RType.vb
@@ -0,0 +1,36 @@
+﻿''' <summary>
+''' Type proxy for <see cref="TypeCodes.list"/> or system primitives
+''' </summary>
+Public Class RType
+
+    Public ReadOnly Property TypeCode As TypeCodes = TypeCodes.list
+
+    ''' <summary>
+    ''' ``operator me``
+    ''' </summary>
+    ''' <param name="operator$"></param>
+    ''' <returns></returns>
+    Public Function GetUnaryOperator(operator$) As Func(Of Object, Object)
+
+    End Function
+
+    ''' <summary>
+    ''' ``other operator me``
+    ''' </summary>
+    ''' <param name="operator$"></param>
+    ''' <param name="a"></param>
+    ''' <returns></returns>
+    Public Function GetBinaryOperator1(operator$, a As Type) As Func(Of Object, Object, Object)
+
+    End Function
+
+    ''' <summary>
+    ''' ``me operator other``
+    ''' </summary>
+    ''' <param name="operator$"></param>
+    ''' <param name="b"></param>
+    ''' <returns></returns>
+    Public Function GetBinaryOperator2(operator$, b As Type) As Func(Of Object, Object, Object)
+
+    End Function
+End Class
