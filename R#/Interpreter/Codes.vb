098e72ed1f6a8d0150f631baeb9cc045e3ff2db9
asuka <amethyst.asuka@gcmodeller.org>
2017-08-09 13:02:07 +0800

    update source file license header

--- R#/Interpreter/Codes.vb
+++ R#/Interpreter/Codes.vb
@@ -1,4 +1,32 @@
-﻿Imports Microsoft.VisualBasic.Scripting.TokenIcer
+﻿#Region "Microsoft.VisualBasic::6ec44276dbd8427c1463120ad05ef2cd, ..\R-sharp\R#\Interpreter\Codes.vb"
+
+    ' Author:
+    ' 
+    '       asuka (amethyst.asuka@gcmodeller.org)
+    '       xieguigang (xie.guigang@live.com)
+    '       xie (genetics@smrucc.org)
+    ' 
+    ' Copyright (c) 2016 GPL3 Licensed
+    ' 
+    ' 
+    ' GNU GENERAL PUBLIC LICENSE (GPL3)
+    ' 
+    ' This program is free software: you can redistribute it and/or modify
+    ' it under the terms of the GNU General Public License as published by
+    ' the Free Software Foundation, either version 3 of the License, or
+    ' (at your option) any later version.
+    ' 
+    ' This program is distributed in the hope that it will be useful,
+    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
+    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    ' GNU General Public License for more details.
+    ' 
+    ' You should have received a copy of the GNU General Public License
+    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+#End Region
+
+Imports Microsoft.VisualBasic.Scripting.TokenIcer
 Imports SMRUCC.Rsharp.Runtime.CodeDOM
 Imports SMRUCC.Rsharp.Runtime
 
