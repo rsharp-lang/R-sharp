4479fce5c6669e9b596f30ebbed6f90fdf8a45fc
xieguigang <xie.guigang@gcmodeller.org>
2017-10-23 18:22:41 +0800

    operator result type widening

--- R#/runtime/PrimitiveTypes/integer.vb
+++ R#/runtime/PrimitiveTypes/integer.vb
@@ -46,38 +46,64 @@ Namespace Runtime.PrimitiveTypes
             BinaryOperator1("*") = New BinaryOperator("*", [integer].BuildIntegerMethodInfo1(op_Multiply, "*"))
             BinaryOperator1("/") = New BinaryOperator("/", [integer].BuildIntegerMethodInfo1(op_Divided, "/"))
             BinaryOperator1("%") = New BinaryOperator("%", [integer].BuildIntegerMethodInfo1(op_Mod, "%"))
-            BinaryOperator1("^") = New BinaryOperator("^", [integer].BuildIntegerMethodInfo1(op_Power, "^"))
+            BinaryOperator1("^") = New BinaryOperator("^", [integer].BuildIntegerMethodInfo1(op_Power, "^", [widening]:=True))
 
             BinaryOperator2("+") = New BinaryOperator("+", [integer].BuildIntegerMethodInfo2(op_Add, "+"))
             BinaryOperator2("-") = New BinaryOperator("-", [integer].BuildIntegerMethodInfo2(op_Minus, "-"))
             BinaryOperator2("*") = New BinaryOperator("*", [integer].BuildIntegerMethodInfo2(op_Multiply, "*"))
             BinaryOperator2("/") = New BinaryOperator("/", [integer].BuildIntegerMethodInfo2(op_Divided, "/"))
             BinaryOperator2("%") = New BinaryOperator("%", [integer].BuildIntegerMethodInfo2(op_Mod, "%"))
-            BinaryOperator2("^") = New BinaryOperator("^", [integer].BuildIntegerMethodInfo2(op_Power, "^"))
+            BinaryOperator2("^") = New BinaryOperator("^", [integer].BuildIntegerMethodInfo2(op_Power, "^", [widening]:=True))
         End Sub
 
         Public Overrides Function ToString() As String
             Return "R# integer"
         End Function
 
-        Public Shared Function BuildIntegerMethodInfo2(op As Func(Of Object, Object, Object), <CallerMemberName> Optional name$ = Nothing) As RMethodInfo()
-            Dim ii = BuildMethodInfo(Of Integer, Integer, Integer)(op, Nothing, Nothing)
-            Dim id = BuildMethodInfo(Of Double, Integer, Double)(op, Nothing, Nothing)
-            Dim ib = BuildMethodInfo(Of Integer, Integer, Integer)(op, Function(x) DirectCast(x, IEnumerable(Of Boolean)).Select(Function(b) If(b, 1, 0)).ToArray, Nothing, fakeX:=GetType(Boolean))
-            Dim ic = BuildMethodInfo(Of Integer, Integer, Integer)(op, Function(x) DirectCast(x, IEnumerable(Of Char)).CodeArray, Nothing, fakeX:=GetType(Char))
-            Dim iu = BuildMethodInfo(Of ULong, Integer, Integer)(op, Nothing, Nothing)
+        Public Shared Function BuildIntegerMethodInfo2(op As Func(Of Object, Object, Object), <CallerMemberName> Optional name$ = Nothing, Optional [widening] As Boolean = False) As RMethodInfo()
 
-            Return {ii, id, ib, ic, iu}
+            If [widening] Then
+                Dim ii = BuildMethodInfo(Of Integer, Integer, Double)(op, Nothing, Nothing)
+                Dim id = BuildMethodInfo(Of Double, Integer, Double)(op, Nothing, Nothing)
+                Dim ib = BuildMethodInfo(Of Integer, Integer, Double)(op, Function(x) DirectCast(x, IEnumerable(Of Boolean)).Select(Function(b) If(b, 1, 0)).ToArray, Nothing, fakeX:=GetType(Boolean))
+                Dim ic = BuildMethodInfo(Of Integer, Integer, Double)(op, Function(x) DirectCast(x, IEnumerable(Of Char)).CodeArray, Nothing, fakeX:=GetType(Char))
+                Dim iu = BuildMethodInfo(Of ULong, Integer, Double)(op, Nothing, Nothing)
+
+                Return {ii, id, ib, ic, iu}
+            Else
+                Dim ii = BuildMethodInfo(Of Integer, Integer, Integer)(op, Nothing, Nothing)
+                Dim id = BuildMethodInfo(Of Double, Integer, Double)(op, Nothing, Nothing)
+                Dim ib = BuildMethodInfo(Of Integer, Integer, Integer)(op, Function(x) DirectCast(x, IEnumerable(Of Boolean)).Select(Function(b) If(b, 1, 0)).ToArray, Nothing, fakeX:=GetType(Boolean))
+                Dim ic = BuildMethodInfo(Of Integer, Integer, Integer)(op, Function(x) DirectCast(x, IEnumerable(Of Char)).CodeArray, Nothing, fakeX:=GetType(Char))
+                Dim iu = BuildMethodInfo(Of ULong, Integer, Integer)(op, Nothing, Nothing)
+
+                Return {ii, id, ib, ic, iu}
+            End If
         End Function
 
-        Public Shared Function BuildIntegerMethodInfo1(op As Func(Of Object, Object, Object), <CallerMemberName> Optional name$ = Nothing) As RMethodInfo()
-            Dim ii = BuildMethodInfo(Of Integer, Integer, Integer)(op, Nothing, Nothing)
-            Dim id = BuildMethodInfo(Of Integer, Double, Double)(op, Nothing, Nothing)
-            Dim ib = BuildMethodInfo(Of Integer, Integer, Integer)(op, Nothing, Function(y) DirectCast(y, IEnumerable(Of Boolean)).Select(Function(b) If(b, 1, 0)).ToArray, fakeY:=GetType(Boolean))
-            Dim ic = BuildMethodInfo(Of Integer, Integer, Integer)(op, Nothing, Function(y) DirectCast(y, IEnumerable(Of Char)).CodeArray, fakeY:=GetType(Char))
-            Dim iu = BuildMethodInfo(Of Integer, ULong, Integer)(op, Nothing, Nothing)
+        Public Shared Function BuildIntegerMethodInfo1(op As Func(Of Object, Object, Object), <CallerMemberName> Optional name$ = Nothing, Optional [widening] As Boolean = False) As RMethodInfo()
 
-            Return {ii, id, ib, ic, iu}
+            If [widening] Then
+
+                Dim ii = BuildMethodInfo(Of Integer, Integer, Double)(op, Nothing, Nothing)
+                Dim id = BuildMethodInfo(Of Integer, Double, Double)(op, Nothing, Nothing)
+                Dim ib = BuildMethodInfo(Of Integer, Integer, Double)(op, Nothing, Function(y) DirectCast(y, IEnumerable(Of Boolean)).Select(Function(b) If(b, 1, 0)).ToArray, fakeY:=GetType(Boolean))
+                Dim ic = BuildMethodInfo(Of Integer, Integer, Double)(op, Nothing, Function(y) DirectCast(y, IEnumerable(Of Char)).CodeArray, fakeY:=GetType(Char))
+                Dim iu = BuildMethodInfo(Of Integer, ULong, Double)(op, Nothing, Nothing)
+
+                Return {ii, id, ib, ic, iu}
+
+            Else
+
+                Dim ii = BuildMethodInfo(Of Integer, Integer, Integer)(op, Nothing, Nothing)
+                Dim id = BuildMethodInfo(Of Integer, Double, Double)(op, Nothing, Nothing)
+                Dim ib = BuildMethodInfo(Of Integer, Integer, Integer)(op, Nothing, Function(y) DirectCast(y, IEnumerable(Of Boolean)).Select(Function(b) If(b, 1, 0)).ToArray, fakeY:=GetType(Boolean))
+                Dim ic = BuildMethodInfo(Of Integer, Integer, Integer)(op, Nothing, Function(y) DirectCast(y, IEnumerable(Of Char)).CodeArray, fakeY:=GetType(Char))
+                Dim iu = BuildMethodInfo(Of Integer, ULong, Integer)(op, Nothing, Nothing)
+
+                Return {ii, id, ib, ic, iu}
+
+            End If
         End Function
     End Class
 End Namespace
