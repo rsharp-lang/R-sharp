8de1b55f5ffc72da1b2211efba22462adab19a69
xieguigang <xie.guigang@gcmodeller.org>
2019-10-26 14:13:13 +0800

    removes old source files

---
+++ R#/Language/SyntaxScanner.vb
@@ -0,0 +1,34 @@
+﻿Imports Microsoft.VisualBasic.Emit.Marshal
+Imports Microsoft.VisualBasic.Text.Parser
+
+Namespace Language
+
+    ''' <summary>
+    ''' The token scanner
+    ''' </summary>
+    Public Class Scanner
+
+        Dim code As CharPtr
+        Dim buffer As New List(Of Char)
+        Dim escape As New Escapes
+
+        Private Class Escapes
+
+            Friend comment, [string] As Boolean
+
+            Public Overrides Function ToString() As String
+                If comment Then
+                    Return "comment"
+                ElseIf [string] Then
+                    Return "string"
+                Else
+                    Return "code"
+                End If
+            End Function
+        End Class
+
+        Sub New(source As String)
+            Me.code = source
+        End Sub
+    End Class
+End Namespace
---
+++ R#/Language/TokenType.vb
@@ -0,0 +1,25 @@
+﻿Namespace Language
+
+    Public Enum TokenType
+        invalid
+
+        [string]
+        comment
+        ''' <summary>
+        ''' 类似于在VisualBasic中的自定义属性的注解语法
+        ''' </summary>
+        annotation
+        identifier
+        keyword
+        [operator]
+        literal
+        ''' <summary>
+        ''' 左边的括号与大括号
+        ''' </summary>
+        open
+        ''' <summary>
+        ''' 右边的括号与大括号
+        ''' </summary>
+        close
+    End Enum
+End Namespace
