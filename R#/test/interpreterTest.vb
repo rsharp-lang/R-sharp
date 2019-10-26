7f24812b4e0dadfbafaa6eb9e65723239a9f967d
xieguigang <xie.guigang@gcmodeller.org>
2019-10-26 21:02:57 +0800

    add interpreter test

---
+++ R#/test/interpreterTest.vb
@@ -0,0 +1,13 @@
+﻿Imports SMRUCC.Rsharp.Interpreter
+
+Module interpreterTest
+
+    Dim R As New RInterpreter
+
+    Sub Main()
+
+        Call R.Evaluate("let x as integer = [999, 888, 777, 666];")
+
+        Pause()
+    End Sub
+End Module
