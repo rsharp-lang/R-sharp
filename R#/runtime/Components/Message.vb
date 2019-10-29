5efdf25122345b2f27e811f84926680ffa6a7bd0
xieguigang <xie.guigang@gcmodeller.org>
2019-10-29 12:38:57 +0800

    exception and warning message

---
+++ R#/runtime/Components/Message.vb
@@ -0,0 +1,16 @@
+﻿Imports Microsoft.VisualBasic.ApplicationServices.Debugging.Diagnostics
+Imports Microsoft.VisualBasic.ApplicationServices.Debugging.Logging
+
+Namespace Runtime
+
+    ''' <summary>
+    ''' The warning message and exception message
+    ''' </summary>
+    Public Class Message
+
+        Public Property Message As String()
+        Public Property MessageLevel As MSG_TYPES
+        Public Property StackTrace As StackFrame()
+
+    End Class
+End Namespace
