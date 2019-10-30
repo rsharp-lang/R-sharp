5a1fb0e8e83ce1e26260d8dc80bf3608656b8202
xieguigang <xie.guigang@gcmodeller.org>
2019-10-30 19:31:45 +0800

    invoke .NET method

--- R#/Runtime/Components/RMethodInfo.vb
+++ R#/Runtime/Components/RMethodInfo.vb
@@ -1,4 +1,6 @@
 ﻿Imports System.Reflection
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports Microsoft.VisualBasic.Linq
 
 Namespace Runtime
 
@@ -10,10 +12,14 @@ Namespace Runtime
         ''' <returns></returns>
         Public ReadOnly Property name As String Implements RFunction.name
         Public ReadOnly Property api As [Delegate]
+        Public ReadOnly Property returns As RType
+        Public ReadOnly Property parameters As NamedValue(Of (type As RType, [default] As Object))()
 
         Sub New(name$, closure As [Delegate])
             Me.name = name
             Me.api = closure
+            Me.returns = New RType(closure.Method.ReturnType)
+            Me.parameters = closure.Method.DoCall(AddressOf parseParameters)
         End Sub
 
         Sub New(name$, closure As MethodInfo)
@@ -21,13 +27,44 @@ Namespace Runtime
             Me.api = Function(params As Object())
                          Return closure.Invoke(Nothing, params)
                      End Function
+            Me.returns = New RType(closure.ReturnType)
+            Me.parameters = closure.DoCall(AddressOf parseParameters)
         End Sub
+
+        Private Shared Function parseParameters(method As MethodInfo) As NamedValue(Of (RType, Object))()
+            Return method.GetParameters _
+                .Select(Function(p)
+                            Return New NamedValue(Of (RType, Object)) With {
+                                .Name = p.Name,
+                                .Value = (New RType(p.ParameterType), p.DefaultValue),
+                                .Description = p.HasDefaultValue
+                            }
+                        End Function) _
+                .ToArray
+        End Function
 
         Public Function Invoke(envir As Environment, arguments As Object()) As Object Implements RFunction.Invoke
             Dim result As Object
+            Dim parameters As New List(Of Object)
 
-            envir = New Environment(envir, stackTag:=name)
-            result = api.Method.Invoke(api.Target, arguments)
+            For i As Integer = 0 To Me.parameters.Length - 1
+                If i >= arguments.Length Then
+                    ' default value
+                    If Not Me.parameters(i).Description.ParseBoolean Then
+                        Return Internal.stop({$"Missing parameter value for '{Me.parameters(i).Name}'!", "function: " & name, "environment: " & envir.ToString}, envir)
+                    Else
+                        parameters += Me.parameters(i).Value.default
+                    End If
+                Else
+                    If Me.parameters(i).Value.type.isArray Then
+                        parameters += CObj(Runtime.asVector(arguments(i), Me.parameters(i).Value.type.raw.GetElementType))
+                    Else
+                        parameters += Runtime.getFirst(arguments(i))
+                    End If
+                End If
+            Next
+
+            result = api.Method.Invoke(api.Target, parameters.ToArray)
 
             Return result
         End Function
