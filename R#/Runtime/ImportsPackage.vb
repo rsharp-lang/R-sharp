67e1e40024bc35b1c3fb773b1fbe6c4e3d2550a0
xieguigang <xie.guigang@gcmodeller.org>
2019-10-30 19:36:26 +0800

    imports .NET methods

---
+++ R#/Runtime/ImportsPackage.vb
@@ -0,0 +1,19 @@
+﻿Imports System.Runtime.CompilerServices
+
+Namespace Runtime
+
+    ''' <summary>
+    ''' Helper methods for add .NET function into <see cref="Environment"/> target
+    ''' </summary>
+    Public Module ImportsPackage
+
+        <Extension>
+        Public Sub ImportsStatic(envir As Environment, package As Type)
+
+        End Sub
+
+        Public Sub ImportsInstance(envir As Environment, target As Object)
+
+        End Sub
+    End Module
+End Namespace
