57e3495d8a6cf7f5bd839be33aa6419c97d68460
xieguigang <xie.guigang@gcmodeller.org>
2019-10-31 18:19:55 +0800

    run R terminal

--- R-terminal/Terminal.vb
+++ R-terminal/Terminal.vb
@@ -1,6 +1,19 @@
-﻿Module Terminal
+﻿Imports Microsoft.VisualBasic.Language.UnixBash
+Imports Microsoft.VisualBasic.Terminal
+Imports SMRUCC.Rsharp.Interpreter
+
+Module Terminal
 
     Public Function RunTerminal() As Integer
+        Dim ps1 As PS1 = PS1.Fedora12
+        Dim R As New RInterpreter
+        Dim exec As Action(Of String) =
+            Sub(script)
+                R.Evaluate(script)
+            End Sub
 
+        Call New Shell(ps1, exec).Run()
+
+        Return 0
     End Function
 End Module
