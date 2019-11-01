7cb82c131807957997b17315b86ae3eaaa12eafd
xieguigang <xie.guigang@gcmodeller.org>
2019-11-01 12:51:57 +0800

    utils

--- R.base/save.vb
+++ R.base/save.vb
@@ -1,3 +1,17 @@
-﻿Partial Module base
+﻿Imports SMRUCC.Rsharp.Runtime
 
+Partial Module base
+
+    ''' <summary>
+    ''' 数据将会被保存为netCDF文件然后进行zip压缩保存
+    ''' </summary>
+    ''' <param name="objects"></param>
+    ''' <param name="file"></param>
+    ''' <param name="envir"></param>
+    ''' <returns></returns>
+    Public Function save(objects As Object, file$, envir As Environment) As Object
+        If file.StringEmpty Then
+            Return Internal.stop("'file' must be specified!", envir)
+        End If
+    End Function
 End Module
---
+++ R.base/utils/utils.vb
@@ -0,0 +1,10 @@
+﻿Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Data.csv.IO
+
+Module utils
+
+    <ExportAPI("write.csv")>
+    Public Function writecsv(data As File, file$, envir As Environment) As Object
+
+    End Function
+End Module
