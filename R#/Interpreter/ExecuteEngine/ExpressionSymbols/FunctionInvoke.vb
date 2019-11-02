8ed631b00dc9c37f410262d620a5ae3662c94ac3
xieguigang <xie.guigang@gcmodeller.org>
2019-11-03 01:27:30 +0800

    R internal function invoke

--- R#/Interpreter/ExecuteEngine/ExpressionSymbols/FunctionInvoke.vb
+++ R#/Interpreter/ExecuteEngine/ExpressionSymbols/FunctionInvoke.vb
@@ -94,7 +94,7 @@ Namespace Interpreter.ExecuteEngine
 
                     Return list
                 Else
-                    Return invokeInternals(envir, funcName, envir.Evaluate(parameters))
+                    Return Runtime.Internal.invokeInternals(envir, funcName, envir.Evaluate(parameters))
                 End If
             ElseIf funcVar.value.GetType Like runtimeFuncs Then
                 ' invoke method create from R# script
@@ -103,50 +103,6 @@ Namespace Interpreter.ExecuteEngine
                 ' invoke .NET method
                 Return DirectCast(funcVar.value, RMethodInfo).Invoke(envir, envir.Evaluate(parameters))
             End If
-        End Function
-
-        ''' <summary>
-        ''' Invoke the runtime internal functions
-        ''' </summary>
-        ''' <param name="envir"></param>
-        ''' <param name="funcName$"></param>
-        ''' <param name="paramVals"></param>
-        ''' <returns></returns>
-        Private Shared Function invokeInternals(envir As Environment, funcName$, paramVals As Object()) As Object
-            Select Case funcName
-                Case "length" : Return DirectCast(paramVals(Scan0), Array).Length
-                Case "round"
-                    Dim x = paramVals(Scan0)
-                    Dim decimals As Integer = Runtime.getFirst(paramVals(1))
-
-                    If x.GetType.IsInheritsFrom(GetType(Array)) Then
-                        Return (From element As Object In DirectCast(x, Array).AsQueryable Select Math.Round(CDbl(element), decimals)).ToArray
-                    Else
-                        Return Math.Round(CDbl(x), decimals)
-                    End If
-                Case "print"
-                    Return Internal.print(paramVals(Scan0))
-                Case "stop"
-                    Return Internal.stop(paramVals(Scan0), envir)
-                Case "cat"
-                    Return Internal.cat(paramVals(Scan0), paramVals.ElementAtOrDefault(1), paramVals.ElementAtOrDefault(2, " "))
-                Case "lapply"
-                    If paramVals.ElementAtOrDefault(1) Is Nothing Then
-                        Return Internal.stop({"Missing apply function!"}, envir)
-                    ElseIf Not paramVals(1).GetType.ImplementInterface(GetType(RFunction)) Then
-                        Return Internal.stop({"Target is not a function!"}, envir)
-                    End If
-
-                    If Program.isException(paramVals(Scan0)) Then
-                        Return paramVals(Scan0)
-                    ElseIf Program.isException(paramVals(1)) Then
-                        Return paramVals(1)
-                    End If
-
-                    Return Internal.lapply(paramVals(Scan0), paramVals(1), envir)
-                Case Else
-                    Return Message.SymbolNotFound(envir, funcName, TypeCodes.closure)
-            End Select
         End Function
     End Class
 End Namespace
