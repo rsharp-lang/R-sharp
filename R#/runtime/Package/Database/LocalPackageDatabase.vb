2773480472295d0f558f0fb6def14fbefa17a6f1
xieguigang <xie.guigang@gcmodeller.org>
2019-11-05 13:49:42 +0800

    build database

--- R#/runtime/Package/Database/LocalPackageDatabase.vb
+++ R#/runtime/Package/Database/LocalPackageDatabase.vb
@@ -1,4 +1,5 @@
 ﻿Imports System.Xml.Serialization
+Imports Microsoft.VisualBasic.ApplicationServices.Development
 Imports Microsoft.VisualBasic.ComponentModel
 Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.Text.Xml.Models
@@ -17,15 +18,33 @@ Namespace Runtime.Package
             End Set
         End Property
 
-        <XmlElement>
-        Public Property packages As PackageLoaderEntry()
+        <XmlElement> Public Property system As AssemblyInfo
+        <XmlElement> Public Property packages As PackageLoaderEntry()
 
-        Public Function GenericEnumerator() As IEnumerator(Of PackageLoaderEntry) Implements Enumeration(Of PackageLoaderEntry).GenericEnumerator
-            Throw New NotImplementedException()
+        Public Shared ReadOnly Property localDb As String = App.LocalData & "/packages.xml"
+
+        Public Shared Function Build(repository As String) As LocalPackageDatabase
+            Dim packages As PackageLoaderEntry() = PackageLoader _
+                .ScanDllFiles(directory:=repository) _
+                .Select(Function(pkg) PackageLoaderEntry.FromLoaderInfo(pkg)) _
+                .ToArray
+            Dim systemInfo As AssemblyInfo = GetType(LocalPackageDatabase).Assembly.FromAssembly
+            Dim localDb As New LocalPackageDatabase With {
+                .packages = packages,
+                .system = systemInfo
+            }
+
+            Return localDb
         End Function
 
-        Public Function GetEnumerator() As IEnumerator Implements Enumeration(Of PackageLoaderEntry).GetEnumerator
-            Throw New NotImplementedException()
+        Public Iterator Function GenericEnumerator() As IEnumerator(Of PackageLoaderEntry) Implements Enumeration(Of PackageLoaderEntry).GenericEnumerator
+            For Each package As PackageLoaderEntry In packages
+                Yield package
+            Next
+        End Function
+
+        Public Iterator Function GetEnumerator() As IEnumerator Implements Enumeration(Of PackageLoaderEntry).GetEnumerator
+            Yield GenericEnumerator()
         End Function
     End Class
 End Namespace
