2d693640027cdeb9d90082fff30d5379c3338806
xieguigang <xie.guigang@gcmodeller.org>
2019-11-05 13:42:54 +0800

    package system

---
+++ R#/runtime/Package/Database/LocalPackageDatabase.vb
@@ -0,0 +1,31 @@
+﻿Imports System.Xml.Serialization
+Imports Microsoft.VisualBasic.ComponentModel
+Imports Microsoft.VisualBasic.Linq
+Imports Microsoft.VisualBasic.Text.Xml.Models
+
+Namespace Runtime.Package
+
+    Public Class LocalPackageDatabase : Inherits XmlDataModel
+        Implements IList(Of PackageLoaderEntry)
+
+        Public Property numOfpackages As Integer Implements IList(Of PackageLoaderEntry).size
+            Get
+                Return packages.SafeQuery.Count
+            End Get
+            Set(value As Integer)
+                ' readonly / do nothing
+            End Set
+        End Property
+
+        <XmlElement>
+        Public Property packages As PackageLoaderEntry()
+
+        Public Function GenericEnumerator() As IEnumerator(Of PackageLoaderEntry) Implements Enumeration(Of PackageLoaderEntry).GenericEnumerator
+            Throw New NotImplementedException()
+        End Function
+
+        Public Function GetEnumerator() As IEnumerator Implements Enumeration(Of PackageLoaderEntry).GetEnumerator
+            Throw New NotImplementedException()
+        End Function
+    End Class
+End Namespace
---
+++ R#/runtime/Package/Database/PackageLoaderEntry.vb
@@ -0,0 +1,55 @@
+﻿Imports System.Runtime.CompilerServices
+Imports System.Xml.Serialization
+Imports Microsoft.VisualBasic.Scripting.MetaData
+
+Namespace Runtime.Package
+
+    Public Class PackageLoaderEntry
+
+        <XmlAttribute>
+        Public Property [namespace] As String
+        <XmlText>
+        Public Property description As String
+        ''' <summary>
+        ''' This plugins project's home page url.
+        ''' </summary>
+        ''' <returns></returns>
+        ''' 
+        <XmlAttribute>
+        Public Property url As String
+        ''' <summary>
+        ''' Your name or E-Mail
+        ''' </summary>
+        ''' <returns></returns>
+        ''' 
+        <XmlElement>
+        Public Property publisher As String
+        <XmlAttribute>
+        Public Property revision As Integer
+        ''' <summary>
+        ''' 这个脚本模块包的文献引用列表
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property cites As String
+        <XmlAttribute>
+        Public Property category As APICategories = APICategories.SoftwareTools
+        Public Property [module] As TypeInfo
+
+        Sub New()
+        End Sub
+
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        Public Shared Function FromLoaderInfo(info As Package) As PackageLoaderEntry
+            Return New PackageLoaderEntry With {
+                .category = info.info.Category,
+                .cites = info.info.Cites,
+                .description = info.info.Description,
+                .[module] = New TypeInfo(info.package),
+                .[namespace] = info.info.Namespace,
+                .publisher = info.info.Publisher,
+                .revision = info.info.Revision,
+                .url = info.info.Url
+            }
+        End Function
+    End Class
+End Namespace
