f55c38ce905164ce566c5f2439ae389757f7a13f
xieguigang <xie.guigang@gcmodeller.org>
2019-11-05 14:52:45 +0800

    tag package info into module

--- R.base/utils/utils.vb
+++ R.base/utils/utils.vb
@@ -1,7 +1,9 @@
 ﻿Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Data.csv.IO
+Imports Microsoft.VisualBasic.Scripting.MetaData
 
-Module utils
+<Package("utils", Category:=APICategories.UtilityTools, Description:="")>
+Public Module utils
 
     <ExportAPI("write.csv")>
     Public Function writecsv(data As File, file$, envir As Environment) As Object
