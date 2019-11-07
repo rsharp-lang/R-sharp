782a35ea97edf6ccd149762014adbe3f90cdcc1c
xieguigang <xie.guigang@gcmodeller.org>
2019-11-07 22:05:10 +0800

    get normal arguments

--- R#/Runtime/Components/Interop/RMethodInfo.vb
+++ R#/Runtime/Components/Interop/RMethodInfo.vb
@@ -38,33 +38,47 @@ Namespace Runtime.Components
         End Function
 
         Public Function Invoke(envir As Environment, params As InvokeParameter()) As Object Implements RFunction.Invoke
-            Dim result As Object
-            Dim parameters As New List(Of Object)
             Dim arguments As Dictionary(Of String, Object) = InvokeParameter.CreateArguments(envir, params)
+            Dim parameters As Object()
+
+            If Me.parameters.Any(Function(a) a.isObjectList) Then
+                Throw New NotImplementedException
+            Else
+                parameters = createNormalArguments(envir, arguments).ToArray
+            End If
+
+            For Each arg In parameters
+                If Not arg Is Nothing AndAlso arg.GetType Is GetType(Message) Then
+                    Return arg
+                End If
+            Next
+
+            Dim result As Object = api.Method.Invoke(api.Target, parameters.ToArray)
+
+            Return result
+        End Function
+
+        Private Iterator Function createNormalArguments(envir As Environment, arguments As Dictionary(Of String, Object)) As IEnumerable(Of Object)
             Dim arg As RMethodArgument
 
             For i As Integer = 0 To Me.parameters.Length - 1
                 arg = Me.parameters(i)
 
                 If arguments.ContainsKey(arg.name) Then
-                    parameters.Add(getValue(arg, arguments(arg.name)))
-                ElseIf i >= params.Length Then
+                    Yield getValue(arg, arguments(arg.name))
+                ElseIf i >= arguments.Count Then
                     ' default value
                     If arg.type.raw Is GetType(Environment) Then
-                        parameters.Add(envir)
-                    ElseIf Not Me.parameters(i).isOptional Then
-                        Return Internal.stop({$"Missing parameter value for '{Me.parameters(i).name}'!", "function: " & name, "environment: " & envir.ToString}, envir)
+                        Yield envir
+                    ElseIf Not arg.isOptional Then
+                        Yield Internal.stop({$"Missing parameter value for '{arg.name}'!", "function: " & name, "environment: " & envir.ToString}, envir)
                     Else
-                        parameters.Add(Me.parameters(i).default)
+                        Yield arg.default
                     End If
                 Else
-                    parameters.Add(getValue(arg, arguments("$" & i)))
+                    Yield getValue(arg, arguments("$" & i))
                 End If
             Next
-
-            result = api.Method.Invoke(api.Target, parameters.ToArray)
-
-            Return result
         End Function
 
         Private Shared Function getValue(arg As RMethodArgument, value As Object) As Object
