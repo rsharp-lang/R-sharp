e947adb1b464611813496959909b0f13b9319f92
xieguigang <xie.guigang@gcmodeller.org>
2019-11-08 13:14:40 +0800

    I/O test success

--- tutorials/io/load.R
+++ tutorials/io/load.R
@@ -3,7 +3,10 @@ let names as string = load(disk);
 
 print("Display loaded data:");
 
+# x, word
 for(name in names) {
     print(`Vector of '${name}':`);
     print(get(name));
 }
+
+print(`Hello ${word}!!!`);
