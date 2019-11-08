6ef2c1f3593b0defeef712e56a0c81564f732f38
xieguigang <xie.guigang@gcmodeller.org>
2019-11-08 20:19:26 +0800

    write csv demo

--- R.base/utils/utils.vb
+++ R.base/utils/utils.vb
@@ -1,12 +1,16 @@
 ﻿Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Data.csv.IO
 Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports Rdataframe = SMRUCC.Rsharp.Runtime.Internal.dataframe
 
 <Package("utils", Category:=APICategories.UtilityTools, Description:="")>
 Public Module utils
 
     <ExportAPI("write.csv")>
-    Public Function writecsv(data As File, file$, envir As Environment) As Object
+    Public Function writecsv(data As Rdataframe, file$, envir As Environment) As Object
+        Dim matrix As String()() = data.GetTable
+        Dim dataframe As New File(matrix.Select(Function(r) New RowObject(r)))
 
+        Return dataframe.Save(path:=file)
     End Function
 End Module
