db6c7696feb5d4107eef0ffa1a8a1ffe1e7774b2
xieguigang <xie.guigang@gcmodeller.org>
2019-11-11 10:50:46 +0800

    configurations

---
+++ R#/Runtime/Components/Config/Options.vb
@@ -0,0 +1,16 @@
+﻿
+Namespace Runtime.Components.Configuration
+
+    ''' <summary>
+    ''' Data reader of <see cref="ConfigFile"/>
+    ''' </summary>
+    Public Class Options
+
+        ReadOnly file As ConfigFile
+
+        Sub New(configs As ConfigFile)
+            Me.file = configs
+        End Sub
+
+    End Class
+End Namespace
