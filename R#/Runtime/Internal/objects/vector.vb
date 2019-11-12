c29ac50fe077976a4d31cfcf7ac54c349784a68a
xieguigang <xie.guigang@gcmodeller.org>
2019-11-12 19:21:22 +0800

    vector

--- R#/Runtime/Internal/objects/vector.vb
+++ R#/Runtime/Internal/objects/vector.vb
@@ -1,6 +1,33 @@
-﻿Namespace Runtime.Internal
+﻿Imports Microsoft.VisualBasic.ComponentModel.Collection
+Imports SMRUCC.Rsharp.Runtime.Components.Interface
 
-    Public Class vector
+Namespace Runtime.Internal
 
+    Public Class vector : Implements RNames
+
+        Public Property data As Array
+
+        Dim names As String()
+        Dim nameIndex As Index(Of String)
+
+        Public Function getNames() As String() Implements RNames.getNames
+            Return names
+        End Function
+
+        Public Function setNames(names() As String, envir As Environment) As Object Implements RNames.setNames
+            If Not names.IsNullOrEmpty Then
+                If names.Length <> data.Length Then
+                    Return Internal.stop($"vector names is not equals in length with the vector element data!", envir)
+                Else
+                    Me.names = names
+                    Me.nameIndex = names.Indexing
+                End If
+            Else
+                Me.names = names
+                Me.nameIndex = Nothing
+            End If
+
+            Return data
+        End Function
     End Class
 End Namespace
