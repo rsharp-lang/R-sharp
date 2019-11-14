32b48b9cf40180ccbf3249e24039845fb8eaf15e
xieguigang <xie.guigang@gcmodeller.org>
2019-11-14 17:50:44 +0800

    parse indexer syntax

--- R#/Interpreter/ExecuteEngine/ExpressionSymbols/SymbolIndexer.vb
+++ R#/Interpreter/ExecuteEngine/ExpressionSymbols/SymbolIndexer.vb
@@ -1,6 +1,8 @@
-﻿Imports SMRUCC.Rsharp.Language.TokenIcer
+﻿Imports Microsoft.VisualBasic.Linq
+Imports SMRUCC.Rsharp.Language.TokenIcer
 Imports SMRUCC.Rsharp.Runtime
 Imports SMRUCC.Rsharp.Runtime.Components
+Imports SMRUCC.Rsharp.Language
 
 Namespace Interpreter.ExecuteEngine
 
@@ -8,12 +10,32 @@ Namespace Interpreter.ExecuteEngine
 
         Public Overrides ReadOnly Property type As TypeCodes
 
-        Sub New(tokens As Token())
+        Dim index As Expression
+        Dim symbol As Expression
+        ''' <summary>
+        ''' X[[name]]
+        ''' </summary>
+        Dim nameIndex As Boolean = False
 
+        Sub New(tokens As Token())
+            symbol = {tokens(Scan0)}.DoCall(AddressOf Expression.CreateExpression)
+            tokens = tokens.Skip(2).Take(tokens.Length - 3).ToArray
+
+            If tokens(Scan0) = (TokenType.open, "[") AndAlso tokens.Last = (TokenType.close, "]") Then
+                nameIndex = True
+                tokens = tokens.Skip(2).Take(tokens.Length - 3).ToArray
+            End If
+
+            index = Expression.CreateExpression(tokens)
         End Sub
 
         Public Overrides Function Evaluate(envir As Environment) As Object
-            Throw New NotImplementedException()
+            Dim sequence = Runtime.asVector(Of Object)(symbol.Evaluate(envir))
+            Dim indexer = Runtime.asVector(Of Object)(index.Evaluate(envir))
+
+            If indexer.Length = 0 Then
+                Return Internal.stop("", envir)
+            End If
         End Function
     End Class
 End Namespace
