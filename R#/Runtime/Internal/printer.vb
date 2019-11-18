1a635771731bcbaac36cce616e68501c376594e4
xieguigang <xie.guigang@gcmodeller.org>
2019-11-18 21:05:12 +0800

    nice print?

---
+++ R#/Runtime/Internal/printer.vb
@@ -0,0 +1,18 @@
+﻿Imports System.Drawing
+Imports Microsoft.VisualBasic.Imaging
+Imports Microsoft.VisualBasic.Serialization
+
+Namespace Runtime.Internal
+
+    ''' <summary>
+    ''' R# console nice print supports.
+    ''' </summary>
+    Module printer
+
+        ReadOnly RtoString As New Dictionary(Of Type, IStringBuilder)
+
+        Sub New()
+            RtoString(GetType(Color)) = Function(c) DirectCast(c, Color).ToHtmlColor
+        End Sub
+    End Module
+End Namespace
--- R.base/grDevices.vb
+++ R.base/grDevices.vb
@@ -1,6 +1,34 @@
-﻿Imports Microsoft.VisualBasic.Scripting.MetaData
+﻿Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Imaging
+Imports Microsoft.VisualBasic.Scripting.MetaData
 
-<Package("grDevices", Category:=APICategories.UtilityTools, Description:="")>
+''' <summary>
+''' The R# Graphics Devices and Support for Colours and Fonts
+''' </summary>
+<Package("grDevices", Category:=APICategories.UtilityTools)>
 Public Module grDevices
 
+    Dim devlist As New List(Of IGraphics)
+    Dim curDev As IGraphics
+
+    ''' <summary>
+    ''' returns the number and name of the new active device 
+    ''' (after the specified device has been shut down).
+    ''' </summary>
+    ''' <param name="which">An integer specifying a device number.</param>
+    ''' <returns></returns>
+    <ExportAPI("dev.off")>
+    Public Function devOff(Optional which% = -1) As Integer
+
+    End Function
+
+    ''' <summary>
+    ''' returns a length-one named integer vector giving the number and name of the 
+    ''' active device, or 1, the null device, if none is active.
+    ''' </summary>
+    ''' <returns></returns>
+    <ExportAPI("dev.cur")>
+    Public Function devCur() As Integer
+
+    End Function
 End Module
