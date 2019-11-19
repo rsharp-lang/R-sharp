bca1d87d1f40e7e1a7a52957be0940611d9e3f25
xieguigang <xie.guigang@gcmodeller.org>
2019-11-19 13:36:54 +0800

    doc test

---
+++ R#/test/docTest.vb
@@ -0,0 +1,11 @@
+﻿Imports SMRUCC.Rsharp.Runtime.Package
+
+Module docTest
+
+    Sub Main()
+        Dim doc As New AnnotationDocs
+        Dim result = doc.GetAnnotations(GetType(LICENSE))
+
+        Pause()
+    End Sub
+End Module
