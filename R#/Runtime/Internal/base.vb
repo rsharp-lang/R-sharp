1d3ec40f066c7ecd00949f494ca39a14ca528f6b
xieguigang <xie.guigang@gcmodeller.org>
2019-11-19 20:55:52 +0800

    improvements of the exception print

--- R#/Runtime/Internal/base.vb
+++ R#/Runtime/Internal/base.vb
@@ -1,44 +1,44 @@
 ﻿#Region "Microsoft.VisualBasic::f2871917fdb327e513228145b18aa5cd, R#\Runtime\Internal\base.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Module base
-    ' 
-    '         Function: [get], [stop], all, any, cat
-    '                   createDotNetExceptionMessage, createMessageInternal, getEnvironmentStack, lapply, names
-    '                   options, print, sapply, warning
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Module base
+' 
+'         Function: [get], [stop], all, any, cat
+'                   createDotNetExceptionMessage, createMessageInternal, getEnvironmentStack, lapply, names
+'                   options, print, sapply, warning
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -164,7 +164,7 @@ Namespace Runtime.Internal
 
             ' add stack info for display
             If exception.StackTrace.StringEmpty Then
-                messages += "stackFrames: " & vbCrLf & "none"
+                messages += "stackFrames: none"
             Else
                 messages += "stackFrames: " & vbCrLf & exception.StackTrace
             End If
