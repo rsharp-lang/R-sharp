62362c001f999e03923a1e1238ad13451bed7484
xieguigang <xie.guigang@gcmodeller.org>
2019-11-19 13:28:16 +0800

    get package annotations from xml assembly

--- R#/Runtime/Package/AnnotationDocs.vb
+++ R#/Runtime/Package/AnnotationDocs.vb
@@ -1,4 +1,5 @@
-﻿Imports Microsoft.VisualBasic.ApplicationServices.Development.XmlDoc.Assembly
+﻿Imports System.Reflection
+Imports Microsoft.VisualBasic.ApplicationServices.Development.XmlDoc.Assembly
 
 Namespace Runtime.Package
 
@@ -6,6 +7,25 @@ Namespace Runtime.Package
     ''' Parser of the <see cref="Project"/> assembly
     ''' </summary>
     Public Class AnnotationDocs
+
+        ReadOnly projects As New Dictionary(Of String, Project)
+
+        Public Function GetAnnotations(package As Type) As ProjectType
+            Dim assembly As Assembly = package.Assembly
+            Dim project As Project
+            Dim projectKey As String = assembly.ToString
+            Dim docXml As String = assembly.Location.TrimSuffix & ".xml"
+            Dim type As ProjectType
+
+            If Not projects.ContainsKey(projectKey) Then
+                projects(projectKey) = ProjectSpace.CreateDocProject(docXml)
+            End If
+
+            project = projects(projectKey)
+            type = project.GetType(package.FullName)
+
+            Return type
+        End Function
 
     End Class
 End Namespace
