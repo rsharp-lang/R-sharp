7668128bed8d231a5202af5b2767f3db0092affa
xieguigang <xie.guigang@gcmodeller.org>
2019-11-19 13:20:39 +0800

    Parser of the <see cref="Project"/> assembly

---
+++ R#/Runtime/Package/AnnotationDocs.vb
@@ -0,0 +1,11 @@
+﻿Imports Microsoft.VisualBasic.ApplicationServices.Development.XmlDoc.Assembly
+
+Namespace Runtime.Package
+
+    ''' <summary>
+    ''' Parser of the <see cref="Project"/> assembly
+    ''' </summary>
+    Public Class AnnotationDocs
+
+    End Class
+End Namespace
