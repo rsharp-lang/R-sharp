0907b1a341b6aa478bd4032780b02b1f85cffd64
xieguigang <xie.guigang@gcmodeller.org>
2019-11-20 17:42:07 +0800

    getwd and setwd

--- R#/Runtime/Internal/utils.vb
+++ R#/Runtime/Internal/utils.vb
@@ -1,48 +1,49 @@
 ﻿#Region "Microsoft.VisualBasic::32e0cd57227bf70c7ee1bd0cefe0a3e0, R#\Runtime\Internal\utils.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Module utils
-    ' 
-    '         Function: installPackages
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Module utils
+' 
+'         Function: installPackages
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports Microsoft.VisualBasic.Linq
 Imports SMRUCC.Rsharp.Runtime.Package
 Imports Microsoft.VisualBasic.Language
+Imports Microsoft.VisualBasic.CommandLine.Reflection
 
 Namespace Runtime.Internal
 
@@ -55,7 +56,7 @@ Namespace Runtime.Internal
         ''' <param name="envir"></param>
         ''' <returns></returns>
         Public Function installPackages(packages As String(), envir As Environment) As Object
-            Dim pkgMgr As PackageManager = envir.GlobalEnvironment.packages
+            Dim pkgMgr As PackageManager = envir.globalEnvironment.packages
             Dim namespaces As New List(Of String)
 
             For Each pkgName As String In packages.SafeQuery
@@ -65,6 +66,27 @@ Namespace Runtime.Internal
             Call pkgMgr.Flush()
 
             Return namespaces.ToArray
+        End Function
+
+        ''' <summary>
+        ''' ## Find Installed Packages
+        ''' 
+        ''' Find (or retrieve) details of all packages installed in the specified libraries.
+        ''' 
+        ''' ``installed.packages`` scans the ‘DESCRIPTION’ files of each package found along 
+        ''' ``lib.loc`` and returns a matrix of package names, library paths and version numbers.
+        '''
+        ''' The information found Is cached (by library) For the R session And specified fields argument, 
+        ''' And updated only If the top-level library directory has been altered, 
+        ''' For example by installing Or removing a package. If the cached information becomes confused, 
+        ''' it can be refreshed by running ``installed.packages(noCache = True)``.
+        ''' </summary>
+        ''' <param name="envir"></param>
+        ''' <returns></returns>
+        <ExportAPI("installed.packages")>
+        Public Function GetInstalledPackages(envir As Environment) As Object
+
+
         End Function
     End Module
 End Namespace
