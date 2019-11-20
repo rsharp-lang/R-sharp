f1e5adf49fff3e44befb06175baa3c3ae3e076c7
xieguigang <xie.guigang@gcmodeller.org>
2019-11-20 21:01:38 +0800

    add function closure test2

---
+++ tutorials/closure_object.R
@@ -0,0 +1,24 @@
+# Use closure simulate a class object in R# scripting
+# just like js function object
+
+let closure as function() {
+
+    let x as integer = 999;
+
+    let setX as function(value) {
+        x = value;
+    }
+
+    let getX as function() {
+        x;
+    }
+
+    list(getX = getX, setX = setX);
+}
+
+let holder = closure();
+print(holder$getX());
+holder$setX([123,233,333]);
+print(holder$getX());
+print(holder$setX('123 AAAAA'));
+print(holder$getX());
