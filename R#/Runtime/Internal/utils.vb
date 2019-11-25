650997e96e76915bb6295dd7e7af74815b295ba7
xieguigang <xie.guigang@gcmodeller.org>
2019-11-25 11:27:54 +0800

    print user class type?

--- R#/Runtime/Internal/utils.vb
+++ R#/Runtime/Internal/utils.vb
@@ -1,48 +1,50 @@
 ﻿#Region "Microsoft.VisualBasic::32e0cd57227bf70c7ee1bd0cefe0a3e0, R#\Runtime\Internal\utils.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Module utils
-    ' 
-    '         Function: installPackages
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Module utils
+' 
+'         Function: installPackages
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
+Imports System.Reflection
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Linq
 Imports SMRUCC.Rsharp.Runtime.Package
-Imports Microsoft.VisualBasic.Language
 
 Namespace Runtime.Internal
 
@@ -63,6 +65,22 @@ Namespace Runtime.Internal
             Next
 
             Return namespaces.ToArray
+        End Function
+
+        ''' <summary>
+        ''' A utils function for print user defined structure and class object instance. 
+        ''' </summary>
+        ''' <param name="obj">
+        ''' The object class is ensure that not nothing!
+        ''' </param>
+        ''' <returns></returns>
+        ''' <remarks>
+        ''' All of the  method is non generic, and the property without arguments!
+        ''' </remarks>
+        Public Function printClass(obj As Object) As String
+            Dim type As Type = obj.GetType
+            Dim properties As PropertyInfo() = type.GetProperties(PublicProperty).Where(Function(p) p.GetIndexParameters.IsNullOrEmpty).ToArray
+            Dim methods As MethodInfo() = type.GetMethods(PublicProperty).Where(Function(m) Not m.ContainsGenericParameters).ToArray
         End Function
     End Module
 End Namespace
