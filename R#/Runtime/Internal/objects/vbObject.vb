fb4c72e3758f7acf4dda40c4f8ae9819f1113c4b
xieguigang <xie.guigang@gcmodeller.org>
2019-11-25 19:03:51 +0800

    vb object

---
+++ R#/Runtime/Internal/objects/vbObject.vb
@@ -0,0 +1,35 @@
+﻿Imports SMRUCC.Rsharp.Runtime.Components.Interface
+Imports SMRUCC.Rsharp.Runtime.Interop
+
+Namespace Runtime.Internal
+
+    ''' <summary>
+    ''' Proxy for VB.NET class <see cref="Object"/>
+    ''' </summary>
+    Public Class vbObject : Implements RNameIndex
+
+        Public ReadOnly Property target As Object
+        Public ReadOnly Property type As RType
+
+        Sub New(obj As Object)
+            target = obj
+            type = New RType(obj.GetType)
+        End Sub
+
+        Public Function getByName(name As String) As Object Implements RNameIndex.getByName
+            Throw New NotImplementedException()
+        End Function
+
+        Public Function getByName(names() As String) As Object Implements RNameIndex.getByName
+            Throw New NotImplementedException()
+        End Function
+
+        Public Function setByName(name As String, value As Object, envir As Environment) As Object Implements RNameIndex.setByName
+            Throw New NotImplementedException()
+        End Function
+
+        Public Function setByName(names() As String, value As Array, envir As Environment) As Object Implements RNameIndex.setByName
+            Throw New NotImplementedException()
+        End Function
+    End Class
+End Namespace
