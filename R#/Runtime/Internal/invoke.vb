3a99d0d8040d0aab1fb7aa0d75dbb6e5ad5f8686
xieguigang <xie.guigang@gcmodeller.org>
2019-11-26 11:24:07 +0800

    RInternalFuncInvoke

--- R#/Runtime/Internal/invoke.vb
+++ R#/Runtime/Internal/invoke.vb
@@ -55,11 +55,33 @@ Imports SMRUCC.Rsharp.Runtime.Components.Interface
 
 Namespace Runtime.Internal
 
-    Delegate Function RFuncInvoke(envir As Environment, funcName$, paramVals As Object()) As Object
+    ''' <summary>
+    ''' 内部函数的调用接口
+    ''' </summary>
+    Public MustInherit Class RInternalFuncInvoke
+
+        ''' <summary>
+        ''' 函数名称（函数名称主要是在抛出错误的时候添加调试信息所使用的）
+        ''' </summary>
+        ''' <returns></returns>
+        Public MustOverride ReadOnly Property funcName As String
+
+        ''' <summary>
+        ''' 执行这个内部函数
+        ''' </summary>
+        ''' <param name="envir">代码所执行的环境对象</param>
+        ''' <param name="paramVals">函数的参数</param>
+        ''' <returns></returns>
+        Public MustOverride Function invoke(envir As Environment, paramVals As Object()) As Object
+
+    End Class
 
     Module invoke
 
-        ReadOnly index As New Dictionary(Of String, RFuncInvoke)
+        ''' <summary>
+        ''' 内部函数索引
+        ''' </summary>
+        ReadOnly index As New Dictionary(Of String, RInternalFuncInvoke)
 
         Sub New()
 
