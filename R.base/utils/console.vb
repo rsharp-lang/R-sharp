b223007b9d9778af751924e180414d775975bf94
xieguigang <xie.guigang@gcmodeller.org>
2019-11-28 15:26:24 +0800

    found a bug in namespace reference

---
+++ R.base/utils/console.vb
@@ -0,0 +1,28 @@
+﻿
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports Microsoft.VisualBasic.Terminal.ProgressBar
+
+<Package("console", Category:=APICategories.SoftwareTools)>
+Module console
+
+    ''' <summary>
+    ''' Pin the top of progress bar
+    ''' </summary>
+    ''' <param name="top"></param>
+    ''' <returns></returns>
+    <ExportAPI("progressbar.pin.top")>
+    Public Function PinProgressBarTop(Optional top As Integer = -1) As Integer
+        If top <= 0 Then
+            top = System.Console.CursorTop
+        End If
+
+        Call ProgressBar.PinTop(top)
+
+        Return top
+    End Function
+
+    Public Sub ClearProgressBarPinned()
+        ProgressBar.ClearPinnedTop()
+    End Sub
+End Module
