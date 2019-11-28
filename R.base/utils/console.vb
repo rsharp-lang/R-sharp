a4cb5062e3fe58bf4e53fea2e720f091a7d2e40a
xieguigang <xie.guigang@gcmodeller.org>
2019-11-28 16:09:24 +0800

    Update console.vb
    get/set console colors

--- R.base/utils/console.vb
+++ R.base/utils/console.vb
@@ -22,7 +22,32 @@ Module console
         Return top
     End Function
 
+    <ExportAPI("progressbar.pin.clear")>
     Public Sub ClearProgressBarPinned()
         ProgressBar.ClearPinnedTop()
     End Sub
+
+    ReadOnly names As Dictionary(Of String, ConsoleColor) = Enums(Of ConsoleColor).ToDictionary(Function(cl) cl.ToString.ToLower)
+
+    <ExportAPI("fore.color")>
+    Public Function ConsoleForeColor(Optional color$ = Nothing) As String
+        If color.StringEmpty Then
+            color = System.Console.ForegroundColor.ToString.ToLower
+        Else
+            System.Console.ForegroundColor = names.TryGetValue(color.ToLower, [default]:=ConsoleColor.White)
+        End If
+
+        Return color
+    End Function
+
+    <ExportAPI("back.color")>
+    Public Function ConsoleBackColor(Optional color$ = Nothing) As String
+        If color.StringEmpty Then
+            color = System.Console.BackgroundColor.ToString.ToLower
+        Else
+            System.Console.BackgroundColor = names.TryGetValue(color.ToLower, [default]:=ConsoleColor.White)
+        End If
+
+        Return color
+    End Function
 End Module
