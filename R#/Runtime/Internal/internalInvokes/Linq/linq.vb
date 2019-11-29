c2890058f781ef637ed60dc8429635481d792196
xieguigang <xie.guigang@gcmodeller.org>
2019-11-29 14:52:03 +0800

    which filter

--- R#/Runtime/Internal/internalInvokes/Linq/linq.vb
+++ R#/Runtime/Internal/internalInvokes/Linq/linq.vb
@@ -11,13 +11,35 @@ Namespace Runtime.Internal.Invokes.LinqPipeline
     Module linq
 
         Sub New()
-            Internal.invoke.add("groupBy", AddressOf linq.groupBy)
+            Call Internal.invoke.add("groupBy", AddressOf linq.groupBy)
             Call Internal.invoke.add("first", AddressOf linq.first)
+            Call Internal.invoke.add("which", AddressOf linq.where)
         End Sub
 
         Friend Sub pushEnvir()
             ' do nothing
         End Sub
+
+        Private Function where(envir As Environment, params As Object()) As Object
+            Dim sequence As Array = Runtime.asVector(Of Object)(params(Scan0))
+            Dim test As RFunction = params(1)
+            Dim pass As Boolean
+            Dim arg As InvokeParameter
+            Dim filter As New List(Of Object)
+
+            For Each item As Object In sequence
+                arg = New InvokeParameter() With {
+                    .value = New RuntimeValueLiteral(item)
+                }
+                pass = Runtime.asLogical(test.Invoke(envir, {arg}))(Scan0)
+
+                If pass Then
+                    Call filter.Add(item)
+                End If
+            Next
+
+            Return filter.ToArray
+        End Function
 
         Private Function first(envir As Environment, params As Object()) As Object
             Dim sequence As Array = Runtime.asVector(Of Object)(params(Scan0))
