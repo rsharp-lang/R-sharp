c5b0fe7306c99dddc9984f83129f4bdf40ae9dc8
xieguigang <xie.guigang@gcmodeller.org>
2019-12-02 13:33:00 +0800

    fix bugs in peek next token

--- R#/test/tokenlicerTest.vb
+++ R#/test/tokenlicerTest.vb
@@ -1,52 +1,54 @@
 ﻿#Region "Microsoft.VisualBasic::456a2b91c908736b7bc30f36394f13ef, R#\test\tokenlicerTest.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module tokenlicerTest
-    ' 
-    '     Sub: cliInvoke, declareFunctionTest, declareTest, elementIndexer, lambdaTest
-    '          linqQueryTest, Main, operatorTest, pipelineTest, sequnceTest
-    '          stringParser, stringValueAssign
-    ' 
-    ' /********************************************************************************/
+' Module tokenlicerTest
+' 
+'     Sub: cliInvoke, declareFunctionTest, declareTest, elementIndexer, lambdaTest
+'          linqQueryTest, Main, operatorTest, pipelineTest, sequnceTest
+'          stringParser, stringValueAssign
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports Microsoft.VisualBasic.Text.Xml.Models
 Imports SMRUCC.Rsharp.Language.TokenIcer
+Imports SMRUCC.Rsharp.Runtime.Components
 
 Module tokenlicerTest
 
     Sub Main()
+        Call specialNameTest()
         Call elementIndexer()
         Call cliInvoke()
 
@@ -61,6 +63,12 @@ Module tokenlicerTest
         Call declareFunctionTest()
         Call stringParser()
         Call declareTest()
+
+        Pause()
+    End Sub
+
+    Sub specialNameTest()
+        Dim tokens = Rscript.FromText("let x as string = (!script)$dir;").GetTokens
 
         Pause()
     End Sub
