cefbcebcda444455160d2bcc419f5410757b04e4
xieguigang <xie.guigang@gcmodeller.org>
2019-12-03 21:35:36 +0800

    improvements for the dya=namics property value

--- R#/Runtime/Internal/objects/vbObject.vb
+++ R#/Runtime/Internal/objects/vbObject.vb
@@ -1,50 +1,51 @@
 ﻿#Region "Microsoft.VisualBasic::a983a46a56e85a369e2a22b9e2cdc674, R#\Runtime\Internal\objects\vbObject.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Class vbObject
-    ' 
-    '         Properties: target, type
-    ' 
-    '         Constructor: (+1 Overloads) Sub New
-    '         Function: (+2 Overloads) getByName, getNames, (+2 Overloads) setByName, ToString
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Class vbObject
+' 
+'         Properties: target, type
+' 
+'         Constructor: (+1 Overloads) Sub New
+'         Function: (+2 Overloads) getByName, getNames, (+2 Overloads) setByName, ToString
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.Reflection
 Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports SMRUCC.Rsharp.Runtime.Components.Interface
 Imports SMRUCC.Rsharp.Runtime.Internal.ConsolePrinter
 Imports SMRUCC.Rsharp.Runtime.Interop
@@ -95,6 +96,8 @@ Namespace Runtime.Internal
                 Return properties(name).GetValue(target)
             ElseIf methods.ContainsKey(name) Then
                 Return methods(name)
+            ElseIf type.haveDynamicsProperty Then
+                Return DirectCast(target, IDynamicsObject).GetItemValue(name)
             Else
                 Return Nothing
             End If
@@ -125,6 +128,8 @@ Namespace Runtime.Internal
                 Else
                     Return Internal.stop($"Target property '{name}' is not writeable!", envir)
                 End If
+            ElseIf type.haveDynamicsProperty Then
+                Call DirectCast(target, IDynamicsObject).SetValue(name, value)
             Else
                 Return Internal.stop($"Missing property '{name}'", envir)
             End If
