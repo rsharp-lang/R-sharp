2775d166485e062f6da2a6543cb64a5e6971850c
xieguigang <xie.guigang@gcmodeller.org>
2019-12-04 14:29:53 +0800

    R web helper module

---
+++ R.web/URL.vb
@@ -0,0 +1,24 @@
+﻿
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports Microsoft.VisualBasic.Net
+
+<Package("URL", Category:=APICategories.UtilityTools)>
+Public Module URL
+
+    <ExportAPI("url.get")>
+    Public Function [get](url As String) As String
+        Return url.GET
+    End Function
+
+    ''' <summary>
+    ''' Do file download
+    ''' </summary>
+    ''' <param name="url$"></param>
+    ''' <param name="saveAs$"></param>
+    ''' <returns></returns>
+    <ExportAPI("wget")>
+    Public Function wget(url$, saveAs$) As Boolean
+        Return Http.wget.Download(url, saveAs)
+    End Function
+End Module
