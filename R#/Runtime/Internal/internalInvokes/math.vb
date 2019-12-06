f76328cb6470b855b9dec74701d20c956c21b1dd
xieguigang <xie.guigang@gcmodeller.org>
2019-12-06 19:38:56 +0800

    add xml docs

--- R#/Runtime/Internal/internalInvokes/math.vb
+++ R#/Runtime/Internal/internalInvokes/math.vb
@@ -4,17 +4,43 @@ Imports stdNum = System.Math
 
 Namespace Runtime.Internal.Invokes
 
+    ''' <summary>
+    ''' 
+    ''' </summary>
     Module math
 
+        ''' <summary>
+        ''' rounds the values in its first argument to the specified number of decimal places (default 0). 
+        ''' See *'Details'* about "round to even" when rounding off a 5.
+        ''' </summary>
+        ''' <param name="x">a numeric vector. Or, for ``round`` and ``signif``, a complex vector.</param>
+        ''' <param name="decimals">
+        ''' integer indicating the number of decimal places (``round``) or significant digits (``signif``) to be used. 
+        ''' Negative values are allowed (see *'Details'*).
+        ''' </param>
+        ''' <returns></returns>
         <ExportAPI("round")>
         Public Function round(x As Object, Optional decimals% = 0) As Object
             If x.GetType.IsInheritsFrom(GetType(Array)) Then
-                Return (From element As Object In DirectCast(x, Array).AsQueryable Select stdNum.Round(CDbl(element), decimals)).ToArray
+                Return (From element As Object In
+                        DirectCast(x, Array).AsQueryable
+                        Select stdNum.Round(CDbl(element), decimals)).ToArray
             Else
                 Return stdNum.Round(CDbl(x), decimals)
             End If
         End Function
 
+        ''' <summary>
+        ''' computes logarithms, by default natural logarithms, log10 computes common (i.e., base 10) logarithms, 
+        ''' and log2 computes binary (i.e., base 2) logarithms. 
+        ''' The general form log(x, base) computes logarithms with base base.
+        ''' </summary>
+        ''' <param name="x">a numeric or complex vector.</param>
+        ''' <param name="newBase">
+        ''' a positive or complex number: the base with respect to which logarithms are computed. 
+        ''' Defaults to ``e=exp(1)``.
+        ''' </param>
+        ''' <returns></returns>
         <ExportAPI("log")>
         Public Function log(x As Double(), Optional newBase As Double = stdNum.E) As Object
             Return Runtime.asVector(Of Double)(x) _
