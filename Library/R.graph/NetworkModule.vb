fc41635c01b37bf2612b82362c5ec9e09411f9ed
关倩倩 <amethyst.asuka@gcmodeller.org>
2019-12-08 16:26:21 +0800

    R network library module

---
+++ Library/R.graph/NetworkModule.vb
@@ -0,0 +1,14 @@
+﻿
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Scripting.MetaData
+
+<Package("igraph")>
+Public Module NetworkModule
+
+    <ExportAPI("save.network")>
+    Public Function SaveNetwork(g As Object, file$, Optional properties As String() = Nothing) As Boolean
+        If g Is Nothing Then
+
+        End If
+    End Function
+End Module
