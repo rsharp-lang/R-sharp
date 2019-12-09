bc23abedaff035a855476a5344a83c6f75e81ffc
xieguigang <xie.guigang@gcmodeller.org>
2019-12-09 10:43:37 +0800

    option test success

---
+++ R#/test/configTest.vb
@@ -0,0 +1,20 @@
+﻿Imports SMRUCC.Rsharp.Interpreter
+
+Module configTest
+
+    Dim R As New RInterpreter
+
+    Sub Main()
+        Call printTest()
+    End Sub
+
+    Sub printTest()
+
+        Call R.Evaluate("options(max.print = 100)")
+        Call R.Print("getOption(""max.print"")")
+        Call R.Print("options(""max.print"")")
+        Call R.Print("1:500 step 0.125")
+
+        Pause()
+    End Sub
+End Module
