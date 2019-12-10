c78bc003c16b523bc05862e7b45ff1d69386d9ee
xieguigang <xie.guigang@gcmodeller.org>
2019-12-10 14:17:58 +0800

    auto flush data

--- R#/System/Package/PackageManager.vb
+++ R#/System/Package/PackageManager.vb
@@ -53,7 +53,7 @@ Imports SMRUCC.Rsharp.Runtime.Components.Configuration
 
 Namespace Runtime.Package
 
-    Public Class PackageManager
+    Public Class PackageManager : Implements IDisposable
 
         ReadOnly pkgDb As LocalPackageDatabase
         ReadOnly config As Options
@@ -101,5 +101,38 @@ Namespace Runtime.Package
         Public Sub Flush()
             Call pkgDb.GetXml.SaveTo(config.lib)
         End Sub
+
+#Region "IDisposable Support"
+        Private disposedValue As Boolean ' To detect redundant calls
+
+        ' IDisposable
+        Protected Overridable Sub Dispose(disposing As Boolean)
+            If Not disposedValue Then
+                If disposing Then
+                    ' TODO: dispose managed state (managed objects).
+                    Call Flush()
+                End If
+
+                ' TODO: free unmanaged resources (unmanaged objects) and override Finalize() below.
+                ' TODO: set large fields to null.
+            End If
+            disposedValue = True
+        End Sub
+
+        ' TODO: override Finalize() only if Dispose(disposing As Boolean) above has code to free unmanaged resources.
+        'Protected Overrides Sub Finalize()
+        '    ' Do not change this code.  Put cleanup code in Dispose(disposing As Boolean) above.
+        '    Dispose(False)
+        '    MyBase.Finalize()
+        'End Sub
+
+        ' This code added by Visual Basic to correctly implement the disposable pattern.
+        Public Sub Dispose() Implements IDisposable.Dispose
+            ' Do not change this code.  Put cleanup code in Dispose(disposing As Boolean) above.
+            Dispose(True)
+            ' TODO: uncomment the following line if Finalize() is overridden above.
+            ' GC.SuppressFinalize(Me)
+        End Sub
+#End Region
     End Class
 End Namespace
