3870756e27382776dc721a0fd66dae40df5fcc81
xieguigang <xie.guigang@gcmodeller.org>
2019-12-16 16:55:40 +0800

    R environment helper

---
+++ R#/Runtime/Internal/internalInvokes/env.vb
@@ -0,0 +1,46 @@
+﻿Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Linq
+Imports SMRUCC.Rsharp.Runtime.Components
+
+Namespace Runtime.Internal.Invokes
+
+    Module env
+
+        <ExportAPI("get")>
+        Public Function [get](x As Object, envir As Environment) As Object
+            Dim name As String = Runtime.asVector(Of Object)(x) _
+                .DoCall(Function(o)
+                            Return Scripting.ToString(Runtime.getFirst(o), null:=Nothing)
+                        End Function)
+
+            If name.StringEmpty Then
+                Return Internal.stop("NULL value provided for object name!", envir)
+            End If
+
+            Dim symbol As Variable = envir.FindSymbol(name)
+
+            If symbol Is Nothing Then
+                Return Message.SymbolNotFound(envir, name, TypeCodes.generic)
+            Else
+                Return symbol.value
+            End If
+        End Function
+
+        <ExportAPI("globalenv")>
+        <DebuggerStepThrough>
+        Private Function globalenv(env As Environment) As Object
+            Return env.globalEnvironment
+        End Function
+
+        <ExportAPI("environment")>
+        <DebuggerStepThrough>
+        Private Function environment(env As Environment) As Object
+            Return env
+        End Function
+
+        <ExportAPI("ls")>
+        Private Function ls(env As Environment) As Object
+            Return env.variables.Keys.ToArray
+        End Function
+    End Module
+End Namespace
--- R#/runtime/Internal/internalInvokes/base.vb
+++ R#/runtime/Internal/internalInvokes/base.vb
@@ -157,12 +157,6 @@ Namespace Runtime.Internal.Invokes
             End If
         End Function
 
-        <ExportAPI("globalenv")>
-        <DebuggerStepThrough>
-        Private Function globalenv(env As Environment) As Object
-            Return env.globalEnvironment
-        End Function
-
         ''' <summary>
         ''' Get vector length
         ''' </summary>
@@ -275,26 +269,6 @@ Namespace Runtime.Internal.Invokes
             End If
 
             Return values
-        End Function
-
-        <ExportAPI("get")>
-        Public Function [get](x As Object, envir As Environment) As Object
-            Dim name As String = Runtime.asVector(Of Object)(x) _
-                .DoCall(Function(o)
-                            Return Scripting.ToString(Runtime.getFirst(o), null:=Nothing)
-                        End Function)
-
-            If name.StringEmpty Then
-                Return Internal.stop("NULL value provided for object name!", envir)
-            End If
-
-            Dim symbol As Variable = envir.FindSymbol(name)
-
-            If symbol Is Nothing Then
-                Return Message.SymbolNotFound(envir, name, TypeCodes.generic)
-            Else
-                Return symbol.value
-            End If
         End Function
 
         <ExportAPI("names")>
