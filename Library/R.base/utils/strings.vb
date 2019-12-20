22eb5c57a70865a06de2fce89c711938277fc3d2
xieguigang <xie.guigang@gcmodeller.org>
2019-12-20 21:17:08 +0800

    add strings levenshtein

---
+++ Library/R.base/utils/strings.vb
@@ -0,0 +1,12 @@
+﻿Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.ComponentModel.Algorithm.DynamicProgramming.Levenshtein
+Imports Microsoft.VisualBasic.Scripting.MetaData
+
+<Package("strings", Category:=APICategories.UtilityTools)>
+Module strings
+
+    <ExportAPI("levenshtein")>
+    Public Function Levenshtein(x$, y$) As DistResult
+        Return LevenshteinDistance.ComputeDistance(x, y)
+    End Function
+End Module
