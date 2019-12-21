7e56a435991b58a37b7491cca837e186bd732d06
xieguigang <xie.guigang@gcmodeller.org>
2019-12-21 13:30:45 +0800

    add logo text

--- R-terminal/Terminal.vb
+++ R-terminal/Terminal.vb
@@ -1,49 +1,50 @@
 ﻿#Region "Microsoft.VisualBasic::b6d2c12507910a62bcc76a14abca47dc, R-terminal\Terminal.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module Terminal
-    ' 
-    '     Constructor: (+1 Overloads) Sub New
-    ' 
-    '     Function: isSimplePrintCall, isValueAssign, RunTerminal
-    ' 
-    '     Sub: doRunScript, doRunScriptWithSpecialCommand
-    ' 
-    ' /********************************************************************************/
+' Module Terminal
+' 
+'     Constructor: (+1 Overloads) Sub New
+' 
+'     Function: isSimplePrintCall, isValueAssign, RunTerminal
+' 
+'     Sub: doRunScript, doRunScriptWithSpecialCommand
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.ApplicationServices.Development
 Imports Microsoft.VisualBasic.ComponentModel.Collection
 Imports Microsoft.VisualBasic.Language.UnixBash
 Imports Microsoft.VisualBasic.Terminal
@@ -59,6 +60,18 @@ Module Terminal
     Dim R As RInterpreter
 
     Sub New()
+        Dim Rcore = GetType(RInterpreter).Assembly.FromAssembly
+        Dim framework = GetType(App).Assembly.FromAssembly
+
+        Call Console.WriteLine($"
+   , __           | 
+  /|/  \  |  |    | Documentation: https://r_lang.dev.SMRUCC.org/
+   |___/--+--+--  |
+   | \  --+--+--  | Version {Rcore.AssemblyVersion} ({Rcore.BuiltTime.ToString})
+   |  \_/ |  |    | sciBASIC.NET Runtime: {framework.AssemblyVersion}               
+                  
+Welcome to the R# language
+")
         Call Console.WriteLine("Type 'demo()' for some demos, 'help()' for on-line help, or
 'help.start()' for an HTML browser interface to help.
 Type 'q()' to quit R.
@@ -72,7 +85,8 @@ Type 'q()' to quit R.
         Call R.LoadLibrary("utils")
         Call R.LoadLibrary("grDevices")
 
-        Call Console.WriteLine()
+        Console.WriteLine()
+        Console.Title = "R# language"
 
         Call New Shell(New PS1("> "), AddressOf doRunScriptWithSpecialCommand) With {
             .Quite = "!.R#::quit"
@@ -88,6 +102,8 @@ Type 'q()' to quit R.
             Case Else
                 Call doRunScript(script)
         End Select
+
+        Console.Title = "R# language"
     End Sub
 
     Private Sub doRunScript(script As String)
