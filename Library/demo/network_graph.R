925f08c0eb45b4c7874aadd297bfe06f9b2e1082
xieguigang <xie.guigang@gcmodeller.org>
2019-12-24 23:49:05 +0800

    create network test success

--- Library/demo/network_graph.R
+++ Library/demo/network_graph.R
@@ -6,5 +6,11 @@ for(id in ["A","B","C","D","E"]) {
 	g :> add.node(label = id);
 }
 
+g :> add.edge("A", "B");
+g :> add.edge("B", "C");
+g :> add.edge("C", "D");
+g :> add.edge("C", "E");
+g :> add.edge("A", "E");
+
 g :> save.network(file = `${!script$dir}/demo_network`);
 
