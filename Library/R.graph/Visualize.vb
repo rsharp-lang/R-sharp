6cd059b91f4d02638d36d7318e173eeb1821ac1b
xieguigang <xie.guigang@gcmodeller.org>
2019-12-24 23:57:21 +0800

    render plot

--- Library/R.graph/Visualize.vb
+++ Library/R.graph/Visualize.vb
@@ -1,7 +1,16 @@
-﻿Imports Microsoft.VisualBasic.Scripting.MetaData
+﻿Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Data.visualize.Network
+Imports Microsoft.VisualBasic.Data.visualize.Network.Graph
+Imports Microsoft.VisualBasic.Imaging.Driver
+Imports Microsoft.VisualBasic.Scripting.MetaData
 
 <Package("igraph.render")>
 Module Visualize
+
+    <ExportAPI("render.Plot")>
+    Public Function renderPlot(g As NetworkGraph) As GraphicsData
+        Return g.DrawImage()
+    End Function
 
 End Module
 
