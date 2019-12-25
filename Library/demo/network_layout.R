6635af79c01f62a446fb7778196943ec06ab54df
xieguigang <xie.guigang@gcmodeller.org>
2019-12-25 20:14:30 +0800

    render the resulted layout network

--- Library/demo/network_layout.R
+++ Library/demo/network_layout.R
@@ -1,5 +1,6 @@
 require(igraph);
 require(igraph.layouts);
+require(igraph.render);
 
 let test.network as string = `${!script$dir}/demo_network`;
 let g = read.network(test.network);
@@ -9,4 +10,7 @@ print(g);
 
 g
 :> layout.orthogonal
-:> save.network(file = test.network);
+# Do network render plot and then 
+# saved the generated image file
+:> render.Plot(canvasSize = [600,400]) 
+:> save.graphics(file = `${!script$dir}/orthogonal.png`);
