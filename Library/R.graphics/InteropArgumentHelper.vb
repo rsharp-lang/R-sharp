b922a5264d4fa2404842194bcfa6773e294eaf72
xieguigang <xie.guigang@gcmodeller.org>
2019-12-25 19:51:37 +0800

    argument helper

---
+++ Library/R.graphics/InteropArgumentHelper.vb
@@ -0,0 +1,31 @@
+﻿Imports System.Drawing
+
+Public Module InteropArgumentHelper
+
+    Public Function getSize(size As Object) As String
+        If size Is Nothing Then
+            Return Nothing
+        End If
+
+        Select Case size.GetType
+            Case GetType(String)
+                Return size
+            Case GetType(String())
+                Return DirectCast(size, String()).ElementAtOrDefault(Scan0)
+            Case GetType(Size)
+                With DirectCast(size, Size)
+                    Return $"{ .Width},{ .Height}"
+                End With
+            Case GetType(SizeF)
+                With DirectCast(size, SizeF)
+                    Return $"{ .Width},{ .Height}"
+                End With
+            Case GetType(Integer()), GetType(Long()), GetType(Single()), GetType(Double()), GetType(Short())
+                With DirectCast(size, Array)
+                    Return $"{ .GetValue(0)},{ .GetValue(1)}"
+                End With
+            Case Else
+                Return Nothing
+        End Select
+    End Function
+End Module
