fbf642a3c49bafdebb66d39cbe385b7655a469d7
xieguigang <xie.guigang@gcmodeller.org>
2019-12-25 20:06:08 +0800

    layout.orthogonal

--- Library/R.graph/Layouts.vb
+++ Library/R.graph/Layouts.vb
@@ -1,8 +1,12 @@
 ﻿
+Imports System.Drawing
 Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Data.visualize.Network.Graph
 Imports Microsoft.VisualBasic.Data.visualize.Network.Layouts
 Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports Microsoft.VisualBasic.Scripting.Runtime
+Imports R.graphics
+Imports SMRUCC.Rsharp.Runtime.Interop
 
 <Package("igraph.layouts")>
 Module Layouts
@@ -13,5 +17,20 @@ Module Layouts
             showProgress:=True,
             iterations:=iterations
         )
+    End Function
+
+    <ExportAPI("layout.orthogonal")>
+    Public Function orthogonalLayout(g As NetworkGraph,
+                                     <RRawVectorArgument>
+                                     Optional gridSize As Object = "10,10",
+                                     Optional delta# = 1) As NetworkGraph
+
+        Dim size As Size = InteropArgumentHelper _
+            .getSize(gridSize) _
+            .SizeParser
+
+        Call Orthogonal.DoLayout(g, size, delta)
+
+        Return g
     End Function
 End Module
