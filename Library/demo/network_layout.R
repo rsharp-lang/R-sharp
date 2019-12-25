7a497dcfab5028daba0e0164fbb8a64fdd778ad9
xieguigang <xie.guigang@gcmodeller.org>
2019-12-25 20:11:02 +0800

    network layout demo

--- Library/demo/network_layout.R
+++ Library/demo/network_layout.R
@@ -1,4 +1,12 @@
 require(igraph);
 require(igraph.layouts);
 
-let g = network("./demo_network");
+let test.network as string = `${!script$dir}/demo_network`;
+let g = read.network(test.network);
+
+# view summary info of the network
+print(g);
+
+g
+:> layout.orthogonal
+:> save.network(file = test.network);
