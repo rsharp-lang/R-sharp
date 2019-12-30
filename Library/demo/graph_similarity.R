cb168b76f4fb7c97546f6874c98661210cbeb442
xieguigang <xie.guigang@gcmodeller.org>
2019-12-30 23:54:04 +0800

    test not success

---
+++ Library/demo/graph_similarity.R
@@ -0,0 +1,16 @@
+imports ["igraph", "igraph.comparison"] from "R.graph.dll";
+
+let g = read.network(`${!script$dir}/demo_network`);
+let z = read.network(`${!script$dir}/demo_network`);
+
+# z is identical to g
+print(graph.jaccard(g, z));
+
+# modify z network
+g :> add.edge("G", "E");
+g :> add.edge("G", "C1");
+g :> add.edge("G", "B1");
+g :> add.edge("Z", "D");
+
+# z is similar to g
+print(graph.jaccard(g, z));
