f239ae19014a17d691a702cc15392a211d1736f4
xieguigang <xie.guigang@gcmodeller.org>
2019-12-30 19:57:48 +0800

    node similarity

---
+++ Library/R.graph/Comparison.vb
@@ -0,0 +1,20 @@
+﻿Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Data.visualize.Network.Graph
+Imports Microsoft.VisualBasic.Data.visualize.Network
+Imports Microsoft.VisualBasic.Scripting.MetaData
+
+<Package("igraph.comparison")>
+Module Comparison
+
+    ''' <summary>
+    ''' calculate node similarity cos score.
+    ''' </summary>
+    ''' <param name="a"></param>
+    ''' <param name="b"></param>
+    ''' <returns></returns>
+    <ExportAPI("node.cos")>
+    Public Function similarity(a As Node, b As Node) As Double
+        Return Analysis.NodeSimilarity(a, b)
+    End Function
+
+End Module
