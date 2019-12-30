7a6d394e0c428a65ee0dc99bfbd3b50b16a46fab
xieguigang <xie.guigang@gcmodeller.org>
2019-12-30 13:22:06 +0800

    Update dataframe.vb
    improvements of the dataframe api

--- Library/R.base/utils/dataframe.vb
+++ Library/R.base/utils/dataframe.vb
@@ -1,42 +1,42 @@
 ﻿#Region "Microsoft.VisualBasic::59dbba85345f6ce223af9d67e06fcd96, Library\R.base\utils\dataframe.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module dataframe
-    ' 
-    '     Constructor: (+1 Overloads) Sub New
-    '     Function: colnames, project, readDataSet, RowToString, vector
-    ' 
-    ' /********************************************************************************/
+' Module dataframe
+' 
+'     Constructor: (+1 Overloads) Sub New
+'     Function: colnames, project, readDataSet, RowToString, vector
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -84,7 +84,7 @@ Module dataframe
 
     <ExportAPI("dataset.colnames")>
     Public Function colnames(dataset As Array, envir As Environment) As Object
-        Dim baseElement As Type = dataset.GetValue(Scan0).GetType
+        Dim baseElement As Type = Runtime.MeasureArrayElementType(dataset)
 
         If baseElement Is GetType(EntityObject) Then
             Return dataset.AsObjectEnumerator _
@@ -117,7 +117,7 @@ Module dataframe
             Return Nothing
         End If
 
-        Dim baseElement As Type = dataset.GetValue(Scan0).GetType
+        Dim baseElement As Type = Runtime.MeasureArrayElementType(dataset)
         Dim vectors As New List(Of Object)()
         Dim isGetter As Boolean = False
         Dim getValue As Func(Of Object) = Nothing
@@ -163,14 +163,14 @@ Module dataframe
     End Function
 
     ''' <summary>
-    ''' 
+    ''' Subset of the given dataframe by columns
     ''' </summary>
     ''' <param name="dataset"></param>
     ''' <param name="cols"></param>
     ''' <returns></returns>
     <ExportAPI("dataset.project")>
-    Public Function project(dataset As Array, cols As String(), envir As Environment) As Object
-        Dim baseElement As Type = dataset.GetValue(Scan0).GetType
+    Public Function project(dataset As Array, cols$(), envir As Environment) As Object
+        Dim baseElement As Type = Runtime.MeasureArrayElementType(dataset)
 
         If baseElement Is GetType(EntityObject) Then
             Return dataset.AsObjectEnumerator _
@@ -201,6 +201,12 @@ Module dataframe
         End If
     End Function
 
+    ''' <summary>
+    ''' Read dataframe
+    ''' </summary>
+    ''' <param name="file$"></param>
+    ''' <param name="mode$"></param>
+    ''' <returns></returns>
     <ExportAPI("read.dataframe")>
     Public Function readDataSet(file$, Optional mode$ = "numeric|character") As Object
         Dim readMode = mode.Split("|"c).First
