e01dd27a88950d5428aee5618a031861ca5cd4ef
xieguigang <xie.guigang@gcmodeller.org>
2019-12-31 23:50:37 +0800

    dijkstra router

--- Library/R.graph/Dijkstra.vb
+++ Library/R.graph/Dijkstra.vb
@@ -1,50 +1,54 @@
 ﻿#Region "Microsoft.VisualBasic::004295d7ccb1e61074f84816dc0b900d, Library\R.graph\Dijkstra.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module Dijkstra
-    ' 
-    '     Function: CreateRouter, DijkstraRoutine
-    ' 
-    ' /********************************************************************************/
+' Module Dijkstra
+' 
+'     Function: CreateRouter, DijkstraRoutine
+' 
+' /********************************************************************************/
 
 #End Region
-
 
 Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Data.GraphTheory.Dijkstra
 Imports Microsoft.VisualBasic.Data.visualize.Network.Graph
 Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.Rsharp.Runtime
+Imports REnv = SMRUCC.Rsharp.Runtime.Internal
 
+''' <summary>
+''' Graph router
+''' </summary>
 <Package("igraph.dijkstra")>
 Module Dijkstra
 
@@ -53,8 +57,22 @@ Module Dijkstra
         Return DijkstraRouter.FromNetwork(g, undirected)
     End Function
 
-    Public Function DijkstraRoutine()
+    <ExportAPI("routine.min_cost")>
+    Public Function DijkstraRoutine(router As DijkstraRouter, from As Object, [to] As Object, Optional env As Environment = Nothing) As Object
+        If from Is Nothing Then
+            Return REnv.debug.stop("start point is nothing!", env)
+        ElseIf [to] Is Nothing Then
+            Return REnv.debug.stop("stop point is not specific!", env)
+        End If
 
+        If from.GetType Is GetType(String) Then
+            from = router.GetLocation(from)
+        End If
+        If [to].GetType Is GetType(String) Then
+            [to] = router.GetLocation([to])
+        End If
+
+        Return router.CalculateMinCost(from, [to])
     End Function
 End Module
 
