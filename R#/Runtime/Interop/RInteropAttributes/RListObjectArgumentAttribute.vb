f899f37250b24a5b6b7d7dc7948bb14460681d41
xieguigang <xie.guigang@gcmodeller.org>
2020-01-02 13:09:38 +0800

    split source files

---
+++ R#/Runtime/Interop/RInteropAttributes/RListObjectArgumentAttribute.vb
@@ -0,0 +1,52 @@
+﻿Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports SMRUCC.Rsharp.Runtime.Components
+Imports SMRUCC.Rsharp.Runtime.Internal.Object
+
+Namespace Runtime.Interop
+
+    ''' <summary>
+    ''' 表示当前的函数参数为一个 ``...`` 可以产生一个字典list对象值的参数列表
+    ''' </summary>
+    <AttributeUsage(AttributeTargets.Parameter, AllowMultiple:=False, Inherited:=True)>
+    Public Class RListObjectArgumentAttribute : Inherits RInteropAttribute
+
+        ''' <summary>
+        ''' Safe get a collection of argument name and value tuple
+        ''' </summary>
+        ''' <param name="objects"></param>
+        ''' <param name="envir"></param>
+        ''' <returns></returns>
+        Public Shared Iterator Function getObjectList(<RListObjectArgument> objects As Object, envir As Environment) As IEnumerable(Of NamedValue(Of Object))
+            Dim type As Type
+
+            If objects Is Nothing Then
+                Return
+            Else
+                type = objects.GetType
+            End If
+
+            If type Is GetType(List) Then
+                objects = DirectCast(objects, List).slots
+                type = GetType(Dictionary(Of String, Object))
+            End If
+
+            If type Is GetType(Dictionary(Of String, Object)) Then
+                For Each item In DirectCast(objects, Dictionary(Of String, Object))
+                    Yield New NamedValue(Of Object) With {
+                        .Name = item.Key,
+                        .Value = item.Value
+                    }
+                Next
+            ElseIf type Is GetType(InvokeParameter()) Then
+                For Each item As InvokeParameter In DirectCast(objects, InvokeParameter())
+                    Yield New NamedValue(Of Object) With {
+                        .Name = item.name,
+                        .Value = item.Evaluate(envir)
+                    }
+                Next
+            Else
+                Throw New NotImplementedException
+            End If
+        End Function
+    End Class
+End Namespace
---
+++ R#/Runtime/Interop/RInteropAttributes/RSymbolTextArgumentAttribute.vb
@@ -0,0 +1,36 @@
+﻿Imports SMRUCC.Rsharp.Interpreter.ExecuteEngine
+Imports SMRUCC.Rsharp.Runtime.Components
+
+Namespace Runtime.Interop
+
+    ''' <summary>
+    ''' 表示当前的这个参数的参数值可以接受文本或者未找到符号的变量名作为参数值
+    ''' </summary>
+    ''' 
+    <AttributeUsage(AttributeTargets.Parameter, AllowMultiple:=False, Inherited:=True)>
+    Public Class RSymbolTextArgumentAttribute : Inherits RInteropAttribute
+
+        ' 例如
+        ' 接受文本参数值   func("text")
+        ' 接受符号名参数值 func(text) 如果text不存在于环境中，则等价于func("text")
+        ' 如果text存在于环境中，并且值为hello，则等价于func("hello")
+
+        Public Shared Function getSymbolText(symbol As Expression, env As Environment) As String
+            Select Case symbol.GetType
+                Case GetType(Literal)
+                    Return DirectCast(symbol, Literal).value
+                Case GetType(SymbolReference)
+                    Dim symbolName$ = DirectCast(symbol, SymbolReference).symbol
+                    Dim var As Variable = env.FindSymbol(symbolName)
+
+                    If var Is Nothing Then
+                        Return symbolName
+                    Else
+                        Return Scripting.ToString(var.value, Nothing)
+                    End If
+                Case Else
+                    Return Scripting.ToString(symbol.Evaluate(env), Nothing)
+            End Select
+        End Function
+    End Class
+End Namespace
