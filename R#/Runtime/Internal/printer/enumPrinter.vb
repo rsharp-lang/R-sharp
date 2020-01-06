249f97c642ed818719f33d88d1249f77e62c5368
xieguigang <xie.guigang@gcmodeller.org>
2020-01-06 20:52:14 +0800

    print enum class ?

---
+++ R#/Runtime/Internal/printer/enumPrinter.vb
@@ -0,0 +1,16 @@
+﻿Namespace Runtime.Internal.ConsolePrinter
+
+    Module enumPrinter
+
+        ''' <summary>
+        ''' 
+        ''' </summary>
+        ''' <param name="obj">
+        ''' This object should be an enum value.
+        ''' </param>
+        ''' <returns></returns>
+        Public Function printClass(obj As Object) As String
+
+        End Function
+    End Module
+End Namespace
