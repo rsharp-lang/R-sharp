4622a83329259284576169da633ee1f1a71bbcc9
xieguigang <xie.guigang@gcmodeller.org>
2020-01-06 22:07:47 +0800

    print enum values

--- R#/Runtime/Internal/printer/enumPrinter.vb
+++ R#/Runtime/Internal/printer/enumPrinter.vb
@@ -23,5 +23,18 @@ Namespace Runtime.Internal.ConsolePrinter
                 Return $"{print} #{describ}"
             End If
         End Function
+
+        Friend Function defaultValueToString([default] As Object, type As Type) As String
+            Dim s As String = [default].ToString
+
+            If s.IsPattern("\d+") Then
+                ' is flag combinations
+                s = GetAllEnumFlags([default], type) _
+                    .Select(Function(flag) flag.ToString) _
+                    .JoinBy("|")
+            End If
+
+            Return $"[{s}]"
+        End Function
     End Module
 End Namespace
--- R#/runtime/Interop/RType.vb
+++ R#/runtime/Interop/RType.vb
@@ -1,50 +1,51 @@
 ﻿#Region "Microsoft.VisualBasic::5865532df870dcb27b70657554e0aa74, R#\Runtime\Interop\RType.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Class RType
-    ' 
-    '         Properties: fullName, haveDynamicsProperty, isArray, isCollection, isEnvironment
-    '                     mode, raw
-    ' 
-    '         Constructor: (+1 Overloads) Sub New
-    '         Function: getNames, GetRawElementType, GetRSharpType, populateNames, ToString
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Class RType
+' 
+'         Properties: fullName, haveDynamicsProperty, isArray, isCollection, isEnvironment
+'                     mode, raw
+' 
+'         Constructor: (+1 Overloads) Sub New
+'         Function: getNames, GetRawElementType, GetRSharpType, populateNames, ToString
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.Reflection
+Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports Microsoft.VisualBasic.Emit.Delegates
 Imports SMRUCC.Rsharp.Runtime.Components
@@ -63,6 +64,8 @@ Namespace Runtime.Interop
         ''' </summary>
         ''' <returns></returns>
         Public ReadOnly Property fullName As String
+            <MethodImpl(MethodImplOptions.AggressiveInlining)>
+            <DebuggerStepThrough>
             Get
                 Return raw.FullName
             End Get
@@ -140,6 +143,9 @@ Namespace Runtime.Interop
         ''' Get method names and property names of target type object instance
         ''' </summary>
         ''' <returns></returns>
+        ''' 
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        <DebuggerStepThrough>
         Public Function getNames() As String() Implements IReflector.getNames
             Return names.Clone
         End Function
@@ -153,5 +159,11 @@ Namespace Runtime.Interop
             Static cache As New Dictionary(Of Type, RType)
             Return cache.ComputeIfAbsent(type, Function(t) New RType(t))
         End Function
+
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        <DebuggerStepThrough>
+        Public Shared Narrowing Operator CType(type As RType) As Type
+            Return type.raw
+        End Operator
     End Class
 End Namespace
