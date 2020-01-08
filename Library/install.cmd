c84651528d984e278233cec4930332afd7a908f9
xieguigang <xie.guigang@gcmodeller.org>
2020-01-08 15:54:32 +0800

    Create install.cmd

---
+++ Library/install.cmd
@@ -0,0 +1,15 @@
+@echo off
+
+CD "../App/"
+
+REM Install a given list of local system library modules into 
+REM the repository database of R# package system.
+
+R# install.packages('devkit.dll');
+R# install.packages('R.base.dll');
+R# install.packages('R.graph.dll');
+R# install.packages('R.graphics.dll');
+R# install.packages('R.plot.dll');
+R# install.packages('R.web.dll');
+
+pause
