2fb040bb3203a0da9429ff8dcef927beaea200dd
xieguigang <xie.guigang@gcmodeller.org>
2020-01-10 14:41:16 +0800

    try to fix for dataframe api

---
+++ R#/runtime/Interop/RArgumentList.vb
@@ -0,0 +1,15 @@
+﻿Imports System.Reflection
+
+Namespace Runtime.Interop
+
+    Public Class RArgumentList
+
+        ''' <summary>
+        ''' Create argument value for <see cref="MethodInfo.Invoke(Object, Object())"/>
+        ''' </summary>
+        ''' <returns></returns>
+        Public Iterator Function CreateArguments() As IEnumerable(Of Object)
+
+        End Function
+    End Class
+End Namespace
