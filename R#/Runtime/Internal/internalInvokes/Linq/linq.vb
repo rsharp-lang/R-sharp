3c9326a60d1fe6bc83fabd7c0b7d2657335e6c7d
xieguigang <xie.guigang@gcmodeller.org>
2020-01-13 18:38:01 +0800

    improvements for the first in linq module

--- R#/Runtime/Internal/internalInvokes/Linq/linq.vb
+++ R#/Runtime/Internal/internalInvokes/Linq/linq.vb
@@ -1,42 +1,42 @@
 ﻿#Region "Microsoft.VisualBasic::a19e21776e5d6ce31637e21e5bef0ba6, R#\Runtime\Internal\internalInvokes\Linq\linq.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Module linq
-    ' 
-    '         Function: first, groupBy, projectAs, unique, where
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Module linq
+' 
+'         Function: first, groupBy, projectAs, unique, where
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -98,9 +98,20 @@ Namespace Runtime.Internal.Invokes.LinqPipeline
         End Function
 
         <ExportAPI("first")>
-        Private Function first(sequence As Array, test As RFunction, envir As Environment) As Object
+        Private Function first(sequence As Array,
+                               Optional test As RFunction = Nothing,
+                               Optional envir As Environment = Nothing) As Object
+
             Dim pass As Boolean
             Dim arg As InvokeParameter()
+
+            If test Is Nothing Then
+                If sequence.Length = 0 Then
+                    Return Nothing
+                Else
+                    Return sequence.GetValue(Scan0)
+                End If
+            End If
 
             For Each item As Object In sequence
                 arg = InvokeParameter.Create(item)
