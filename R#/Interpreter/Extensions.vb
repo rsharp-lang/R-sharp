b5badad8af5caa5ef63057e9b86b9d8ab074c3f3
xieguigang <xie.guigang@gcmodeller.org>
2020-01-22 09:53:18 +0800

    improvements of the debug stacktrace of the script parser

--- R#/Interpreter/Extensions.vb
+++ R#/Interpreter/Extensions.vb
@@ -1,44 +1,44 @@
 ﻿#Region "Microsoft.VisualBasic::5aeb06fd6880dd6ef596174a3b514c00, R#\Interpreter\Extensions.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Module Extensions
-    ' 
-    '         Function: (+2 Overloads) GetExpressions, isTerminator
-    ' 
-    '         Sub: SyntaxErrorHelper
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Module Extensions
+' 
+'         Function: (+2 Overloads) GetExpressions, isTerminator
+' 
+'         Sub: SyntaxErrorHelper
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -117,8 +117,10 @@ Namespace Interpreter
 
             message &= vbCrLf & vbCrLf & "Syntax error nearby:"
             message &= vbCrLf & vbCrLf & rawText
+            message &= vbCrLf & vbCrLf & $"Range from {tokens.First.span.start} at line {tokens.First.span.line}, to {tokens.Last.span.stops} at line {tokens.Last.span.line}."
             message &= vbCrLf & vbCrLf & "The parser stack trace:"
             message &= vbCrLf & vbCrLf & syntaxResult.stackTrace
+            message &= vbCrLf & vbCrLf & "   --=== End of the R# Parser StackTrace ===--"
             message &= vbCrLf & vbCrLf
 
             Throw New Exception(message)
