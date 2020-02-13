5cf5094517670b22c7f2fcb0d9db386a21c2f981
xieguigang <xie.guigang@gcmodeller.org>
2020-02-13 15:37:50 +0800

    add demo scripts

---
+++ tutorials/Rscript/multiples/app.R
@@ -0,0 +1,5 @@
+# script file will be search under current script directory
+imports "part1.R";
+imports "debugger.R";
+
+getStackInfo();
---
+++ tutorials/Rscript/multiples/part1.R
@@ -0,0 +1,8 @@
+let callers as function() {
+    let internal as function() {
+        # get stack trace information
+        traceback();
+    }
+
+    internal();
+}
