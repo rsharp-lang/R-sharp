a2d0e3fad7d44ba9fee1c5f8677deeb5a3f68224
xieguigang <xie.guigang@gcmodeller.org>
2020-02-13 16:46:48 +0800

    test R folder

--- tutorials/Rscript/multiples/app.R
+++ tutorials/Rscript/multiples/app.R
@@ -1,5 +1,5 @@
 # script file will be search under current script directory
-imports ["part1.R", "missing.R"];
-imports "debugger.R";
+# imports ["part1.R", "missing.R"];
+imports ["debugger.R", "part1.R"];
 
 getStackInfo();
