a99d79e0415d31253a59467b9c2be58cb72a0d6e
xieguigang <xie.guigang@gcmodeller.org>
2020-02-13 17:34:33 +0800

    config loop stack information test

--- tutorials/Rscript/multiples/R/part1.R
+++ tutorials/Rscript/multiples/R/part1.R
@@ -1,8 +1,19 @@
+
 let callers as function() {
     let internal as function() {
-        # get stack trace information
-        traceback();
+          doLoop();
     }
 
     internal();
+}
+
+let doLoop as function() {
+
+	for(i in [1,2]) {
+		if (i == 2) {
+		 # get stack trace information
+        traceback();
+		}
+	}
+
 }
