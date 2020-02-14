ffed99e826d62549868a1dfe0ca37b27cb9baf0f
xieguigang <xie.guigang@gcmodeller.org>
2020-02-14 11:36:12 +0800

    test success

--- tutorials/eval_anonymous.R
+++ tutorials/eval_anonymous.R
@@ -1,6 +1,31 @@
+(function() {
+
+print('111111XXXXXXXXX999999999');
+print(traceback());
+
+})();
+
+(function(x = '111111XXXXXXXXX') {
+
+print(x);
+print(traceback());
+
+})();
+
+
 (function(x) {
 
 print(x);
 print(traceback());
 
 })('XXXXXXXXX');
+
+
+(function(x,y ,z =["abc","HHHHHHHHHHHH"] ) {
+
+print(x);
+print(y);
+print(z);
+print(traceback());
+
+})('XXXXXXXXX', "aaaaaaa");
