f1371855ed00d3aed67ed42181d910293417f0f2
xieguigang <xie.guigang@gcmodeller.org>
2020-02-14 18:05:31 +0800

    create 3D camera

---
+++ Library/R.graphics/gr3D.vb
@@ -0,0 +1,31 @@
+﻿
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Imaging.Drawing3D
+Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports Microsoft.VisualBasic.Scripting.Runtime
+
+<Package("grDevices.gr3D")>
+Module gr3D
+
+    <ExportAPI("vector3D")>
+    Public Function vector3D(x#, y#, z#) As Point3D
+        Return New Point3D With {
+            .X = x,
+            .Y = y,
+            .Z = z
+        }
+    End Function
+
+    <ExportAPI("camera")>
+    Public Function camera(Optional viewAngle As Object = Nothing,
+                           Optional viewDistance# = -40,
+                           Optional fov# = 256,
+                           Optional size As Object = "2560,1440") As Camera
+
+        Return New Camera(InteropArgumentHelper.getVector3D(viewAngle)) With {
+            .ViewDistance = viewDistance,
+            .fov = fov,
+            .screen = InteropArgumentHelper.getSize(size).SizeParser
+        }
+    End Function
+End Module
--- Library/R_graphic.interop/InteropArgumentHelper.vb
+++ Library/R_graphic.interop/InteropArgumentHelper.vb
@@ -1,52 +1,73 @@
 ﻿#Region "Microsoft.VisualBasic::51adbdf42b242cd79d133e5058723ac7, Library\R_graphic.interop\InteropArgumentHelper.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module InteropArgumentHelper
-    ' 
-    '     Function: getColor, getPadding, getSize
-    ' 
-    ' /********************************************************************************/
+' Module InteropArgumentHelper
+' 
+'     Function: getColor, getPadding, getSize
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.Drawing
 Imports Microsoft.VisualBasic.Imaging
 Imports Microsoft.VisualBasic.Imaging.Drawing2D
+Imports Microsoft.VisualBasic.Imaging.Drawing3D
 
 ''' <summary>
 ''' R# graphics argument scripting helper
 ''' </summary>
 Public Module InteropArgumentHelper
+
+    Public Function getVector3D(obj As Object) As Point3D
+        If obj Is Nothing Then
+            Return New Point3D
+        ElseIf TypeOf obj Is Point3D Then
+            Return DirectCast(obj, Point3D)
+        ElseIf TypeOf obj Is Double() Then
+            With DirectCast(obj, Double())
+                If .Length >= 3 Then
+                    Return New Point3D(.GetValue(0), .GetValue(1), .GetValue(2))
+                ElseIf .Length = 2 Then
+                    Return New Point3D(.GetValue(0), .GetValue(1), 0)
+                Else
+                    Return Nothing
+                End If
+            End With
+        End If
+
+        Return Nothing
+    End Function
 
     Public Function getPadding(padding As Object, Optional default$ = g.DefaultPadding) As String
         If padding Is Nothing Then
