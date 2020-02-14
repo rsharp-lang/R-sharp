ab5721f37707e499ed9c05671ea0562c9117ba2a
xieguigang <xie.guigang@gcmodeller.org>
2020-02-14 18:06:26 +0800

    add dev note

--- Library/R.graphics/gr3D.vb
+++ Library/R.graphics/gr3D.vb
@@ -7,8 +7,15 @@ Imports Microsoft.VisualBasic.Scripting.Runtime
 <Package("grDevices.gr3D")>
 Module gr3D
 
+    ''' <summary>
+    ''' Create a new 3D point
+    ''' </summary>
+    ''' <param name="x"></param>
+    ''' <param name="y"></param>
+    ''' <param name="z"></param>
+    ''' <returns></returns>
     <ExportAPI("vector3D")>
-    Public Function vector3D(x#, y#, z#) As Point3D
+    Public Function vector3D(x#, y#, Optional z# = 0) As Point3D
         Return New Point3D With {
             .X = x,
             .Y = y,
@@ -16,6 +23,14 @@ Module gr3D
         }
     End Function
 
+    ''' <summary>
+    ''' Create a new 3D camera object.
+    ''' </summary>
+    ''' <param name="viewAngle"></param>
+    ''' <param name="viewDistance"></param>
+    ''' <param name="fov"></param>
+    ''' <param name="size"></param>
+    ''' <returns></returns>
     <ExportAPI("camera")>
     Public Function camera(Optional viewAngle As Object = Nothing,
                            Optional viewDistance# = -40,
