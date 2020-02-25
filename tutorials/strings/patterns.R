ff2579407732ede28f9bb9f0254ba77e4842dd82
xieguigang <xie.guigang@gcmodeller.org>
2020-02-25 08:50:49 +0800

    Update patterns.R

--- tutorials/strings/patterns.R
+++ tutorials/strings/patterns.R
@@ -23,6 +23,6 @@ print(["123.txt", "1234.txt"] != $"\d{4}\.txt");
 # [regexp pattern](string)
 #
 let number.pattern = $"\d{4}";
-print(number.pattern(["12144123@1231233", "333333333"]));
+print(number.pattern(["12144123@1231233", "333333333"]) :> str);
 # or 
-print($"\d{4}"(["12144123@1231233", "333333333"]));
+print($"\d{4}"(["12144123@1231233", "333333333"]) :> str);
