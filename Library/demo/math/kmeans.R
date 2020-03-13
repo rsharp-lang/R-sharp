b01e33cd364949990956dddc59d3094e62153ce5
xieguigang <xie.guigang@gcmodeller.org>
2020-03-13 09:46:53 +0800

    add debug options

--- Library/demo/math/kmeans.R
+++ Library/demo/math/kmeans.R
@@ -9,7 +9,7 @@ let input.matrix as string = ?"--data" || "bezdekIris.csv";
 let centers as integer = ?"--centers"  || 4;
 let result = input.matrix
 :> read.dataframe(mode = "numeric")
-:> kmeans(centers = centers, parallel = TRUE)
+:> kmeans(centers = centers, parallel = TRUE, debug = !script$debug)
 ;
 
 if (length(result) < 100) {
