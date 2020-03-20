371e8e77c58e7b4a561ef8bd19c3f73edfe5f5a9
xieguigang <xie.guigang@gcmodeller.org>
2020-03-20 12:18:38 +0800

    how to convert tuple?

--- R#/Test/makeObjecttest.vb
+++ R#/Test/makeObjecttest.vb
@@ -1,14 +1,33 @@
 ﻿Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Scripting.MetaData
 Imports Microsoft.VisualBasic.Serialization.JSON
+Imports SMRUCC.Rsharp.Interpreter
 
 <Package("makeObject")>
 Module makeObjecttest
+
+    Dim R As New RInterpreter With {.debug = False}
+
+    Sub Main()
+        Call R.LoadLibrary(GetType(makeObjecttest))
+
+        Call R.Evaluate("tuple(list(a=1,b= '99999', c=TRUE,d = [599,3.3,9987.01]))")
+
+        Pause()
+    End Sub
 
     <ExportAPI("debug_echo")>
     Public Sub createArguments(args As arguments)
         Call Console.WriteLine(args.GetJson)
         Call Console.WriteLine(args.GetType.FullName)
+    End Sub
+
+    <ExportAPI("tuple")>
+    Public Sub tupleTest(x As (a As Integer, b As String(), c As Boolean, d As Double()))
+        Call Console.WriteLine(x.a)
+        Call Console.WriteLine(x.b.GetJson)
+        Call Console.WriteLine(x.c)
+        Call Console.WriteLine(x.d.GetJson)
     End Sub
 End Module
 
