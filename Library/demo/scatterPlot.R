d4aba30aeae8364c2e4f02cb05b9350537172b7b
xieguigang <xie.guigang@gcmodeller.org>
2020-03-22 18:04:03 +0800

    fix a possible parameter bugs about generic function

--- Library/demo/scatterPlot.R
+++ Library/demo/scatterPlot.R
@@ -5,28 +5,49 @@ let dataTbl <- read.csv(data);
 
 print(colnames(dataTbl));
 
-for(xlab in colnames(dataTbl)) {
-	for(ylab in colnames(dataTbl)) {
-		if (xlab != ylab) {
-			let x <- as.numeric(dataTbl[, xlab]);
-			let y <- as.numeric(dataTbl[, ylab]);
-			let main as string = `${xlab} ~ ${ylab}`;
-			let line <- serial(x,y, main, color = "black", ptSize = 8);
-			let save.png as string <- `${dirname(data)}/${basename(data)}/${main :> gsub("(\\|/)", "_", regexp =TRUE)}.png`;
+# for(xlab in colnames(dataTbl)) {
+	# for(ylab in colnames(dataTbl)) {
+		# if (xlab != ylab) {
+			# let x <- as.numeric(dataTbl[, xlab]);
+			# let y <- as.numeric(dataTbl[, ylab]);
+			# let main as string = `${xlab} ~ ${ylab}`;
+			# let line <- serial(x,y, main, color = "black", ptSize = 8);
+			# let save.png as string <- `${dirname(data)}/${basename(data)}/${main :> gsub("(\\|/)", "_", regexp =TRUE)}.png`;
 			
-			print(save.png);
+			# print(save.png);
 			
-			plot(line, 
-				 padding = "padding: 300px 125px 200px 250px;",
-				 x.lab   = xlab,
-				 y.lab   = ylab,
-				 legend.anchor = [1900.0, 1450.0],
-				 legendBgFill = "white",
-				 showLegend = FALSE,
-				 title = main
-			) 
-			:> save.graphics(file = save.png)
-			;
-		}
-	}
+			# plot(line, 
+				 # padding = "padding: 300px 125px 200px 250px;",
+				 # x.lab   = xlab,
+				 # y.lab   = ylab,
+				 # legend.anchor = [1900.0, 1450.0],
+				 # legendBgFill = "white",
+				 # showLegend = FALSE,
+				 # title = main
+			# ) 
+			# :> save.graphics(file = save.png)
+			# ;
+		# }
+	# }
+# }
+
+for(ylab in colnames(dataTbl)) {
+	let y <- as.numeric(dataTbl[, ylab]);
+	let x <- 1:length(y);
+	let line <- serial(x,y, ylab, color = "black", ptSize = 8);
+	let save.png as string <- `${dirname(data)}/${basename(data)}1/${ylab :> gsub("(\\|/)", "_", regexp =TRUE)}.png`;
+	
+	print(save.png);
+	
+	plot(line, 
+		 padding = "padding: 300px 125px 200px 250px;",
+		 x.lab   = "X",
+		 y.lab   = ylab,
+		 legend.anchor = [1900.0, 1450.0],
+		 legendBgFill = "white",
+		 showLegend = FALSE,
+		 title = ylab
+	) 
+	:> save.graphics(file = save.png)
+	;
 }
