b2756c1527be8570d06b503bf4147f3abb03f38f
xieguigang <xie.guigang@gcmodeller.org>
2020-03-31 18:51:23 +0800

    config test of the save function

--- tutorials/io/save.R
+++ tutorials/io/save.R
@@ -1,6 +1,7 @@
 # create two data sequence object
 let x = 1:100;
 let word = ["world", "R#"];
+let l = list(a = [1,2,3,4,5], b = FALSE, c = ["hello", "world"]);
 
 # and then save these two sequence object to disk file
-save(x, word, file = "./R#save.rda");
+save(x, word, l, file = "./R#save.rda");
