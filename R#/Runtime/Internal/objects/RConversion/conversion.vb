63d44a3b465c0cb1ecd56f75109d60dbf457fbcd
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-03-31 09:39:05 +0800

    fix bugs of unlist cast

--- R#/Runtime/Internal/objects/RConversion/conversion.vb
+++ R#/Runtime/Internal/objects/RConversion/conversion.vb
@@ -1,44 +1,44 @@
 ﻿#Region "Microsoft.VisualBasic::de3ae272302c1024630feab2641cf8a2, R#\Runtime\Internal\objects\RConversion\conversion.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Module RConversion
-    ' 
-    '         Function: asCharacters, asDataframe, asDate, asInteger, asList
-    '                   asLogicals, asNumeric, asObject, asRaw, asVector
-    '                   isCharacter, unlist, unlistOfRList
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Module RConversion
+' 
+'         Function: asCharacters, asDataframe, asDate, asInteger, asList
+'                   asLogicals, asNumeric, asObject, asRaw, asVector
+'                   isCharacter, unlist, unlistOfRList
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -46,6 +46,7 @@ Imports System.Reflection
 Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.ApplicationServices.Debugging.Logging
 Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports Microsoft.VisualBasic.Emit.Delegates
 Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Linq
@@ -115,6 +116,7 @@ Namespace Runtime.Internal.Object.Converts
             If list Is Nothing Then
                 Return Nothing
             End If
+
             If Not [typeof] Is Nothing Then
                 Select Case [typeof].GetType
                     Case GetType(Type) ' do nothing
@@ -135,12 +137,20 @@ Namespace Runtime.Internal.Object.Converts
                 End Select
             End If
 
-            If list.GetType Is GetType(list) Then
+            Dim listType As Type = list.GetType
+
+            If listType.IsArray Then
+                Return list
+            ElseIf DataFramework.IsPrimitive(listType) Then
+                Return list
+            ElseIf listType Is GetType(list) Then
                 Return DirectCast(list, list).unlistOfRList([typeof], env)
-            ElseIf list.GetType.ImplementInterface(GetType(IDictionary)) Then
+            ElseIf listType.ImplementInterface(GetType(IDictionary)) Then
                 Return New list(list).unlistOfRList([typeof], env)
             Else
-                Return Internal.debug.stop(New InvalidCastException(list.GetType.FullName), env)
+                ' Return Internal.debug.stop(New InvalidCastException(list.GetType.FullName), env)
+                ' is a single uer defined .NET object 
+                Return list
             End If
         End Function
 
