77d0ac3b91a75bbe3be9aa4ff038b00c4a0e3c65
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-03-31 08:59:04 +0800

    net cdf toolkit?

---
+++ Library/R.base/base/netCDFutils.vb
@@ -0,0 +1,12 @@
+﻿Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Data.IO.netCDF
+Imports Microsoft.VisualBasic.Scripting.MetaData
+
+<Package("netCDF.utils")>
+Module netCDFutils
+
+    <ExportAPI("open.netCDF")>
+    Public Function openCDF(file As String) As netCDFReader
+        Return netCDFReader.Open(file)
+    End Function
+End Module
