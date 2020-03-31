b11327b22b9895a55cf2fbf3e274b7911de714ef
xieguigang <xie.guigang@gcmodeller.org>
2020-03-31 19:39:02 +0800

    fix a bug about set dataframe column data

--- tutorials/io/save.R
+++ tutorials/io/save.R
@@ -1,7 +1,15 @@
 # create two data sequence object
 let x = 1:100;
 let word = ["world", "R#"];
-let l = list(a = [1,2,3,4,5], b = FALSE, c = ["hello", "world"]);
+let l = list();
+let d = data.frame();
+
+d[, "a"] <- 9;
+d[, "b"] <- [FALSE,FALSE,FALSE,TRUE,TRUE,TRUE];
+
+l$a = [1,2,3,4,5];
+l$b = FALSE;
+l$c = ["hello", "world"];
 
 # and then save these two sequence object to disk file
-save(x, word, l, file = "./R#save.rda");
+save(x, word, l, d, file = "./R#save.rda");
