f4fbd4c9ae77b3d85d50e7bd576236eee1bf84ce
xieguigang <xie.guigang@gcmodeller.org>
2020-04-01 12:37:56 +0800

    improvements of the CDF file api

--- Library/R.base/base/netCDFutils.vb
+++ Library/R.base/base/netCDFutils.vb
@@ -1,15 +1,28 @@
-﻿Imports Microsoft.VisualBasic.CommandLine.Reflection
+﻿Imports System.IO
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Data.IO
 Imports Microsoft.VisualBasic.Data.IO.netCDF
 Imports Microsoft.VisualBasic.Scripting.MetaData
 Imports SMRUCC.Rsharp.Runtime
+Imports SMRUCC.Rsharp.Runtime.Internal.Object
+Imports SMRUCC.Rsharp.Runtime.Interop
 Imports Rdataframe = SMRUCC.Rsharp.Runtime.Internal.Object.dataframe
 
 <Package("netCDF.utils")>
 Module netCDFutils
 
     <ExportAPI("open.netCDF")>
-    Public Function openCDF(file As String) As netCDFReader
-        Return netCDFReader.Open(file)
+    <RApiReturn(GetType(netCDFReader))>
+    Public Function openCDF(file As Object, Optional env As Environment = Nothing) As Object
+        If TypeOf file Is String Then
+            Return netCDFReader.Open(DirectCast(file, String))
+        ElseIf TypeOf file Is Stream Then
+            Return New netCDFReader(New BinaryDataReader(DirectCast(file, Stream)))
+        ElseIf TypeOf file Is pipeline AndAlso DirectCast(file, pipeline).elementType Like GetType(Byte) Then
+            Return New netCDFReader(New BinaryDataReader(New MemoryStream(DirectCast(file, pipeline).populates(Of Byte).ToArray)))
+        Else
+            Return Internal.debug.stop(New InvalidProgramException, env)
+        End If
     End Function
 
     <ExportAPI("globalAttributes")>
