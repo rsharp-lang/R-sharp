878deeccb49a430194fd57f35df101a3199dc885
xieguigang <xie.guigang@gcmodeller.org>
2020-04-01 20:14:28 +0800

    move to api folder

---
+++ R#/test/listObjectArgumentTest.vb
@@ -0,0 +1,13 @@
+﻿Imports Microsoft.VisualBasic.Scripting.MetaData
+
+<Package("bbbbb")>
+Module listObjectArgumentTest
+
+    Sub Main()
+
+    End Sub
+
+    Public Function left()
+
+    End Function
+End Module
