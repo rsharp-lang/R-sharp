23b555072b94c706bbe2566cc47a56c300b6de11
xieguigang <xie.guigang@gcmodeller.org>
2020-04-01 20:37:26 +0800

    confgi for argument test

--- R#/test/listObjectArgumentTest.vb
+++ R#/test/listObjectArgumentTest.vb
@@ -1,13 +1,42 @@
-﻿Imports Microsoft.VisualBasic.Scripting.MetaData
+﻿Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports Microsoft.VisualBasic.Serialization.JSON
+Imports SMRUCC.Rsharp.Interpreter
+Imports SMRUCC.Rsharp.Runtime
+Imports SMRUCC.Rsharp.Runtime.Internal.Invokes
+Imports SMRUCC.Rsharp.Runtime.Internal.Object
+Imports SMRUCC.Rsharp.Runtime.Interop
 
 <Package("bbbbb")>
 Module listObjectArgumentTest
 
-    Sub Main()
+    Dim R As New RInterpreter With {.debug = True}
 
+    Sub Main()
+        Call R.LoadLibrary(GetType(listObjectArgumentTest))
+
+        Call R.Evaluate("left(aa=1,bb=3,cc='d',a='99999')")
+        Call R.Evaluate("right(888,b='a', a= 'b', aa = 1111,c=FALSE,d=[12,34,6])")
+
+        Pause()
     End Sub
 
-    Public Function left()
+    <ExportAPI("left")>
+    Public Function left(<RListObjectArgument> l As Object, Optional XX As Integer = 9, Optional a As String = "aaaa", Optional env As Environment = Nothing)
+        Dim list = base.Rlist(l, env)
 
+        Console.WriteLine(a)
+        Console.WriteLine(XX)
+        Console.WriteLine(DirectCast(list, list).slots.GetJson)
+    End Function
+
+    <ExportAPI("right")>
+    Public Function right(XX As Integer, a As String, b As String, <RListObjectArgument> l As Object, Optional env As Environment = Nothing)
+        Dim list = base.Rlist(l, env)
+
+        Console.WriteLine(a)
+        Console.WriteLine(b)
+        Console.WriteLine(XX)
+        Console.WriteLine(DirectCast(list, list).slots.GetJson)
     End Function
 End Module
