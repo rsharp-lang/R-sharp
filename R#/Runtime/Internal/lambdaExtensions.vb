548a0e9614f94070ce90f41161ecb65fe327f6e6
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-04-01 09:16:36 +0800

    lambda helpers

--- R#/Runtime/Internal/lambdaExtensions.vb
+++ R#/Runtime/Internal/lambdaExtensions.vb
@@ -1,4 +1,7 @@
-﻿Imports SMRUCC.Rsharp.Runtime.Components
+﻿Imports Microsoft.VisualBasic.Emit.Delegates
+Imports Microsoft.VisualBasic.Language
+Imports SMRUCC.Rsharp.Runtime.Components
+Imports SMRUCC.Rsharp.Runtime.Components.Interface
 
 Namespace Runtime.Internal
 
@@ -13,6 +16,18 @@ Namespace Runtime.Internal
         <DebuggerStepThrough>
         Friend Function invokeArgument(value As Object) As InvokeParameter()
             Return InvokeParameter.Create(value)
+        End Function
+
+        Friend Function getRFunc(FUN As Object, env As Environment) As [Variant](Of RFunction, Message)
+            If FUN Is Nothing Then
+                Return Internal.debug.stop({"Missing apply function!"}, env)
+            ElseIf TypeOf FUN Is Message Then
+                Return DirectCast(FUN, Message)
+            ElseIf Not FUN.GetType.ImplementInterface(GetType(RFunction)) Then
+                Return Internal.debug.stop({"Target is not a function!"}, env)
+            End If
+
+            Return DirectCast(FUN, RFunction)
         End Function
     End Module
 End Namespace
