1986dc83bee6f7d7e152a78e5429257daa8a07df
xieguigang <xie.guigang@gcmodeller.org>
2020-04-02 15:58:13 +0800

    netCDF api for R# scripting test success

--- Library/demo/dev/netCDF.R
+++ Library/demo/dev/netCDF.R
@@ -1,6 +1,12 @@
 imports "netCDF.utils" from "R.base";
 
-using file as open.netCDF("D:\GCModeller\src\R-sharp\tutorials\io\R#save.rda") {
-	print(file :> globalAttributes);
+using zip as open.zip("D:\GCModeller\src\R-sharp\tutorials\io\R#save.rda") {
+	using file as open.netCDF(zip[["R#.Data"]]) {
+		print(file :> globalAttributes);
+		
+		print("this r dataset file contains symbols:");		
+		print(file :> variables);
+	}
 }
+
 
