b9cc011564d62215e8eff12852b0e1d6c9aa71bf
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-04-06 09:44:31 +0800

    split source files

--- R#/Interpreter/Program.vb
+++ R#/Interpreter/Program.vb
@@ -64,24 +64,6 @@ Imports SMRUCC.Rsharp.Runtime.Interop
 
 Namespace Interpreter
 
-    ''' <summary>
-    ''' Enumerations of breaks the executation of the current code closure.
-    ''' </summary>
-    Public Enum ExecuteBreaks
-        ''' <summary>
-        ''' returns value for function
-        ''' </summary>
-        ReturnValue
-        ''' <summary>
-        ''' continute to next for/while/loop
-        ''' </summary>
-        ContinuteNext
-        ''' <summary>
-        ''' break of while/loop
-        ''' </summary>
-        BreakLoop
-    End Enum
-
     Public Class Program : Implements IEnumerable(Of Expression)
 
         Friend execQueue As Expression()
