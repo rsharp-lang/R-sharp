70d375a6eb57a099dd5945ebed84b7d4398cedb8
xieguigang <xie.guigang@gcmodeller.org>
2020-04-13 19:18:43 +0800

    create new binary operator system for imports .NET operators

---
+++ R#/Runtime/Interop/RsharpOperator/BinaryIndex.vb
@@ -0,0 +1,15 @@
+﻿Namespace Runtime.Interop
+
+    Public Class BinaryIndex
+
+        ''' <summary>
+        ''' the operator symbol text
+        ''' </summary>
+        ''' <returns></returns>
+        Public ReadOnly Property symbol As String
+
+        Sub New(symbol As String)
+            Me.symbol = symbol
+        End Sub
+    End Class
+End Namespace
