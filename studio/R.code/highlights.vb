f9a5836e57528f97e49b8cf775808111e550fa04
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-04-25 17:39:55 +0800

    tohtml ?

---
+++ studio/R.code/highlights.vb
@@ -0,0 +1,10 @@
+﻿Imports System.Runtime.CompilerServices
+Imports SMRUCC.Rsharp.Interpreter
+
+Module highlights
+
+    <Extension>
+    Public Function toHtml(program As Program) As String
+
+    End Function
+End Module
