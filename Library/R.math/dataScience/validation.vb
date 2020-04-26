68687b9fd9213cf91b5e2eb0d641ce31e32e0a8e
xieguigang <xie.guigang@gcmodeller.org>
2020-04-26 13:34:26 +0800

    ROC validation of ANN training result

---
+++ Library/R.math/dataScience/validation.vb
@@ -0,0 +1,9 @@
+﻿Imports Microsoft.VisualBasic.Scripting.MetaData
+
+<Package("machineLearning.validation", Category:=APICategories.ResearchTools, Publisher:="xie.guigang@gcmodeller.org")>
+Module validation
+
+    Public Function ANN_ROC()
+
+    End Function
+End Module
