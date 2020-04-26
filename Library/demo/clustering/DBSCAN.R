ced14dfc8c6c99d275c4ef9616e54a40a69192b0
xieguigang <xie.guigang@gcmodeller.org>
2020-04-26 20:59:56 +0800

    test success

--- Library/demo/clustering/DBSCAN.R
+++ Library/demo/clustering/DBSCAN.R
@@ -8,7 +8,7 @@ let raw = read.csv("multishapes.csv")[, ["x","y"]];
 print("previews of the raw input data:");
 print(head(raw));
 
-let result = as.object(dbscan(raw, 1))$cluster;
+let result = as.object(dbscan(raw, 1.125))$cluster;
 
 str(summary(result));
 
@@ -20,18 +20,18 @@ table :> write.csv(file = "multishapes_dbscan.csv", row_names = FALSE);
 let cluster = table[, "Cluster"];
 let unique_clusters = unique(cluster);
 let points = [];
-let colorSet = colors("Clusters") :> loop;
+let colorSet = colors("Clusters", -1) :> loop;
 
 for(id in unique_clusters) {
 	let partition = table[cluster == id, ];
 	let x = as.numeric(partition[, "x"]);
 	let y = as.numeric(partition[, "y"]);
 	
-	points = points << serial(x,y, name = id, color = colorSet());
+	points = points << serial(x,y, name = id, color = colorSet(), ptSize = 30, alpha = 200);
 }
 
 points
-:> plot
+:> plot(padding = "padding: 150px 100px 200px 200px;")
 :> save.graphics(file = "multishapes_dbscan.png")
 ;
 
