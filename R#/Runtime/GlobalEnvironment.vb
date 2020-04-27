41c9337d7b1197b8387c48c341a6700d23f500e4
xieguigang <xie.guigang@gcmodeller.org>
2020-04-27 20:25:49 +0800

    fix the bug of silent option

--- R#/Runtime/GlobalEnvironment.vb
+++ R#/Runtime/GlobalEnvironment.vb
@@ -1,45 +1,45 @@
 ﻿#Region "Microsoft.VisualBasic::f4cddd59f29e3bf35cc60613a4e9ee97, R#\Runtime\GlobalEnvironment.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Class GlobalEnvironment
-    ' 
-    '         Properties: debugMode, lastException, options, packages, Rscript
-    ' 
-    '         Constructor: (+1 Overloads) Sub New
-    '         Function: (+2 Overloads) LoadLibrary, MissingPackage
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Class GlobalEnvironment
+' 
+'         Properties: debugMode, lastException, options, packages, Rscript
+' 
+'         Constructor: (+1 Overloads) Sub New
+'         Function: (+2 Overloads) LoadLibrary, MissingPackage
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -83,8 +83,10 @@ Namespace Runtime
             Dim exception As Exception = Nothing
             Dim package As RPkg = packages.FindPackage(packageName, exception)
 
-            If Not silent AndAlso Not packageName Like packages.loadedPackages Then
-                Call Console.WriteLine($"Loading required package: {packageName}")
+            If Not packageName Like packages.loadedPackages Then
+                If Not silent Then
+                    Call Console.WriteLine($"Loading required package: {packageName}")
+                End If
             Else
                 Return Nothing
             End If
@@ -92,7 +94,7 @@ Namespace Runtime
             If package Is Nothing Then
                 Return MissingPackage(packageName, exception)
             Else
-                Return LoadLibrary(package.package, packageName)
+                Return LoadLibrary(package.package, packageName, silent)
             End If
         End Function
 
@@ -101,12 +103,12 @@ Namespace Runtime
         ''' </summary>
         ''' <param name="package"></param>
         ''' <returns></returns>
-        Public Function LoadLibrary(package As Type, Optional packageName$ = Nothing) As Message
+        Public Function LoadLibrary(package As Type, Optional packageName$ = Nothing, Optional silent As Boolean = False) As Message
             Dim masked As String() = ImportsPackage _
                 .ImportsStatic(Me, package) _
                 .ToArray
 
-            If masked.Length > 0 Then
+            If Not silent AndAlso masked.Length > 0 Then
                 If packageName.StringEmpty Then
                     packageName = package.NamespaceEntry.Namespace
                 End If
