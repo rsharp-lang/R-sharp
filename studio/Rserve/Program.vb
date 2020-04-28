a2bc4494e6c5cb7f2320887b8dfb17dd683ac0d1
xieguigang <xie.guigang@gcmodeller.org>
2020-04-28 08:30:27 +0800

    Rserve cli

---
+++ studio/Rserve/Program.vb
@@ -0,0 +1,19 @@
+﻿Imports Microsoft.VisualBasic.CommandLine
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+
+Module Program
+
+    Public Function Main() As Integer
+        Return GetType(Program).RunCLI(App.CommandLine)
+    End Function
+
+    <ExportAPI("--start")>
+    <Usage("--start --port 7452 --Rweb <directory> [--n_threads <max_threads, default=8>]")>
+    Public Function start(args As CommandLine) As Integer
+        Dim port As Integer = args <= "--port"
+        Dim Rweb As String = args <= "--Rweb"
+        Dim n_threads As Integer = args("--n_threads") Or 8
+
+    End Function
+
+End Module
