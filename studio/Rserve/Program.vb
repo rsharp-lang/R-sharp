7dfdc5e4a1b13cdd6531df16d332ea36ae0a5491
xieguigang <xie.guigang@gcmodeller.org>
2020-04-28 09:12:36 +0800

    R web server

--- studio/Rserve/Program.vb
+++ studio/Rserve/Program.vb
@@ -1,6 +1,12 @@
-﻿Imports Microsoft.VisualBasic.CommandLine
+﻿Imports System.IO
+Imports System.Net.Sockets
+Imports System.Runtime.CompilerServices
+Imports System.Text
+Imports Microsoft.VisualBasic.CommandLine
 Imports Microsoft.VisualBasic.CommandLine.Reflection
-Imports SMRUCC.WebCloud.HTTPInternal.Platform
+Imports SMRUCC.Rsharp.Interpreter
+Imports SMRUCC.Rsharp.System.Configuration
+Imports SMRUCC.WebCloud.HTTPInternal.Core
 
 Module Program
 
@@ -14,14 +20,78 @@ Module Program
         Dim port As Integer = args <= "--port"
         Dim Rweb As String = args <= "--Rweb"
         Dim n_threads As Integer = args("--n_threads") Or 8
-        Dim http As New PlatformEngine(
-            HOME, port,
-            nullExists:=True,
-            threads:=n_threads,
-            cache:=False
-        )
 
-        Return http.Run()
+        Using http As New Rweb(Rweb, port, threads:=n_threads)
+            Return http.Run()
+        End Using
     End Function
 
 End Module
+
+Public Class Rweb : Inherits HttpServer
+
+    Dim Rweb As String
+
+    Public Sub New(Rweb$, port As Integer, Optional threads As Integer = -1)
+        MyBase.New(port, threads)
+
+        Me.Rweb = Rweb
+    End Sub
+
+    Public Overrides Sub handleGETRequest(p As HttpProcessor)
+        Using response As New HttpResponse(p.outputStream, AddressOf p.writeFailure)
+            ' /<scriptFileName>?...args
+            Dim request As New HttpRequest(p)
+            Dim Rscript As String = Rweb & "/" & Strings.Trim(request.URL.Split("?"c).FirstOrDefault).Trim("."c, "/"c) & ".R"
+
+            If Not Rscript.FileExists Then
+                Call p.writeFailure(404, "file not found!")
+            Else
+                Using output As New MemoryStream(), Rstd_out As New StreamWriter(output)
+                    Dim result As Object
+                    Dim code As Integer
+
+                    ' run rscript
+                    Using R As RInterpreter = RInterpreter _
+                        .FromEnvironmentConfiguration(ConfigFile.localConfigs) _
+                        .RedirectOutput(Rstd_out)
+
+                        For Each pkgName As String In R.configFile.GetStartupLoadingPackages
+                            Call R.LoadLibrary(packageName:=pkgName, silent:=True)
+                        Next
+
+                        result = R.Source(Rscript)
+                        code = Rserve.Rscript.handleResult(result, R.globalEnvir, Nothing)
+                    End Using
+
+                    Call Rstd_out.Flush()
+
+                    If code <> 0 Then
+                        Call response.WriteError(code, Encoding.UTF8.GetString(output.ToArray))
+                    Else
+                        Call response.Write(output.ToArray)
+                    End If
+                End Using
+            End If
+        End Using
+    End Sub
+
+    Public Overrides Sub handlePOSTRequest(p As HttpProcessor, inputData As String)
+        Call p.writeFailure(404, "not allowed!")
+    End Sub
+
+    Public Overrides Sub handlePUTMethod(p As HttpProcessor, inputData As String)
+        Call p.writeFailure(404, "not allowed!")
+    End Sub
+
+    Public Overrides Sub handleOtherMethod(p As HttpProcessor)
+        Call p.writeFailure(404, "not allowed!")
+    End Sub
+
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
+    Protected Overrides Function __httpProcessor(client As TcpClient) As HttpProcessor
+        Return New HttpProcessor(client, Me, MAX_POST_SIZE:=App.BufferSize) With {
+            ._404Page = Function() "404 Not Found"
+        }
+    End Function
+End Class
