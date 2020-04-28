59146c9c63f634438518fa37a970064101a32039
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-04-28 08:20:25 +0800

    R name alias

--- R#/Runtime/MagicScriptSymbol.vb
+++ R#/Runtime/MagicScriptSymbol.vb
@@ -1,10 +1,14 @@
-﻿Namespace Runtime
+﻿Imports SMRUCC.Rsharp.Runtime.Interop
+
+Namespace Runtime
 
     Public Class MagicScriptSymbol
 
         Public Property dir As String
         Public Property file As String
         Public Property fullName As String
+
+        <RNameAlias("startup.time")>
         Public Property startup_time As String
         Public Property debug As Boolean
         Public Property silent As Boolean
