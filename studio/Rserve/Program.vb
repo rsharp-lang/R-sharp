454308709609553cd6e43da2d0c454b97f648228
xieguigang <xie.guigang@gcmodeller.org>
2020-05-07 13:06:14 +0800

    split source files

--- studio/Rserve/Program.vb
+++ studio/Rserve/Program.vb
@@ -1,18 +1,6 @@
-﻿Imports System.Collections.Specialized
-Imports System.IO
-Imports System.Net.Sockets
-Imports System.Runtime.CompilerServices
-Imports System.Text
-Imports Flute.Http.Core
+﻿Imports System.ComponentModel
 Imports Microsoft.VisualBasic.CommandLine
 Imports Microsoft.VisualBasic.CommandLine.Reflection
-Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
-Imports Microsoft.VisualBasic.Serialization.JSON
-Imports Microsoft.VisualBasic.Text
-Imports SMRUCC.Rsharp.Interpreter
-Imports SMRUCC.Rsharp.System.Configuration
-Imports SMRUCC.WebCloud.HTTPInternal.AppEngine
-Imports SMRUCC.WebCloud.HTTPInternal.Core
 
 Module Program
 
@@ -20,7 +8,13 @@ Module Program
         Return GetType(Program).RunCLI(App.CommandLine)
     End Function
 
+    ''' <summary>
+    ''' Run rscript
+    ''' </summary>
+    ''' <param name="args"></param>
+    ''' <returns></returns>
     <ExportAPI("--start")>
+    <Description("Run R# script with http get request.")>
     <Usage("--start [--port <port number, default=7452> --Rweb <directory, default=./Rweb> --show_error --n_threads <max_threads, default=8>]")>
     Public Function start(args As CommandLine) As Integer
         Dim port As Integer = args("--port") Or 7452
@@ -33,117 +27,13 @@ Module Program
         End Using
     End Function
 
-End Module
+    <ExportAPI("--session")>
+    <Description("Run GCModeller workbench R# backend session.")>
+    <Usage("--session [--port <port number, default=8848> --workspace <directory, default=./>]")>
+    Public Function runSession(args As CommandLine) As Integer
+        Dim port As Integer = args("--port") Or 8848
+        Dim workspace As String = args("--workspace") Or "./"
 
-''' <summary>
-''' Rweb is not design for general web programming, it is 
-''' design for running a background data task.
-''' </summary>
-Public Class Rweb : Inherits HttpServer
-
-    Dim Rweb As String
-    Dim showError As Boolean
-
-    Public Sub New(Rweb$, port As Integer, show_error As Boolean, Optional threads As Integer = -1)
-        MyBase.New(port, threads)
-
-        Me.Rweb = Rweb
-        Me.showError = show_error
-    End Sub
-
-    Public Overrides Sub handleGETRequest(p As HttpProcessor)
-        Using response As New HttpResponse(p.outputStream, AddressOf p.writeFailure)
-            ' /<scriptFileName>?...args
-            Dim request As New HttpRequest(p)
-            Dim Rscript As String = Rweb & "/" & Strings.Trim(request.URL.Split("?"c).FirstOrDefault).Trim("."c, "/"c) & ".R"
-            Dim application As String = "", api As String = "", parameters As String = ""
-            Dim url As String = request.URL
-
-            Call APPEngine.GetParameter(url, application, api, parameters)
-
-            request.URLParameters = parameters.QueryStringParameters
-
-            If Not Rscript.FileExists Then
-                Call p.writeFailure(404, "file not found!")
-            Else
-                Call runRweb(Rscript, request.URLParameters, response)
-            End If
-        End Using
-    End Sub
-
-    Private Sub runRweb(Rscript As String, args As NameValueCollection, response As HttpResponse)
-        Using output As New MemoryStream(), Rstd_out As New StreamWriter(output, Encodings.UTF8WithoutBOM.CodePage)
-            Dim result As Object
-            Dim code As Integer
-            Dim content_type As String
-            Dim raw_args As Dictionary(Of String, String) = args.ToDictionary(allStrings:=False)
-            Dim http As NamedValue(Of Object)() = raw_args _
-                .Select(Function(t)
-                            Return New NamedValue(Of Object) With {
-                                .Name = t.Key,
-                                .Value = t.Value,
-                                .Description = t.Value
-                            }
-                        End Function) _
-                .ToArray
-
-            Call raw_args.GetJson.__DEBUG_ECHO
-
-            ' run rscript
-            Using R As RInterpreter = RInterpreter _
-                .FromEnvironmentConfiguration(ConfigFile.localConfigs) _
-                .RedirectOutput(Rstd_out)
-
-                R.silent = True
-                R.redirectError2stdout = showError
-
-                For Each pkgName As String In R.configFile.GetStartupLoadingPackages
-                    Call R.LoadLibrary(packageName:=pkgName, silent:=True)
-                Next
-
-                result = R.Source(Rscript, http)
-                code = Rserve.Rscript.handleResult(result, R.globalEnvir, Nothing)
-
-                If R.globalEnvir.stdout.recommendType Is Nothing Then
-                    content_type = "text/html"
-                Else
-                    content_type = R.globalEnvir.stdout.recommendType
-                End If
-            End Using
-
-            Call Rstd_out.Flush()
-
-            If code <> 0 Then
-                Dim err As String = Encoding.UTF8.GetString(output.ToArray)
-
-                If showError Then
-                    Call response.WriteHTML(err)
-                Else
-                    Call response.WriteError(code, err)
-                End If
-            Else
-                Call response.WriteHeader(content_type, output.Length)
-                Call response.Write(output.ToArray)
-            End If
-        End Using
-    End Sub
-
-    Public Overrides Sub handlePOSTRequest(p As HttpProcessor, inputData As String)
-        Call p.writeFailure(404, "not allowed!")
-    End Sub
-
-    Public Overrides Sub handlePUTMethod(p As HttpProcessor, inputData As String)
-        Call p.writeFailure(404, "not allowed!")
-    End Sub
-
-    Public Overrides Sub handleOtherMethod(p As HttpProcessor)
-        Call p.writeFailure(404, "not allowed!")
-    End Sub
-
-    <MethodImpl(MethodImplOptions.AggressiveInlining)>
-    Protected Overrides Function __httpProcessor(client As TcpClient) As HttpProcessor
-        Return New HttpProcessor(client, Me, MAX_POST_SIZE:=App.BufferSize) With {
-            ._404Page = Function() "404 Not Found"
-        }
     End Function
-End Class
+
+End Module
---
+++ studio/Rserve/RSession.vb
@@ -0,0 +1,18 @@
+﻿Imports System.Collections.Specialized
+Imports System.ComponentModel
+Imports System.IO
+Imports System.Net.Sockets
+Imports System.Runtime.CompilerServices
+Imports System.Text
+Imports Flute.Http.Core
+Imports Microsoft.VisualBasic.CommandLine
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports Microsoft.VisualBasic.Serialization.JSON
+Imports Microsoft.VisualBasic.Text
+Imports SMRUCC.Rsharp.Interpreter
+Imports SMRUCC.Rsharp.System.Configuration
+
+Public Class RSession : Inherits HttpServer
+
+End Class
