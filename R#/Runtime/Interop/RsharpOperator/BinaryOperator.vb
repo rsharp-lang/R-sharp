29eb2e04a8f24095ffc95d2b97a78bdbc0779fba
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-05-12 17:19:36 +0800

    implements unit formula

--- R#/Runtime/Interop/RsharpOperator/BinaryOperator.vb
+++ R#/Runtime/Interop/RsharpOperator/BinaryOperator.vb
@@ -1,4 +1,7 @@
-﻿Namespace Runtime.Interop
+﻿Imports Microsoft.VisualBasic.ApplicationServices.Debugging.Logging
+Imports SMRUCC.Rsharp.Runtime.Internal.Object
+
+Namespace Runtime.Interop
 
     Public Delegate Function IBinaryOperator(left As Object, right As Object, env As Environment) As Object
 
@@ -14,8 +17,50 @@
             operation = op
         End Sub
 
+        ''' <summary>
+        ''' 主要是为了兼容R#语言的<see cref="unit"/>特性
+        ''' </summary>
+        ''' <param name="left"></param>
+        ''' <param name="right"></param>
+        ''' <param name="env"></param>
+        ''' <returns></returns>
         Public Function Execute(left As Object, right As Object, env As Environment) As Object
             Dim result As Object = operation(left, right, env)
+            Dim unitL As unit = Nothing
+            Dim unitR As unit = Nothing
+            Dim unit As unit
+
+            If TypeOf left Is vector Then
+                unitL = DirectCast(left, vector).unit
+            End If
+            If TypeOf right Is vector Then
+                unitR = DirectCast(right, vector).unit
+            End If
+
+            If (Not result Is Nothing) AndAlso (Not (unitL Is Nothing AndAlso unitR Is Nothing)) Then
+                If unitL Is Nothing Then
+                    unit = unitR
+                ElseIf unitR Is Nothing Then
+                    unit = unitL
+                Else
+                    unit = New unit With {
+                        .name = $"{unitL.name}{operatorSymbol}{unitR.name}"
+                    }
+                End If
+
+                If TypeOf result Is vector Then
+                    DirectCast(result, vector).unit = unit
+                ElseIf result.GetType.IsArray Then
+                    result = New vector(MeasureRealElementType(result), result, env)
+                    DirectCast(result, vector).unit = unit
+                Else
+                    env.AddMessage({
+                         $"the result value is not supported by R# unit, unit ({unit}) will be ignored...",
+                         $"unit: " & unit.name,
+                         $"value: " & result.GetType.FullName
+                    }, MSG_TYPES.WRN)
+                End If
+            End If
 
             Return result
         End Function
