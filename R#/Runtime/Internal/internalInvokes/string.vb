4545a44e1faf75a7b909edea975a80c76f6fc819
xieguigang <xie.guigang@gcmodeller.org>
2020-05-17 00:06:34 +0800

    improvements of the xml api

--- R#/Runtime/Internal/internalInvokes/string.vb
+++ R#/Runtime/Internal/internalInvokes/string.vb
@@ -1,61 +1,63 @@
 ﻿#Region "Microsoft.VisualBasic::333fc3b377562705be1a590a4180b0b6, R#\Runtime\Internal\internalInvokes\string.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Module stringr
-    ' 
-    '         Function: [string], Csprintf, grep, html, json
-    '                   match, nchar, paste, regexp, sprintfSingle
-    '                   str_pad, str_replace, strsplit, xml
-    ' 
-    '     Enum str_padSides
-    ' 
-    '         both, left, right
-    ' 
-    '  
-    ' 
-    ' 
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Module stringr
+' 
+'         Function: [string], Csprintf, grep, html, json
+'                   match, nchar, paste, regexp, sprintfSingle
+'                   str_pad, str_replace, strsplit, xml
+' 
+'     Enum str_padSides
+' 
+'         both, left, right
+' 
+'  
+' 
+' 
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
+Imports System.Reflection
 Imports System.Text.RegularExpressions
 Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Language.C
 Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.Scripting
 Imports Microsoft.VisualBasic.Serialization.JSON
+Imports Microsoft.VisualBasic.Text.Xml.Models
 Imports SMRUCC.Rsharp.Runtime.Internal.ConsolePrinter
 Imports SMRUCC.Rsharp.Runtime.Internal.Object
 Imports SMRUCC.Rsharp.Runtime.Interop
@@ -103,16 +105,27 @@ Namespace Runtime.Internal.Invokes
         ''' <param name="env"></param>
         ''' <returns></returns>
         <ExportAPI("xml")>
+        <RApiReturn(GetType(String))>
         Public Function xml(<RRawVectorArgument> x As Object, env As Environment) As Object
             If x Is Nothing Then
                 Return "<?xml version=""1.0"" encoding=""utf-16""?>"
-            Else
-                Try
-                    Return XmlExtensions.GetXml(x, x.GetType)
-                Catch ex As Exception
-                    Return Internal.debug.stop(ex, env)
-                End Try
+            ElseIf x.GetType.IsArray Then
+                Dim template As Type = GetType(XmlList(Of ))
+                Dim type As Type = template.MakeGenericType(x.GetType.GetElementType)
+                Dim list As Object = Activator.CreateInstance(type)
+                Dim writer As PropertyInfo = type _
+                    .GetProperties(BindingFlags.Public Or BindingFlags.Instance) _
+                    .Where(Function(p) p.Name = NameOf(XmlList(Of String).items)) _
+                    .FirstOrDefault
+
+                Call writer.SetValue(list, x)
             End If
+
+            Try
+                Return XmlExtensions.GetXml(x, x.GetType)
+            Catch ex As Exception
+                Return Internal.debug.stop(ex, env)
+            End Try
         End Function
 
         ''' <summary>
