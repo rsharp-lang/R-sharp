2e57cd6bbbaa07086cbfcd38f4b1b1d52bb4e283
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-05-20 08:14:37 +0800

    R# debugger

---
+++ studio/Rstudio/debugger.vb
@@ -0,0 +1,12 @@
+﻿Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.Rsharp.Runtime
+
+<Package("debug")>
+Module debugger
+
+    <ExportAPI("view")>
+    Public Sub view(symbol As Object, Optional env As Environment = Nothing)
+
+    End Sub
+End Module
