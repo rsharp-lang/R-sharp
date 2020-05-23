203da71f9411899ebf85f77a16c7f9d93fc00b6d
xieguigang <xie.guigang@gcmodeller.org>
2020-05-24 00:26:53 +0800

    Update HtmlMarkdownPrinter.vb

--- R#/System/Document/HtmlMarkdownPrinter.vb
+++ R#/System/Document/HtmlMarkdownPrinter.vb
@@ -10,23 +10,12 @@ Namespace System
 
         Public Sub printHtml(api As RMethodInfo, docs As ProjectMember, markdown As RContentOutput)
             If Not docs Is Nothing Then
-                Call markdown.WriteLine(docs.Summary)
-                Call markdown.WriteLine()
-
-                For Each param As param In docs.Params
-                    Call markdown.WriteLine($"``{param.name}:``  " & param.text.Trim(" "c, ASCII.CR, ASCII.LF))
-                Next
-
-                Call markdown.WriteLine()
-
-                If Not docs.Returns.StringEmpty Then
-                    Call markdown.WriteLine(" [**returns**]: ")
-                    Call markdown.WriteLine(docs.Returns)
-                    Call markdown.WriteLine()
-                End If
+                Call printMarkdownDocs(docs, markdown)
             End If
 
-            Call markdown.WriteLine(api.GetPrintContent)
+            Call markdown.WriteLine("```")
+            Call markdown.WriteLine(api.GetPrintContent.Replace("`", ""))
+            Call markdown.WriteLine("```")
 
             Dim enums = api.parameters _
                 .Where(Function(par) par.type.raw.IsEnum) _
@@ -36,6 +25,7 @@ Namespace System
 
             If enums.Length > 0 Then
                 Call markdown.WriteLine(" where have enum values:")
+                Call markdown.WriteLine("```")
 
                 For Each [enum] As REnum In enums.Select(Function(tg) REnum.GetEnumList(tg.First))
                     Call markdown.WriteLine()
@@ -47,6 +37,40 @@ Namespace System
 
                     Call markdown.WriteLine($"{New String(" "c, 2)}}}")
                 Next
+
+                Call markdown.WriteLine("```")
+            End If
+        End Sub
+
+        Private Sub printMarkdownDocs(docs As ProjectMember, markdown As RContentOutput)
+            Dim summaryLines = docs.Summary.Trim(" "c, ASCII.CR, ASCII.LF).LineTokens
+            Dim first = summaryLines.First
+
+            If Not first.StartsWith("#") Then
+                first = "#" & first
+            End If
+
+            Call markdown.WriteLine(first)
+            Call markdown.WriteLine(summaryLines.Skip(1).JoinBy(vbCrLf))
+            Call markdown.WriteLine()
+
+            Call markdown.WriteLine("## arguments")
+
+            For Each param As param In docs.Params
+                Call markdown.WriteLine($"``{param.name}:``  " & param.text.Trim(" "c, ASCII.CR, ASCII.LF))
+            Next
+
+            Call markdown.WriteLine()
+
+            If Not docs.Returns.StringEmpty Then
+                Call markdown.WriteLine("## value")
+                Call markdown.WriteLine(docs.Returns)
+                Call markdown.WriteLine()
+            End If
+
+            If Not docs.Remarks.StringEmpty Then
+                Call markdown.WriteLine("## details")
+                Call markdown.WriteLine(docs.Remarks)
             End If
         End Sub
     End Module
