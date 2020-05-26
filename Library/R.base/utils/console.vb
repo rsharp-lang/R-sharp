e0480705d80bf8d754b9f27b0573208f45514eb0
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-05-26 15:01:53 +0800

    add dev notes

--- Library/R.base/utils/console.vb
+++ Library/R.base/utils/console.vb
@@ -1,43 +1,43 @@
 ﻿#Region "Microsoft.VisualBasic::7081e654296aa5d3249cc6d0312a75e3, Library\R.base\utils\console.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module console
-    ' 
-    '     Function: ConsoleBackColor, ConsoleForeColor, CreateProgressBar, log, PinProgressBarTop
-    ' 
-    '     Sub: ClearProgressBarPinned
-    ' 
-    ' /********************************************************************************/
+' Module console
+' 
+'     Function: ConsoleBackColor, ConsoleForeColor, CreateProgressBar, log, PinProgressBarTop
+' 
+'     Sub: ClearProgressBarPinned
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -46,13 +46,20 @@ Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Language.C
 Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.Rsharp.Runtime
 Imports CMD = System.Console
 
 ''' <summary>
 ''' R# console utilities
 ''' </summary>
+''' <remarks>
+''' all of the api functions in this package module is not affected by 
+''' the <see cref="GlobalEnvironment.stdout"/> I/O redirect.
+''' </remarks>
 <Package("console", Category:=APICategories.SoftwareTools)>
 Module console
+
+    ' 这个模块之中的终端输出函数是不会受到输出环境的影响的
 
     ''' <summary>
     ''' Writes the specified string value to the standard output stream.
