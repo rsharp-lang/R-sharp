061dcc594af13dddf9bd99047dc3008f188c9bf0
xieguigang <xie.guigang@gcmodeller.org>
2020-05-27 22:38:22 +0800

    fix of single imports keyword syntax

--- R#/Interpreter/Syntax/SyntaxImplements/RequireSyntax.vb
+++ R#/Interpreter/Syntax/SyntaxImplements/RequireSyntax.vb
@@ -1,42 +1,42 @@
 ﻿#Region "Microsoft.VisualBasic::74007338224288a57bdb61a6df657b63, R#\Interpreter\Syntax\SyntaxImplements\RequireSyntax.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Module RequireSyntax
-    ' 
-    '         Function: [Imports], Require
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Module RequireSyntax
+' 
+'         Function: [Imports], Require
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -44,6 +44,7 @@ Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Linq
 Imports SMRUCC.Rsharp.Interpreter.ExecuteEngine
 Imports SMRUCC.Rsharp.Interpreter.ExecuteEngine.ExpressionSymbols
+Imports SMRUCC.Rsharp.Interpreter.ExecuteEngine.ExpressionSymbols.DataSets
 Imports SMRUCC.Rsharp.Language
 Imports SMRUCC.Rsharp.Language.TokenIcer
 
@@ -93,6 +94,11 @@ Namespace Interpreter.SyntaxParser.SyntaxImplements
                         )
                     End If
                 ElseIf .ElementAt(Scan0).isKeyword("imports") Then
+                    If .Length = 1 Then
+                        ' try interpreted as symbol reference
+                        Return New SyntaxResult(New SymbolReference(.ElementAt(Scan0)(Scan0)))
+                    End If
+
                     ' imports <*.R/*.dll file>
                     Dim files As SyntaxResult =
                         .ElementAt(1) _
