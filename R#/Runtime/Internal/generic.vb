07a2656a30a9c9e0a73c7739f2e8359233a2c601
xieguigang <xie.guigang@gcmodeller.org>
2020-06-09 18:22:13 +0800

    parse generic reference

--- R#/Runtime/Internal/generic.vb
+++ R#/Runtime/Internal/generic.vb
@@ -1,52 +1,53 @@
 ﻿#Region "Microsoft.VisualBasic::8a02a1ef3d9574db047b833f530922a9, R#\Runtime\Internal\generic.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Delegate Function
-    ' 
-    ' 
-    '     Module generic
-    ' 
-    '         Function: exists, invokeGeneric
-    ' 
-    '         Sub: add
-    ' 
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Delegate Function
+' 
+' 
+'     Module generic
+' 
+'         Function: exists, invokeGeneric
+' 
+'         Sub: add
+' 
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports Microsoft.VisualBasic.Linq
 Imports SMRUCC.Rsharp.Runtime.Internal.Object
 
@@ -81,6 +82,28 @@ Namespace Runtime.Internal
 
         Public Function exists(funcName As String) As Boolean
             Return generics.ContainsKey(funcName)
+        End Function
+
+        ''' <summary>
+        ''' ``genericName.&lt;type>``
+        ''' </summary>
+        ''' <param name="funcName"></param>
+        ''' <param name="env"></param>
+        ''' <returns></returns>
+        Public Function parseGeneric(funcName As String, env As Environment) As NamedValue(Of Type)
+            For Each type As String In env.globalEnvironment.types.Keys
+                If funcName.EndsWith("." & type) Then
+                    Dim genericName As String = funcName.Replace("." & type, "")
+                    Dim target As Type = env.globalEnvironment.types(type)
+
+                    Return New NamedValue(Of Type) With {
+                        .Name = genericName,
+                        .Value = target
+                    }
+                End If
+            Next
+
+            Return Nothing
         End Function
 
         <Extension>
