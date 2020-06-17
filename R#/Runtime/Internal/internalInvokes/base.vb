779077fc4da44292739ac6b1d0f9832bae086f87
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-06-17 13:30:49 +0800

    improvements of the debug helper api

--- R#/Runtime/Internal/internalInvokes/base.vb
+++ R#/Runtime/Internal/internalInvokes/base.vb
@@ -1,50 +1,50 @@
 ﻿#Region "Microsoft.VisualBasic::0f52f6aeda2b974bde2ec7b16804d3f2, R#\Runtime\Internal\internalInvokes\base.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Module base
-    ' 
-    '         Function: [stop], allocate, append, autoDispose, cat
-    '                   cbind, colnames, createDotNetExceptionMessage, CreateMessageInternal, doPrintInternal
-    '                   getEnvironmentStack, getOption, head, invisible, isEmpty
-    '                   isNull, length, names, ncol, neg
-    '                   nrow, options, print, rbind, Rdataframe
-    '                   rep, replace, Rlist, rownames, source
-    '                   str, summary, t, unitOfT, warning
-    ' 
-    '         Sub: q, quit
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Module base
+' 
+'         Function: [stop], allocate, append, autoDispose, cat
+'                   cbind, colnames, createDotNetExceptionMessage, CreateMessageInternal, doPrintInternal
+'                   getEnvironmentStack, getOption, head, invisible, isEmpty
+'                   isNull, length, names, ncol, neg
+'                   nrow, options, print, rbind, Rdataframe
+'                   rep, replace, Rlist, rownames, source
+'                   str, summary, t, unitOfT, warning
+' 
+'         Sub: q, quit
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -810,6 +810,12 @@ Namespace Runtime.Internal.Invokes
                     Throw DirectCast(message, Exception)
                 Else
                     Return DirectCast(message, Exception).createDotNetExceptionMessage(envir)
+                End If
+            ElseIf message.GetType Is GetType(Message) Then
+                If debugMode Then
+                    Throw New Exception(DirectCast(message, Message).message.JoinBy("; "))
+                Else
+                    Return message
                 End If
             Else
                 If debugMode Then
