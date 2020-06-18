c72edeccac4537b4f624da37306207e4d0d50262
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-06-18 14:20:04 +0800

    graphics2D

---
+++ Library/R.graphics/gr2D.vb
@@ -0,0 +1,12 @@
+﻿
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Scripting.MetaData
+
+<Package("graphics2D")>
+Module gr2D
+
+    <ExportAPI("labeler")>
+    Public Function labeler()
+
+    End Function
+End Module
