01e9b29559b5b77d56a3e798af2c2a697904f43d
xieguigang <xie.guigang@gcmodeller.org>
2020-06-30 20:43:30 +0800

    open hdf5 file

---
+++ Library/R.base/base/HDF5utils.vb
@@ -0,0 +1,24 @@
+﻿Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Data.IO.HDF5
+Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.Rsharp.Runtime
+Imports SMRUCC.Rsharp.Runtime.Components
+
+<Package("HDF5.utils")>
+Module HDF5utils
+
+    <ExportAPI("open.hdf5")>
+    Public Function openHDF5(file As Object, Optional env As Environment = Nothing) As Object
+        If file Is Nothing Then
+            Return Internal.debug.stop("file for open can not be nothing!", env)
+        ElseIf TypeOf file Is String Then
+            If DirectCast(file, String).FileExists Then
+                Return New HDF5File(DirectCast(file, String))
+            Else
+                Return Internal.debug.stop("the given file is not found on your filesystem!", env)
+            End If
+        Else
+            Return Internal.debug.stop(Message.InCompatibleType(GetType(String), file.GetType, env), env)
+        End If
+    End Function
+End Module
