857cbf247705e7ffea22a5213f437cca4842b0cc
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-07-08 16:53:36 +0800

    Update base.vb

--- R#/Runtime/Internal/internalInvokes/base.vb
+++ R#/Runtime/Internal/internalInvokes/base.vb
@@ -1,51 +1,51 @@
 ﻿#Region "Microsoft.VisualBasic::767cb5bb368244719a127d0c25cccc65, R#\Runtime\Internal\internalInvokes\base.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Module base
-    ' 
-    '         Function: [stop], allocate, append, autoDispose, cat
-    '                   cbind, colnames, createDotNetExceptionMessage, CreateMessageInternal, doPrintInternal
-    '                   getEnvironmentStack, getOption, head, invisible, isEmpty
-    '                   isNull, length, names, ncol, neg
-    '                   nrow, options, print, rbind, Rdataframe
-    '                   rep, replace, Rlist, rownames, sink
-    '                   source, str, summary, t, unitOfT
-    '                   warning
-    ' 
-    '         Sub: q, quit
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Module base
+' 
+'         Function: [stop], allocate, append, autoDispose, cat
+'                   cbind, colnames, createDotNetExceptionMessage, CreateMessageInternal, doPrintInternal
+'                   getEnvironmentStack, getOption, head, invisible, isEmpty
+'                   isNull, length, names, ncol, neg
+'                   nrow, options, print, rbind, Rdataframe
+'                   rep, replace, Rlist, rownames, sink
+'                   source, str, summary, t, unitOfT
+'                   warning
+' 
+'         Sub: q, quit
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -581,10 +581,10 @@ Namespace Runtime.Internal.Invokes
         ''' such strings To the current encoding.
         ''' </remarks>
         <ExportAPI("sink")>
-        Public Sub sink(Optional file$ = Nothing,
-                        Optional append As Boolean = False,
-                        Optional split As Boolean = True,
-                        Optional env As Environment = Nothing)
+        Public Function sink(Optional file$ = Nothing,
+                             Optional append As Boolean = False,
+                             Optional split As Boolean = True,
+                             Optional env As Environment = Nothing) As Object
 
             Dim stdout As RContentOutput = env.globalEnvironment.stdout
 
@@ -595,7 +595,9 @@ Namespace Runtime.Internal.Invokes
                 ' 结束当前的日志会话
                 Call stdout.closeSink()
             End If
-        End Sub
+
+            Return Nothing
+        End Function
 
         ''' <summary>
         ''' # Length of an Object
