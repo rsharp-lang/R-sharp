fedf16b796e38777055a31501be5027bd9ebb052
xieguigang <xie.guigang@gcmodeller.org>
2020-07-15 13:21:23 +0800

    parse json string as R# object

--- Library/R.base/utils/strings.vb
+++ Library/R.base/utils/strings.vb
@@ -1,48 +1,56 @@
 ﻿#Region "Microsoft.VisualBasic::02e99087d1358fd1f6b3cb2418e9c5c5, Library\R.base\utils\strings.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module strings
-    ' 
-    '     Constructor: (+1 Overloads) Sub New
-    '     Function: Levenshtein
-    ' 
-    ' /********************************************************************************/
+' Module strings
+' 
+'     Constructor: (+1 Overloads) Sub New
+'     Function: Levenshtein
+' 
+' /********************************************************************************/
 
 #End Region
 
+Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.ComponentModel.Algorithm.DynamicProgramming.Levenshtein
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports Microsoft.VisualBasic.Language
+Imports Microsoft.VisualBasic.MIME.application.json
+Imports Microsoft.VisualBasic.MIME.application.json.Javascript
 Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.Rsharp.Runtime
+Imports SMRUCC.Rsharp.Runtime.Components
+Imports SMRUCC.Rsharp.Runtime.Internal.Object
 Imports RHtml = SMRUCC.Rsharp.Runtime.Internal.htmlPrinter
 
 <Package("strings", Category:=APICategories.UtilityTools)>
@@ -55,5 +63,46 @@ Module strings
     <ExportAPI("levenshtein")>
     Public Function Levenshtein(x$, y$) As DistResult
         Return LevenshteinDistance.ComputeDistance(x, y)
+    End Function
+
+    <ExportAPI("fromJSON")>
+    Public Function fromJSON(str As String, Optional env As Environment = Nothing) As Object
+        Return New JsonParser().OpenJSON(str).createRObj(env)
+    End Function
+
+    <Extension>
+    Private Function createRObj(json As JsonElement, env As Environment) As Object
+        If TypeOf json Is JsonValue Then
+            Return DirectCast(json, JsonValue).GetStripString
+        ElseIf TypeOf json Is JsonArray Then
+            Dim array As JsonArray = DirectCast(json, JsonArray)
+
+            If array.All(Function(a) TypeOf a Is JsonValue) Then
+                Return array _
+                    .Select(Function(a) a.createRObj(env)) _
+                    .ToArray
+            Else
+                Dim list As New list With {.slots = New Dictionary(Of String, Object)}
+                Dim i As i32 = 1
+
+                For Each item As JsonElement In array
+                    list.slots.Add($"[[{++i}]]", item.createRObj(env))
+                Next
+
+                Return list
+            End If
+        ElseIf TypeOf json Is JsonObject Then
+            Dim list As New list With {
+                .slots = New Dictionary(Of String, Object)
+            }
+
+            For Each item As NamedValue(Of JsonElement) In DirectCast(json, JsonObject)
+                Call list.slots.Add(item.Name, item.Value.createRObj(env))
+            Next
+
+            Return list
+        Else
+            Return Internal.debug.stop(Message.InCompatibleType(GetType(JsonElement), json.GetType, env), env)
+        End If
     End Function
 End Module
