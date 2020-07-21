0af99d28b0883e31166ddc7e9258150da53df706
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-07-21 16:55:46 +0800

    config of the demo project

--- Library/devkit/devkit.vb
+++ Library/devkit/devkit.vb
@@ -1,41 +1,41 @@
 ﻿#Region "Microsoft.VisualBasic::984338578db995a1c4eef8ccee313398, Library\devkit\devkit.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module devkit
-    ' 
-    '     Function: AssemblyInfo, gitLog, svnLog
-    ' 
-    ' /********************************************************************************/
+' Module devkit
+' 
+'     Function: AssemblyInfo, gitLog, svnLog
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -45,6 +45,7 @@ Imports Microsoft.VisualBasic.ApplicationServices.Development.VisualStudio
 Imports Microsoft.VisualBasic.ApplicationServices.Development.VisualStudio.VersionControl
 Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.Rsharp.Runtime.Interop
 
 ''' <summary>
 ''' VisualBasic.NET application development kit
@@ -70,5 +71,13 @@ Module devkit
     <ExportAPI("git.log")>
     Public Function gitLog(file As String) As log()
         Return log.ParseGitLogText(file.SolveStream).ToArray
+    End Function
+
+    <ExportAPI("il")>
+    Public Function showIL(api As RMethodInfo) As String()
+        Dim il As New IL.MethodBodyReader(api.GetRawDeclares)
+        Dim msil = il.instructions.ToArray
+
+        Return msil.Select(Function(a) a.ToString)
     End Function
 End Module
