ff5834672c04a2518369b2f40826abe88771a0cd
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-07-22 11:21:43 +0800

    config project for test

--- Library/demo/dev/IL.R
+++ Library/demo/dev/IL.R
@@ -1,3 +1,10 @@
 imports "VisualStudio" from "devkit";
 
-print(il(sapply));
+print(il);
+print(il(il));
+
+print(install.packages);
+print(il(install.packages));
+
+print(installed.packages);
+print(il(installed.packages));
