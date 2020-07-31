f9205e37b06e922eaacb50091c626de4c41bbe26
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-07-31 10:36:41 +0800

    create a new general signal data object

--- Library/R.base/utils/strings.vb
+++ Library/R.base/utils/strings.vb
@@ -1,42 +1,42 @@
 ﻿#Region "Microsoft.VisualBasic::696f1f51b6b7c101835ad1d9c8e4b6c8, Library\R.base\utils\strings.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module strings
-    ' 
-    '     Constructor: (+1 Overloads) Sub New
-    '     Function: createRObj, fromJSON, Levenshtein
-    ' 
-    ' /********************************************************************************/
+' Module strings
+' 
+'     Constructor: (+1 Overloads) Sub New
+'     Function: createRObj, fromJSON, Levenshtein
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -47,6 +47,7 @@ Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.MIME.application.json
 Imports Microsoft.VisualBasic.MIME.application.json.Javascript
+Imports Microsoft.VisualBasic.MIME.application.xml
 Imports Microsoft.VisualBasic.Scripting.MetaData
 Imports SMRUCC.Rsharp.Runtime
 Imports SMRUCC.Rsharp.Runtime.Components
@@ -104,5 +105,15 @@ Module strings
         Else
             Return Internal.debug.stop(Message.InCompatibleType(GetType(JsonElement), json.GetType, env), env)
         End If
+    End Function
+
+    <ExportAPI("fromXML")>
+    Public Function fromXML(str As String, Optional env As Environment = Nothing) As Object
+        Return XmlElement.ParseXmlText(str).createRObj(env)
+    End Function
+
+    <Extension>
+    Private Function createRObj(json As XmlElement, env As Environment) As Object
+
     End Function
 End Module
--- Library/R.math/dataScience/signalProcessing.vb
+++ Library/R.math/dataScience/signalProcessing.vb
@@ -1,54 +1,56 @@
 ﻿#Region "Microsoft.VisualBasic::ce283f11a3566e102e84c04ad8d53725, Library\R.math\dataScience\signalProcessing.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module signalProcessing
-    ' 
-    '     Constructor: (+1 Overloads) Sub New
-    '     Function: asMatrix
-    ' 
-    ' /********************************************************************************/
+' Module signalProcessing
+' 
+'     Constructor: (+1 Overloads) Sub New
+'     Function: asMatrix
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports Microsoft.VisualBasic.ApplicationServices.Debugging.Logging
 Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Data.csv.IO
+Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.Math.SignalProcessing
 Imports Microsoft.VisualBasic.Scripting.MetaData
 Imports SMRUCC.Rsharp.Runtime
 Imports SMRUCC.Rsharp.Runtime.Internal.Object
 Imports SMRUCC.Rsharp.Runtime.Interop
+Imports REnv = SMRUCC.Rsharp.Runtime
 
 <Package("signal_kit")>
 Module signalProcessing
@@ -56,6 +58,23 @@ Module signalProcessing
     Sub New()
 
     End Sub
+
+    <ExportAPI("as.signal")>
+    Public Function asGeneral(measure As vector, signals As vector,
+                              Optional title$ = "general signal",
+                              <RListObjectArgument>
+                              Optional meta As list = Nothing,
+                              Optional env As Environment = Nothing) As GeneralSignal
+
+        Return New GeneralSignal With {
+            .description = title,
+            .Measures = REnv.asVector(Of Double)(measure),
+            .measureUnit = measure.unit?.ToString Or "n/a".AsDefault,
+            .meta = meta.AsGeneric(Of String)(env),
+            .reference = App.NextTempName,
+            .Strength = REnv.asVector(Of Double)(signals)
+        }
+    End Function
 
     ''' <summary>
     ''' 
