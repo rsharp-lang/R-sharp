ef5af8552cc61dc2982cc857135cfee733be498c
xieguigang <xie.guigang@gcmodeller.org>
2020-08-03 16:38:53 +0800

    improvements of the help system

--- studio/R-terminal/CLI/CLI.vb
+++ studio/R-terminal/CLI/CLI.vb
@@ -1,49 +1,52 @@
 ﻿#Region "Microsoft.VisualBasic::b5341d6fba43cab77cb8ec2c03a8711f, studio\R-terminal\CLI\CLI.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module CLI
-    ' 
-    '     Function: ConfigStartups, Info, InitializeEnvironment, Install, SyntaxText
-    '               unixman, Version
-    ' 
-    ' /********************************************************************************/
+' Module CLI
+' 
+'     Function: ConfigStartups, Info, InitializeEnvironment, Install, SyntaxText
+'               unixman, Version
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.ComponentModel
+Imports System.IO
 Imports System.Reflection
+Imports System.Runtime.CompilerServices
 Imports System.Text
 Imports Microsoft.VisualBasic.ApplicationServices.Development
+Imports Microsoft.VisualBasic.ApplicationServices.Development.XmlDoc.Assembly
 Imports Microsoft.VisualBasic.ApplicationServices.Terminal.Utility
 Imports Microsoft.VisualBasic.CommandLine
 Imports Microsoft.VisualBasic.CommandLine.InteropService.SharedORM
@@ -191,21 +194,12 @@ Module CLI
         Dim out$ = args("--out") Or "./"
         Dim module$ = args("--module")
         Dim env As New RInterpreter
-        Dim xmldocs = env.globalEnvir.packages.packageDocs
+        Dim xmldocs As AnnotationDocs = env.globalEnvir.packages.packageDocs
         Dim utf8 As Encoding = Encodings.UTF8WithoutBOM.CodePage
 
         If [module].FileExists Then
             For Each pkg As Package In PackageLoader.ParsePackages(dll:=[module])
-                For Each ref As String In pkg.ls
-                    Dim symbol As RMethodInfo = pkg.GetFunction(apiName:=ref)
-                    Dim docs = xmldocs.GetAnnotations(symbol.GetRawDeclares)
-                    Dim help As UnixManPage = UnixManPagePrinter.CreateManPage(symbol, docs)
-
-                    Call UnixManPage _
-                        .ToString(help, "man page create by R# package system.") _
-                        .SaveTo($"{out}/{pkg.namespace}/{ref}.1", utf8)
-                Next
-
+                Call pkg.unixMan(xmldocs, out)
                 Call $"load: {pkg.info.Namespace}".__INFO_ECHO
             Next
         Else
@@ -213,23 +207,41 @@ Module CLI
             For Each pkg As Package In env.globalEnvir.packages.AsEnumerable
                 If pkg.isMissing Then
                     Call $"missing package: {pkg.namespace}...".PrintException
-                    Continue For
+                Else
+                    Call pkg.unixMan(xmldocs, out)
                 End If
-
-                For Each ref As String In pkg.ls
-                    Dim symbol As RMethodInfo = pkg.GetFunction(apiName:=ref)
-                    Dim docs = xmldocs.GetAnnotations(symbol.GetRawDeclares)
-                    Dim help As UnixManPage = UnixManPagePrinter.CreateManPage(symbol, docs)
-
-                    Call UnixManPage _
-                        .ToString(help, "man page create by R# package system.") _
-                        .SaveTo($"{out}/{pkg.namespace}/{ref}.1", utf8)
-                Next
             Next
         End If
 
         Return 0
     End Function
+
+    <Extension>
+    Private Sub unixMan(pkg As Package, xmldocs As AnnotationDocs, out$)
+        Dim annoDocs As ProjectType = xmldocs.GetAnnotations(pkg.package)
+
+        For Each ref As String In pkg.ls
+            Dim symbol As RMethodInfo = pkg.GetFunction(apiName:=ref)
+            Dim docs = xmldocs.GetAnnotations(symbol.GetRawDeclares)
+            Dim help As UnixManPage = UnixManPagePrinter.CreateManPage(symbol, docs)
+
+            Call UnixManPage _
+                .ToString(help, "man page create by R# package system.") _
+                .SaveTo($"{out}/{pkg.namespace}/{ref}.1", UTF8)
+        Next
+
+        Using markdown As StreamWriter = $"{out}/{pkg.namespace}.md".OpenWriter
+            Call markdown.WriteLine("# " & pkg.namespace)
+            Call markdown.WriteLine()
+            Call markdown.WriteLine(annoDocs.Summary)
+
+            If Not annoDocs.Remarks.StringEmpty Then
+                For Each line As String In annoDocs.Remarks.LineTokens
+                    Call markdown.WriteLine("> " & line)
+                Next
+            End If
+        End Using
+    End Sub
 
     <ExportAPI("--info")>
     <Description("Print R# interpreter version information and R# terminal version information.")>
