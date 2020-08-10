fde1351a06a289db5702120cf75468514361162c
xieguigang <xie.guigang@gcmodeller.org>
2020-08-10 13:50:21 +0800

    config of the attribute training in ANN script

--- Library/demo/machineLearning/trainingANN.R
+++ Library/demo/machineLearning/trainingANN.R
@@ -6,9 +6,17 @@ let inputFile as string = ?"--data"   || stop("no training data set was provided
 let output as string    = ?"--save"   || `${dirname(inputFile)}/${basename(inputFile)}.trainingResult/`;
 let maxLoops as integer = ?"--loops"  || 10000;
 let hiddens as string   = ?"--hidden" || "120,300,200,20";
+let attr as string      = ?"--attr";
 let dataset = inputFile
 :> read.ML_model
 ;
+
+if (attr == "") {
+	attr = -1;
+} else {
+	attr = as.integer(attr);
+	print("a single specific output attribute will be trained...");
+}
 
 print("checking of the ML dataset...");
 print(check.ML_model(dataset));
@@ -18,7 +26,7 @@ print(output);
 
 ANN.training_model(
 	inputSize      = input.size(dataset),
-	outputSize     = 1, # output.size(dataset),
+	outputSize     = (attr < 0) ? output.size(dataset) : 1,
 	hiddenSize     = as.integer(strsplit(hiddens, ',')), 
 	learnRate      = 0.125, 
 	momentum       = 0.9, 
@@ -27,7 +35,7 @@ ANN.training_model(
 	outputSnapshot = TRUE	
 )
 :> configuration(softmax = FALSE, selectiveMode = TRUE)
-:> set.trainingSet(dataset, attribute = 0)
+:> set.trainingSet(dataset, attribute = attr)
 :> training(maxIterations  = maxLoops)
 :> write.ANN_network(output)
 ;
