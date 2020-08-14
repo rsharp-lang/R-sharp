3b1e3f4c7461f057dc05fadea38ac9684bf0522f
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-08-14 11:27:41 +0800

    print welcome message

--- MLkit/zzz.vb
+++ MLkit/zzz.vb
@@ -1,6 +1,11 @@
-﻿Public Class zzz
+﻿Imports Microsoft.VisualBasic.ApplicationServices.Development
+Imports Microsoft.VisualBasic.CommandLine
+
+Public Class zzz
 
     Public Shared Sub onLoad()
-
+        Call GetType(zzz).Assembly _
+            .FromAssembly _
+            .AppSummary("Welcome to the SMRUCC Machine Learning toolkit!", "", App.StdOut)
     End Sub
 End Class
