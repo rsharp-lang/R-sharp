09b7c28564b709cd1a205a9c218176fb040fe903
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-08-17 17:34:13 +0800

    binary tree to cluster model

--- MLkit/dataMining/btreeCluster.vb
+++ MLkit/dataMining/btreeCluster.vb
@@ -9,7 +9,19 @@ Public Class btreeCluster
     Public Property members As String()
 
     Public Shared Function GetClusters(btree As AVLTree(Of String, String)) As btreeCluster
-
+        Return GetClusters(btree.root)
     End Function
 
+    Private Shared Function GetClusters(btree As BinaryTree(Of String, String)) As btreeCluster
+        If btree Is Nothing Then
+            Return Nothing
+        Else
+            Return New btreeCluster With {
+                .key = btree.Key,
+                .members = btree.Members,
+                .left = GetClusters(btree.Left),
+                .right = GetClusters(btree.Right)
+            }
+        End If
+    End Function
 End Class
