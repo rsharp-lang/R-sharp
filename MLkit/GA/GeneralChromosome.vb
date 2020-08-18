bb9feb15f64e52e1ea70a4accdc0c1d4e2f5d679
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-08-18 18:52:05 +0800

    split source file

--- MLkit/GA/GeneralChromosome.vb
+++ MLkit/GA/GeneralChromosome.vb
@@ -5,42 +5,6 @@ Imports Microsoft.VisualBasic.MachineLearning.Darwinism.Models
 Imports Microsoft.VisualBasic.Scripting.MetaData
 Imports SMRUCC.Rsharp.Runtime.Components.Interface
 
-''' <summary>
-''' A Genetic Algorithm Toolkit for R# language
-''' </summary>
-<Package("GA_toolkit")>
-Module GAToolkit
-
-    ''' <summary>
-    ''' create a new chromosome template.
-    ''' </summary>
-    ''' <param name="seed"></param>
-    ''' <param name="crossover"></param>
-    ''' <param name="mutate"></param>
-    ''' <param name="uniqueId"></param>
-    ''' <param name="env"></param>
-    ''' <returns></returns>
-    <ExportAPI("template")>
-    Public Function template(seed As Object, crossover As RFunction, mutate As RFunction, uniqueId As RFunction, Optional env As Environment = Nothing) As GeneralChromosome
-
-    End Function
-
-    <ExportAPI("population")>
-    Public Function population(ancestor As GeneralChromosome,
-                               Optional mutation_rate As Double = 0.2,
-                               Optional population_size As Integer = 500,
-                               Optional env As Environment = Nothing) As Population(Of GeneralChromosome)
-
-        If mutation_rate <= 0 Then
-
-        End If
-
-        ancestor.MutationRate = mutation_rate
-
-        Return ancestor.InitialPopulation(popSize:=population_size, parallel:=True)
-    End Function
-End Module
-
 Public Class GeneralChromosome : Implements Chromosome(Of GeneralChromosome)
 
     Public Property MutationRate As Double Implements Chromosome(Of GeneralChromosome).MutationRate
