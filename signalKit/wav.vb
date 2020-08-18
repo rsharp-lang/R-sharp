8ae4a7b714732d3bef41c895a3e720399a59d427
xieguigang <xie.guigang@gcmodeller.org>
2020-08-18 22:49:15 +0800

    Update wav.vb

--- signalKit/wav.vb
+++ signalKit/wav.vb
@@ -1,8 +1,11 @@
 ﻿
 Imports System.IO
+Imports System.Text
 Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Data.IO
 Imports Microsoft.VisualBasic.Data.Wave
+Imports Microsoft.VisualBasic.Language
+Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.Scripting.MetaData
 Imports SMRUCC.Rsharp.Runtime
 Imports SMRUCC.Rsharp.Runtime.Components
@@ -16,7 +19,29 @@ Public Module wavToolkit
     End Sub
 
     Private Function wavToString(wav As WaveFile) As String
+        Dim summary As New StringBuilder
 
+        Call summary.AppendLine($"[{wav.magic}] {wav.fileSize} bytes")
+        Call summary.AppendLine($"format: {wav.format}")
+        Call summary.AppendLine()
+        Call summary.AppendLine($"fmt_data_chunk:")
+        Call summary.AppendLine($"     audio_format: {wav.fmt.audioFormat.ToString}")
+        Call summary.AppendLine($"         channels: {wav.fmt.channels.ToString}")
+        Call summary.AppendLine($"      sample_rate: {wav.fmt.SampleRate}")
+        Call summary.AppendLine($"        byte_rate: {wav.fmt.ByteRate}")
+        Call summary.AppendLine($"      block_align: {wav.fmt.BlockAlign}")
+        Call summary.AppendLine($"  bits_per_sample: {wav.fmt.BitsPerSample}")
+        Call summary.AppendLine($"           is_PCM: {wav.fmt.isPCM.ToString.ToUpper}")
+        Call summary.AppendLine()
+        Call summary.AppendLine($"wav_data:")
+
+        Dim i As i32 = 1
+
+        For Each sample In wav.data.AsEnumerable
+            Call summary.AppendLine($"  sample #{++i}: {sample.channels.Take(6).JoinBy(", ")}...")
+        Next
+
+        Return summary.ToString
     End Function
 
     <ExportAPI("read.wav")>
