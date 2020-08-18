d9fa2cf169b795800b7eb279ede5c11b34020b20
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-08-18 16:18:32 +0800

    genetic algorithm toolkit?

---
+++ MLkit/GeneticAlgorithmToolkit.vb
@@ -0,0 +1,9 @@
+﻿Imports Microsoft.VisualBasic.Scripting.MetaData
+
+''' <summary>
+''' A Genetic Algorithm Toolkit for R# language
+''' </summary>
+<Package("GA_toolkit")>
+Module GeneticAlgorithmToolkit
+
+End Module
