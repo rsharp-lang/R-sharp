98ad5b50375ebef98d29a6048bac5a070c60483a
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-08-19 16:19:53 +0800

    removes duplicated edges

--- Library/R.graph/NetworkModule.vb
+++ Library/R.graph/NetworkModule.vb
@@ -1,45 +1,45 @@
 ﻿#Region "Microsoft.VisualBasic::61fc734c0fe6105f14bcebb387af1f26, Library\R.graph\NetworkModule.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module NetworkModule
-    ' 
-    '     Constructor: (+1 Overloads) Sub New
-    '     Function: addEdge, addEdges, addNode, addNodes, attributes
-    '               computeNetwork, connectedNetwork, DecomposeGraph, degree, emptyNetwork
-    '               getByGroup, getEdges, getElementByID, getNodes, LoadNetwork
-    '               printGraph, SaveNetwork, setAttributes, typeGroupOfNodes
-    ' 
-    ' /********************************************************************************/
+' Module NetworkModule
+' 
+'     Constructor: (+1 Overloads) Sub New
+'     Function: addEdge, addEdges, addNode, addNodes, attributes
+'               computeNetwork, connectedNetwork, DecomposeGraph, degree, emptyNetwork
+'               getByGroup, getEdges, getElementByID, getNodes, LoadNetwork
+'               printGraph, SaveNetwork, setAttributes, typeGroupOfNodes
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -137,6 +137,17 @@ Public Module NetworkModule
         End If
 
         Return g
+    End Function
+
+    ''' <summary>
+    ''' removes duplicated edges in the network
+    ''' </summary>
+    ''' <param name="g"></param>
+    ''' <param name="directedGraph"></param>
+    ''' <returns></returns>
+    <ExportAPI("trim.edges")>
+    Public Function trimEdges(g As NetworkGraph, Optional directedGraph As Boolean = False) As NetworkGraph
+        Return g.RemoveDuplicated(directedGraph)
     End Function
 
     ''' <summary>
