59cee00b9833c0dcf973f61c69f29ca967783463
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-08-21 15:44:43 +0800

    merge projects

--- studio/R.exec/loader.R
+++
@@ -1,8 +0,0 @@
-imports "R.executable.loader" from "R.exec";
-
-# base64 binary R# program data
-# this string literal will set to last variable
-# and then run from the last variable
-"";
-
-run();
