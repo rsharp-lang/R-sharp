bc985162f2f7bbf086b3a14e3cf2b34875301eca
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-08-26 17:04:19 +0800

    Run ANN training under the GA framework

--- studio/Rsharp_kit/MLkit/GA/GAToolkit.vb
+++ studio/Rsharp_kit/MLkit/GA/GAToolkit.vb
@@ -1,47 +1,51 @@
 ﻿#Region "Microsoft.VisualBasic::f8392247f3aa4d2671c269fa20225c2b, studio\Rsharp_kit\MLkit\GA\GAToolkit.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module GAToolkit
-    ' 
-    '     Function: population, template
-    ' 
-    ' /********************************************************************************/
+' Module GAToolkit
+' 
+'     Function: population, template
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.MachineLearning.Darwinism.GAF
 Imports Microsoft.VisualBasic.MachineLearning.Darwinism.GAF.Helper
+Imports Microsoft.VisualBasic.MachineLearning.NeuralNetwork
+Imports Microsoft.VisualBasic.MachineLearning.NeuralNetwork.DarwinismHybrid
+Imports Microsoft.VisualBasic.MachineLearning.StoreProcedure
 Imports Microsoft.VisualBasic.Scripting.MetaData
 Imports SMRUCC.Rsharp.Runtime
 Imports SMRUCC.Rsharp.Runtime.Components.Interface
@@ -85,6 +89,34 @@ Module GAToolkit
         ancestor.MutationRate = mutation_rate
 
         Return ancestor.InitialPopulation(popSize:=population_size, parallel:=True)
+    End Function
+
+    ''' <summary>
+    ''' Run ANN training under the GA framework
+    ''' </summary>
+    ''' <param name="network"></param>
+    ''' <param name="trainingSet"></param>
+    ''' <param name="mutationRate#"></param>
+    ''' <param name="populationSize%"></param>
+    ''' <param name="iterations%"></param>
+    ''' <returns></returns>
+    <ExportAPI("ANN.training")>
+    Public Function runANNTraining(network As Network, trainingSet As DataSet,
+                                   Optional mutationRate# = 0.2,
+                                   Optional populationSize% = 1000,
+                                   Optional iterations% = 10000) As Network
+
+        Dim trainingMatrix As Sample() = trainingSet _
+            .DataSamples _
+            .AsEnumerable _
+            .ToArray
+
+        Return network.RunGATrainer(
+            trainingSet:=trainingMatrix,
+            mutationRate:=mutationRate,
+            populationSize:=populationSize,
+            iterations:=iterations
+        )
     End Function
 End Module
 
