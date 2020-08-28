3a35eac31f7777e5a3df0b0d7f4d765f2f819ef0
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-08-28 10:23:46 +0800

    append problem data

--- studio/Rsharp_kit/MLkit/SVM.vb
+++ studio/Rsharp_kit/MLkit/SVM.vb
@@ -4,8 +4,10 @@ Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.MachineLearning.SVM
 Imports Microsoft.VisualBasic.Scripting.MetaData
 Imports SMRUCC.Rsharp.Runtime
+Imports SMRUCC.Rsharp.Runtime.Components
 Imports SMRUCC.Rsharp.Runtime.Internal.Object
 Imports SMRUCC.Rsharp.Runtime.Interop
+Imports REnv = SMRUCC.Rsharp.Runtime
 
 <Package("SVM")>
 <RTypeExport("problem", GetType(Problem))>
@@ -46,9 +48,48 @@ Module SVM
     End Function
 
     <ExportAPI("append.trainingSet")>
-    Public Function expandProblem(problem As Problem, tag As Integer, data As dataframe) As Problem
+    <RApiReturn(GetType(Problem))>
+    Public Function expandProblem(problem As Problem, tag As Integer(), data As Object, Optional env As Environment = Nothing) As Object
         Dim part As New List(Of Node())()
         Dim labels As New List(Of Double)()
+        Dim vectors As New Dictionary(Of String, Double())
 
+        If data Is Nothing Then
+            Return Internal.debug.stop("no problem data was provided!", env)
+        ElseIf TypeOf data Is list Then
+            vectors = DirectCast(data, list).AsGeneric(Of Double())(env)
+        ElseIf TypeOf data Is dataframe Then
+            vectors = DirectCast(data, dataframe).columns.ToDictionary(Function(a) a.Key, Function(a) DirectCast(REnv.asVector(Of Double)(a.Value), Double()))
+        Else
+            Return Message.InCompatibleType(GetType(dataframe), data.GetType, env)
+        End If
+
+        Dim n As Integer = vectors.Values.First.Length
+        Dim getTag As Func(Of Integer, Integer)
+
+        If tag.Length = 1 Then
+            getTag = Function() tag(Scan0)
+        Else
+            getTag = Function(i) tag(i)
+        End If
+
+        Dim index As Integer
+        Dim row As Node()
+
+        For i As Integer = 0 To n - 1
+            labels.Add(getTag(i))
+            index = i
+            row = problem.DimensionNames _
+                .Select(Function([dim], j)
+                            Return New Node(j + 1, vectors([dim])(index))
+                        End Function) _
+                .ToArray
+            part.Add(row)
+        Next
+
+        problem.X = problem.X.AsList + part.AsEnumerable
+        problem.Y = problem.Y.AsList + labels.AsEnumerable
+
+        Return problem
     End Function
 End Module
