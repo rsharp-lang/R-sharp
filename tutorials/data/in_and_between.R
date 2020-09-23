10d2d36672da4aa32d4e770f36a9b438dd9e1132
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-09-23 15:30:42 +0800

    Update in_and_between.R

--- tutorials/data/in_and_between.R
+++ tutorials/data/in_and_between.R
@@ -8,6 +8,8 @@ print([1, 2, 2.5, 3, 4, 5] in [2, 3]);
 
 # between test each element in x collection
 # is in a given value range?
+#
+# each x between a given value range?
 print([1, 2, 2.5, 3, 4, 5] between [2, 3]);
 # result:
 # [1] FALSE TRUE TRUE TRUE FALSE FALSE
