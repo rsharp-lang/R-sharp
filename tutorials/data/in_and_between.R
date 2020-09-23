52fd0c246e1754bad2f21eefdfcb7bdd78eae8bd
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-09-23 15:29:37 +0800

    Create in_and_between.R

---
+++ tutorials/data/in_and_between.R
@@ -0,0 +1,13 @@
+# in test each element in x collection
+# for exact match in collection b
+#
+# each x in b? 
+print([1, 2, 2.5, 3, 4, 5] in [2, 3]);
+# result:
+# [1] FALSE TRUE FALSE TRUE FALSE FALSE
+
+# between test each element in x collection
+# is in a given value range?
+print([1, 2, 2.5, 3, 4, 5] between [2, 3]);
+# result:
+# [1] FALSE TRUE TRUE TRUE FALSE FALSE
