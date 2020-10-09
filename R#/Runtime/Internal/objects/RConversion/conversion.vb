1df6f7ecea57d02080e3e2c46a2a4df6578c1c56
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-10-09 09:23:30 +0800

    try to fix of cast group to vector

--- R#/Runtime/Internal/objects/RConversion/conversion.vb
+++ R#/Runtime/Internal/objects/RConversion/conversion.vb
@@ -1,45 +1,45 @@
 ﻿#Region "Microsoft.VisualBasic::959d89fb07695bb60c62e26c0f65e1d4, R#\Runtime\Internal\objects\RConversion\conversion.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Module RConversion
-    ' 
-    '         Function: asCharacters, asDataframe, asDate, asInteger, asList
-    '                   asLogicals, asNumeric, asObject, asPipeline, asRaw
-    '                   asVector, castArrayOfGeneric, castArrayOfObject, castType, isCharacter
-    '                   tryUnlistArray, unlist, unlistOfRList, unlistRecursive
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Module RConversion
+' 
+'         Function: asCharacters, asDataframe, asDate, asInteger, asList
+'                   asLogicals, asNumeric, asObject, asPipeline, asRaw
+'                   asVector, castArrayOfGeneric, castArrayOfObject, castType, isCharacter
+'                   tryUnlistArray, unlist, unlistOfRList, unlistRecursive
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -59,8 +59,10 @@ Imports SMRUCC.Rsharp.Interpreter
 Imports SMRUCC.Rsharp.Interpreter.ExecuteEngine.ExpressionSymbols
 Imports SMRUCC.Rsharp.Runtime.Components
 Imports SMRUCC.Rsharp.Runtime.Internal.Invokes
+Imports SMRUCC.Rsharp.Runtime.Internal.Invokes.LinqPipeline
 Imports SMRUCC.Rsharp.Runtime.Interop
 Imports Rset = SMRUCC.Rsharp.Runtime.Internal.Invokes.set
+Imports REnv = SMRUCC.Rsharp.Runtime
 
 Namespace Runtime.Internal.Object.Converts
 
@@ -429,9 +431,11 @@ RE0:
                 DirectCast(x, vector).elementType = classType
                 Return x
             ElseIf x.GetType.IsArray Then
-                Return New vector With {.data = x}
+                Return New vector With {.data = REnv.asVector(x, classType.raw, env), .elementType = classType}
             ElseIf TypeOf x Is pipeline Then
                 Return DirectCast(x, pipeline).createVector(env)
+            ElseIf TypeOf x Is Group Then
+                Return New vector With {.data = REnv.asVector(DirectCast(x, Group).group, classType.raw, env), .elementType = classType}
             Else
                 Dim interfaces = x.GetType.GetInterfaces
 
