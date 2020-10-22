0b91761cd84edf5ce34facf49aed63115e74700a
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-10-22 15:01:57 +0800

    improvements of the ctype system in R# scripting

--- R#/Runtime/Internal/objects/RConversion/castList.vb
+++ R#/Runtime/Internal/objects/RConversion/castList.vb
@@ -1,42 +1,42 @@
 ﻿#Region "Microsoft.VisualBasic::5fb5b5746ca6d3a6eb090444f5c8c1dd, R#\Runtime\Internal\objects\RConversion\castList.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Module castList
-    ' 
-    '         Function: CTypeList, dataframe_castList, listInternal, objCastList
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Module castList
+' 
+'         Function: CTypeList, dataframe_castList, listInternal, objCastList
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -45,6 +45,7 @@ Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports Microsoft.VisualBasic.Emit.Delegates
 Imports SMRUCC.Rsharp.Runtime.Components
+Imports SMRUCC.Rsharp.Runtime.Interop.CType
 Imports REnv = SMRUCC.Rsharp.Runtime
 
 Namespace Runtime.Internal.Object.Converts
@@ -73,6 +74,10 @@ Namespace Runtime.Internal.Object.Converts
 
         Friend Function listInternal(obj As Object, args As list, env As Environment) As Object
             Dim type As Type = obj.GetType
+
+            If type.ImplementInterface(Of ICTypeList) Then
+                Return DirectCast(obj, ICTypeList).toList
+            End If
 
             Select Case type
                 Case GetType(Dictionary(Of String, Object))
---
+++ R#/Runtime/Interop/CType/ICType.vb
@@ -0,0 +1,11 @@
+﻿Imports SMRUCC.Rsharp.Runtime.Internal.Object
+
+Namespace Runtime.Interop.CType
+
+    Public Interface ICTypeList
+
+        Function toList() As list
+
+    End Interface
+
+End Namespace
