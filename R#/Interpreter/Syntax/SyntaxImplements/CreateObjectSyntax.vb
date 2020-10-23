73593192acf8987d7570338c3165a8f473913094
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-10-23 15:18:09 +0800

    there is a syntax parser bugs

--- R#/Interpreter/Syntax/SyntaxImplements/CreateObjectSyntax.vb
+++ R#/Interpreter/Syntax/SyntaxImplements/CreateObjectSyntax.vb
@@ -1,45 +1,46 @@
 ﻿#Region "Microsoft.VisualBasic::515684b9d472d168e46616608b2b1c98, R#\Interpreter\Syntax\SyntaxImplements\CreateObjectSyntax.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Module CreateObjectSyntax
-    ' 
-    '         Function: CreateNewObject
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Module CreateObjectSyntax
+' 
+'         Function: CreateNewObject
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
+Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.ApplicationServices.Debugging.Diagnostics
 Imports SMRUCC.Rsharp.Interpreter.ExecuteEngine
 Imports SMRUCC.Rsharp.Interpreter.ExecuteEngine.ExpressionSymbols.DataSets
@@ -52,7 +53,25 @@ Namespace Interpreter.SyntaxParser.SyntaxImplements
         Public Function CreateNewObject(keyword As Token, tokens As Token(), opts As SyntaxBuilderOptions) As SyntaxResult
             Dim type As String = tokens(Scan0).text
             Dim stackFrame As StackFrame = opts.GetStackTrace(keyword, $"[{type}].cor")
-            Dim args = tokens.Skip(2).Take(tokens.Length - 3).ToArray
+            Dim blocks = tokens.Skip(1).SplitByTopLevelDelimiter(Language.TokenType.close, True, ")")
+
+            If blocks > 2 Then
+                Dim left As SyntaxResult = blocks(Scan0) _
+                    .Skip(1) _
+                    .ToArray _
+                    .CreateNewObject(type, stackFrame, opts)
+
+                Throw New NotImplementedException
+            Else
+                Return blocks(Scan0) _
+                    .Skip(1) _
+                    .ToArray _
+                    .CreateNewObject(type, stackFrame, opts)
+            End If
+        End Function
+
+        <Extension>
+        Private Function CreateNewObject(args As Token(), type$, stackFrame As StackFrame, opts As SyntaxBuilderOptions) As SyntaxResult
             Dim parameters As New List(Of Expression)
 
             For Each a As SyntaxResult In args.getInvokeParameters(opts)
