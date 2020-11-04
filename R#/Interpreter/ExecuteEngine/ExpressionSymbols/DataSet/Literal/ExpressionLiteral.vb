6fb4ba6d788f2e15bb239a6564254c846945eb88
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-11-04 09:41:47 +0800

    debug of parse expression literal

--- R#/Interpreter/ExecuteEngine/ExpressionSymbols/DataSet/Literal/ExpressionLiteral.vb
+++ R#/Interpreter/ExecuteEngine/ExpressionSymbols/DataSet/Literal/ExpressionLiteral.vb
@@ -1,9 +1,12 @@
-﻿Imports SMRUCC.Rsharp.Runtime
+﻿Imports Microsoft.VisualBasic.ApplicationServices.Debugging.Diagnostics
+Imports SMRUCC.Rsharp.Runtime
 Imports SMRUCC.Rsharp.Runtime.Components
+Imports SMRUCC.Rsharp.Runtime.Components.Interface
 
 Namespace Interpreter.ExecuteEngine.ExpressionSymbols.DataSets
 
     Public Class ExpressionLiteral : Inherits Expression
+        Implements IRuntimeTrace
 
         Public Overrides ReadOnly Property type As TypeCodes
             Get
@@ -11,14 +14,17 @@ Namespace Interpreter.ExecuteEngine.ExpressionSymbols.DataSets
             End Get
         End Property
 
+        Public ReadOnly Property stackFrame As StackFrame Implements IRuntimeTrace.stackFrame
+
         Dim expression As Expression
 
-        Sub New(expression As Expression)
+        Sub New(expression As Expression, stackFrame As StackFrame)
             Me.expression = expression
+            Me.stackFrame = stackFrame
         End Sub
 
         Public Overrides Function Evaluate(envir As Environment) As Object
-            Return Expression
+            Return expression
         End Function
 
         Public Overrides Function ToString() As String
--- R#/Interpreter/Syntax/SyntaxImplements/FormulaExpressionSyntax.vb
+++ R#/Interpreter/Syntax/SyntaxImplements/FormulaExpressionSyntax.vb
@@ -1,5 +1,5 @@
-﻿Imports Microsoft.VisualBasic.Linq
-Imports SMRUCC.Rsharp.Interpreter.ExecuteEngine
+﻿Imports Microsoft.VisualBasic.ApplicationServices.Debugging.Diagnostics
+Imports Microsoft.VisualBasic.Linq
 Imports SMRUCC.Rsharp.Interpreter.ExecuteEngine.ExpressionSymbols.DataSets
 Imports SMRUCC.Rsharp.Language.TokenIcer
 
@@ -16,6 +16,17 @@ Namespace Interpreter.SyntaxParser.SyntaxImplements
             End If
 
             Return New FormulaExpression(y, expression.expression)
+        End Function
+
+        Public Function GetExpressionLiteral(code As Token()(), opts As SyntaxBuilderOptions) As SyntaxResult
+            Dim stackFrame As StackFrame = opts.GetStackTrace(code(Scan0)(Scan0), "__expression__literal")
+            Dim literal As SyntaxResult = SyntaxResult.CreateExpression(code.Skip(1).IteratesALL, opts)
+
+            If literal.isException Then
+                Return literal
+            Else
+                Return New ExpressionLiteral(literal.expression, stackFrame)
+            End If
         End Function
     End Module
 End Namespace
