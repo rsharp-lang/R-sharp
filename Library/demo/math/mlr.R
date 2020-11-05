91a342c3577fc2fc5c4c9b261eb494e9d8b9f79c
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-11-05 11:17:45 +0800

    Update mlr.R

--- Library/demo/math/mlr.R
+++ Library/demo/math/mlr.R
@@ -2,10 +2,20 @@ let a <- [1,2,3,4,5];
 let b <- [5,7,9,11,13];
 let c <- [15,31.5,1.8,1.5,71.5];
 let d <- [3,3,3,3,3.3];
-let mydata <- data.frame(a, b,c, d);
+let e <- [3,3,3,3,3.3];
+let f <- [3,3,3,3,3.3];
+let g <- [3,3,3,3,3.3];
+let h <- [3,3,3,3,3.3];
+let i <- [3,3,3,3,3.3];
+let j <- [3,3,3,3,3.3];
+let k <- [3,3,3,3,3.3];
+let mydata <- data.frame(a, b,c, d, e,f,g,h,i,j,k);
 
 let mlr = lm(b ~ a + c + d, mydata);
 
 print(mlr);
 
 print(mlr :> predict(mydata));
+
+
+print(lm(b ~ a + c + d +e +f +g +h +i+ j+k, mydata));
