d939660084ebdaf51eff6823bf69400ea3513cab
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-11-05 10:25:37 +0800

    run prediction test

--- Library/demo/math/lm.R
+++ Library/demo/math/lm.R
@@ -1,6 +1,8 @@
-let a <- [1,2,3,4,5];
+let a <- [1,2,3.2,4,5];
 let b <- [2,4,6,8,10];
 let mydata <- data.frame(a, b);
+
+print(mydata);
 
 let w = lm(b ~ a, mydata, weights = 1 / a);
 let uw = lm(b ~ a, mydata);
@@ -11,4 +13,16 @@ print(w);
 print("unweighted:");
 print(uw);
 
-print(as.formula(uw));
+# print(as.formula(uw));
+
+let test = data.frame(
+	
+	a = [0.91,2,3,4,5.3],
+	x = [FALSE]
+	
+);
+
+rownames(test) = ["alpha","beta","gamma","XX","YY"];
+
+print(w :> predict(test));
+print(uw :> predict(test));
