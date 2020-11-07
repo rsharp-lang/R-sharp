1a3308babee3704972b1f0bf79c19fff927a70b4
xieguigang <xie.guigang@gcmodeller.org>
2020-11-07 18:42:27 +0800

    object buffers

---
+++ R#/Runtime/Serialize/Buffer.vb
@@ -0,0 +1,56 @@
+﻿Imports System.Drawing
+Imports System.IO
+Imports System.Runtime.CompilerServices
+Imports System.Text
+Imports Microsoft.VisualBasic.Linq
+Imports Microsoft.VisualBasic.Serialization
+
+Namespace Runtime.Serialize
+
+    Public Class Buffer : Inherits RawStream
+
+        Public Property code As BufferObjects
+        Public Property data As BufferObject
+
+        Public Shared Function ParseBuffer(raw As Stream) As Buffer
+            Dim code As BufferObjects = MeasureBufferMagic(raw)
+            Dim bufferObject As BufferObject
+            Dim data As MemoryStream = BufferObject.SubStream(raw, 4, raw.Length - 4)
+
+            Select Case code
+                Case BufferObjects.raw
+                    bufferObject = New rawBuffer With {.buffer = data}
+                Case BufferObjects.text
+                    bufferObject = New textBuffer With {.text = Encoding.UTF8.GetString(data.ToArray)}
+                Case BufferObjects.bitmap
+                    bufferObject = New bitmapBuffer With {.bitmap = Image.FromStream(data)}
+                Case BufferObjects.vector
+                    bufferObject = vectorBuffer.CreateBuffer(data)
+                Case Else
+                    Throw New NotImplementedException(code.Description)
+            End Select
+
+            Return New Buffer With {
+                .code = code,
+                .data = bufferObject
+            }
+        End Function
+
+        Public Shared Function MeasureBufferMagic(raw As Stream) As BufferObjects
+            Dim magic As Byte() = New Byte(3) {}
+
+            Call raw.Seek(Scan0, SeekOrigin.Begin)
+            Call raw.Read(magic, Scan0, magic.Length)
+
+            Return CType(BitConverter.ToInt32(magic, Scan0), BufferObjects)
+        End Function
+
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        Public Overrides Function Serialize() As Byte()
+            Return BitConverter _
+                .GetBytes(CInt(code)) _
+                .JoinIterates(data.Serialize) _
+                .ToArray
+        End Function
+    End Class
+End Namespace
--- R#/Runtime/Serialize/BufferObject.vb
+++ R#/Runtime/Serialize/BufferObject.vb
@@ -1,8 +1,18 @@
-﻿Imports Microsoft.VisualBasic.Serialization
+﻿Imports System.IO
+Imports Microsoft.VisualBasic.Serialization
 
 Namespace Runtime.Serialize
 
     Public MustInherit Class BufferObject : Inherits RawStream
 
+
+        Public Shared Function SubStream(stream As Stream, from As Long, length As Integer) As MemoryStream
+            Dim buffer As Byte() = New Byte(length - 1) {}
+
+            stream.Seek(from, SeekOrigin.Begin)
+            stream.Read(buffer, Scan0, length)
+
+            Return New MemoryStream(buffer)
+        End Function
     End Class
 End Namespace
