913411aca382ccf0027339eb23549707132829b2
xieguigang <xie.guigang@gcmodeller.org>
2020-11-07 15:59:35 +0800

    text buffer

---
+++ R#/Runtime/Serialize/textBuffer.vb
@@ -0,0 +1,21 @@
+﻿Imports Microsoft.VisualBasic.Serialization
+Imports Microsoft.VisualBasic.Text
+
+Namespace Runtime.Serialize
+
+    Public Class textBuffer : Inherits RawStream
+
+        Public Property text As String
+
+        Sub New()
+        End Sub
+
+        Sub New(raw As Byte())
+            text = Encodings.UTF8.CodePage.GetString(raw)
+        End Sub
+
+        Public Overrides Function Serialize() As Byte()
+            Return Encodings.UTF8.CodePage.GetBytes(text)
+        End Function
+    End Class
+End Namespace
