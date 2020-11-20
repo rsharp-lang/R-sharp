03614111eed6567c5a8adde8f66cfd31bd99cf8a
xieguigang <xie.guigang@gcmodeller.org>
2020-11-20 20:42:16 +0800

    fix of command line arguments

--- studio/R-terminal/CLI/IPC.vb
+++ studio/R-terminal/CLI/IPC.vb
@@ -59,10 +59,8 @@ Imports IPEndPoint = Microsoft.VisualBasic.Net.IPEndPoint
 Partial Module CLI
 
     <ExportAPI("--slave")>
-    <Description("Run the specific R# script file, and then post back the result data json to the specific master listener.")>
     <Usage("--slave /exec <script.R> /args <json_base64> /request-id <request_id> /PORT=<port_number> [/timeout=<timeout in ms, default=1000> /retry=<retry_times, default=5> /MASTER=<ip, default=localhost> /entry=<function_name, default=NULL>]")>
     <Description("Create a R# cluster node for run background or parallel task. This IPC command will run a R# script file that specified by the ``/exec`` argument, and then post back the result data json to the specific master listener.")>
-    <Usage("--slave /exec <script.R> /args <json_base64> /request-id <request_id> /PORT=<port_number> [/MASTER=<ip, default=localhost> /entry=<function_name, default=NULL>]")>
     <Argument("/exec", False, CLITypes.File, AcceptTypes:={GetType(String)}, Extensions:="*.R", Description:="a specific R# script for run")>
     <Argument("/args", False, CLITypes.Base64, PipelineTypes.std_in,
               AcceptTypes:={GetType(Dictionary(Of String, String))},
@@ -79,6 +77,9 @@ Partial Module CLI
     <Argument("/PORT", False, CLITypes.Integer,
               AcceptTypes:={GetType(Integer)},
               Description:="the port number for master node listen to this callback post data.")>
+    <Argument("/retry", False, CLITypes.Integer,
+              AcceptTypes:={GetType(Integer)},
+              Description:="How many times that this cluster node should retry to send callback data if the TCP request timeout.")>
     Public Function slaveMode(args As CommandLine) As Integer
         Dim script As String = args <= "/exec"
         Dim arguments As Dictionary(Of String, String()) = args("/args") _
