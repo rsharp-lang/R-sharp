97d2ad4a9bfe3b02a4d08dcf2b7095941868f960
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-11-30 17:23:27 +0800

    R# package description file

---
+++ R#/System/Package/PackageFile/DESCRIPTION.vb
@@ -0,0 +1,19 @@
+﻿Namespace System.Package.File
+
+    Public Class DESCRIPTION
+
+' Package: biodeepMSMS
+' Type: Package
+' Title: biodeep MS/MS alignment and Build internal reference database
+' Version: 4.133.718.669
+' Date: 2019-09-20
+' Author: xieguigang
+' Maintainer: xieguigang <gg.xie@bionovogene.com>
+' Description: biodeep MS/MS alignment and Build internal reference database,
+    ' contains internal database: metlin, monoall LC_MS databases.
+' License: bionovogene
+' RoxygenNote: 7.1.1
+
+
+    End Class
+End Namespace
