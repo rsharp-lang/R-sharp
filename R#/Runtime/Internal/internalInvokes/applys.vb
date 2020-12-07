83c7655bb642faa9cc8d33f5705b6ecda08ff525
xieguigang <xie.guigang@gcmodeller.org>
2020-12-07 16:41:45 +0800

    fix of null reference errors

--- R#/Runtime/Internal/internalInvokes/applys.vb
+++ R#/Runtime/Internal/internalInvokes/applys.vb
@@ -110,7 +110,7 @@ Namespace Runtime.Internal.Invokes
         <ExportAPI("apply")>
         Public Function apply(x As Object, margin As margins, FUN As Object, Optional env As Environment = Nothing) As Object
             If x Is Nothing Then
-                Return x
+                Return New Object() {}
             ElseIf TypeOf x Is dataframe Then
                 Return doApply.apply(DirectCast(x, dataframe), margin, FUN, env)
             Else
@@ -124,6 +124,10 @@ Namespace Runtime.Internal.Invokes
         ''' <returns></returns>
         <ExportAPI("parSapply")>
         Public Function parSapply(<RRawVectorArgument> X As Object, FUN As Object, envir As Environment) As Object
+            If X Is Nothing Then
+                Return New Object() {}
+            End If
+
             Dim check = checkInternal(X, FUN, envir)
 
             If Not TypeOf check Is Boolean Then
@@ -216,6 +220,10 @@ Namespace Runtime.Internal.Invokes
         <ExportAPI("sapply")>
         <RApiReturn(GetType(vector))>
         Public Function sapply(<RRawVectorArgument> X As Object, FUN As Object, envir As Environment) As Object
+            If X Is Nothing Then
+                Return New Object() {}
+            End If
+
             Dim check = checkInternal(X, FUN, envir)
 
             If Not TypeOf check Is Boolean Then
@@ -288,6 +296,10 @@ Namespace Runtime.Internal.Invokes
         Public Function lapply(<RRawVectorArgument> X As Object, FUN As Object,
                                Optional names As RFunction = Nothing,
                                Optional envir As Environment = Nothing) As Object
+
+            If X Is Nothing Then
+                Return New list With {.slots = New Dictionary(Of String, Object)}
+            End If
 
             Dim check = checkInternal(X, FUN, envir)
 
