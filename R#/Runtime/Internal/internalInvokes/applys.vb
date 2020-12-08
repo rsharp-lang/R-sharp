016f5c15466f83609b9727e0080660f3c28934a1
xieguigang <xie.guigang@gcmodeller.org>
2020-12-08 14:34:58 +0800

    try to measure vector element type

--- R#/Runtime/Internal/internalInvokes/applys.vb
+++ R#/Runtime/Internal/internalInvokes/applys.vb
@@ -1,43 +1,43 @@
 ﻿#Region "Microsoft.VisualBasic::309076b0b5f08cb0dc6ad696062789e3, R#\Runtime\Internal\internalInvokes\applys.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Module applys
-    ' 
-    '         Function: apply, checkInternal, (+2 Overloads) keyNameAuto, lapply, parSapply
-    '                   sapply
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Module applys
+' 
+'         Function: apply, checkInternal, (+2 Overloads) keyNameAuto, lapply, parSapply
+'                   sapply
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -49,6 +49,7 @@ Imports Microsoft.VisualBasic.Emit.Delegates
 Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Linq
 Imports SMRUCC.Rsharp.Interpreter
+Imports SMRUCC.Rsharp.Runtime.Components
 Imports SMRUCC.Rsharp.Runtime.Components.Interface
 Imports SMRUCC.Rsharp.Runtime.Internal.Object
 Imports SMRUCC.Rsharp.Runtime.Internal.Object.Converts
@@ -253,7 +254,16 @@ Namespace Runtime.Internal.Invokes
                     names.Add(Scripting.ToString(key))
                 Next
 
-                Return New RObj.vector(names, seq.ToArray, envir)
+                Dim a As Object = TryCastGenericArray(seq.ToArray, envir)
+                Dim type As Type
+
+                If TypeOf a Is Message Then
+                    type = GetType(Object)
+                Else
+                    type = a.GetType.GetElementType
+                End If
+
+                Return New RObj.vector(names, DirectCast(a, Array), RType.GetRSharpType(type), envir)
             Else
                 Dim seq As New List(Of Object)
                 Dim value As Object
@@ -270,7 +280,16 @@ Namespace Runtime.Internal.Invokes
                     End If
                 Next
 
-                Return New RObj.vector With {.data = seq.ToArray}
+                Dim a As Object = TryCastGenericArray(seq.ToArray, envir)
+                Dim type As Type
+
+                If TypeOf a Is Message Then
+                    type = GetType(Object)
+                Else
+                    type = a.GetType.GetElementType
+                End If
+
+                Return New RObj.vector(DirectCast(a, Array), RType.GetRSharpType(type))
             End If
         End Function
 
