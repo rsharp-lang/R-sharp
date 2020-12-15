6f2e7d61fbafd591ec4c68656bd0060c3610ff16
xieguigang <xie.guigang@gcmodeller.org>
2020-12-15 19:59:08 +0800

    json to expression model

--- R#/System/Package/PackageFile/Expression/RExpression.vb
+++ R#/System/Package/PackageFile/Expression/RExpression.vb
@@ -1,4 +1,7 @@
-﻿Imports SMRUCC.Rsharp.Interpreter.ExecuteEngine
+﻿Imports Microsoft.VisualBasic.Language
+Imports SMRUCC.Rsharp.Interpreter.ExecuteEngine
+Imports SMRUCC.Rsharp.Interpreter.ExecuteEngine.ExpressionSymbols
+Imports SMRUCC.Rsharp.Interpreter.ExecuteEngine.ExpressionSymbols.DataSets
 Imports SMRUCC.Rsharp.Runtime.Components
 
 Namespace System.Package.File.Expressions
@@ -18,6 +21,19 @@ Namespace System.Package.File.Expressions
         Public Property [module] As String
 
         Public Overrides Function GetExpression() As Expression
+            Dim names As Expression() = packages _
+                .Select(Function(name) New Literal(name)) _
+                .ToArray
+
+            If [module].StringEmpty Then
+                ' require
+                Return New Require(names)
+            Else
+                Return New [Imports](New VectorLiteral(names), New Literal([module]))
+            End If
+        End Function
+
+        Public Shared Function GetRImports(require As Require) As [Variant](Of RImports, Message)
 
         End Function
     End Class
