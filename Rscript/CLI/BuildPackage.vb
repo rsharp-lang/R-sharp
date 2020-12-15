71da53f083f96529ee3ed22383dde109577c3c8b
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-12-15 14:48:07 +0800

    how to build package?

---
+++ Rscript/CLI/BuildPackage.vb
@@ -0,0 +1,12 @@
+﻿Module BuildPackage
+
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <param name="dir">the R source input directory</param>
+    ''' <param name="output">the R package output file path</param>
+    ''' <returns></returns>
+    Public Function BuildPackage(dir As String, output As String)
+
+    End Function
+End Module
