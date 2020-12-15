a9b8932051da782101b908b03268523aa3eddb2d
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-12-15 15:05:37 +0800

    cli for build R package

---
+++ Rscript/RSystem/BuildPackage.vb
@@ -0,0 +1,17 @@
+﻿Imports System.Runtime.CompilerServices
+Imports SMRUCC.Rsharp.System.Package.File
+
+Module BuildPackagePipeline
+
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <param name="dir">the R source input directory</param>
+    ''' <param name="output">the R package output file path</param>
+    ''' <returns></returns>
+    ''' 
+    <Extension>
+    Public Function BuildPackage(meta As DESCRIPTION, dir As String, output As String) As Boolean
+
+    End Function
+End Module
