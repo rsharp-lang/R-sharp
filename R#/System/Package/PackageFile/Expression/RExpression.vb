0ff92ffd9a8b1fe9e178c336ed820fee5b92a9c0
xieguigang <xie.guigang@gcmodeller.org>
2020-12-15 19:08:17 +0800

    package file model

---
+++ R#/System/Package/PackageFile/Expression/RExpression.vb
@@ -0,0 +1,6 @@
+﻿Namespace System.Package.File.Expression
+
+    Public Class RExpression
+
+    End Class
+End Namespace
--- R#/System/Package/PackageFile/PackageModel.vb
+++ R#/System/Package/PackageFile/PackageModel.vb
@@ -1,54 +1,61 @@
 ﻿#Region "Microsoft.VisualBasic::93db2c490af0bc77a91e90860a42ac58, R#\System\Package\PackageFile\PackageModel.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Class PackageModel
-    ' 
-    '         Properties: info
-    ' 
-    '         Sub: Flush
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Class PackageModel
+' 
+'         Properties: info
+' 
+'         Sub: Flush
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.IO
+Imports SMRUCC.Rsharp.System.Package.File.Expression
 
 Namespace System.Package.File
 
     Public Class PackageModel
 
         Public Property info As DESCRIPTION
+
+        ''' <summary>
+        ''' only allows function and constant.
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property symbols As Dictionary(Of String, RExpression)
 
         Public Sub Flush(outfile As Stream)
 
