8a70b3344353297a57b74009b8d56dd4bfc5fc87
xieguigang <xie.guigang@gcmodeller.org>
2020-12-17 15:30:50 +0800

    fix of missing types

--- R#/Runtime/Internal/internalInvokes/string.vb
+++ R#/Runtime/Internal/internalInvokes/string.vb
@@ -1,57 +1,58 @@
 ﻿#Region "Microsoft.VisualBasic::8fec7243e2aba824dfaf581468292b1f, R#\Runtime\Internal\internalInvokes\string.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Module stringr
-    ' 
-    '         Function: [string], base64, base64Decode, bencode, chr
-    '                   Csprintf, decodeObject, fromBstring, grep, html
-    '                   json, match, nchar, paste, regexp
-    '                   sprintfSingle, str_empty, str_pad, str_replace, strPad_internal
-    '                   strsplit, tagvalue, tolower, urldecode, xml
-    ' 
-    '     Enum str_padSides
-    ' 
-    '         both, left, right
-    ' 
-    '  
-    ' 
-    ' 
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Module stringr
+' 
+'         Function: [string], base64, base64Decode, bencode, chr
+'                   Csprintf, decodeObject, fromBstring, grep, html
+'                   json, match, nchar, paste, regexp
+'                   sprintfSingle, str_empty, str_pad, str_replace, strPad_internal
+'                   strsplit, tagvalue, tolower, urldecode, xml
+' 
+'     Enum str_padSides
+' 
+'         both, left, right
+' 
+'  
+' 
+' 
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
+Imports System.Drawing
 Imports System.IO
 Imports System.Reflection
 Imports System.Runtime.CompilerServices
@@ -215,7 +216,21 @@ Namespace Runtime.Internal.Invokes
                 GetType(Dictionary(Of String, Object)),
                 GetType(Integer()),
                 GetType(Boolean()),
-                GetType(String())
+                GetType(String()),
+                GetType(Double),
+                GetType(Double()),
+                GetType(Single),
+                GetType(Single()),
+                GetType(Date),
+                GetType(Date()),
+                GetType(Long),
+                GetType(Long()),
+                GetType(Byte),
+                GetType(Byte()),
+                GetType(Short),
+                GetType(Short()),
+                GetType(Color),
+                GetType(Color())
             }
 
             Try
