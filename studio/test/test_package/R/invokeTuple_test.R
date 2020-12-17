9a1245455483b86b9bb0dffa52629d36c01e21a6
xieguigang <xie.guigang@gcmodeller.org>
2020-12-17 22:16:00 +0800

    tuple function test success

---
+++ studio/test/test_package/R/invokeTuple_test.R
@@ -0,0 +1,15 @@
+
+let run_tuple as function() {
+	let add1 = x -> x + 1;
+	let add2 = x -> x + 2;
+	let add5 = x -> x + 5;
+	let addWith as function(x, y) {x + y};
+
+	let x as integer <- [99,8,5, 11];
+	let [a,b,c,d] = x :> [add1, add2, add5, addWith(9)];
+
+	print(a);
+	print(b);
+	print(c);
+	print(d);
+}
