804926dbf4cfcca5c5fe74adb0c1946be539eec9
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-12-18 09:29:40 +0800

    fix of missing expression type

--- R#/Runtime/Serialize/bufferObjects/messageBuffer.vb
+++ R#/Runtime/Serialize/bufferObjects/messageBuffer.vb
@@ -1,53 +1,53 @@
 ﻿#Region "Microsoft.VisualBasic::b4350e904b495bdb8965b8ba21fa685e, R#\Runtime\Serialize\bufferObjects\messageBuffer.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Class messageBuffer
-    ' 
-    '         Properties: code, environmentStack, level, message, source
-    '                     trace
-    ' 
-    '         Constructor: (+2 Overloads) Sub New
-    '         Function: CreateBuffer, GetErrorMessage, Serialize
-    '         Class TextExpression
-    ' 
-    '             Properties: text, type
-    ' 
-    '             Function: Evaluate, ToString
-    ' 
-    ' 
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Class messageBuffer
+' 
+'         Properties: code, environmentStack, level, message, source
+'                     trace
+' 
+'         Constructor: (+2 Overloads) Sub New
+'         Function: CreateBuffer, GetErrorMessage, Serialize
+'         Class TextExpression
+' 
+'             Properties: text, type
+' 
+'             Function: Evaluate, ToString
+' 
+' 
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -59,6 +59,7 @@ Imports Microsoft.VisualBasic.Serialization
 Imports Microsoft.VisualBasic.Text
 Imports SMRUCC.Rsharp.Interpreter.ExecuteEngine
 Imports SMRUCC.Rsharp.Runtime.Components
+Imports SMRUCC.Rsharp.System.Package.File
 
 Namespace Runtime.Serialize
 
@@ -92,6 +93,12 @@ Namespace Runtime.Serialize
             Public Overrides ReadOnly Property type As TypeCodes
                 Get
                     Return TypeCodes.string
+                End Get
+            End Property
+
+            Public Overrides ReadOnly Property expressionName As ExpressionTypes
+                Get
+                    Return ExpressionTypes.Literal
                 End Get
             End Property
 
