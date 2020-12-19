d4a93b2003e7d12126f793af703c5b0a77fc655a
xieguigang <xie.guigang@gcmodeller.org>
2020-12-19 20:35:24 +0800

    add package test

---
+++ studio/test/packageTest.R
@@ -0,0 +1,3 @@
+# R# "E:\GCModeller\src\R-sharp\studio\test\packageTest.R" --attach "E:\GCModeller\src\R-sharp\studio\test\LipidSearch_1.0.0.1254.zip"
+
+hello_world(["A","AABBCCDD","!!!!","world","ADDDDD"]); 
--- studio/test/test_package/R/binaryparserTest.R
+++ studio/test/test_package/R/binaryparserTest.R
@@ -1,7 +1,12 @@
 imports "package_utils" from "devkit";
 
-const expression = readBin(`${!script$dir}/../../LipidSearch_1.0.0.1254/src/8873003873514dda0c83f683dfa27a1d`)
-:> read
-:> eval(globalenv())
-:> print
-;
+let function_not_run as function() {
+	if (FALSE) {
+		const expression = readBin(`${!script$dir}/../../LipidSearch_1.0.0.1254/src/8873003873514dda0c83f683dfa27a1d`)
+		:> read
+		:> eval(globalenv())
+		:> print
+		;	
+	}
+}
+
