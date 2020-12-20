b0656a645afb16b7dbb003cf16cbaa786de07f90
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-12-20 09:11:49 +0800

    code style improvements

--- R#/System/Package/PackageFile/PackageLoader.vb
+++ R#/System/Package/PackageFile/PackageLoader.vb
@@ -1,49 +1,50 @@
 ﻿#Region "Microsoft.VisualBasic::c887fa840d8fcf3c634c21f97be655aa, R#\System\Package\PackageFile\PackageLoader.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Module PackageLoader2
-    ' 
-    '         Function: GetPackageDirectory
-    ' 
-    '         Sub: LoadPackage
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Module PackageLoader2
+' 
+'         Function: GetPackageDirectory
+' 
+'         Sub: LoadPackage
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.IO
 Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.Serialization.JSON
 Imports SMRUCC.Rsharp.Interpreter.ExecuteEngine.ExpressionSymbols.Closure
 Imports SMRUCC.Rsharp.Runtime
@@ -78,11 +79,26 @@ Namespace System.Package.File
                 End Using
             Next
 
+            Call env.callOnLoad(dir, meta)
+        End Sub
+
+        <Extension>
+        Private Sub loadDependency(env As GlobalEnvironment, dir As String, meta As DESCRIPTION)
+
+        End Sub
+
+        <Extension>
+        Private Sub callOnLoad(env As GlobalEnvironment, dir As String, meta As DESCRIPTION)
             Dim onLoad As String = $"{dir}/.onload"
 
             If onLoad.FileExists Then
                 Using bin As New BinaryReader(onLoad.Open)
-                    Call DirectCast(BlockReader.Read(bin).Parse(desc:=meta).Evaluate(env), DeclareNewFunction).Invoke(env, params:={})
+                    Call BlockReader.Read(bin) _
+                        .Parse(desc:=meta) _
+                        .DoCall(Function(func)
+                                    Return DirectCast(func, DeclareNewFunction)
+                                End Function) _
+                        .Invoke(env, params:={})
                 End Using
             End If
         End Sub
---
+++ studio/test/load_test2.R
@@ -0,0 +1,5 @@
+imports "package_utils" from "devkit";
+
+const test_package as string = "F:\LipidSearch_1.0.0.1254";
+
+print(loadPackage(test_package));
