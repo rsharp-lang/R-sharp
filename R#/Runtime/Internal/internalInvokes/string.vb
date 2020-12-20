6d5416cbf2dc418bd2552138881348bd4c7ad7e9
xieguigang <xie.guigang@gcmodeller.org>
2020-12-20 18:59:54 +0800

    load xml auto

--- R#/Runtime/Internal/internalInvokes/string.vb
+++ R#/Runtime/Internal/internalInvokes/string.vb
@@ -1,54 +1,54 @@
 ﻿#Region "Microsoft.VisualBasic::c875262996c3a9cf995c52bed38bab45, R#\Runtime\Internal\internalInvokes\string.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Module stringr
-    ' 
-    '         Function: [string], base64, base64Decode, bencode, chr
-    '                   Csprintf, decodeObject, fromBstring, grep, html
-    '                   json, match, nchar, paste, regexp
-    '                   sprintfSingle, str_empty, str_pad, str_replace, strPad_internal
-    '                   strsplit, tagvalue, tolower, urldecode, xml
-    ' 
-    '     Enum str_padSides
-    ' 
-    '         both, left, right
-    ' 
-    '  
-    ' 
-    ' 
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Module stringr
+' 
+'         Function: [string], base64, base64Decode, bencode, chr
+'                   Csprintf, decodeObject, fromBstring, grep, html
+'                   json, match, nchar, paste, regexp
+'                   sprintfSingle, str_empty, str_pad, str_replace, strPad_internal
+'                   strsplit, tagvalue, tolower, urldecode, xml
+' 
+'     Enum str_padSides
+' 
+'         both, left, right
+' 
+'  
+' 
+' 
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -59,6 +59,7 @@ Imports System.Runtime.CompilerServices
 Imports System.Text
 Imports System.Text.RegularExpressions
 Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.ComponentModel
 Imports Microsoft.VisualBasic.ComponentModel.Collection
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports Microsoft.VisualBasic.Language.C
@@ -107,6 +108,17 @@ Namespace Runtime.Internal.Invokes
                 Return printer.getStrings(x, Nothing, env.globalEnvironment).ToArray
             Else
                 Return printer.ToString(x.GetType, env.globalEnvironment, True, False)(x)
+            End If
+        End Function
+
+        <ExportAPI("loadXml")>
+        Public Function loadXml(file As String, Optional env As Environment = Nothing) As Object
+            Dim type = DigitalSignature.GetModelInfo(file)
+
+            If type Is Nothing Then
+                Return Internal.debug.stop(New NotImplementedException(), env)
+            Else
+                Return file.LoadXml(type.GetType(True))
             End If
         End Function
 
