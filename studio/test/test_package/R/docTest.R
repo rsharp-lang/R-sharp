f92060c7a43dae94a89b72bbd7e60af823c4e6c1
xieguigang <xie.guigang@gcmodeller.org>
2020-12-26 20:56:48 +0800

    Create docTest.R

---
+++ studio/test/test_package/R/docTest.R
@@ -0,0 +1,20 @@
+
+#' test function title
+#' 
+#' @description test description with
+#'    multiple lines
+#' 
+#' @param a a string parameter
+#' @param a integer vector with default value \code{[1,2,3]}
+#' 
+#' @return nothing?
+#' 
+#' @author xieguigang
+#' @details this is the remark text about this
+#' function
+#' 
+#' @keywords keyword1,keyword2,keyword3
+#' 
+let func as function(a as string, b = [1,2,3]) {
+
+}
