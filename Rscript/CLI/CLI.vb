fb397bf844aa6e1662147950f314b9876840d122
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-12-31 10:09:52 +0800

    build package from current directory

--- Rscript/CLI/CLI.vb
+++ Rscript/CLI/CLI.vb
@@ -53,13 +53,13 @@ Imports RProgram = SMRUCC.Rsharp.Interpreter.Program
 
     <ExportAPI("--build")>
     <Description("build R# package")>
-    <Usage("--build /src <folder> [/save <Rpackage.zip>]")>
+    <Usage("--build [/src <folder, default=./> /save <Rpackage.zip>]")>
     <Argument("/src", False, CLITypes.File, PipelineTypes.std_in,
               AcceptTypes:={GetType(String)},
               Description:="A folder path that contains the R source files and meta data files of the target R package, 
               a folder that exists in this folder path which is named 'R' is required!")>
     Public Function Compile(args As CommandLine) As Integer
-        Dim src$ = args <= "/src"
+        Dim src$ = args("/src") Or App.CurrentDirectory
         Dim meta As DESCRIPTION = DESCRIPTION.Parse($"{src}/DESCRIPTION")
         Dim save$ = args("/save") Or $"{src}/../{meta.Package}_{meta.Version}.zip"
 
