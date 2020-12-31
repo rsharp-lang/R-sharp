2ca87442171c98afca9d78399ebdebf04c6aa199
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-12-31 18:16:23 +0800

    get quantile levels

--- Library/R.math/stats.vb
+++ Library/R.math/stats.vb
@@ -1,52 +1,52 @@
 ﻿#Region "Microsoft.VisualBasic::c92b87e4292a8a3299af68a39d3850ce, Library\R.math\stats.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module stats
-    ' 
-    '     Constructor: (+1 Overloads) Sub New
-    '     Function: asDist, corr, dataframeRow, dist, fisher_test
-    '               matrixDataFrame, prcomp, printMatrix, printTtest, printTwoSampleTTest
-    '               spline, tabulateMode, ttest
-    ' 
-    ' Enum SplineAlgorithms
-    ' 
-    '     Bezier, BSpline, CatmullRom, CubiSpline
-    ' 
-    '  
-    ' 
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Module stats
+' 
+'     Constructor: (+1 Overloads) Sub New
+'     Function: asDist, corr, dataframeRow, dist, fisher_test
+'               matrixDataFrame, prcomp, printMatrix, printTtest, printTwoSampleTTest
+'               spline, tabulateMode, ttest
+' 
+' Enum SplineAlgorithms
+' 
+'     Bezier, BSpline, CatmullRom, CubiSpline
+' 
+'  
+' 
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -60,6 +60,7 @@ Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.Math.DataFrame
 Imports Microsoft.VisualBasic.Math.Distributions
 Imports Microsoft.VisualBasic.Math.LinearAlgebra.Prcomp
+Imports Microsoft.VisualBasic.Math.Quantile
 Imports Microsoft.VisualBasic.Math.Statistics.Hypothesis
 Imports Microsoft.VisualBasic.Math.Statistics.Hypothesis.FishersExact
 Imports Microsoft.VisualBasic.Scripting.MetaData
@@ -326,6 +327,21 @@ Module stats
         Dim cor As CorrelationMatrix = rows.Correlation(spearman)
 
         Return cor
+    End Function
+
+    ''' <summary>
+    ''' get quantile levels
+    ''' </summary>
+    ''' <param name="q"></param>
+    ''' <param name="level"></param>
+    ''' <param name="env"></param>
+    ''' <returns></returns>
+    <ExportAPI("level")>
+    Public Function getQuantileLevels(q As QuantileQuery, <RRawVectorArgument> level As Object, Optional env As Environment = Nothing) As Object
+        Dim levels As Double() = REnv.asVector(Of Double)(level)
+        Dim result As Double() = levels.Select(AddressOf q.Query).ToArray
+
+        Return result
     End Function
 
     ''' <summary>
