062196a414b3864a52f9e42aa674139ca597419b
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2021-01-12 17:31:27 +0800

    parallel by individual process?

---
+++ studio/Rsharp_kit/parallel/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' 有关程序集的一般信息由以下
+' 控制。更改这些特性值可修改
+' 与程序集关联的信息。
+
+'查看程序集特性的值
+
+<Assembly: AssemblyTitle("parallel library for R# HPC on linux")>
+<Assembly: AssemblyDescription("parallel library for R# HPC on linux")>
+<Assembly: AssemblyCompany("i@xieguigang.me")>
+<Assembly: AssemblyProduct("parallel")>
+<Assembly: AssemblyCopyright("Copyright © R#Studio 2021")>
+<Assembly: AssemblyTrademark("sciBASIC")>
+
+<Assembly: ComVisible(False)>
+
+'如果此项目向 COM 公开，则下列 GUID 用于 typelib 的 ID
+<Assembly: Guid("a666c63d-2b2a-4977-ad4b-d596151662dc")>
+
+' 程序集的版本信息由下列四个值组成: 
+'
+'      主版本
+'      次版本
+'      生成号
+'      修订号
+'
+'可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值
+'通过使用 "*"，如下所示:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ studio/Rsharp_kit/parallel/SlaveTask.vb
@@ -0,0 +1,33 @@
+﻿Imports System.IO
+Imports Microsoft.VisualBasic
+
+Public Class SlaveTask
+
+    Dim toBuffers As New Dictionary(Of Type, Func(Of Object, Stream))
+    Dim fromBuffer As New Dictionary(Of Type, Func(Of Stream, Object))
+
+    Sub New()
+    End Sub
+
+    Public Function Emit(Of T)(streamAs As Func(Of T, Stream)) As SlaveTask
+        toBuffers(GetType(T)) = Function(obj) streamAs(obj)
+        Return Me
+    End Function
+
+    Public Function Emit(Of T)(fromStream As Func(Of Stream, T)) As SlaveTask
+        fromBuffer(GetType(T)) = Function(buf) fromStream(buf)
+        Return Me
+    End Function
+
+    Public Function RunTask(entry As [Delegate], ParamArray parameters As Object()) As Object
+
+    End Function
+
+End Class
+
+Public Class IDelegate
+
+    Public Property name As String
+    Public Property type As typeinfo
+
+End Class
