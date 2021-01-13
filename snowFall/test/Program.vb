265957777a7c4dc6637594806234bd69f8b97a6d
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2021-01-13 13:03:17 +0800

    fix of demo test

--- snowFall/test/Program.vb
+++ snowFall/test/Program.vb
@@ -4,12 +4,38 @@ Imports snowFall.Protocol
 Module Program
 
     Sub Main()
-        Dim list As Integer() = New SlaveTask(Host.CreateProcessor, AddressOf Host.SlaveTask).RunTask(New Func(Of Integer, Integer, Integer())(AddressOf demoTask), {5, 2})
+        Dim taskApi As New Func(Of integerValue, integerValue, integerValue())(AddressOf demoTask)
+        Dim list As integerValue = New SlaveTask(Host.CreateProcessor, AddressOf Host.SlaveTask, 1802).RunTask(taskApi, CType(5, integerValue), CType(2, integerValue))
 
         Pause()
     End Sub
 
-    Public Function demoTask(a As Integer, b As Integer) As Integer()
+    Public Function demoTask(a As integerValue, b As integerValue) As integerValue()
         Return {a + b, b ^ 5}
     End Function
 End Module
+
+Public Class integerValue
+
+    Public Property vector As Integer()
+
+    Public Shared Widening Operator CType(i As Integer) As integerValue
+        Return New integerValue() With {.vector = {i}}
+    End Operator
+
+    Public Shared Operator +(a As integerValue, b As integerValue) As integerValue
+        Return New integerValue With {
+            .vector = a.vector _
+                .Select(Function(x, i) x + b.vector(i)) _
+                .ToArray
+        }
+    End Operator
+
+    Public Shared Operator ^(x As integerValue, p As Double) As integerValue
+        Return New integerValue With {
+            .vector = x.vector _
+                .Select(Function(xi) CInt(xi ^ p)) _
+                .ToArray
+        }
+    End Operator
+End Class
