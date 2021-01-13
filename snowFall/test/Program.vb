9b27404f1bb7e850ebd42eb036a535481bc8a7a4
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2021-01-13 11:13:18 +0800

    add demo test

--- snowFall/test/Program.vb
+++ snowFall/test/Program.vb
@@ -1,6 +1,15 @@
-﻿Module Program
+﻿Imports Parallel
+Imports snowFall.Protocol
+
+Module Program
 
     Sub Main()
+        Dim list As Integer() = New SlaveTask(Host.CreateProcessor, AddressOf Host.SlaveTask).RunTask(New Func(Of Integer, Integer, Integer())(AddressOf demoTask), {5, 2})
 
+        Pause()
     End Sub
+
+    Public Function demoTask(a As Integer, b As Integer) As Integer()
+        Return {a + b, b ^ 5}
+    End Function
 End Module
