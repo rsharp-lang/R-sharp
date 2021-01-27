131e7800bb96eb30b89ba0829dd030247b975ae2
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2021-01-27 09:34:16 +0800

    Update Program.vb

--- snowFall/test/Program.vb
+++ snowFall/test/Program.vb
@@ -1,54 +1,55 @@
 ﻿#Region "Microsoft.VisualBasic::021a8a46ab096044efab303cf60fb77b, snowFall\test\Program.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module Program
-    ' 
-    '     Function: demoTask, populate
-    ' 
-    '     Sub: Main
-    ' 
-    ' Class integerValue
-    ' 
-    '     Properties: vector
-    '     Operators: ^, +
-    ' 
-    ' /********************************************************************************/
+' Module Program
+' 
+'     Function: demoTask, populate
+' 
+'     Sub: Main
+' 
+' Class integerValue
+' 
+'     Properties: vector
+'     Operators: ^, +
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.Threading
 Imports Microsoft.VisualBasic.Serialization.JSON
 Imports Parallel
+Imports Parallel.IpcStream
 Imports snowFall.Protocol
 
 Module Program
@@ -64,8 +65,30 @@ Module Program
         Throw New InvalidProgramException($"test error message: '{message}'")
     End Function
 
-    Sub Main()
+    Sub serializeTest()
+        Try
+            Try
+                Try
+                    Call createErrorMessage("12345")
+                Catch ex As Exception
+                    Throw New SyntaxErrorException("bbbbb", ex)
+                End Try
+            Catch ex As Exception
+                Throw New NotImplementedException("aaaaaa", ex)
+            End Try
+        Catch ex As Exception
+            Dim err As New IPCError(ex)
 
+            For Each line As String In err.GetAllErrorMessages
+                Call Console.WriteLine(line)
+            Next
+
+            Pause()
+        End Try
+    End Sub
+
+    Sub Main()
+        ' Call serializeTest()
         Call ErrorTest()
 
         Dim taskApi As New Func(Of integerValue, integerValue, integerValue())(AddressOf demoTask)
