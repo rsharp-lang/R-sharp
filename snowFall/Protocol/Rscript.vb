4e9bd011cb95fd3c5a54a790d30caa516a8bb8bc
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2021-01-27 10:47:21 +0800

    update source file module

--- snowFall/Protocol/Rscript.vb
+++ snowFall/Protocol/Rscript.vb
@@ -1,46 +1,3 @@
-﻿#Region "Microsoft.VisualBasic::555d831911a6722cece6826fc52c265f, snowFall\Protocol\Rscript.vb"
-
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
-
-
-
-    ' /********************************************************************************/
-
-    ' Summaries:
-
-    ' Class Rscript
-    ' 
-    '     Constructor: (+1 Overloads) Sub New
-    '     Function: FromEnvironment
-    ' 
-    ' 
-    ' /********************************************************************************/
-
-#End Region
-
 Imports System.Runtime.CompilerServices
 Imports System.Text
 Imports Microsoft.VisualBasic.CommandLine
@@ -54,11 +11,11 @@ Imports Microsoft.VisualBasic.ApplicationServices
 '  // 
 '  // R# scripting host
 '  // 
-'  // VERSION:   1.99.7683.19270
-'  // ASSEMBLY:  Rscript, Version=1.99.7683.19270, Culture=neutral, PublicKeyToken=null
+'  // VERSION:   1.99.7697.19371
+'  // ASSEMBLY:  Rscript, Version=1.99.7697.19371, Culture=neutral, PublicKeyToken=null
 '  // COPYRIGHT: Copyright (c) SMRUCC genomics  2020
 '  // GUID:      16d477b1-e7fb-41eb-9b61-7ea75c5d2939
-'  // BUILT:     1/13/2021 10:42:20 AM
+'  // BUILT:     1/27/2021 10:45:42 AM
 '  // 
 ' 
 ' 
@@ -260,3 +217,6 @@ Return CLI.ToString()
 End Function
 End Class
 End Namespace
+
+
+
