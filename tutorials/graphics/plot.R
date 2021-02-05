f3ecfc236537f691afc6e8f04cafc98865487842
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2021-02-05 22:58:59 +0800

    update syntax demo test

--- tutorials/graphics/plot.R
+++ tutorials/graphics/plot.R
@@ -1,7 +1,5 @@
-imports "plot.charts" from "R.plot.dll";
+imports "charts" from "R.plot";
 
-setwd(!script$dir);
-
-plot(x -> x ^2 , x= 1: 16 step 0.5)
-:> save.graphics(file = "demo-math-plot.png")
-;
+bitmap(file = `${dirname(@script)}/demo-math-plot.png`) {
+	plot(x -> x ^2 , x= 1: 16 step 0.5);
+}
