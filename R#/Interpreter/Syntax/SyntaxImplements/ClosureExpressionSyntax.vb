4016ad5504915374137baecece4d514cc7fbb9bc
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2021-02-05 22:47:21 +0800

    bugs fixed of parse closure

--- R#/Interpreter/Syntax/SyntaxImplements/ClosureExpressionSyntax.vb
+++ R#/Interpreter/Syntax/SyntaxImplements/ClosureExpressionSyntax.vb
@@ -1,47 +1,48 @@
 ﻿#Region "Microsoft.VisualBasic::09b727a138692a4fe294e91f7d6e8846, R#\Interpreter\Syntax\SyntaxImplements\ClosureExpressionSyntax.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Module ClosureExpressionSyntax
-    ' 
-    '         Function: ClosureExpression
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Module ClosureExpressionSyntax
+' 
+'         Function: ClosureExpression
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports SMRUCC.Rsharp.Interpreter.ExecuteEngine
 Imports SMRUCC.Rsharp.Interpreter.ExecuteEngine.ExpressionSymbols.Closure
+Imports SMRUCC.Rsharp.Language
 Imports SMRUCC.Rsharp.Language.TokenIcer
 
 Namespace Interpreter.SyntaxParser.SyntaxImplements
@@ -58,8 +59,14 @@ Namespace Interpreter.SyntaxParser.SyntaxImplements
         ''' <returns></returns>
         Public Function ClosureExpression(tokens As IEnumerable(Of Token), opts As SyntaxBuilderOptions) As SyntaxResult
             Dim [error] As SyntaxResult = Nothing
+            Dim allTokens As Token() = tokens.ToArray
+
+            If allTokens.First = (TokenType.open, "{") AndAlso allTokens.Last = (TokenType.close, "}") AndAlso allTokens.SplitByTopLevelDelimiter(TokenType.terminator) = 1 Then
+                allTokens = allTokens.Skip(1).Take(allTokens.Length - 2).ToArray
+            End If
+
             Dim lines As Expression() = Interpreter _
-                .GetExpressions(tokens.ToArray, Nothing, Sub(ex, null) [error] = ex, opts) _
+                .GetExpressions(allTokens, Nothing, Sub(ex, null) [error] = ex, opts) _
                 .ToArray
 
             If Not [error] Is Nothing Then
