fefa3c7c4d063182c78af0b5a87a6ed114de873f
xieguigang <xie.guigang@gcmodeller.org>
2021-02-20 00:00:42 +0800

    add zip decompress

--- Library/R.base/utils/buffer.vb
+++ Library/R.base/utils/buffer.vb
@@ -1,60 +1,77 @@
 ﻿#Region "Microsoft.VisualBasic::183677a70d1a3e7139954fe5f1c74fd9, Library\R.base\utils\buffer.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module buffer
-    ' 
-    '     Function: float, numberFramework, toInteger
-    ' 
-    ' /********************************************************************************/
+' Module buffer
+' 
+'     Function: float, numberFramework, toInteger
+' 
+' /********************************************************************************/
 
 #End Region
 
+Imports System.IO
 Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Linq
+Imports Microsoft.VisualBasic.Net.Http
 Imports Microsoft.VisualBasic.Scripting.MetaData
 Imports SMRUCC.Rsharp.Runtime
 Imports SMRUCC.Rsharp.Runtime.Components
 Imports SMRUCC.Rsharp.Runtime.Interop
 Imports Rsharp = SMRUCC.Rsharp
 
+''' <summary>
+''' binary data buffer modules 
+''' </summary>
 <Package("buffer", Category:=APICategories.UtilityTools)>
 Module buffer
 
+    ''' <summary>
+    ''' apply bit convert of the bytes stream data as floats numbers
+    ''' </summary>
+    ''' <param name="stream"></param>
+    ''' <param name="networkOrder"></param>
+    ''' <param name="sizeOf"></param>
+    ''' <param name="env"></param>
+    ''' <returns></returns>
     <ExportAPI("float")>
     <RApiReturn(GetType(Double))>
-    Public Function float(<RRawVectorArgument> stream As Object, Optional networkOrder As Boolean = False, Optional sizeOf As Integer = 32, Optional env As Environment = Nothing) As Object
+    Public Function float(<RRawVectorArgument> stream As Object,
+                          Optional networkOrder As Boolean = False,
+                          Optional sizeOf As Integer = 32,
+                          Optional env As Environment = Nothing) As Object
+
         If sizeOf = 32 Then
             Return env.numberFramework(stream, networkOrder, 4, AddressOf BitConverter.ToSingle)
         ElseIf sizeOf = 64 Then
@@ -94,9 +111,20 @@ Module buffer
         End If
     End Function
 
+    ''' <summary>
+    ''' apply bit convert of the bytes stream data as integer numbers
+    ''' </summary>
+    ''' <param name="stream"></param>
+    ''' <param name="networkOrder"></param>
+    ''' <param name="sizeOf"></param>
+    ''' <param name="env"></param>
+    ''' <returns></returns>
     <ExportAPI("integer")>
     <RApiReturn(GetType(Integer))>
-    Public Function toInteger(<RRawVectorArgument> stream As Object, Optional networkOrder As Boolean = False, Optional sizeOf As Integer = 32, Optional env As Environment = Nothing) As Object
+    Public Function toInteger(<RRawVectorArgument> stream As Object,
+                              Optional networkOrder As Boolean = False,
+                              Optional sizeOf As Integer = 32,
+                              Optional env As Environment = Nothing) As Object
         If sizeOf = 32 Then
             Return env.numberFramework(stream, networkOrder, 4, AddressOf BitConverter.ToInt32)
         ElseIf sizeOf = 64 Then
@@ -105,4 +133,27 @@ Module buffer
             Return Internal.debug.stop($"the given size value '{sizeOf}' is invalid!", env)
         End If
     End Function
+
+    ''' <summary>
+    ''' zlib decompression of the raw data buffer
+    ''' </summary>
+    ''' <param name="stream"></param>
+    ''' <param name="env"></param>
+    ''' <returns></returns>
+    <ExportAPI("zlib.decompress")>
+    <RApiReturn(GetType(Byte))>
+    Public Function zlibDecompress(stream As Object, Optional env As Environment = Nothing) As Object
+        Dim buffer As [Variant](Of Byte(), Message) = Rsharp.Buffer(stream, env)
+
+        If buffer Is Nothing Then
+            Return Nothing
+        ElseIf buffer Like GetType(Message) Then
+            Return buffer.TryCast(Of Message)
+        End If
+
+        Using ms As MemoryStream = buffer.TryCast(Of Byte()).UnZipStream
+            Return ms.ToArray
+        End Using
+    End Function
+
 End Module
