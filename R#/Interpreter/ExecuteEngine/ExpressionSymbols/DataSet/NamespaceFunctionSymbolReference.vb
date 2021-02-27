1b0802b4ec450baabe445cac9c8488644bde4d8e
xieguigang <xie.guigang@gcmodeller.org>
2021-02-27 17:32:17 +0800

    bugs fixed of the namespace reference

--- R#/Interpreter/ExecuteEngine/ExpressionSymbols/DataSet/NamespaceFunctionSymbolReference.vb
+++ R#/Interpreter/ExecuteEngine/ExpressionSymbols/DataSet/NamespaceFunctionSymbolReference.vb
@@ -1,53 +1,55 @@
 ﻿#Region "Microsoft.VisualBasic::db758a4beb963d9e54b68c20f791bd00, R#\Interpreter\ExecuteEngine\ExpressionSymbols\DataSet\NamespaceFunctionSymbolReference.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Class NamespaceFunctionSymbolReference
-    ' 
-    '         Properties: [namespace], expressionName, symbol, type
-    ' 
-    '         Constructor: (+1 Overloads) Sub New
-    '         Function: Evaluate, getFuncNameSymbolText, getPackageApiImpl, ToString
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Class NamespaceFunctionSymbolReference
+' 
+'         Properties: [namespace], expressionName, symbol, type
+' 
+'         Constructor: (+1 Overloads) Sub New
+'         Function: Evaluate, getFuncNameSymbolText, getPackageApiImpl, ToString
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
+Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.ApplicationServices.Debugging.Diagnostics
+Imports SMRUCC.Rsharp.Development.Package.File
 Imports SMRUCC.Rsharp.Runtime
 Imports SMRUCC.Rsharp.Runtime.Components
 Imports SMRUCC.Rsharp.Runtime.Components.Interface
 Imports SMRUCC.Rsharp.Runtime.Interop
-Imports SMRUCC.Rsharp.Development.Package.File
 Imports RPkg = SMRUCC.Rsharp.Development.Package.Package
 
 Namespace Interpreter.ExecuteEngine.ExpressionSymbols.DataSets
@@ -56,27 +58,34 @@ Namespace Interpreter.ExecuteEngine.ExpressionSymbols.DataSets
     ''' ``namespace::function(xxx)``
     ''' </summary>
     Public Class NamespaceFunctionSymbolReference : Inherits Expression
+        Implements IRuntimeTrace
 
         Public ReadOnly Property [namespace] As String
         Public ReadOnly Property symbol As Expression
 
         Public Overrides ReadOnly Property type As TypeCodes
+            <MethodImpl(MethodImplOptions.AggressiveInlining)>
             Get
                 Return TypeCodes.closure
             End Get
         End Property
 
         Public Overrides ReadOnly Property expressionName As ExpressionTypes
+            <MethodImpl(MethodImplOptions.AggressiveInlining)>
             Get
                 Return ExpressionTypes.SymbolNamespaceReference
             End Get
         End Property
 
-        Public Sub New(namespace$, symbol As Expression)
+        Public ReadOnly Property stackFrame As StackFrame Implements IRuntimeTrace.stackFrame
+
+        Public Sub New(namespace$, symbol As Expression, stackFrame As StackFrame)
             Me.symbol = symbol
             Me.namespace = [namespace]
+            Me.stackFrame = stackFrame
         End Sub
 
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
         Public Overrides Function Evaluate(envir As Environment) As Object
             Return getPackageApiImpl(envir, [namespace], symbol)
         End Function
@@ -126,6 +135,7 @@ Namespace Interpreter.ExecuteEngine.ExpressionSymbols.DataSets
             End If
         End Function
 
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
         Public Overrides Function ToString() As String
             Return $"&{[namespace]}::<{symbol.ToString}>"
         End Function
