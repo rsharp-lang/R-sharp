557c8a2adddd522c7f3683b85c8810eac7f624fa
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2021-03-02 16:15:38 +0800

    Rdata reader?

---
+++ studio/RData/Reader.vb
@@ -0,0 +1,10 @@
+﻿Imports System.IO
+Imports Microsoft.VisualBasic.Data.IO.Bzip2
+
+Public Class Reader
+
+    Public Function OpenFile(file As Stream) As Reader
+        Dim bz2 As New BZip2InputStream(inputStream:=file, headerless:=True)
+
+    End Function
+End Class
