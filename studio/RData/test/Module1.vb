97015c64857cde31eee4f0b8854d8893fbf05cf8
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2021-03-02 16:42:55 +0800

    probably the rda file is not a bzip file

---
+++ studio/RData/test/Module1.vb
@@ -0,0 +1,14 @@
+﻿Imports System.IO
+Imports Microsoft.VisualBasic.Data.IO.Bzip2
+
+Module Module1
+
+    Sub Main()
+        Dim output2 = New MemoryStream()
+        Dim decompressor = New BZip2InputStream("F:\report.rda.tar".Open, True)
+        decompressor.CopyTo(output2)
+
+        Call output2.FlushStream("F:\report.rda\report2.rda")
+    End Sub
+
+End Module
