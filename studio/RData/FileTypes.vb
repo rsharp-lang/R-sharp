693fc1d559573601b6380dd180d01a6d40bc4efc
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2021-03-03 11:00:44 +0800

    add RDA data models

---
+++ studio/RData/FileTypes.vb
@@ -0,0 +1,148 @@
+﻿Imports System.ComponentModel
+
+''' <summary>
+''' Type of file containing a R file.
+''' </summary>
+Public Enum FileTypes
+    <Description("bz2")> bzip2
+    <Description("gzip")> gzip
+    <Description("xz")> xz
+    <Description("rdata version 2 (binary)")> rdata_binary_v2
+    <Description("rdata version 3 (binary)")> rdata_binary_v3
+End Enum
+
+''' <summary>
+''' Format of a R file.
+''' </summary>
+Public Enum RdataFormats
+    <Description("XDR")> XDR
+    <Description("ASCII")> ASCII
+    <Description("binary")> binary
+End Enum
+
+''' <summary>
+''' Type of a R object.
+''' </summary>
+Public Enum RObjectType
+    ''' <summary>
+    ''' NULL
+    ''' </summary>
+    NIL = 0
+    ''' <summary>
+    ''' symbols
+    ''' </summary>
+    SYM = 1
+    ''' <summary>
+    ''' pairlists
+    ''' </summary>
+    LIST = 2
+    ''' <summary>
+    ''' closures
+    ''' </summary>
+    CLO = 3
+    ''' <summary>
+    ''' environments
+    ''' </summary>
+    ENV = 4
+    ''' <summary>
+    ''' promises
+    ''' </summary>
+    PROM = 5
+    ''' <summary>
+    ''' language objects
+    ''' </summary>
+    LANG = 6
+    ''' <summary>
+    ''' special functions
+    ''' </summary>
+    SPECIAL = 7
+    ''' <summary>
+    ''' builtin functions
+    ''' </summary>
+    BUILTIN = 8
+    ''' <summary>
+    ''' internal character strings
+    ''' </summary>
+    [Char] = 9
+    ''' <summary>
+    ''' logical vectors
+    ''' </summary>
+    LGL = 10
+    ''' <summary>
+    ''' Integer vectors
+    ''' </summary>
+    INT = 13
+    ''' <summary>
+    ''' numeric vectors
+    ''' </summary>
+    REAL = 14
+    ''' <summary>
+    ''' complex vectors
+    ''' </summary>
+    CPLX = 15
+    ''' <summary>
+    ''' character vectors
+    ''' </summary>
+    STR = 16
+    ''' <summary>
+    ''' dot-dot-dot Object
+    ''' </summary>
+    DOT = 17
+    ''' <summary>
+    ''' make “any” args work
+    ''' </summary>
+    ANY = 18
+    ''' <summary>
+    ''' list (generic vector)
+    ''' </summary>
+    VEC = 19
+    ''' <summary>
+    ''' expression vector
+    ''' </summary>
+    EXPR = 20
+    ''' <summary>
+    ''' Byte code
+    ''' </summary>
+    BCODE = 21
+    ''' <summary>
+    ''' external pointer
+    ''' </summary>
+    EXTPTR = 22
+    ''' <summary>
+    ''' weak reference
+    ''' </summary>
+    WEAKREF = 23
+    ''' <summary>
+    ''' raw vector
+    ''' </summary>
+    RAW = 24
+    ''' <summary>
+    ''' S4 classes Not Of simple type
+    ''' </summary>
+    S4 = 25
+    ''' <summary>
+    ''' Empty environment
+    ''' </summary>
+    EMPTYENV = 242
+    ''' <summary>
+    ''' Global environment
+    ''' </summary>
+    GLOBALENV = 253
+    ''' <summary>
+    ''' NIL value
+    ''' </summary>
+    NILVALUE = 254
+    ''' <summary>
+    ''' Reference
+    ''' </summary>
+    REF = 255
+End Enum
+
+Public Enum CharFlags
+    HAS_HASH = 1
+    BYTES = 1 << 1
+    LATIN1 = 1 << 2
+    UTF8 = 1 << 3
+    CACHED = 1 << 5
+    ASCII = 1 << 6
+End Enum
---
+++ studio/RData/Parser.vb
@@ -0,0 +1,36 @@
+﻿Imports System.IO
+
+''' <summary>
+''' Parser interface for a R file.
+''' </summary>
+Module Parser
+
+    ReadOnly magic_dict As New Dictionary(Of FileTypes, Byte()) From {
+        {FileTypes.bzip2, bytes("\x42\x5a\x68")},
+        {FileTypes.gzip, bytes("\x1f\x8b")},
+        {FileTypes.xz, bytes("\xFD7zXZ\x00")},
+        {FileTypes.rdata_binary_v2, bytes("RDX2\n")},
+        {FileTypes.rdata_binary_v3, bytes("RDX3\n")}
+    }
+
+    ReadOnly format_dict As New Dictionary(Of RdataFormats, Byte()) From {
+        {RdataFormats.XDR, bytes("X\n")},
+        {RdataFormats.ASCII, bytes("A\n")},
+        {RdataFormats.binary, bytes("B\n")}
+    }
+
+    Private Function bytes(binaryStr As String) As Byte()
+        Throw New NotImplementedException
+    End Function
+
+    ''' <summary>
+    ''' Returns the type of the file.
+    ''' </summary>
+    ''' <param name="file"></param>
+    ''' <returns></returns>
+    Public Function file_type(file As Stream) As FileTypes
+        For Each item In magic_dict
+
+        Next
+    End Function
+End Module
