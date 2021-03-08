3ce1e85250fe236c295b1901ababe3d567978ec4
xieguigang <xie.guigang@gcmodeller.org>
2021-03-08 15:15:40 +0800

    add exception test

---
+++ R#/test/createException.vb
@@ -0,0 +1,16 @@
+﻿Imports SMRUCC.Rsharp.Interpreter
+Imports SMRUCC.Rsharp.Runtime
+Imports SMRUCC.Rsharp.Runtime.Components
+
+Module createException
+
+    Sub Main()
+        Dim env = New RInterpreter().globalEnvir
+        Dim msg As Message = Internal.debug.stop({"hello", "12345"}, env, suppress:=True)
+        Dim ex As Exception = msg.ToException
+
+        Call App.LogException(ex)
+
+        Throw ex
+    End Sub
+End Module
