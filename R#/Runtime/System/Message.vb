d9d6611892ce9a4e5106ed5031d52aa75a48e907
xieguigang <xie.guigang@gcmodeller.org>
2021-03-08 15:05:23 +0800

    runtime error message?

--- R#/Runtime/System/Message.vb
+++ R#/Runtime/System/Message.vb
@@ -1,45 +1,45 @@
 ﻿#Region "Microsoft.VisualBasic::78c93d7e275537022bdc955a107542a4, R#\Runtime\System\Message.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Class Message
-    ' 
-    '         Properties: environmentStack, level, message, source, trace
-    ' 
-    '         Function: GetEnumerator, IEnumerable_GetEnumerator, InCompatibleType, SymbolNotFound, SyntaxNotImplemented
-    '                   ToString
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Class Message
+' 
+'         Properties: environmentStack, level, message, source, trace
+' 
+'         Function: GetEnumerator, IEnumerable_GetEnumerator, InCompatibleType, SymbolNotFound, SyntaxNotImplemented
+'                   ToString
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -48,6 +48,7 @@ Imports System.Xml.Serialization
 Imports Microsoft.VisualBasic.ApplicationServices.Debugging.Diagnostics
 Imports Microsoft.VisualBasic.ApplicationServices.Debugging.Logging
 Imports Microsoft.VisualBasic.Linq
+Imports SMRUCC.Rsharp.Development
 Imports SMRUCC.Rsharp.Interpreter.ExecuteEngine
 
 Namespace Runtime.Components
@@ -66,7 +67,7 @@ Namespace Runtime.Components
         <XmlIgnore>
         Public Property source As Expression
 
-        Public Function ToException() As Exception
+        Public Function ToException() As RuntimeError
 
         End Function
 
---
+++ R#/System/RuntimeError.vb
@@ -0,0 +1,11 @@
+﻿Imports Microsoft.VisualBasic.ApplicationServices.Debugging
+
+Namespace Development
+
+    Public Class RuntimeError : Inherits VisualBasicAppException
+
+        Public Sub New(ex As Exception, calls As String)
+            MyBase.New(ex, calls)
+        End Sub
+    End Class
+End Namespace
