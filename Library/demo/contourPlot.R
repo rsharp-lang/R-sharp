2094a32da85cd8e10f5a9156d3a6f5f7504617fd
xieguigang <xie.guigang@gcmodeller.org>
2021-04-18 13:31:03 +0800

    renames output

--- Library/demo/contourPlot.R
+++ Library/demo/contourPlot.R
@@ -1,5 +1,5 @@
 imports "charts" from "graphics";
 
-bitmap(file = `${dirname(@script)}/formula.png`) {
+bitmap(file = `${dirname(@script)}/contour.png`) {
 	contourPlot([x,y] => x ^ 2 + y ^ 3, x = [-1, 1], y = [-1,1]);
 }
