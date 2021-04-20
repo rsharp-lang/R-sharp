fc2541505aab416a68f56ec3bda8408a39dc8fff
xieguigang <xie.guigang@gcmodeller.org>
2021-04-20 21:28:31 +0800

    meta text parser for description document and script annotation meta?

---
+++ R#/System/Document/MetaTextParser.vb
@@ -0,0 +1,13 @@
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+
+Namespace Development
+
+    Module MetaTextParser
+
+        <Extension>
+        Public Iterator Function ParseTagData(lines As IEnumerable(Of String)) As IEnumerable(Of NamedValue(Of String))
+
+        End Function
+    End Module
+End Namespace
