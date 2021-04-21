deb6bcb84227f4b24f16ec1676db5cd0964f7b88
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2021-04-21 17:25:47 +0800

    bugs fixed of write.csv

--- R#/Runtime/Internal/objects/utils/TableFormatter.vb
+++ R#/Runtime/Internal/objects/utils/TableFormatter.vb
@@ -1,42 +1,42 @@
 ﻿#Region "Microsoft.VisualBasic::ea99acbf41287b7fbdfecac205899e6c, R#\Runtime\Internal\objects\utils\TableFormatter.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Class TableFormatter
-    ' 
-    '         Function: GetTable
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Class TableFormatter
+' 
+'         Function: GetTable
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -49,18 +49,8 @@ Namespace Runtime.Internal.Object.Utils
 
     Public Class TableFormatter
 
-        ''' <summary>
-        ''' Each element in a return result array is a row in table matrix
-        ''' </summary>
-        ''' <returns></returns>
-        Public Shared Function GetTable(df As dataframe, env As GlobalEnvironment, Optional printContent As Boolean = True, Optional showRowNames As Boolean = True) As String()()
-            Dim table As String()() = New String(df.nrows + 1)() {}
-            Dim rIndex As Integer
-            Dim colNames$() = df.columns.Keys.ToArray
-            Dim col As Array
-            Dim row As String() = {""}.Join(colNames)
-            Dim rownames = df.getRowNames()
-            Dim typeRow As String() = colNames _
+        Private Shared Function GetTypeArray(df As dataframe, colNames As String()) As String()
+            Return colNames _
                 .Select(Function(name)
                             Dim arrayType As Type = df(name).GetType
                             Dim type As RType = RType.GetRSharpType(arrayType)
@@ -68,15 +58,9 @@ Namespace Runtime.Internal.Object.Utils
                             Return $"<{type}>"
                         End Function) _
                 .ToArray
+        End Function
 
-            If showRowNames Then
-                table(Scan0) = row.ToArray
-                table(1) = {"<mode>"}.JoinIterates(typeRow).ToArray
-            Else
-                table(Scan0) = row.Skip(1).ToArray
-                table(1) = typeRow
-            End If
-
+        Private Shared Function GetFormatters(df As dataframe, colNames As String(), env As GlobalEnvironment, isPrintContent As Boolean) As IStringBuilder()
             Dim elementTypes As Type() = colNames _
                 .Select(Function(key)
                             Return df.columns(key).GetType.GetElementType
@@ -84,9 +68,43 @@ Namespace Runtime.Internal.Object.Utils
                 .ToArray
             Dim formatters As IStringBuilder() = elementTypes _
                 .Select(Function(type)
-                            Return printer.ToString(type, env, printContent)
+                            Return printer.ToString(type, env, isPrintContent)
                         End Function) _
                 .ToArray
+
+            Return formatters
+        End Function
+
+        ''' <summary>
+        ''' Each element in a return result array is a row in table matrix
+        ''' </summary>
+        ''' <returns></returns>
+        Public Shared Function GetTable(df As dataframe, env As GlobalEnvironment,
+                                        Optional printContent As Boolean = True,
+                                        Optional showRowNames As Boolean = True) As String()()
+
+            Dim table As String()() = New String(df.nrows + If(printContent, 1, 0))() {}
+            Dim rIndex As Integer
+            Dim colNames$() = df.columns.Keys.ToArray
+            Dim col As Array
+            Dim row As String() = {""}.Join(colNames)
+            Dim rownames As String() = df.getRowNames()
+            Dim typeRow As String() = GetTypeArray(df, colNames)
+            Dim formatters As IStringBuilder() = GetFormatters(df, colNames, env, printContent)
+
+            If showRowNames Then
+                table(Scan0) = row.ToArray
+
+                If printContent Then
+                    table(1) = {"<mode>"}.JoinIterates(typeRow).ToArray
+                End If
+            Else
+                table(Scan0) = row.Skip(1).ToArray
+
+                If printContent Then
+                    table(1) = typeRow
+                End If
+            End If
 
             For i As Integer = 2 To table.Length - 1
                 rIndex = i - 2
