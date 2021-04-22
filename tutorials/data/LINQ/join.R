75cd348a423b36fdb27d8c614363b691a53973f5
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2021-04-22 17:11:27 +0800

    add demo test for join data table

---
+++ tutorials/data/LINQ/join.R
@@ -0,0 +1,23 @@
+# make query after join two table
+const employee = data.frame(
+    ID        = [1,        2,          3,        4,         5,      6       ],
+    Name      = ["Preety", "Priyanka", "Anurag", "Pranaya", "Hina", "Sambit"],
+    AddressId = [1,        2,          0,        0,         5,      6       ]
+);
+
+const address = data.frame(
+    ID          = [1,             2,             5,             6             ], 
+    AddressLine = ["AddressLine1","AddressLine2","AddressLine5","AddressLine6"]
+);
+
+# the join operation
+let result = {
+    FROM [ID, Name, AddressId] IN employee
+    JOIN [ID, AddressLine] IN address
+    ON employee.AddressId == address.ID
+    WHERE employee.ID > 5
+}
+# will produce a join table output
+# employee.ID, Name, AddressId, address.ID, AddressLine
+
+print(result);
