bc48c24c5be7d60e3975e80c57b5f06fbcccb2f7
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2021-04-25 11:31:07 +0800

    test break in for loop

--- tutorials/data/forloop.R
+++ tutorials/data/forloop.R
@@ -2,6 +2,48 @@ let add1 as function(x) {
 	print(`x + 1 -> ${x+1}`);
 }
 
-for(i in 1:10 step 2.5) {
+const all = for(i in 1:10 step 2.5) {
 	add1(x = i);
 }
+
+print("all contents:");
+print(all);
+
+# break test of the for loop
+for(i in 1:10) {
+	break;
+	print("this message should never display!");
+}
+
+for(i in 1:10) {
+	if (i > 5) {
+		break;
+		print("this message should never display!");
+	}
+
+	if (i > 5) {
+		print("this message should never display!");
+	}
+}
+
+# should print 999
+print((function() {
+	# test of break in function
+	for(i in 1:10) {
+		if (i > 5) {
+			break;
+			print("this message should never display!");
+		} else {
+			cat(i);
+			cat("\t");
+		}
+	}
+
+	for(i in 1:10) {
+		if (i > 3) {
+			return 999;
+		} else {
+			print(i);
+		}
+	}
+})());
