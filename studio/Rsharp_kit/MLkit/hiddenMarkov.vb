0532437e340d5f8c4afc37864394640f374e005a
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2021-04-27 13:03:11 +0800

    hiddenMarkov package

---
+++ studio/Rsharp_kit/MLkit/hiddenMarkov.vb
@@ -0,0 +1,14 @@
+﻿
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.Rsharp.Runtime.Interop
+
+<Package("hiddenMarkov")>
+Module hiddenMarkov
+
+    <ExportAPI("")>
+    Public Function statesMatrix(<RRawVectorArgument> matrix As Object, Optional env As Environment = Nothing) As Object
+
+    End Function
+
+End Module
