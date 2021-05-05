a77426b8439c68679d1b7b76b9ff76e05436f505
xieguigang <xie.guigang@gcmodeller.org>
2021-05-05 16:56:41 +0800

    Update DataLeftJoin.vb

--- R#/Interpreter/ExecuteEngine/Linq/Expression/DataLeftJoin.vb
+++ R#/Interpreter/ExecuteEngine/Linq/Expression/DataLeftJoin.vb
@@ -8,6 +8,24 @@
             End Get
         End Property
 
+        ''' <summary>
+        ''' join xxx in xxx
+        ''' </summary>
+        Dim anotherData As QuerySource
+        Dim key1 As MemberReference
+        Dim key2 As MemberReference
+
+        Sub New(symbol As SymbolDeclare, sequence As Expression)
+            anotherData = New QuerySource(symbol, sequence)
+        End Sub
+
+        Public Function SetKeyBinary(left As MemberReference, right As MemberReference) As DataLeftJoin
+            key1 = left
+            key2 = right
+
+            Return Me
+        End Function
+
         Public Overrides Function Exec(context As ExecutableContext) As Object
             Throw New NotImplementedException()
         End Function
