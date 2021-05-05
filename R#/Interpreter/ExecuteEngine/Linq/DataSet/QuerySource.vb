54a14336d5c742e53f53bf324470cdc2118ecf2a
xieguigang <xie.guigang@gcmodeller.org>
2021-05-05 16:45:53 +0800

    query source

---
+++ R#/Interpreter/ExecuteEngine/Linq/DataSet/QuerySource.vb
@@ -0,0 +1,13 @@
+﻿Namespace Interpreter.ExecuteEngine.LINQ
+
+    Public Class QuerySource
+
+        Protected Friend ReadOnly sequence As Expression
+        Protected Friend ReadOnly symbol As SymbolDeclare
+
+        Sub New(symbol As SymbolDeclare, sequence As Expression)
+            Me.symbol = symbol
+            Me.sequence = sequence
+        End Sub
+    End Class
+End Namespace
---
+++ R#/Interpreter/ExecuteEngine/Linq/Expression/DataLeftJoin.vb
@@ -0,0 +1,15 @@
+﻿Namespace Interpreter.ExecuteEngine.LINQ
+
+    Public Class DataLeftJoin : Inherits LinqKeywordExpression
+
+        Public Overrides ReadOnly Property keyword As String
+            Get
+                Return "JOIN"
+            End Get
+        End Property
+
+        Public Overrides Function Exec(context As ExecutableContext) As Object
+            Throw New NotImplementedException()
+        End Function
+    End Class
+End Namespace
