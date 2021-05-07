ca320a0e61dbcd50f65171ef2131b91149f38962
xieguigang <xie.guigang@gcmodeller.org>
2021-05-07 22:33:33 +0800

    Rfilesystem

---
+++ R#/System/RFileSystem.vb
@@ -0,0 +1,15 @@
+﻿Imports SMRUCC.Rsharp.Runtime
+
+Namespace Development
+
+    Public Class RFileSystem
+
+        Public Shared Function ListPackageDir(env As Environment) As IEnumerable(Of String)
+
+        End Function
+
+        Public Shared Function PackageInstalled(packageName As String, env As Environment) As Boolean
+
+        End Function
+    End Class
+End Namespace
