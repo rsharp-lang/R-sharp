6af2c30a529e961c5d4bc3d7286df772e55dcff4
xieguigang <xie.guigang@gcmodeller.org>
2021-05-24 21:12:31 +0800

    union and joint two dataframe

---
+++ R#/Runtime/Internal/objects/utils/TableJoint.vb
@@ -0,0 +1,52 @@
+﻿Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Linq
+Imports REnv = SMRUCC.Rsharp.Runtime
+
+Namespace Runtime.Internal.Object.Utils
+
+    Module TableJoint
+
+        <ExportAPI("union_joint")>
+        Public Function union(x As dataframe, y As dataframe, Optional env As Environment = Nothing) As dataframe
+            If x Is Nothing Then
+                Return y
+            ElseIf y Is Nothing Then
+                Return x
+            End If
+
+            Dim unionSet As New dataframe
+            Dim colleft As String() = x.columns.Keys.ToArray
+            Dim colright As String() = y.columns.Keys.ToArray
+            Dim left = TableRow.CreateRows(x, colleft).GroupBy(Function(i) i.id).ToDictionary(Function(i) i.Key, Function(i) i.First)
+            Dim right = TableRow.CreateRows(y, colright).GroupBy(Function(i) i.id).ToDictionary(Function(i) i.Key, Function(i) i.First)
+            Dim unionId = left.Keys.AsList + right.Keys
+            Dim unionFeatures = (colleft.AsList + colright) _
+                .makeNames(unique:=True) _
+                .Select(Function(vec) (vec, New List(Of Object))) _
+                .ToArray
+            Dim emptyLeft As New TableRow With {.cells = Repeats(Of Object)(Nothing, colleft.Length)}
+            Dim emptyRight As New TableRow With {.cells = Repeats(Of Object)(Nothing, colright.Length)}
+
+            For Each id As String In unionId
+                Dim cl As TableRow = left.TryGetValue(id, [default]:=emptyLeft)
+                Dim cr As TableRow = right.TryGetValue(id, [default]:=emptyRight)
+
+                For i As Integer = 0 To colleft.Length - 1
+                    unionFeatures(i).Item2.Add(cl.cells(i))
+                Next
+                For j As Integer = 0 To colright.Length - 1
+                    unionFeatures(j + colleft.Length).Item2.Add(cr.cells(j))
+                Next
+            Next
+
+            unionSet.columns = unionFeatures _
+                .ToDictionary(Function(vec) vec.Item1,
+                              Function(vec)
+                                  Return DirectCast(REnv.TryCastGenericArray(vec.Item2.ToArray, env), Array)
+                              End Function)
+            unionSet.rownames = unionId
+
+            Return unionSet
+        End Function
+    End Module
+End Namespace
--- R#/Runtime/Internal/objects/utils/TableRow.vb
+++ R#/Runtime/Internal/objects/utils/TableRow.vb
@@ -1,46 +1,47 @@
 ﻿#Region "Microsoft.VisualBasic::008675953c4f94d14f1a547f2e7d9edf, R#\Runtime\Internal\objects\utils\TableRow.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Class TableRow
-    ' 
-    '         Function: (+2 Overloads) ToString
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Class TableRow
+' 
+'         Function: (+2 Overloads) ToString
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports Microsoft.VisualBasic.Text
+Imports any = Microsoft.VisualBasic.Scripting
 
 Namespace Runtime.Internal.Object.Utils
 
@@ -49,7 +50,11 @@ Namespace Runtime.Internal.Object.Utils
     ''' </summary>
     Public Class TableRow
 
-        Friend cells As String()
+        Friend cells As Object()
+        ''' <summary>
+        ''' the row id
+        ''' </summary>
+        Friend id As String
 
         Public Overrides Function ToString() As String
             Return ToString(",")
@@ -57,6 +62,7 @@ Namespace Runtime.Internal.Object.Utils
 
         Public Overloads Function ToString(format As String) As String
             Return cells _
+                .Select(Function(obj) any.ToString(obj)) _
                 .Select(Function(str)
                             If str.IndexOfAny({","c, ASCII.TAB}) > -1 Then
                                 Return $"""{str}"""
@@ -65,6 +71,18 @@ Namespace Runtime.Internal.Object.Utils
                             End If
                         End Function) _
                 .JoinBy(format)
+        End Function
+
+        Public Shared Function CreateRows(table As dataframe, Optional orders As String() = Nothing) As TableRow()
+            Return table _
+                .forEachRow(colKeys:=orders) _
+                .Select(Function(r)
+                            Return New TableRow With {
+                                .id = r.name,
+                                .cells = r.value
+                            }
+                        End Function) _
+                .ToArray
         End Function
     End Class
 End Namespace
