77bd9647cf9bed582c70bd20c36c0b0b07398bfa
guigang xie <gg.xie@bionovogene.com>
2021-06-01 16:52:45 +0800

    Create switch.R

---
+++ tutorials/closures/switch.R
@@ -0,0 +1,26 @@
+const common_compute as function(x, type) {
+	switch(type, default -> stop(`invalid method name '${type}'!`)) {
+		mean   = mean(x),
+		median = median(x),
+		sum    = sum(x),
+		max    = max(x),
+		min    = min(x),
+		sqrt   = sqrt(x),
+		err    = {
+			stop("this is an error test in switch expression!");
+		}
+	}
+}
+
+const x = [11, 23, 9, 49, 46, 92, 29, 3];
+
+print(x |> common_compute("mean"));
+print(x |> common_compute("median"));
+print(x |> common_compute("sum"));
+print(x |> common_compute("max"));
+print(x |> common_compute("min"));
+print(x |> common_compute("sqrt"));
+
+# test other operation
+# like throw error message
+print(x |> common_compute("err"));
