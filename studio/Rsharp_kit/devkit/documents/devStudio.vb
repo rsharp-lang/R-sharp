9fdb0b70e530f1a3aa6b454ef09cd63bf7860673
guigang xie <gg.xie@bionovogene.com>
2021-06-02 14:16:08 +0800

    fix of symbol reference errors

--- studio/Rsharp_kit/devkit/documents/devStudio.vb
+++ studio/Rsharp_kit/devkit/documents/devStudio.vb
@@ -1,43 +1,43 @@
 ﻿#Region "Microsoft.VisualBasic::f1993e686cc1581c04bd9a29ded3eca7, studio\Rsharp_kit\devkit\documents\devStudio.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module devStudio
-    ' 
-    '     Function: getRequiredPackages
-    ' 
-    '     Sub: getRequiredPackages
-    ' 
-    ' /********************************************************************************/
+' Module devStudio
+' 
+'     Function: getRequiredPackages
+' 
+'     Sub: getRequiredPackages
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -80,8 +80,8 @@ Module devStudio
                         ' ignores
                     End If
                 Next
-            Case GetType(ValueAssign)
-                Call getRequiredPackages(DirectCast(code, ValueAssign).value, packageNames, solveSymbols)
+            Case GetType(ValueAssignExpression)
+                Call getRequiredPackages(DirectCast(code, ValueAssignExpression).value, packageNames, solveSymbols)
             Case GetType(MemberValueAssign)
                 Call getRequiredPackages(DirectCast(code, MemberValueAssign).value, packageNames, solveSymbols)
             Case GetType(FunctionInvoke)
