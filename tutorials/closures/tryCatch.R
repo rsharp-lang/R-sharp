a0cc7c2073f36c30aafbb0ce01d6650c00560599
guigang xie <gg.xie@bionovogene.com>
2021-06-02 10:27:09 +0800

    Create tryCatch.R

---
+++ tutorials/closures/tryCatch.R
@@ -0,0 +1,11 @@
+# catch error
+const catch = try(ex -> stop(123)) {
+	print(ex);
+	
+	# returns a default value
+	# if an error has been catched
+	23333;
+}
+
+# get try-error
+const tryErr = try(ex -> stop(123));
