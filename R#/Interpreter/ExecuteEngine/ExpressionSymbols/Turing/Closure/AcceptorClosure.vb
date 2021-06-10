0bbc5389c44e0a6a81c4320cd106cc5ae4caeead
guigang xie <gg.xie@bionovogene.com>
2021-06-10 17:32:10 +0800

    improvments of the program data model

--- R#/Interpreter/ExecuteEngine/ExpressionSymbols/Turing/Closure/AcceptorClosure.vb
+++ R#/Interpreter/ExecuteEngine/ExpressionSymbols/Turing/Closure/AcceptorClosure.vb
@@ -1,9 +1,20 @@
-﻿Namespace Interpreter.ExecuteEngine.ExpressionSymbols.Closure
+﻿Imports SMRUCC.Rsharp.Runtime
+
+Namespace Interpreter.ExecuteEngine.ExpressionSymbols.Closure
 
     Public Class AcceptorClosure : Inherits ClosureExpression
 
         Public Sub New(code() As Expression)
             MyBase.New(code)
         End Sub
+
+        Public Overrides Function Evaluate(envir As Environment) As Object
+            If TypeOf program.Last Is FunctionInvoke Then
+                Dim arguments = envir.acceptorArguments
+                Dim newProgram As New Program(program.Take(program.lines - 1))
+            Else
+                Return MyBase.Evaluate(envir)
+            End If
+        End Function
     End Class
 End Namespace
--- R#/Runtime/Interop/RInteropAttributes/RDefaultExpressionAttribute.vb
+++ R#/Runtime/Interop/RInteropAttributes/RDefaultExpressionAttribute.vb
@@ -1,45 +1,46 @@
 ﻿#Region "Microsoft.VisualBasic::a0ea1ef51888d2281346f8c114184638, R#\Runtime\Interop\RInteropAttributes\RDefaultExpressionAttribute.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Class RDefaultExpressionAttribute
-    ' 
-    '         Function: ParseDefaultExpression
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Class RDefaultExpressionAttribute
+' 
+'         Function: ParseDefaultExpression
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
+Imports Microsoft.VisualBasic.Linq
 Imports SMRUCC.Rsharp.Interpreter
 Imports SMRUCC.Rsharp.Interpreter.ExecuteEngine
 Imports SMRUCC.Rsharp.Runtime.Components
@@ -49,7 +50,12 @@ Namespace Runtime.Interop
     Public Class RDefaultExpressionAttribute : Inherits RInteropAttribute
 
         Public Shared Function ParseDefaultExpression(strExp As String) As Expression
-            Return Program.CreateProgram(Rscript.FromText(strExp.Trim("~"c)), debug:=False, [error]:=Nothing).execQueue.First
+            Return Rscript _
+                .FromText(strExp.Trim("~"c)) _
+                .DoCall(Function(script)
+                            Return Program.CreateProgram(script, debug:=False, [error]:=Nothing)
+                        End Function) _
+                .First
         End Function
 
     End Class
