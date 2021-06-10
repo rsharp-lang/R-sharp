2990d448c8feca31dd433e5e686a8b67454ba434
guigang xie <gg.xie@bionovogene.com>
2021-06-10 17:18:21 +0800

    how to pass parameter？

---
+++ R#/Interpreter/ExecuteEngine/ExpressionSymbols/Turing/Closure/AcceptorClosure.vb
@@ -0,0 +1,9 @@
+﻿Namespace Interpreter.ExecuteEngine.ExpressionSymbols.Closure
+
+    Public Class AcceptorClosure : Inherits ClosureExpression
+
+        Public Sub New(code() As Expression)
+            MyBase.New(code)
+        End Sub
+    End Class
+End Namespace
---
+++ tutorials/closures/parameterTest.R
@@ -0,0 +1,4 @@
+bitmap(file = "./plot.png", width = 300, height = 300, dpi = 300) {
+	print("run test!");
+    plot(NULL);
+}
