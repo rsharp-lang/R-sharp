0c03814a8cb771c4b72c282cf3a871e0307f0b0f
xieguigang <xie.guigang@gcmodeller.org>
2021-06-27 00:12:04 +0800

    IDataEmbedding

--- studio/Rsharp_kit/MLkit/tSNETool.vb
+++ studio/Rsharp_kit/MLkit/tSNETool.vb
@@ -1,5 +1,6 @@
 ﻿
 Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.DataMining.ComponentModel
 Imports Microsoft.VisualBasic.MachineLearning.tSNE
 Imports Microsoft.VisualBasic.Scripting.MetaData
 Imports SMRUCC.Rsharp.Runtime
@@ -56,8 +57,16 @@ Module tSNETool
 
     <ExportAPI("solve")>
     Public Function Solve(tSNE As tSNEDataSet, Optional iterations% = 500) As dataframe
+        Dim nEpochs As Integer = iterations
+
+        Console.WriteLine("Calculating..")
+
         For i As Integer = 0 To iterations
             Call tSNE.algorithm.Step()
+
+            If (100 * i / nEpochs) Mod 5 = 0 Then
+                Console.WriteLine($"- Completed {i + 1} of {nEpochs} [{CInt(100 * i / nEpochs)}%]")
+            End If
         Next
 
         Return tSNE.GetOutput
@@ -65,10 +74,16 @@ Module tSNETool
 
 End Module
 
-Public Class tSNEDataSet
+Public Class tSNEDataSet : Inherits IDataEmbedding
 
     Friend algorithm As tSNE
     Friend labels As String()
+
+    Public Overrides ReadOnly Property dimension As Integer
+        Get
+            Return algorithm.dimension
+        End Get
+    End Property
 
     Public Function GetOutput() As dataframe
         Dim matrix = algorithm.GetEmbedding
@@ -86,4 +101,7 @@ Public Class tSNEDataSet
         Return result
     End Function
 
+    Public Overrides Function GetEmbedding() As Double()()
+        Return algorithm.GetEmbedding
+    End Function
 End Class
