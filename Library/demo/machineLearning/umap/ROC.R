c181a9e89d63629c5c41343493be65724c0b73dd
xieguigang <xie.guigang@gcmodeller.org>
2021-06-28 18:46:38 +0800

    Update ROC.R

--- Library/demo/machineLearning/umap/ROC.R
+++ Library/demo/machineLearning/umap/ROC.R
@@ -1,6 +1,6 @@
 imports "validation" from "MLkit";
 
-const data = read.csv(file = `${dirname(@script)}/ROC.csv`);
+const data = read.csv(file = "github://rsharp-lang/R-sharp/blob/master/Library/demo/machineLearning/umap/ROC.csv");
 const pred = prediction(data[, 1], data[, 2]);
 
 print(as.data.frame(pred));
