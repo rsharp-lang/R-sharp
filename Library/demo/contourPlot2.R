11339eeac21eb780c6125f7ac87cfec69571714b
xieguigang <xie.guigang@gcmodeller.org>
2021-06-30 20:23:21 +0800

    fix of data scale

--- Library/demo/contourPlot2.R
+++ Library/demo/contourPlot2.R
@@ -3,6 +3,6 @@ imports "charts" from "graphics";
 bitmap(file = `${dirname(@script)}/1_Contour.png`) {
 	`${dirname(@script)}/1_Contour.csv`
 	|> read.csv(row.names = NULL)
-	|> contourPlot()
+	|> contourPlot(colorSet = "Jet")
 	;
 }
