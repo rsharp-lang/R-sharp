a8c47c2d7a3de187ed9eb9f1dfaadbe52ede9533
guigang xie <gg.xie@bionovogene.com>
2021-07-15 13:15:54 +0800

    plot linear fittting result

--- Library/R.math/math.vb
+++ Library/R.math/math.vb
@@ -1,43 +1,43 @@
 ﻿#Region "Microsoft.VisualBasic::cf1f4bf8c985833507c2fb75e8e4e7e1, Library\R.math\math.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module math
-    ' 
-    '     Constructor: (+1 Overloads) Sub New
-    '     Function: asFormula, create_deSolve_DataFrame, Hist, lm, predict
-    '               (+2 Overloads) RK4, sim, ssm, summaryFit
-    ' 
-    ' /********************************************************************************/
+' Module math
+' 
+'     Constructor: (+1 Overloads) Sub New
+'     Function: asFormula, create_deSolve_DataFrame, Hist, lm, predict
+'               (+2 Overloads) RK4, sim, ssm, summaryFit
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -46,6 +46,7 @@ Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports Microsoft.VisualBasic.Data.Bootstrapping
 Imports Microsoft.VisualBasic.Data.Bootstrapping.Multivariate
+Imports Microsoft.VisualBasic.Data.ChartPlots.Statistics
 Imports Microsoft.VisualBasic.Data.csv.IO
 Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Linq
@@ -82,9 +83,24 @@ Module math
     Sub New()
         REnv.Internal.Object.Converts.makeDataframe.addHandler(GetType(ODEsOut), AddressOf create_deSolve_DataFrame)
 
+        REnv.Internal.generic.add("plot", GetType(IFitted), AddressOf plotLinearYFit)
         REnv.Internal.generic.add("summary", GetType(lmCall), AddressOf summaryFit)
         REnv.Internal.ConsolePrinter.AttachConsoleFormatter(Of lmCall)(Function(o) o.ToString)
     End Sub
+
+    Public Function plotLinearYFit(fit As IFitted, args As list, env As Environment) As Object
+        Dim size As String = InteropArgumentHelper.getSize(args!size, "2100,1600")
+        Dim gridFill As String = RColorPalette.getColor(args("grid.fill"), "rgb(245,245,245)")
+        Dim showLegend As Boolean = args.getValue("show.legend", env, True)
+        Dim showYFit As Boolean = args.getValue("show.yfit", env, True)
+
+        Return fit.Plot(
+            size:=size,
+            gridFill:=gridFill,
+            showLegend:=showLegend,
+            showYFitPoints:=showYFit
+        )
+    End Function
 
     Private Function summaryFit(x As lmCall, args As list, env As Environment) As Object
         Throw New NotImplementedException
