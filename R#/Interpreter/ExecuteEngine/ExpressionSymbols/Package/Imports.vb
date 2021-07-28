c89ba2623b3bb1837fa3a123c05d80ba294be6a4
xieguigang <xie.guigang@gcmodeller.org>
2021-07-28 10:27:01 +0800

    add debug echo of the library imports error

--- R#/Interpreter/ExecuteEngine/ExpressionSymbols/Package/Imports.vb
+++ R#/Interpreter/ExecuteEngine/ExpressionSymbols/Package/Imports.vb
@@ -1,47 +1,47 @@
 ﻿#Region "Microsoft.VisualBasic::305db9a3d500e71dea5d93640947a0f5, R#\Interpreter\ExecuteEngine\ExpressionSymbols\Package\Imports.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Class [Imports]
-    ' 
-    '         Properties: expressionName, isImportsScript, library, packages, scriptSource
-    '                     type
-    ' 
-    '         Constructor: (+2 Overloads) Sub New
-    '         Function: Evaluate, GetDllFile, GetExternalScriptFile, importsExternalScript, importsLibrary
-    '                   importsPackages, isImportsAllPackages, LoadLibrary, ToString
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Class [Imports]
+' 
+'         Properties: expressionName, isImportsScript, library, packages, scriptSource
+'                     type
+' 
+'         Constructor: (+2 Overloads) Sub New
+'         Function: Evaluate, GetDllFile, GetExternalScriptFile, importsExternalScript, importsLibrary
+'                   importsPackages, isImportsAllPackages, LoadLibrary, ToString
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -57,6 +57,7 @@ Imports SMRUCC.Rsharp.Runtime.Components
 Imports SMRUCC.Rsharp.Runtime.Internal.Object
 Imports SMRUCC.Rsharp.Development.Package
 Imports SMRUCC.Rsharp.Development.Package.File
+Imports SMRUCC.Rsharp.Runtime.Internal.Invokes
 
 Namespace Interpreter.ExecuteEngine.ExpressionSymbols
 
@@ -329,7 +330,14 @@ load:       Return LoadLibrary(filepath, env, names)
         ''' <returns></returns>
         Public Shared Function LoadLibrary(libDll$, envir As Environment, names As Index(Of String)) As Object
             Dim importsAll As Boolean = names.DoCall(AddressOf isImportsAllPackages)
-            Dim packages = PackageLoader.ParsePackages(libDll) _
+            Dim packages As Dictionary(Of String, Package)
+            Dim globalEnv As GlobalEnvironment = envir.globalEnvironment
+
+            If globalEnv.debugMode Then
+                Call base.print(libDll, envir)
+            End If
+
+            packages = PackageLoader.ParsePackages(libDll) _
                 .Where(Function(pkg)
                            If importsAll Then
                                Return True
@@ -342,7 +350,6 @@ load:       Return LoadLibrary(filepath, env, names)
                               Function(group)
                                   Return group.First
                               End Function)
-            Dim globalEnv As GlobalEnvironment = envir.globalEnvironment
 
             If importsAll Then
                 For Each [namespace] As Package In packages.Values
