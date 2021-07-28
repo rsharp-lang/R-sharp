289f3d4562cd089a1a3176e495da19f18d9e8940
xieguigang <xie.guigang@gcmodeller.org>
2021-07-28 21:21:03 +0800

    implements the config.json of the command line?

---
+++ studio/Rsharp_kit/devkit/config.json.vb
@@ -0,0 +1,31 @@
+﻿Imports Microsoft.VisualBasic.MIME.application.json.Javascript
+Imports SMRUCC.Rsharp.Runtime.Internal.Object
+
+''' <summary>
+''' config of the commandline script parameters via ``config.json`` file.
+''' </summary>
+''' <remarks>
+''' just looking for the ``config.json`` file under the current working directory.
+''' </remarks>
+Public Class ConfigJSON
+
+    Dim config As list
+
+    ''' <summary>
+    ''' just looking for the ``config.json`` file under the current working directory.
+    ''' </summary>
+    ''' <returns></returns>
+    Public Shared Function LoadConfig() As ConfigJSON
+        Dim config As String = "./config.json"
+        Dim json As JsonElement = JsonElement.ParseJSON(jsonStr:=config.ReadAllText)
+
+        If Not TypeOf json Is JsonObject Then
+            Return Nothing
+        Else
+            Return New ConfigJSON With {
+                .config =
+            }
+        End If
+    End Function
+
+End Class
