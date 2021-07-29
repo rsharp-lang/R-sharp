414a4fcfb8b935a83e15c3fc25a689ab27d6dc7b
xieguigang <xie.guigang@gcmodeller.org>
2021-07-29 10:40:05 +0800

    logical test success

--- tutorials/commandline/using_config.R
+++ tutorials/commandline/using_config.R
@@ -12,10 +12,15 @@ const filename as string = ?"--filename" || "test.png";
 
 # test of missing in config.json
 [@config "/missing/in/config.json"]
-const missing1 a string = ?"--missing-in-config.json" || 12345;
-
+const missing1 as string = ?"--missing-in-config.json" || 12345;
 # test of no config mapping
 const noConfigMapping as string = ?"--no-config-mapping" || 33333;
+# test of logical
+const logical1 as boolean = ?"--flag1";
+[@config "/render/blur"]
+const logical2 as boolean = ?"--flag2";
+[@config "/render/grayscale"]
+const logical3 as boolean = ?"--flag3";
 
 print(`data input dir: ${dir}`);
 print(`image output: '${dir}/${filename}' in format of ${format} and image resolution ppi value is '${ppi}'`);
@@ -26,5 +31,15 @@ print(missing1);
 
 print("no config.json mapping:");
 print(noConfigMapping);
+
+print("test of the logcial flags in the config mapping");
+print("flag1:");
+print(logical1);
+
+print("flag2:");
+print(logical2);
+
+print("flag3:");
+print(logical3);
 
 pause();
