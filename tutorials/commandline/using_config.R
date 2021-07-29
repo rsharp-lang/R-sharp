c813ff262597fdf9eec07300227bb122994cb8f3
xieguigang <xie.guigang@gcmodeller.org>
2021-07-29 10:00:27 +0800

    bugs fixed for populate syntax error

--- tutorials/commandline/using_config.R
+++ tutorials/commandline/using_config.R
@@ -10,7 +10,21 @@ const ppi as integer     = ?"--ppi"      || 300;
 [@config "/image/filename"]
 const filename as string = ?"--filename" || "test.png";
 
+# test of missing in config.json
+[@config "/missing/in/config.json"]
+const missing1 a string = ?"--missing-in-config.json" || 12345;
+
+# test of no config mapping
+const noConfigMapping as string = ?"--no-config-mapping" || 33333;
+
 print(`data input dir: ${dir}`);
 print(`image output: '${dir}/${filename}' in format of ${format} and image resolution ppi value is '${ppi}'`);
+
+print("test situations:");
+print("missing in config.json:");
+print(missing1);
+
+print("no config.json mapping:");
+print(noConfigMapping);
 
 pause();
