1d4d17d52d1c85d8f0f4ae426c374fbfa488d74d
guigang xie <gg.xie@bionovogene.com>
2021-08-11 22:05:13 +0800

    Create lpSolve.vb

---
+++ Library/R.math/lpSolve.vb
@@ -0,0 +1,16 @@
+﻿
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Scripting.MetaData
+
+''' <summary>
+''' linear programming solver
+''' </summary>
+<Package("lpSolve")>
+Module lpSolve
+
+    <ExportAPI("lp")>
+    Public Function lp()
+
+    End Function
+
+End Module
