c787b0d20572a2ce6eb49e4faf557d6e9de13cb0
guigang xie <gg.xie@bionovogene.com>
2021-08-12 14:17:37 +0800

    Update lpsolve.R

--- Library/demo/math/lpsolve.R
+++ Library/demo/math/lpsolve.R
@@ -5,6 +5,23 @@ const subject_to = ~[
         x1 + 2 * x2 + 3 * x3 = 9,
     3 * x1 + 2 * x2 + 2 * x3 = 15
 ];
-const lpp = lp("max", objective, subject_to);
+const lpp = lp(objective, subject_to, "max");
 
 str(lpp);
+
+print("solve problem 2:");
+
+# [1] 0.0 4.5 0.0
+# the objective function is 40.5
+str(lp(
+	
+	  ~     x1 + 9 * x2 +     x3,
+	
+	[
+	  ~     x1 + 2 * x2 + 3 * x3 <= 9,
+      ~ 3 * x1 + 2 * x2 + 2 * x3 <= 15
+	],
+	
+	"max"
+
+));
