423a6f6be9f1dbd8f06e0a7e74089ffd8a49f116
guigang xie <gg.xie@bionovogene.com>
2021-09-02 19:28:06 +0800

    create dataframe from a dataset collection

--- R#/Runtime/Internal/objects/RConversion/makeDataframe.vb
+++ R#/Runtime/Internal/objects/RConversion/makeDataframe.vb
@@ -1,57 +1,58 @@
 ﻿#Region "Microsoft.VisualBasic::8a431b56cb8c7a92c668c7ab33b8303a, R#\Runtime\Internal\objects\RConversion\makeDataframe.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Delegate Function
-    ' 
-    ' 
-    '     Module makeDataframe
-    ' 
-    '         Constructor: (+1 Overloads) Sub New
-    ' 
-    '         Function: CheckDimension, createColumnVector, createDataframe, fromList, is_ableConverts
-    '                   RDataframe, TracebackDataFrmae, tryTypeLineage
-    ' 
-    '         Sub: [addHandler]
-    ' 
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Delegate Function
+' 
+' 
+'     Module makeDataframe
+' 
+'         Constructor: (+1 Overloads) Sub New
+' 
+'         Function: CheckDimension, createColumnVector, createDataframe, fromList, is_ableConverts
+'                   RDataframe, TracebackDataFrmae, tryTypeLineage
+' 
+'         Sub: [addHandler]
+' 
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.ApplicationServices.Debugging.Diagnostics
 Imports Microsoft.VisualBasic.ComponentModel.Collection
+Imports Microsoft.VisualBasic.ComponentModel.Collection.Generic
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Linq
@@ -163,6 +164,39 @@ Namespace Runtime.Internal.Object.Converts
             Else
                 Return data
             End If
+        End Function
+
+        <Extension>
+        Public Function RDataframe(Of T As {INamedValue, DynamicPropertyBase(Of String)})(rows As IEnumerable(Of T)) As dataframe
+            Dim columns As New Dictionary(Of String, List(Of String))
+            Dim all As T() = rows.ToArray
+            Dim allProps As String() = all _
+                .Select(Function(t1) t1.Properties.Keys) _
+                .IteratesALL _
+                .Distinct _
+                .ToArray
+            Dim names As New List(Of String)
+
+            For Each name As String In allProps
+                columns.Add(name, New List(Of String))
+            Next
+
+            For Each row As T In all
+                For Each name As String In allProps
+                    Call columns(name).Add(row(name))
+                Next
+
+                Call names.Add(row.Key)
+            Next
+
+            Return New dataframe With {
+                .columns = columns _
+                    .ToDictionary(Function(c) c.Key,
+                                  Function(c)
+                                      Return DirectCast(c.Value.ToArray, Array)
+                                  End Function),
+                .rownames = names.ToArray
+            }
         End Function
 
         ''' <summary>
