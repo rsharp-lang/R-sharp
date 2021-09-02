233aa1266293f7f67b99069e8542f5add44234b6
guigang xie <gg.xie@bionovogene.com>
2021-09-02 19:34:38 +0800

    Create DataframeGroup.vb

---
+++ R#/Runtime/Internal/objects/Linq/DataframeGroup.vb
@@ -0,0 +1,12 @@
+﻿Imports System.Runtime.CompilerServices
+
+Namespace Runtime.Internal.Object
+
+    Public Module DataframeGroup
+
+        <Extension>
+        Public Function GroupBy(data As dataframe, key As String) As Dictionary(Of String, dataframe)
+
+        End Function
+    End Module
+End Namespace
