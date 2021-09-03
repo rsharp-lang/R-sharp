2130b6979959e8705720bdfefad3a84b8702007d
xieguigang <xie.guigang@gcmodeller.org>
2021-09-04 03:03:35 +0800

    how to use XGBoost?

---
+++ studio/Rsharp_kit/MLkit/xgboost.vb
@@ -0,0 +1,19 @@
+﻿
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.Rsharp.Runtime.Internal.Object
+
+<Package("xgboost")>
+Public Module xgboost
+
+    <ExportAPI("xgboost")>
+    Public Function xgboost()
+
+    End Function
+
+    <ExportAPI("xgb.DMatrix")>
+    Public Function DMatrix(data As dataframe, label As String())
+
+    End Function
+
+End Module
