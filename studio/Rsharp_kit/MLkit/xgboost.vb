babbbc06db0c2caa9b01047b87773358fdb43d78
xieguigang <xie.guigang@gcmodeller.org>
2021-09-04 10:49:08 +0800

    Update xgboost.vb

--- studio/Rsharp_kit/MLkit/xgboost.vb
+++ studio/Rsharp_kit/MLkit/xgboost.vb
@@ -1,18 +1,49 @@
 ﻿
 Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.MachineLearning.XGBoost.util
 Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.Rsharp.Runtime
 Imports SMRUCC.Rsharp.Runtime.Internal.Object
+Imports SMRUCC.Rsharp.Runtime.Interop
 
+''' <summary>
+''' Extreme Gradient Boosting
+''' </summary>
 <Package("xgboost")>
 Public Module xgboost
 
+    ''' <summary>
+    ''' eXtreme Gradient Boosting Training
+    ''' </summary>
+    ''' <param name="data"></param>
+    ''' <param name="label"></param>
+    ''' <param name="params"></param>
+    ''' <param name="env"></param>
+    ''' <returns></returns>
     <ExportAPI("xgboost")>
-    Public Function xgboost()
+    Public Function xgboost(data As Object, label As Object,
+                            <RListObjectArgument>
+                            Optional params As list = Nothing,
+                            Optional env As Environment = Nothing)
 
     End Function
 
+    ''' <summary>
+    ''' ### Construct xgb.DMatrix object
+    ''' 
+    ''' Construct xgb.DMatrix object from either a dense matrix, 
+    ''' a sparse matrix, or a local file. Supported input file 
+    ''' formats are either a libsvm text file or a binary file 
+    ''' that was created previously by xgb.DMatrix.save).
+    ''' </summary>
+    ''' <param name="data">
+    ''' a matrix object (either numeric or integer), a dgCMatrix 
+    ''' object, or a character string representing a filename.
+    ''' </param>
+    ''' <param name="label"></param>
+    ''' <returns></returns>
     <ExportAPI("xgb.DMatrix")>
-    Public Function DMatrix(data As dataframe, label As String())
+    Public Function DMatrix(data As dataframe, label As String()) As DMatrix
 
     End Function
 
