838544d797bd61890fac737183521c6ee35e7689
xieguigang <xie.guigang@gcmodeller.org>
2021-09-10 22:50:25 +0800

    run lena test success

--- Library/demo/imageProcessing/effects.R
+++ Library/demo/imageProcessing/effects.R
@@ -5,3 +5,23 @@ setwd(@dir);
 bitmap(file = "./lena_emboss.png") {
 	readImage("lena.jpg") |> emboss;
 }
+
+bitmap(file = "./lena_pencil.png") {
+	readImage("lena.jpg") |> pencil;
+}
+
+bitmap(file = "./lena_wood_carving.png") {
+	readImage("lena.jpg") |> wood_carving;
+}
+
+bitmap(file = "./lena_diffusion.png") {
+	readImage("lena.jpg") |> diffusion;
+}
+
+bitmap(file = "./lena_soft.png") {
+	readImage("lena.jpg") |> soft;
+}
+
+bitmap(file = "./lena_sharp.png") {
+	readImage("lena.jpg") |> sharp;
+}
