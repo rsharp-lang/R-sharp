1548096931a0ea9e81c48d896e0bd30fee07c441
guigang xie <gg.xie@bionovogene.com>
2021-09-18 09:56:06 +0800

    colSums

--- R#/Runtime/Internal/invoke.vb
+++ R#/Runtime/Internal/invoke.vb
@@ -1,48 +1,48 @@
 ﻿#Region "Microsoft.VisualBasic::ec5891de760637ac7eafafff5071cf30, R#\Runtime\Internal\invoke.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Class invoke
-    ' 
-    '         Properties: ls
-    ' 
-    '         Constructor: (+1 Overloads) Sub New
-    ' 
-    '         Function: [stop], getFunction, invalidParameter, invokeInternals, missingParameter
-    ' 
-    '         Sub: pushEnvir
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Class invoke
+' 
+'         Properties: ls
+' 
+'         Constructor: (+1 Overloads) Sub New
+' 
+'         Function: [stop], getFunction, invalidParameter, invokeInternals, missingParameter
+' 
+'         Sub: pushEnvir
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -116,6 +116,7 @@ Namespace Runtime.Internal
             Call GetType(ranking).DoCall(AddressOf pushEnvir)
             Call GetType(TableJoint).DoCall(AddressOf pushEnvir)
             Call GetType(Rinterop).DoCall(AddressOf pushEnvir)
+            Call GetType(dataframeBase).DoCall(AddressOf pushEnvir)
         End Sub
 
         Private Shared Sub pushEnvir(baseModule As Type)
---
+++ R#/Runtime/Internal/objects/base/dataframeBase.vb
@@ -0,0 +1,21 @@
+﻿
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports REnv = SMRUCC.Rsharp.Runtime
+
+Namespace Runtime.Internal.Object
+
+    Module dataframeBase
+
+        <ExportAPI("colSums")>
+        Public Function colSums(x As dataframe, Optional env As Environment = Nothing) As vector
+            Dim names As String() = x.colnames
+            Dim vec As Double() = names _
+                .Select(Function(v)
+                            Return DirectCast(REnv.asVector(Of Double)(x.columns(v)), Double()).Sum
+                        End Function) _
+                .ToArray
+
+            Return New vector(names, vec, env)
+        End Function
+    End Module
+End Namespace
