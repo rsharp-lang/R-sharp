15d5fd1351dc3816d3c306dc180099d787d2a42a
xieguigang <xie.guigang@gcmodeller.org>
2021-10-09 11:41:32 +0800

    make bugs fixed of expression parser

--- R#/Interpreter/Syntax/SyntaxTree/ExpressionTree.vb
+++ R#/Interpreter/Syntax/SyntaxTree/ExpressionTree.vb
@@ -1,42 +1,42 @@
 ﻿#Region "Microsoft.VisualBasic::262283fb47bedbb4544cea3e22e2d646, R#\Interpreter\Syntax\SyntaxTree\ExpressionTree.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Module ExpressionTree
-    ' 
-    '         Function: CreateTree, ObjectInvoke, ParseExpressionTree, simpleSequence
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Module ExpressionTree
+' 
+'         Function: CreateTree, ObjectInvoke, ParseExpressionTree, simpleSequence
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -63,6 +63,8 @@ Namespace Interpreter.SyntaxParser
 
                 If Not expression Is Nothing Then
                     Return expression
+                ElseIf blocks = 1 AndAlso blocks(Scan0)(Scan0) = (TokenType.operator, "~") Then
+                    Return FormulaExpressionSyntax.GetExpressionLiteral(blocks, opts)
                 Else
                     ' 是一个复杂的表达式
                     Return blocks(Scan0).ParseExpressionTree(opts)
