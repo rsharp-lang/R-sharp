b94b0da050ead196080f2ac8a94373f445f95d15
xieguigang <xie.guigang@gcmodeller.org>
2021-10-13 15:08:53 +0800

    make bugs fixed of parameter overrides

--- R#/Runtime/System/InvokeParameter.vb
+++ R#/Runtime/System/InvokeParameter.vb
@@ -1,46 +1,46 @@
 ﻿#Region "Microsoft.VisualBasic::20d2f0fbbbed37289a6ebc1ef1174902, R#\Runtime\System\InvokeParameter.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Class InvokeParameter
-    ' 
-    '         Properties: haveSymbolName, index, isAcceptor, isProbablyVectorNameTuple, isSymbolAssign
-    '                     name, value
-    ' 
-    '         Constructor: (+2 Overloads) Sub New
-    '         Function: Create, CreateArguments, CreateLiterals, Evaluate, ToString
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Class InvokeParameter
+' 
+'         Properties: haveSymbolName, index, isAcceptor, isProbablyVectorNameTuple, isSymbolAssign
+'                     name, value
+' 
+'         Constructor: (+2 Overloads) Sub New
+'         Function: Create, CreateArguments, CreateLiterals, Evaluate, ToString
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -221,7 +221,9 @@ Namespace Runtime.Components
                 If Program.isException(argVal) Then
                     Return DirectCast(argVal, Message)
                 Else
-                    Call argVals.Add(keyName, argVal)
+                    If Not argVals.ContainsKey(keyName) Then
+                        Call argVals.Add(keyName, argVal)
+                    End If
 
                     If Not acceptor Is Nothing Then
                         If arg.value.isSymbolAssign Then
