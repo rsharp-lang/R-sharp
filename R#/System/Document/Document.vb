868440188546150a7a62c01b5b0a09f2992a8689
xieguigang <xie.guigang@gcmodeller.org>
2021-10-14 11:37:51 +0800

    man page not display docs comments

--- R#/System/Document/Document.vb
+++ R#/System/Document/Document.vb
@@ -77,7 +77,13 @@ Namespace Development
                 .OPTIONS = declares.parameters _
                     .SafeQuery _
                     .Select(Function(arg)
-                                Return New NamedValue(Of String)(arg.name, arg.text)
+                                Dim info As NamedValue = parameters _
+                                    .Where(Function(p) p.name = arg.name) _
+                                    .FirstOrDefault
+
+                                Return New NamedValue(Of String)(arg.name, arg.text) With {
+                                    .Description = info?.text
+                                }
                             End Function) _
                     .ToArray,
                 .PROLOG = declares.ToString,
