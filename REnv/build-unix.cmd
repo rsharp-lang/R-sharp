2d8fd0a0c479d521eddaae8701126f0e6ab66000
xieguigang <xie.guigang@gcmodeller.org>
2021-10-16 14:44:05 +0800

    build packages

--- REnv/build-unix.cmd
+++ REnv/build-unix.cmd
@@ -1,9 +1,10 @@
 @echo off
 
-SET R_HOME=D:\GCModeller\src\R-sharp\App\net5.0
+SET R_HOME=../App\net5.0
 SET Rscript="%R_HOME%\Rscript.exe"
+SET REnv="%R_HOME%/R#.exe"
 
 %Rscript% --build /save ./REnv.zip
-"%R_HOME%/R#.exe" --install.packages /module ./REnv.zip
+%REnv% --install.packages /module ./REnv.zip
 
 pause
