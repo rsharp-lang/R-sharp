6ebcf982c9d3c9e81f3a86defe34f85e94aefb9a
xieguigang <xie.guigang@gcmodeller.org>
2021-10-16 21:17:04 +0800

    fix of package namespace imports

--- REnv/R/geom.R
+++ REnv/R/geom.R
@@ -1,9 +1,11 @@
+imports "geometry2D" from "graphics";
+
 #' Evaluate geom density of 2d points 
 #' 
 #' @param qcut the quantile cutoff threshold for define 
 #'    points as low density points(noise).
 #' 
-const density2DCut as function(data, k=6, qcut = 0.1) {
+const density2DCut as function(data, k = 6, qcut = 0.1) {
     const [x, y]  = data;
     const density = density2D(x, y, k);
     const q       = quantile(density, probs = NULL); 
