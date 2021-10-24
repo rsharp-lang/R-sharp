dd25d48c37b8c0295aedfa79d2125dd52f92a929
xieguigang <xie.guigang@gcmodeller.org>
2021-10-24 15:57:48 +0800

    makes bugs fixed for code comment parser

--- R#/Interpreter/ExecuteEngine/ExpressionSymbols/CodeComment.vb
+++ R#/Interpreter/ExecuteEngine/ExpressionSymbols/CodeComment.vb
@@ -1,45 +1,45 @@
 ﻿#Region "Microsoft.VisualBasic::90e3aaa45d12a1ad052f9f1f40ed40ec, R#\Interpreter\ExecuteEngine\ExpressionSymbols\CodeComment.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Class CodeComment
-    ' 
-    '         Properties: expressionName, type
-    ' 
-    '         Constructor: (+2 Overloads) Sub New
-    '         Function: Evaluate, ToString
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Class CodeComment
+' 
+'         Properties: expressionName, type
+' 
+'         Constructor: (+2 Overloads) Sub New
+'         Function: Evaluate, ToString
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -47,6 +47,7 @@ Imports SMRUCC.Rsharp.Language.TokenIcer
 Imports SMRUCC.Rsharp.Runtime
 Imports SMRUCC.Rsharp.Runtime.Components
 Imports SMRUCC.Rsharp.Development.Package.File
+Imports Microsoft.VisualBasic.Text
 
 Namespace Interpreter.ExecuteEngine.ExpressionSymbols
 
@@ -76,6 +77,13 @@ Namespace Interpreter.ExecuteEngine.ExpressionSymbols
 
         Public Overrides Function Evaluate(envir As Environment) As Object
             Return Nothing
+        End Function
+
+        Public Shared Function FromBlockComments(block As IEnumerable(Of Token)) As CodeComment
+            Dim lines As String() = block.Select(Function(t) t.Trim("#"c, " "c)).ToArray
+            Dim text As String = lines.JoinBy(ASCII.LF)
+
+            Return New CodeComment(text)
         End Function
 
         Public Overrides Function ToString() As String
--- R#/Interpreter/Extensions.vb
+++ R#/Interpreter/Extensions.vb
@@ -1,44 +1,44 @@
 ﻿#Region "Microsoft.VisualBasic::a8334029ba39667b00d91f1faf964e6a, R#\Interpreter\Extensions.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Module Extensions
-    ' 
-    '         Function: (+2 Overloads) GetExpressions, isTerminator
-    ' 
-    '         Sub: SyntaxErrorHelper
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Module Extensions
+' 
+'         Function: (+2 Overloads) GetExpressions, isTerminator
+' 
+'         Sub: SyntaxErrorHelper
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -47,6 +47,7 @@ Imports System.Text
 Imports Microsoft.VisualBasic.ComponentModel.Collection
 Imports Microsoft.VisualBasic.Linq
 Imports SMRUCC.Rsharp.Interpreter.ExecuteEngine
+Imports SMRUCC.Rsharp.Interpreter.ExecuteEngine.ExpressionSymbols
 Imports SMRUCC.Rsharp.Interpreter.SyntaxParser
 Imports SMRUCC.Rsharp.Language
 Imports SMRUCC.Rsharp.Language.TokenIcer
@@ -153,7 +154,21 @@ Namespace Interpreter
                         .Split(2)
 
                     For Each joinBlock As Token()() In parts
-                        block = joinBlock(Scan0).JoinIterates(joinBlock.ElementAtOrDefault(1)).ToArray
+                        block = joinBlock(Scan0) _
+                            .JoinIterates(joinBlock.ElementAtOrDefault(1)) _
+                            .ToArray
+
+                        If opts.keepsCommentLines Then
+                            If block.All(Function(t) t.name = TokenType.comment) Then
+                                ' is top level code comments
+                                Yield CodeComment.FromBlockComments(block)
+                            Else
+                                block = block _
+                                    .Where(Function(t) t.name <> TokenType.comment) _
+                                    .ToArray
+                            End If
+                        End If
+
                         expr = Expression.CreateExpression(block, opts)
 
                         If expr.isException Then
