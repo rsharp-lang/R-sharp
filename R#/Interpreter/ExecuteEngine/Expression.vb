e817ad14c631011b63fd189895cfbfc96ac754f2
xieguigang <xie.guigang@gcmodeller.org>
2021-10-24 16:00:09 +0800

    fix of missing symbol reference errors

--- R#/Interpreter/ExecuteEngine/Expression.vb
+++ R#/Interpreter/ExecuteEngine/Expression.vb
@@ -1,46 +1,47 @@
 ﻿#Region "Microsoft.VisualBasic::631be5917fe1e6a3f042b0e6df5a1e8b, R#\Interpreter\ExecuteEngine\Expression.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Class Expression
-    ' 
-    '         Function: CreateExpression
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Class Expression
+' 
+'         Function: CreateExpression
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Linq
 Imports SMRUCC.Rsharp.Development.Package.File
 Imports SMRUCC.Rsharp.Interpreter.SyntaxParser
 Imports SMRUCC.Rsharp.Language
@@ -89,7 +90,11 @@ Namespace Interpreter.ExecuteEngine
         End Function
 
         Public Shared Function ParseLines(rscript As Rscript, Optional keepsCommentLines As Boolean = False) As IEnumerable(Of Expression)
-            Return rscript.GetExpressions(New SyntaxBuilderOptions With {.keepsCommentLines = keepsCommentLines})
+            Return New SyntaxBuilderOptions With {
+                .keepsCommentLines = keepsCommentLines,
+                .source = rscript
+            } _
+            .DoCall(AddressOf rscript.GetExpressions)
         End Function
     End Class
 End Namespace
