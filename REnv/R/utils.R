a65af621dd185ab50cfa542d6b0cb990a749c4db
guigang xie <gg.xie@bionovogene.com>
2021-10-28 09:37:03 +0800

    Update utils.R

---
+++ REnv/R/utils.R
@@ -0,0 +1,18 @@
+imports ["Html", "http", "graphquery"] from "webKit";
+
+#' Run graphquery on html document
+#' 
+#' @param url the url or local filepath of the target html document
+#' @param graphquery the script text of a required given graphquery
+#' 
+#' @return A ``R#`` object that parsed from the target html web 
+#'    page with given query expression value. 
+#' 
+const queryWeb as function(url, graphquery) {
+    const query = graphquery::parseQuery(graphquery);
+    
+    getHtml(url) 
+    |> Html::parse
+    |> graphquery::query(query)
+    ;
+}
