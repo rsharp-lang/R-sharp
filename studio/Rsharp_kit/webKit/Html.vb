1f63bac8da64a70c2cd4a938862393837e1fc173
guigang xie <gg.xie@bionovogene.com>
2021-11-04 09:14:51 +0800

    get html plain text

--- studio/Rsharp_kit/webKit/Html.vb
+++ studio/Rsharp_kit/webKit/Html.vb
@@ -1,45 +1,46 @@
 ﻿#Region "Microsoft.VisualBasic::ea06e7d756b52e47ee4d3c4803a7b776, studio\Rsharp_kit\webKit\Html.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module Html
-    ' 
-    '     Constructor: (+1 Overloads) Sub New
-    '     Function: documentDebugView, parse, QueryHtmlTables
-    ' 
-    ' /********************************************************************************/
+' Module Html
+' 
+'     Constructor: (+1 Overloads) Sub New
+'     Function: documentDebugView, parse, QueryHtmlTables
+' 
+' /********************************************************************************/
 
 #End Region
 
+Imports System.Runtime.CompilerServices
 Imports System.Text
 Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.MIME.Html.Document
@@ -113,18 +114,10 @@ Module Html
 
             For Each column As String() In matrix
                 ' 20210422 请注意，下面的两个空格符号是不一样的
-                Dim name As String = column(Scan0) _
-                    .StripHTMLTags _
-                    .UnescapeHTML _
-                    .Trim(" "c, ASCII.CR, ASCII.LF, ASCII.TAB, " "c, ASCII.NUL)
+                Dim name As String = column(Scan0).getPlainText
                 Dim data As String() = column _
                     .Skip(1) _
-                    .Select(Function(cell)
-                                Return cell _
-                                    .StripHTMLTags _
-                                    .UnescapeHTML.TrimNull _
-                                    .Trim(" "c, ASCII.CR, ASCII.LF, ASCII.TAB, " "c, ASCII.NUL)
-                            End Function) _
+                    .Select(AddressOf getPlainText) _
                     .ToArray
 
                 table.columns(name) = data
@@ -136,4 +129,18 @@ Module Html
         Return result
     End Function
 
+    ''' <summary>
+    ''' get all internal document text from the given html document text. 
+    ''' </summary>
+    ''' <param name="html"></param>
+    ''' <returns></returns>
+    <ExportAPI("plainText")>
+    <Extension>
+    Public Function getPlainText(html As String) As String
+        Return html _
+            .StripHTMLTags _
+            .UnescapeHTML _
+            .TrimNull _
+            .Trim(" "c, ASCII.CR, ASCII.LF, ASCII.TAB, " "c, ASCII.NUL)
+    End Function
 End Module
