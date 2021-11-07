28c3d2cdd7c6c0276e4a1665b18cef7283dfebad
xieguigang <xie.guigang@gcmodeller.org>
2021-11-07 15:37:59 +0800

    config test

--- Library/demo/machineLearning/NLP/segmentation.R
+++ Library/demo/machineLearning/NLP/segmentation.R
@@ -2,6 +2,10 @@ imports "NLP" from "MLkit";
 
 options(strict = FALSE);
 
-data = NLP::segmentation(readText("E:\GCModeller\src\runtime\sciBASIC#\Data\Trinity\alice30.txt"));
+data = NLP::segmentation(readText("E:\biodeep\biodeepdb_v3\Flavor\wikipedia\raw\honey\terraria.fandom.com\wiki\Honey.txt"));
 
-print(data);
+# print(data);
+
+for(p in data) {
+	print(sapply(as.object(p)$sentences, toString));
+}
--- studio/Rsharp_kit/MLkit/NLP.vb
+++ studio/Rsharp_kit/MLkit/NLP.vb
@@ -1,4 +1,5 @@
 ﻿Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Data.NLP
 Imports Microsoft.VisualBasic.Data.NLP.Model
 Imports Microsoft.VisualBasic.Scripting.MetaData
 
@@ -11,6 +12,15 @@ Module NLP
     <ExportAPI("segmentation")>
     Public Function Tokenice(text As String) As Paragraph()
         Return Paragraph.Segmentation(text).ToArray
+    End Function
+
+    <ExportAPI("article")>
+    Public Function CrawlerText(html As String,
+                                Optional depth As Integer = 6,
+                                Optional limitCount As Integer = 180,
+                                Optional appendMode As Boolean = False) As Article
+
+        Return Article.ParseText(html, depth, limitCount, appendMode)
     End Function
 
 End Module
