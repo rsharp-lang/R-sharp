5da4086a00abc1ac3fc663a61fe47a888bff7b3e
xieguigang <xie.guigang@gcmodeller.org>
2021-11-07 12:34:55 +0800

    add demo api

---
+++ studio/Rsharp_kit/MLkit/NLP.vb
@@ -0,0 +1,16 @@
+﻿Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Data.NLP.Model
+Imports Microsoft.VisualBasic.Scripting.MetaData
+
+''' <summary>
+''' NLP tools
+''' </summary>
+<Package("NLP")>
+Module NLP
+
+    <ExportAPI("segmentation")>
+    Public Function Tokenice(text As String) As Paragraph()
+        Return Paragraph.Segmentation(text).ToArray
+    End Function
+
+End Module
