1d60c01a786d0e994611014e185b2afebef26a7d
xieguigang <xie.guigang@gcmodeller.org>
2021-11-09 19:26:57 +0800

    search nuget

---
+++ R#/System/Package/NuGet/Search.vb
@@ -0,0 +1,44 @@
+﻿Namespace Development.Package.NuGet
+
+    Public Class Search
+
+        Public Property data As PackageData()
+        Public Property totalHits As Integer
+
+        Public Shared Narrowing Operator CType(search As Search) As PackageData()
+            Return search.data
+        End Operator
+
+    End Class
+
+    Public Class PackageData
+
+        Public Property authors As String()
+        Public Property description As String
+        Public Property iconUrl As String
+        Public Property id As String
+        Public Property licenseUrl As String
+        Public Property owners As String()
+        Public Property packageTypes As PackageType()
+        Public Property projectUrl As String
+        Public Property registration As String
+        Public Property summary As String
+        Public Property tags As String()
+        Public Property title As String
+        Public Property totalDownloads As Integer
+        Public Property verified As Boolean
+        Public Property version As String
+        Public Property versions As PackageVersion()
+
+    End Class
+
+    Public Class PackageVersion
+        Public Property version As String
+        Public Property downloads As Integer
+    End Class
+
+    Public Class PackageType
+
+        Public Property name As String
+    End Class
+End Namespace
---
+++ R#/System/Package/NuGet/WebRequest.vb
@@ -0,0 +1,43 @@
+﻿Imports Microsoft.VisualBasic.Serialization.JSON
+
+Namespace Development.Package.NuGet
+
+    Public Enum NuGetMirrors
+        nuget
+        azuresearch
+        azure_cn
+    End Enum
+
+    ''' <summary>
+    ''' use nuget package repository system as R# package repository
+    ''' </summary>
+    Public Class WebRequest
+
+        Public Shared ReadOnly mirrors As IReadOnlyDictionary(Of String, String) =
+            New Dictionary(Of String, String) From {
+                {"azuresearch", "https://azuresearch-usnc.nuget.org"}
+            }
+
+        Shared m_mirror As String
+
+        Sub New()
+            Call SetMirror(NuGetMirrors.azuresearch)
+        End Sub
+
+        Public Shared Sub SetMirror(mirror As String)
+            WebRequest.m_mirror = mirror
+        End Sub
+
+        Public Shared Sub SetMirror(mirror As NuGetMirrors)
+            WebRequest.m_mirror = mirrors(mirror.Description)
+        End Sub
+
+        Public Shared Function Query(term As String, Optional pre_release As Boolean = False) As PackageData()
+            Dim url As String = $"{m_mirror}/query?q={term.UrlEncode}&prerelease={pre_release.ToString.ToLower}"
+            Dim json As String = url.GET
+            Dim data = json.LoadJSON(Of NuGet.Search)
+
+            Return data
+        End Function
+    End Class
+End Namespace
