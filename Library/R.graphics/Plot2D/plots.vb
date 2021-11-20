0cacd57bdba9e5cefe1ee31b5393f69dcc3d1c23
xieguigang <xie.guigang@gcmodeller.org>
2021-11-20 11:15:29 +0800

    tweaks of the axis scale

--- Library/R.graphics/Plot2D/plots.vb
+++ Library/R.graphics/Plot2D/plots.vb
@@ -1,47 +1,47 @@
 ﻿#Region "Microsoft.VisualBasic::e496e7f532360ad98f12fe3e85b1feb7, Library\R.graphics\Plot2D\plots.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module plots
-    ' 
-    '     Function: barplot, ContourPlot, CreateSerial, doViolinPlot, findNumberVector
-    '               measureDataTable, modelWithClass, modelWithoutClass, plot_binBox, plot_categoryBars
-    '               plot_corHeatmap, plot_deSolveResult, plot_hclust, plotArray, plotContourLayers
-    '               plotFormula, plotLinearYFit, plotLmCall, plotODEResult, plotPieChart
-    '               PlotPolygon, plotSerials, plotVector, UpSetPlot
-    ' 
-    '     Sub: Main
-    ' 
-    ' /********************************************************************************/
+' Module plots
+' 
+'     Function: barplot, ContourPlot, CreateSerial, doViolinPlot, findNumberVector
+'               measureDataTable, modelWithClass, modelWithoutClass, plot_binBox, plot_categoryBars
+'               plot_corHeatmap, plot_deSolveResult, plot_hclust, plotArray, plotContourLayers
+'               plotFormula, plotLinearYFit, plotLmCall, plotODEResult, plotPieChart
+'               PlotPolygon, plotSerials, plotVector, UpSetPlot
+' 
+'     Sub: Main
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -716,6 +716,7 @@ Module plots
         Dim spline As Splines = args.getValue(Of Splines)("interplot", env, Splines.None)
         Dim xlim As Double = args.getValue("xlim", env, Double.NaN)
         Dim ylim As Double = args.getValue("ylim", env, Double.NaN)
+        Dim absoluteScale As Boolean = args.getValue("absolute_scale", env, False)
 
         If args.hasName("showLegend") Then
             showLegend = getFirst(asLogical(args!showLegend))
@@ -742,7 +743,9 @@ Module plots
             axisLabelCSS:=args.getValue("axis.cex", env, CSSFont.Win7VeryLarge),
             gridFill:=RColorPalette.getColor(args("grid.fill"), "lightgray"),
             xlim:=xlim,
-            ylim:=ylim
+            ylim:=ylim,
+            XaxisAbsoluteScalling:=absoluteScale,
+            YaxisAbsoluteScalling:=absoluteScale
         )
     End Function
 
