37b5577ba17d3d818c83c6ad0fab522766ad9b16
guigang xie <gg.xie@bionovogene.com>
2021-11-23 14:35:53 +0800

    Update inspect_CDF.R

--- Library/demo/buffer/inspect_CDF.R
+++ Library/demo/buffer/inspect_CDF.R
@@ -1,6 +1,7 @@
 require(netCDF.utils);
 
 options(strict = FALSE);
+options(max.print = 100);
 
 using cdf as open.netCDF("F:\20211123_CDF\P210702366.cdf") {
 	print(cdf |> dimensions);
@@ -8,4 +9,9 @@ using cdf as open.netCDF("F:\20211123_CDF\P210702366.cdf") {
 	print(cdf |> variables);
 	
 	print(cdf |> var("total_intensity"));
+	print(cdf |> attr("total_intensity"));
+	print(cdf |> attr("mass_values"));
+	print(cdf |> attr("intensity_values"));
+	
+	print(cdf |> var("scan_index") |> getValue());
 }
