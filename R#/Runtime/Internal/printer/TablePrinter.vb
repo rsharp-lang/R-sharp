3ad62cdb1aacac8c50d64f4c462ea96b86006772
guigang xie <gg.xie@bionovogene.com>
2021-12-02 11:44:29 +0800

    create table print data

--- R#/Runtime/Internal/printer/TablePrinter.vb
+++ R#/Runtime/Internal/printer/TablePrinter.vb
@@ -1,13 +1,31 @@
 ﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.ApplicationServices.Terminal.TablePrinter
+Imports Microsoft.VisualBasic.ApplicationServices.Terminal.TablePrinter.Flags
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports Microsoft.VisualBasic.Linq
 Imports SMRUCC.Rsharp.Runtime.Internal.Object
 
 Namespace Runtime.Internal.ConsolePrinter
 
-    Module TablePrinter
+    Module tablePrinter
+
+        <Extension>
+        Public Function ToContent(table As dataframe) As ConsoleTableBaseData
+            Dim data As New ConsoleTableBaseData With {
+                .Column = New List(Of Object)(New Object() {""}.JoinIterates(table.colnames)),
+                .Rows = New List(Of List(Of Object))
+            }
+
+            For Each row As NamedCollection(Of Object) In table.forEachRow
+                Call data.AppendLine(New Object() {row.name}.JoinIterates(row))
+            Next
+
+            Return data
+        End Function
 
         <Extension>
         Public Sub PrintTable(table As dataframe, output As RContentOutput, env As GlobalEnvironment)
-
+            Call ConsoleTableBuilder.From(table.ToContent).WithFormat(ConsoleTableBuilderFormat.Minimal).ExportAndWriteLine()
         End Sub
     End Module
 End Namespace
