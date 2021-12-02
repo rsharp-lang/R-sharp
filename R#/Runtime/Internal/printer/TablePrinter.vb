ac1e7505b380010c754feebe08566e1f646faa3c
guigang xie <gg.xie@bionovogene.com>
2021-12-02 11:16:23 +0800

    how to make table printer

---
+++ R#/Runtime/Internal/printer/TablePrinter.vb
@@ -0,0 +1,13 @@
+﻿Imports System.Runtime.CompilerServices
+Imports SMRUCC.Rsharp.Runtime.Internal.Object
+
+Namespace Runtime.Internal.ConsolePrinter
+
+    Module TablePrinter
+
+        <Extension>
+        Public Sub PrintTable(table As dataframe, output As RContentOutput, env As GlobalEnvironment)
+
+        End Sub
+    End Module
+End Namespace
--- R#/Runtime/Internal/printer/printer.vb
+++ R#/Runtime/Internal/printer/printer.vb
@@ -1,52 +1,52 @@
 ﻿#Region "Microsoft.VisualBasic::fdf6fc7f45c982ee4d91b8ce9e682964, R#\Runtime\Internal\printer\printer.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Delegate Function
-    ' 
-    ' 
-    '     Module printer
-    ' 
-    '         Constructor: (+1 Overloads) Sub New
-    ' 
-    '         Function: DateToString, f64_InternalToString, getMaxColumns, getStrings, printStream
-    '                   ToString, ValueToString
-    ' 
-    '         Sub: AttachConsoleFormatter, AttachInternalConsoleFormatter, printArray, printContentArray, printInternal
-    '              printList
-    ' 
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Delegate Function
+' 
+' 
+'     Module printer
+' 
+'         Constructor: (+1 Overloads) Sub New
+' 
+'         Function: DateToString, f64_InternalToString, getMaxColumns, getStrings, printStream
+'                   ToString, ValueToString
+' 
+'         Sub: AttachConsoleFormatter, AttachInternalConsoleFormatter, printArray, printContentArray, printInternal
+'              printList
+' 
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -218,10 +218,7 @@ Namespace Runtime.Internal.ConsolePrinter
                                 DirectCast(list, IDictionary).printList(listPrefix, maxPrint, env)
                             End Sub)
             ElseIf valueType Is GetType(dataframe) Then
-                Call TableFormatter _
-                    .GetTable(DirectCast(x, dataframe), env) _
-                    .Print(addBorder:=False) _
-                    .DoCall(AddressOf output.WriteLine)
+                Call TablePrinter.PrintTable(DirectCast(x, dataframe), output, env)
             ElseIf valueType Is GetType(vbObject) Then
                 Call DirectCast(x, vbObject).ToString.DoCall(AddressOf output.WriteLine)
             Else
