eca219a5d82b0d8dfd49e6387f7a467a17e4e743
guigang xie <gg.xie@bionovogene.com>
2021-12-02 13:53:58 +0800

    how to move data type to headers?

--- R#/Runtime/Internal/printer/TablePrinter.vb
+++ R#/Runtime/Internal/printer/TablePrinter.vb
@@ -4,6 +4,7 @@ Imports Microsoft.VisualBasic.ApplicationServices.Terminal.TablePrinter.Flags
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports Microsoft.VisualBasic.Linq
 Imports SMRUCC.Rsharp.Runtime.Internal.Object
+Imports SMRUCC.Rsharp.Runtime.Interop
 Imports REnv = SMRUCC.Rsharp.Runtime
 
 Namespace Runtime.Internal.ConsolePrinter
@@ -12,14 +13,17 @@ Namespace Runtime.Internal.ConsolePrinter
 
         <Extension>
         Public Iterator Function ToContent(table As dataframe, env As GlobalEnvironment) As IEnumerable(Of ConsoleTableBaseData)
-            Dim rowsNames As String() = table.getRowNames
+            Dim rowsNames As String() = {"<mode>"}.JoinIterates(table.getRowNames).ToArray
             Dim maxRowNames As Integer = rowsNames.MaxLengthString.Length
             Dim maxColumns As Integer = env.getMaxColumns
             Dim nrows As Integer = table.nrows
             Dim columns As NamedCollection(Of String)() = table.colnames _
                 .Select(Function(colname)
-                            Dim arr As String() = REnv.asVector(Of String)(table(colname))
-                            Dim max As String = {colname}.JoinIterates(arr).MaxLengthString
+                            Dim type As Type = Nothing
+                            Dim arr As String() = printer.getStrings(table(colname), type, env).ToArray
+                            Dim max As String = {colname, RType.GetRSharpType(type).ToString} _
+                                .JoinIterates(arr) _
+                                .MaxLengthString
 
                             arr = arr _
                                 .Select(Function(str)
@@ -33,7 +37,9 @@ Namespace Runtime.Internal.ConsolePrinter
 
                             Return New NamedCollection(Of String) With {
                                 .name = New String(" "c, max.Length - colname.Length) & colname,
-                                .value = arr,
+                                .value = {RType.GetRSharpType(type).ToString} _
+                                    .JoinIterates(arr) _
+                                    .ToArray,
                                 .description = max.Length
                             }
                         End Function) _
@@ -72,7 +78,7 @@ Namespace Runtime.Internal.ConsolePrinter
             }
             Dim index As Integer
 
-            For i As Integer = 0 To nrows - 1
+            For i As Integer = 0 To nrows
                 index = i
                 data.AppendLine(part.Select(Function(v) CObj(v(index))))
             Next
