c27c8806651598383a8adff6673f4a9ca0b794f0
guigang xie <gg.xie@bionovogene.com>
2021-12-03 15:15:13 +0800

    R data handler

---
+++ studio/RData/Convertor/ConvertToR.vb
@@ -0,0 +1,32 @@
+﻿Imports System.Runtime.CompilerServices
+Imports SMRUCC.Rsharp.Runtime.Internal.Object
+
+''' <summary>
+''' Convert to R# object
+''' </summary>
+Public Module ConvertToR
+
+    ''' <summary>
+    ''' Convert to R# object
+    ''' </summary>
+    ''' <param name="rdata"></param>
+    ''' <returns></returns>
+    Public Function ToRObject(rdata As RData) As Object
+
+    End Function
+
+    <Extension>
+    Private Function CreatePairList(robj As RObject) As list
+
+    End Function
+
+    <Extension>
+    Private Function CreateRVector(robj As RObject) As vector
+
+    End Function
+
+    <Extension>
+    Private Function CreateRTable(robj As RObject) As dataframe
+
+    End Function
+End Module
---
+++ studio/RData/Convertor/Deserializer.vb
@@ -0,0 +1,10 @@
+﻿Imports System.Runtime.CompilerServices
+
+Public Module Deserializer
+
+    <Extension>
+    Public Function ToObject(Of T As Class)(robj As RObject) As T
+
+    End Function
+
+End Module
