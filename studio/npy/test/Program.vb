cbb1cfd067063290f4c427c637381badcfd64774
xieguigang <xie.guigang@gcmodeller.org>
2021-12-04 21:36:25 +0800

    try to disable remove prefix while space in token scanner

--- studio/npy/test/Program.vb
+++ studio/npy/test/Program.vb
@@ -3,15 +3,30 @@ Imports SMRUCC.Python.Language
 
 Module Program
     Sub Main(args As String())
-
+        Call parseFunction()
         Call parseHelloWorld()
 
+    End Sub
+
+    Sub parseFunction()
+        Dim func = "
+def hello(): 
+
+   print(""hello world!"")
+   x = message
+
+"
+        Dim scanner As New PyScanner(func)
+        Dim tokens = scanner.GetTokens.ToArray
+
+        Pause()
     End Sub
 
     Sub parseHelloWorld()
         Dim hello = "print(""Hello World!"")"
         Dim scanner As New PyScanner(hello)
         Dim tokens = scanner.GetTokens.ToArray
+
 
         Pause()
     End Sub
