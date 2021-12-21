d90ca7046867ea6725f38073a5d07cce56933641
xieguigang <xie.guigang@gcmodeller.org>
2021-12-21 22:28:47 +0800

    fix of a possible null reference error

--- R#/Interpreter/ExecuteEngine/ExpressionSymbols/DataSet/LinqQuery.vb
+++ R#/Interpreter/ExecuteEngine/ExpressionSymbols/DataSet/LinqQuery.vb
@@ -1,45 +1,45 @@
 ﻿#Region "Microsoft.VisualBasic::55dc39e5ea06e7a70d280c40c2a14c10, R#\Interpreter\ExecuteEngine\ExpressionSymbols\DataSet\LinqQuery.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Class LinqQuery
-    ' 
-    '         Properties: expressionName, stackFrame, type
-    ' 
-    '         Constructor: (+1 Overloads) Sub New
-    '         Function: Evaluate, newDataFrame, produceSequenceVector, ToString
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Class LinqQuery
+' 
+'         Properties: expressionName, stackFrame, type
+' 
+'         Constructor: (+1 Overloads) Sub New
+'         Function: Evaluate, newDataFrame, produceSequenceVector, ToString
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -136,7 +136,9 @@ Namespace Interpreter.ExecuteEngine.ExpressionSymbols.DataSets
         End Function
 
         Friend Shared Function produceSequenceVector(sequence As Object, ByRef isList As Boolean) As Object
-            If sequence.GetType Is GetType(list) Then
+            If sequence Is Nothing Then
+                Return New Object() {}
+            ElseIf sequence.GetType Is GetType(list) Then
                 sequence = DirectCast(sequence, list).slots
             End If
 
