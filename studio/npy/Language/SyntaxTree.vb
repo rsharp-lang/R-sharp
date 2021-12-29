d0f0fd5208ac962cb010ea01d94449a172f71f20
guigang xie <gg.xie@bionovogene.com>
2021-12-29 14:28:58 +0800

    fix of project reference errors

--- studio/npy/Language/SyntaxTree.vb
+++ studio/npy/Language/SyntaxTree.vb
@@ -1,13 +1,10 @@
-﻿Imports System.Data
-Imports System.Runtime.CompilerServices
-Imports Microsoft.VisualBasic.ApplicationServices.Debugging.Diagnostics
+﻿Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.ComponentModel.Collection
 Imports Microsoft.VisualBasic.Language
 Imports SMRUCC.Python.Language
 Imports SMRUCC.Rsharp.Interpreter
 Imports SMRUCC.Rsharp.Interpreter.ExecuteEngine
 Imports SMRUCC.Rsharp.Interpreter.ExecuteEngine.ExpressionSymbols
-Imports SMRUCC.Rsharp.Interpreter.ExecuteEngine.ExpressionSymbols.Blocks
 Imports SMRUCC.Rsharp.Interpreter.ExecuteEngine.ExpressionSymbols.Closure
 Imports SMRUCC.Rsharp.Interpreter.ExecuteEngine.ExpressionSymbols.DataSets
 Imports SMRUCC.Rsharp.Interpreter.ExecuteEngine.ExpressionSymbols.Operators
@@ -15,7 +12,7 @@ Imports SMRUCC.Rsharp.Interpreter.SyntaxParser
 Imports SMRUCC.Rsharp.Interpreter.SyntaxParser.SyntaxImplements
 Imports SMRUCC.Rsharp.Language
 Imports SMRUCC.Rsharp.Language.TokenIcer
-Imports SMRUCC.Rsharp.Runtime.Components
+Imports Rscript = SMRUCC.Rsharp.Runtime.Components.Rscript
 
 Public Module SyntaxTree
 
