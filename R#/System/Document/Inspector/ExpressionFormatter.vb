b27d26f96ebfc44a3341d38a29aeb164b70ffcd4
xieguigang <xie.guigang@gcmodeller.org>
2022-01-10 21:23:47 +0800

    how to create formatted script text?

---
+++ R#/System/Document/Inspector/ExpressionFormatter.vb
@@ -0,0 +1,30 @@
+﻿Imports System.IO
+Imports SMRUCC.Rsharp.Interpreter.ExecuteEngine
+Imports SMRUCC.Rsharp.Interpreter.ExecuteEngine.ExpressionSymbols.Operators
+
+Namespace Development.Inspector
+
+    Public Class ExpressionFormatter
+
+        Dim text As TextWriter
+        Dim indent As Integer = 0
+
+        Sub New(text As TextWriter)
+            Me.text = text
+        End Sub
+
+        Public Sub WriteScript(expr As Expression)
+            If TypeOf expr Is ValueAssignExpression Then
+
+            End If
+
+        End Sub
+
+
+
+        Public Shared Sub WriteScript(expr As Expression, text As TextWriter)
+            Call New ExpressionFormatter(text).WriteScript(expr)
+        End Sub
+
+    End Class
+End Namespace
