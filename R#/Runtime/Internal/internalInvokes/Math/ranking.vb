db8983c9cdfde8bdf16aa69e9b605970fda19cd4
xieguigang <xie.guigang@gcmodeller.org>
2022-01-13 11:37:53 +0800

    try to make Performance improvements of order() function

--- R#/Runtime/Internal/internalInvokes/Math/ranking.vb
+++ R#/Runtime/Internal/internalInvokes/Math/ranking.vb
@@ -1,42 +1,42 @@
 ﻿#Region "Microsoft.VisualBasic::a11cb6969da2d2ef519f473d9688dba7, R#\Runtime\Internal\internalInvokes\Math\ranking.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Module ranking
-    ' 
-    '         Function: order, orderNumbers, orderStrings, rank
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Module ranking
+' 
+'         Function: order, orderNumbers, orderStrings, rank
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -171,10 +171,16 @@ Namespace Runtime.Internal.Invokes
             Dim rank As Integer() = x.OrdinalRankingOrder(desc:=decreasing)
             Dim ii As New List(Of Integer)
             Dim di As Integer
+            Dim test As (flag As Boolean, i As Integer)
 
             For i As Integer = 1 To rank.Length
                 di = i
-                ii.Add(which(rank.Select(Function(xi) xi = di)).First + 1)
+                test = rank _
+                    .Select(Function(xi, j) (xi = di, j)) _
+                    .Where(Function(xi) xi.Item1) _
+                    .First
+
+                Call ii.Add(test.i + 1)
             Next
 
             Return ii.ToArray
