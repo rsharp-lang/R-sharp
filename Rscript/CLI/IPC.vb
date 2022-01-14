eb9b405e7207b99b4bfb47d6ee56a2e89ea36e5f
xieguigang <xie.guigang@gcmodeller.org>
2022-01-14 20:33:26 +0800

    still not working on linux platform?

--- Rscript/CLI/IPC.vb
+++ Rscript/CLI/IPC.vb
@@ -1,41 +1,41 @@
 ﻿#Region "Microsoft.VisualBasic::907c5f21ad6c35b2f903cfa1bae93743, Rscript\CLI\IPC.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module CLI
-    ' 
-    '     Function: postResult, slaveMode, tryHandleJSON
-    ' 
-    ' /********************************************************************************/
+' Module CLI
+' 
+'     Function: postResult, slaveMode, tryHandleJSON
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -159,13 +159,25 @@ Partial Module CLI
 
         R.debug = isDebugMode
 
+        If isDebugMode Then
+            Call Console.WriteLine("[init] load startup packages")
+        End If
+
         For Each pkgName As String In R.configFile.GetStartupLoadingPackages
             Call R.LoadLibrary(packageName:=pkgName)
         Next
 
+        If isDebugMode Then
+            Call Console.WriteLine("[load] source target script...")
+        End If
+
         result = R.Source(script, parameters)
 
         If TypeOf result Is Message Then
+            If isDebugMode Then
+                Call Console.WriteLine("[end] script text contains invalid syntax?")
+            End If
+
             Return R.globalEnvir.postResult(
                 result:=result,
                 master:=upstream,
@@ -179,6 +191,11 @@ Partial Module CLI
             End If
 
             result = R.Invoke(entry, parameters)
+        Else
+        End If
+
+        If isDebugMode Then
+            Call Console.WriteLine("[end] finalized")
         End If
 
         ' post result data back to the master node
