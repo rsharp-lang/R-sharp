2e1610255d186d569efbafb1460ed92e7ad6cc15
xieguigang <xie.guigang@gcmodeller.org>
2022-01-21 23:32:51 +0800

    complex type?

---
+++ R#/Runtime/Internal/objects/dataset/complex.vb
@@ -0,0 +1,12 @@
+﻿Namespace Runtime.Internal.Object
+
+    ''' <summary>
+    ''' 复数
+    ''' </summary>
+    Public Class complex
+
+        Public Property real As Double
+        Public Property image As Double
+
+    End Class
+End Namespace
