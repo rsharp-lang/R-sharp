3bea73be07cd858c3c5e916de641872b169406bd
xieguigang <xie.guigang@gcmodeller.org>
2022-01-21 23:24:33 +0800

    Update pisum.jl

--- studio/test/julia/pisum.jl
+++ studio/test/julia/pisum.jl
@@ -3,8 +3,8 @@
 function pisum()
     sum = 0.0
     for j = 1:500
-        sum = 0.0
-        for k = 1:10000
+        # sum = 0.0
+        for k = 1:100
             sum += 1.0/(k*k)
         end
     end
