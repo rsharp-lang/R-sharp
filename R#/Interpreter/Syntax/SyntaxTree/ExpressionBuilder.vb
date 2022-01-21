e56773e830f443737879dd35b9536ed3a09763a8
xieguigang <xie.guigang@gcmodeller.org>
2022-01-21 22:40:44 +0800

    fix of iterate value assign

--- R#/Interpreter/Syntax/SyntaxTree/ExpressionBuilder.vb
+++ R#/Interpreter/Syntax/SyntaxTree/ExpressionBuilder.vb
@@ -1,48 +1,49 @@
 ﻿#Region "Microsoft.VisualBasic::e143e740957590c109c1d482c5a3eaba, R#\Interpreter\Syntax\SyntaxTree\ExpressionBuilder.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Module ExpressionBuilder
-    ' 
-    '         Function: getTupleSymbols, getValueAssign, keywordExpressionHandler, ParseExpression, parseInvoke
-    '                   parseSymbolIndex
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Module ExpressionBuilder
+' 
+'         Function: getTupleSymbols, getValueAssign, keywordExpressionHandler, ParseExpression, parseInvoke
+'                   parseSymbolIndex
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.ApplicationServices.Debugging.Diagnostics
+Imports Microsoft.VisualBasic.ComponentModel.Collection
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Linq
@@ -156,6 +157,8 @@ Namespace Interpreter.SyntaxParser
             End Select
         End Function
 
+        ReadOnly iterateAssign As Index(Of String) = {"+=", "-=", "*=", "/="}
+
         <Extension>
         Friend Function ParseExpression(code As List(Of Token()), opts As SyntaxBuilderOptions) As SyntaxResult
             If code = 0 Then
@@ -215,6 +218,8 @@ Namespace Interpreter.SyntaxParser
 
                     If opText = "=" OrElse opText = "<-" Then
                         Return SyntaxImplements.ValueAssign(code, opts)
+                    ElseIf opText Like iterateAssign Then
+                        Return SyntaxImplements.ValueIterateAssign(code(Scan0), opText, code.Skip(2).IteratesALL.ToArray, opts)
                     ElseIf opText = "~" Then
                         Return SyntaxImplements.FormulaExpressionSyntax.RunParse(code, opts)
                     End If
