b5d5ceef991d45f056dafcd2358db39987ff0268
xieguigang <xie.guigang@gcmodeller.org>
2022-01-23 02:43:31 +0800

    Update hypot.jl

--- tutorials/Rscript/hypot.jl
+++ tutorials/Rscript/hypot.jl
@@ -1,17 +1,17 @@
-function hypot(x,y)
-    x = abs(x)
-    y = abs(y)
-    if x > y
-      r = y/x
-      return x*sqrt(1+r*r)
-    end
-    if y == 0
-      return zero(x)
-    end
-    r = x/y
-    return y*sqrt(1+r*r)
+function hypot(x, y)
+  x = abs(x)
+  y = abs(y)
+
+  if x > y
+    r = y/x
+    return x*sqrt(1+r*r)
   end
 
+  if y == 0
+    return zero(x)
+  end
 
-  # print(hypot(2,3))
-  # print(hypot(0,0))
+  r = x/y
+  
+  return y*sqrt(1+r*r)
+end
