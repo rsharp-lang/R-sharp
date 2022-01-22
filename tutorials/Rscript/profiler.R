252aee92facce4d47f37c62a118b4745cdced562
xieguigang <xie.guigang@gcmodeller.org>
2022-01-22 22:59:28 +0800

    Create profiler.R

---
+++ tutorials/Rscript/profiler.R
@@ -0,0 +1,21 @@
+hypot = function(x, y) {
+[x, y] = abs([x, y]);
+
+if (x > y) {
+	r = y /x;
+	return x * sqrt(1+r ^ 2);
+}
+if (y == 0) {
+	return 0;
+}
+
+r = x/y;
+
+return y*sqrt(1+r ^ 2);
+}
+
+@profile {
+	for(i in 1: 5) {
+		print(hypot(i, 3));
+	}
+}
