fd71a773faf0259bd1d18f8f55d5b72bf5bd3efa
xieguigang <xie.guigang@gcmodeller.org>
2022-01-22 22:18:26 +0800

    write text

--- studio/njl/Runtime/io.vb
+++ studio/njl/Runtime/io.vb
@@ -4,6 +4,7 @@ Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Scripting.MetaData
 Imports SMRUCC.Rsharp.Runtime
 Imports SMRUCC.Rsharp.Runtime.Interop
+Imports REnv = SMRUCC.Rsharp.Runtime
 
 <Package("io")>
 Public Module io
@@ -23,6 +24,18 @@ Public Module io
                          Optional mode As String = "w",
                          Optional env As Environment = Nothing) As Object
 
+        If TypeOf f Is String Then
+            mode = filename
+            filename = f
+        End If
+
+        If mode = "w" Then
+            Return New StreamWriter(filename.Open(FileMode.OpenOrCreate, doClear:=True, [readOnly]:=False))
+        ElseIf mode = "a" Then
+            Return New StreamWriter(filename.Open(FileMode.OpenOrCreate, doClear:=False, [readOnly]:=False))
+        Else
+            Return New StreamReader(filename.Open(FileMode.Open, doClear:=False, [readOnly]:=True))
+        End If
     End Function
 
     ''' <summary>
@@ -33,9 +46,40 @@ Public Module io
     ''' <param name="env"></param>
     ''' <returns></returns>
     <ExportAPI("write")>
-    Public Function write(file As Stream, <RRawVectorArgument> x As Object,
+    Public Function write(file As StreamWriter, <RRawVectorArgument> x As Object,
                           Optional env As Environment = Nothing)
 
+        Dim vec As Array = REnv.TryCastGenericArray(REnv.asVector(Of Object)(x), env)
+        Dim type As Type = vec.GetType.GetElementType
+
+        Select Case type
+            Case GetType(String)
+                For Each str As String In DirectCast(vec, String())
+                    Call file.Write(str)
+                Next
+            Case GetType(Char)
+                Call file.Write(New String(DirectCast(vec, Char())))
+            Case GetType(Integer)
+                For Each i As Integer In DirectCast(vec, Integer())
+                    Call file.Write(i)
+                Next
+            Case GetType(Long)
+                For Each l As Long In DirectCast(vec, Long())
+                    Call file.Write(l)
+                Next
+            Case GetType(Double)
+                For Each d As Double In DirectCast(vec, Double())
+                    Call file.Write(d)
+                Next
+            Case GetType(Single)
+                For Each s As Single In DirectCast(vec, Single())
+                    Call file.Write(s)
+                Next
+            Case Else
+                Throw New NotImplementedException(type.ToString)
+        End Select
+
+        Return Nothing
     End Function
 
     ''' <summary>
@@ -45,7 +89,12 @@ Public Module io
     ''' <param name="type"></param>
     ''' <returns></returns>
     <ExportAPI("read")>
-    Public Function read(file As Stream, Optional type As String = "String") As Object
-
+    Public Function read(file As StreamReader, Optional type As String = "String") As Object
+        Select Case LCase(type)
+            Case "string"
+                Return file.ReadToEnd
+            Case Else
+                Throw New NotImplementedException(type)
+        End Select
     End Function
 End Module
