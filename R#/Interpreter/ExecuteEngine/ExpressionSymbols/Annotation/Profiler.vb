a7ef0652507e881bfaec47e38302d42ad644a29c
xieguigang <xie.guigang@gcmodeller.org>
2022-01-22 22:52:23 +0800

    how to dump profile data?

---
+++ R#/Interpreter/ExecuteEngine/ExpressionSymbols/Annotation/Profiler.vb
@@ -0,0 +1,31 @@
+﻿
+Imports SMRUCC.Rsharp.Development.Package.File
+Imports SMRUCC.Rsharp.Runtime
+Imports SMRUCC.Rsharp.Runtime.Components
+
+Namespace Interpreter.ExecuteEngine.ExpressionSymbols.Annotation
+
+    ''' <summary>
+    ''' ``@profile``
+    ''' 
+    ''' 开始进行性能计数
+    ''' </summary>
+    Public Class Profiler : Inherits Expression
+
+        Public Overrides ReadOnly Property type As TypeCodes
+            Get
+                Return TypeCodes.generic
+            End Get
+        End Property
+
+        Public Overrides ReadOnly Property expressionName As ExpressionTypes
+            Get
+                Return ExpressionTypes.Annotation
+            End Get
+        End Property
+
+        Public Overrides Function Evaluate(envir As Environment) As Object
+            Throw New NotImplementedException()
+        End Function
+    End Class
+End Namespace
