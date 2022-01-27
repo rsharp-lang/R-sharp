f081aec35753720202ed83e9f2d49b0d2ed7e36e
xieguigang <xie.guigang@gcmodeller.org>
2022-01-27 11:35:16 +0800

    row names is not working?

--- docs/demo/umap23/UMAP.py
+++ docs/demo/umap23/UMAP.py
@@ -9,21 +9,26 @@ MNIST_LabelledVectorArray = MNIST_LabelledVectorArray.read.mnist.labelledvector(
 tags                                = rownames(MNIST_LabelledVectorArray)
 rownames(MNIST_LabelledVectorArray) = `X${1:nrow(MNIST_LabelledVectorArray)}`
 
+print("previews of the number tags:")
+print(tags, max.print = 100)
+
 bmp2       = "./MNIST-LabelledVectorArray-20000x100.umap_scatter2d.png"
 bmp3       = "./MNIST-LabelledVectorArray-20000x100.umap_scatter3d.png"
 umap3_file = "./MNIST-LabelledVectorArray-20000x100.umap_scatter3.csv"
 umap2_file = "./MNIST-LabelledVectorArray-20000x100.umap_scatter2.csv"
 
 # umap 3d scatter
-manifold = umap(MNIST_LabelledVectorArray, dimension = 3, debug = True)
-umap3    = as.data.frame(manifold$umap, row.names = tags)
+manifold        = umap(MNIST_LabelledVectorArray, dimension = 3, debug = True)
+umap3           = as.data.frame(manifold$umap)
+rownames(umap3) = tags
 
 # save data matrix
 write.csv(umap3, file = umap3_file)
 
 # continue to 2d scatter
-manifold = umap(umap3, dimension = 2, debug = True)
-umap2    = as.data.frame(manifold$umap, row.names = tags)
+manifold        = umap(umap3, dimension = 2, debug = True)
+umap2           = as.data.frame(manifold$umap)
+rownames(umap2) = tags
 
 # save data matrix
 write.csv(umap2, file = umap2_file)
