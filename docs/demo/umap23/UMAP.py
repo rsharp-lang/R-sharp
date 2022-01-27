339d25290d41b03eef7b1fbb49ba03738c09a535
xieguigang <xie.guigang@gcmodeller.org>
2022-01-27 11:01:17 +0800

    Update UMAP.py

--- docs/demo/umap23/UMAP.py
+++ docs/demo/umap23/UMAP.py
@@ -3,23 +3,27 @@ from MLkit import umap
 
 setwd(@dir)
 
-filename = "MNIST-LabelledVectorArray-60000x100.msgpack"
-MNIST_LabelledVectorArray = `../${filename}`.read.mnist.labelledvector(takes = 50000)
-tags = rownames(MNIST_LabelledVectorArray)
+MNIST_LabelledVectorArray = "../../../Library/demo/machineLearning/umap/MNIST-LabelledVectorArray-60000x100.msgpack"
+MNIST_LabelledVectorArray = MNIST_LabelledVectorArray.read.mnist.labelledvector(takes = 10000)
+
+tags                                = rownames(MNIST_LabelledVectorArray)
 rownames(MNIST_LabelledVectorArray) = `X${1:nrow(MNIST_LabelledVectorArray)}`
 
-bmp2 = "./MNIST-LabelledVectorArray-20000x100.umap_scatter2d.png"
-bmp3 = "./MNIST-LabelledVectorArray-20000x100.umap_scatter3d.png"
-
-manifold = umap(MNIST_LabelledVectorArray, dimension = 3, debug = TRUE)
-umap3 = as.data.frame(manifold$umap, row.names = tags)
+bmp2       = "./MNIST-LabelledVectorArray-20000x100.umap_scatter2d.png"
+bmp3       = "./MNIST-LabelledVectorArray-20000x100.umap_scatter3d.png"
 umap3_file = "./MNIST-LabelledVectorArray-20000x100.umap_scatter3.csv"
-
-write.csv(umap3, file = umap3_file)
-
-manifold = umap(umap3, dimension = 2, debug = TRUE)
-
-umap2 = as.data.frame(manifold$umap, row.names = tags)
 umap2_file = "./MNIST-LabelledVectorArray-20000x100.umap_scatter2.csv"
 
+# umap 3d scatter
+manifold = umap(MNIST_LabelledVectorArray, dimension = 3, debug = True)
+umap3    = as.data.frame(manifold$umap, row.names = tags)
+
+# save data matrix
+write.csv(umap3, file = umap3_file)
+
+# continue to 2d scatter
+manifold = umap(umap3, dimension = 2, debug = True)
+umap2    = as.data.frame(manifold$umap, row.names = tags)
+
+# save data matrix
 write.csv(umap2, file = umap2_file)
