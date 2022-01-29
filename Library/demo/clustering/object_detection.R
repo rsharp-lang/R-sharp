8ab1d70e3bfc339328e6472a51da768bc0ebcff6
xieguigang <xie.guigang@gcmodeller.org>
2022-01-29 23:22:16 +0800

    class renames of noise

--- Library/demo/clustering/object_detection.R
+++ Library/demo/clustering/object_detection.R
@@ -11,11 +11,13 @@ print(multishapes, max.print = 13);
 
 # detect object from umap data
 objects = graphics2D::pointVector(multishapes[, "x"], multishapes[, "y"]) |> dbscan_objects();
+objects[objects == "-1"] = "noise";
+objects = ifelse(objects == "noise", objects, `object_${objects}`);
 
 # show object detection result
 bitmap(file = "./object_detection.png") {
 	plot(multishapes[, "x"], multishapes[, "y"], 
-		class     = `object_${objects}`, 
+		class     = objects, 
 		grid.fill = "white",
 		padding   = "padding: 125px 300px 200px 200px;",
 		colorSet  = "paper"
