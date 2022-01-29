701ec39047251c34ae95919ffaaf33ec861777e0
xieguigang <xie.guigang@gcmodeller.org>
2022-01-29 21:31:16 +0800

    Create object_detection.py

---
+++ Library/demo/clustering/object_detection.py
@@ -0,0 +1,20 @@
+# load R# library for run dbscan
+from MLkit import clustering 
+# load R# library for plots
+import graphics2D
+
+setwd(@dir)
+
+multishapes = read.csv("./multishapes.csv")
+x, y        = (multishapes[, "x"], multishapes[, "y"])
+
+print(`contains ${x.length()} data points.`)
+print(multishapes, max.print = 13)
+
+# detect object from umap data
+objects = `object_${graphics2D::pointVector(x, y).dbscan_objects()}`
+# show object detection result
+margin  = "padding: 125px 300px 200px 200px;"
+plt     = plot(x, y, class = objects, grid.fill = "white", padding = margin)
+
+bitmap(plt, file = "./object_detection.png")
