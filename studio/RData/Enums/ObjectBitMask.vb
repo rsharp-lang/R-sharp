ce2dec5ff0cc0bdcaa4ab86b51324032ab7fef4e
xieguigang <xie.guigang@gcmodeller.org>
2022-02-05 20:07:58 +0800

    Create ObjectBitMask.vb

---
+++ studio/RData/Enums/ObjectBitMask.vb
@@ -0,0 +1,11 @@
+﻿Namespace Flags
+
+    ''' <summary>
+    ''' R object attribute data mask
+    ''' </summary>
+    Public Enum ObjectBitMask
+        IS_OBJECT_BIT_MASK = 1 << 8
+        HAS_ATTR_BIT_MASK = 1 << 9
+        HAS_TAG_BIT_MASK = 1 << 10
+    End Enum
+End Namespace
