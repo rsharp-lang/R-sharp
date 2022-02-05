24bea6811525f03d8d8df724b200af89598e8454
xieguigang <xie.guigang@gcmodeller.org>
2022-02-05 20:56:59 +0800

    write pair list and generic value

--- studio/RData/Writer.vb
+++ studio/RData/Writer.vb
@@ -3,14 +3,18 @@ Imports System.Text
 Imports Microsoft.VisualBasic.Data.IO
 Imports SMRUCC.Rsharp.RDataSet.Flags
 Imports SMRUCC.Rsharp.RDataSet.Struct
+Imports SMRUCC.Rsharp.Runtime
 Imports SMRUCC.Rsharp.Runtime.Internal.Object
+Imports REnv = SMRUCC.Rsharp.Runtime
 
 Public Class Writer
 
     ReadOnly file As BinaryDataWriter
+    ReadOnly env As Environment
 
-    Private Sub New(file As BinaryDataWriter)
+    Private Sub New(file As BinaryDataWriter, env As Environment)
         Me.file = file
+        Me.env = env
 
         Call file.Write(Parser.magic_dict(FileTypes.rdata_binary_v3))
         Call file.Write(Parser.format_dict(RdataFormats.XDR))
@@ -57,9 +61,33 @@ Public Class Writer
     Public Sub WriteSymbols(symbols As list)
         Dim info_int As Integer = RObjectInfo.LISTSXP.EncodeInfoInt32
 
+        For Each key As String In symbols.getNames
+            Call Xdr.EncodeInt32(info_int, file)
+            Call ByteEncoder.EncodeSymbol(key, file)
+            Call Write(any:=symbols.getByName(key))
+        Next
+    End Sub
 
+    Public Sub Write(any As Object)
+        If any Is Nothing Then
+            Call stringVector({})
+        ElseIf TypeOf any Is list Then
+            Call WriteSymbols(any)
+        ElseIf TypeOf any Is dataframe Then
+            Throw New NotImplementedException
+        Else
+            Dim vec As Array = REnv.TryCastGenericArray(REnv.asVector(Of Object)(any), env)
+            Dim baseType As Type = vec.GetType.GetElementType
 
-        Call Xdr.EncodeInt32(info_int, Me.file)
+            Select Case baseType
+                Case GetType(String) : Call stringVector(vec)
+                Case GetType(Integer) : Call intVector(vec)
+                Case GetType(Double) : Call realVector(vec)
+                Case GetType(Boolean) : Call logicalVector(vec)
+                Case Else
+                    Throw New NotImplementedException(baseType.FullName)
+            End Select
+        End If
     End Sub
 
     ''' <summary>
@@ -74,12 +102,20 @@ Public Class Writer
         End Using
     End Sub
 
-    Public Shared Sub Save(symbols As list, file As BinaryDataWriter)
-        Call New Writer(file).WriteSymbols(symbols)
+    Public Shared Sub Save(symbols As list, file As BinaryDataWriter, Optional env As Environment = Nothing)
+        If env Is Nothing Then
+            env = GlobalEnvironment.defaultEmpty
+        End If
+
+        Call New Writer(file, env).WriteSymbols(symbols)
     End Sub
 
-    Public Shared Function Open(file As Stream) As Writer
-        Return New Writer(New BinaryDataWriter(file))
+    Public Shared Function Open(file As Stream, Optional env As Environment = Nothing) As Writer
+        If env Is Nothing Then
+            env = GlobalEnvironment.defaultEmpty
+        End If
+
+        Return New Writer(New BinaryDataWriter(file), env)
     End Function
 
     Private Sub writeVersion()
