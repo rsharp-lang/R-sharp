6a5fb19d4b2538a5068a6556fae8c5d165574e7d
xieguigang <xie.guigang@gcmodeller.org>
2022-02-09 16:59:08 +0800

    Update bitView.vb

--- R#/Runtime/Internal/internalInvokes/dev/bitView.vb
+++ R#/Runtime/Internal/internalInvokes/dev/bitView.vb
@@ -1,4 +1,5 @@
 ﻿Imports System.IO
+Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Scripting.MetaData
 
@@ -9,7 +10,36 @@ Namespace Runtime.Internal.Invokes
 
         <ExportAPI("doubles")>
         Public Function doubles(buffer As MemoryStream) As Double()
-            Return buffer.ToArray.Split(8).Where(Function(b) b.Length = 8).Select(AddressOf BitConverter.ToDouble).ToArray
+            Return buffer.bitConvert(sizeof:=8, AddressOf BitConverter.ToDouble)
+        End Function
+
+        <ExportAPI("integers")>
+        Public Function integers(buffer As MemoryStream) As Integer()
+            Return buffer.bitConvert(sizeof:=4, AddressOf BitConverter.ToInt32)
+        End Function
+
+        <ExportAPI("floats")>
+        Public Function floats(buffer As MemoryStream) As Single()
+            Return buffer.bitConvert(sizeof:=4, AddressOf BitConverter.ToSingle)
+        End Function
+
+        <ExportAPI("int16s")>
+        Public Function shorts(buffer As MemoryStream) As Short()
+            Return buffer.bitConvert(sizeof:=2, AddressOf BitConverter.ToInt16)
+        End Function
+
+        <ExportAPI("int64s")>
+        Public Function int64(buffer As MemoryStream) As Long()
+            Return buffer.bitConvert(sizeof:=8, AddressOf BitConverter.ToInt64)
+        End Function
+
+        <Extension>
+        Private Function bitConvert(Of T)(buffer As MemoryStream, sizeof As Integer, load As Func(Of Byte(), Integer, T)) As T()
+            Return buffer.ToArray _
+                .Split(sizeof) _
+                .Where(Function(b) b.Length = sizeof) _
+                .Select(Function(byts) load(byts, Scan0)) _
+                .ToArray
         End Function
 
     End Module
