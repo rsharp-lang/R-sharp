7c6fd8fa5984a4edbdc00d6f53b7fe98285aef1d
xieguigang <xie.guigang@gcmodeller.org>
2022-03-01 01:11:43 +0800

    tweaks of plot

--- tutorials/graphics/colorSet.R
+++ tutorials/graphics/colorSet.R
@@ -2,7 +2,7 @@ require(graphics2D);
 
 wmf(file = `${@dir}/colors.wmf`) {
 	maps = colorMap.legend(
-		colors = "viridis:turbo", ticks = 0:100 step 10, title = "Intensity Scale"
+		colors = "viridis:turbo", ticks = 0:100 step 20, title = "Intensity Scale"
 	);
 	
 	plot(maps, size = [200,400], padding = [10, 10, 20, 30]);
