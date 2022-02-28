65a9689c1fc91433612378121a6f7a68cb1ab692
guigang xie <gg.xie@bionovogene.com>
2022-02-28 19:25:18 +0800

    config R engine

--- R#/Runtime/Internal/internalInvokes/etc.vb
+++ R#/Runtime/Internal/internalInvokes/etc.vb
@@ -1,45 +1,45 @@
 ﻿#Region "Microsoft.VisualBasic::a0a03f13727efa654178b7911d08e797, R#\Runtime\Internal\internalInvokes\etc.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Module etc
-    ' 
-    '         Function: contributors, getActivators, license, man, platformID
-    '                   RVer, sessionInfo, Sys_getlocale, Sys_info, unixtimestamp
-    ' 
-    '         Sub: demo
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Module etc
+' 
+'         Function: contributors, getActivators, license, man, platformID
+'                   RVer, sessionInfo, Sys_getlocale, Sys_info, unixtimestamp
+' 
+'         Sub: demo
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -145,6 +145,20 @@ Namespace Runtime.Internal.Invokes
         <ExportAPI("unixtimestamp")>
         Public Function unixtimestamp() As Double
             Return App.UnixTimeStamp
+        End Function
+
+        ''' <summary>
+        ''' ### Get Environment Variables
+        ''' 
+        ''' ``Sys.getenv`` obtains the values of the environment variables.
+        ''' </summary>
+        ''' <param name="x">
+        ''' a character vector, or NULL.
+        ''' </param>
+        ''' <returns></returns>
+        <ExportAPI("Sys.getenv")>
+        Public Function Sys_getenv(x As String) As String
+            Return Global.System.Environment.GetEnvironmentVariable(x)
         End Function
 
         ''' <summary>
