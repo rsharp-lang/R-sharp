94ffb987450e1b0163bbfc2e67bcc813bdc22f78
xieguigang <xie.guigang@gcmodeller.org>
2022-03-14 01:47:39 +0800

    Update taskParallel.R

--- tutorials/Rscript/taskParallel.R
+++ tutorials/Rscript/taskParallel.R
@@ -4,6 +4,8 @@ const seqVals = [1,3,5,6,7,8,9];
 const bbb     = [2,2,2,22,2,1,9999];
 const g as  double = 2.888888;
 
+print("start run parallel....");
+
 const y = parallel(x = seqVals, z = bbb, n_threads = 2) {
 	sleep(1 + rnd() * 10);
 	print(x);
