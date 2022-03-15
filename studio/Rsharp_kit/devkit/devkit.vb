9096f57cdd7bb79934cfd4cb59ec4dea377a1c85
xieguigang <xie.guigang@gcmodeller.org>
2022-03-15 21:54:38 +0800

    write source file banners

--- studio/Rsharp_kit/devkit/devkit.vb
+++ studio/Rsharp_kit/devkit/devkit.vb
@@ -1,53 +1,53 @@
 ﻿#Region "Microsoft.VisualBasic::1a6fc004d0d570a735a1f1dfac3b37e9, R-sharp\studio\Rsharp_kit\devkit\devkit.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 101
-    '    Code Lines: 63
-    ' Comment Lines: 26
-    '   Blank Lines: 12
-    '     File Size: 3.75 KB
+' Summaries:
 
 
-    ' Module devkit
-    ' 
-    '     Constructor: (+1 Overloads) Sub New
-    '     Function: AssemblyInfo, decodeSourceMap, encodeSourceMap, gitLog, inspect
-    '               showIL, svnLog
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 101
+'    Code Lines: 63
+' Comment Lines: 26
+'   Blank Lines: 12
+'     File Size: 3.75 KB
+
+
+' Module devkit
+' 
+'     Constructor: (+1 Overloads) Sub New
+'     Function: AssemblyInfo, decodeSourceMap, encodeSourceMap, gitLog, inspect
+'               showIL, svnLog
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -55,6 +55,7 @@ Imports System.Reflection
 Imports Microsoft.VisualBasic.ApplicationServices.Debugging.Diagnostics
 Imports Microsoft.VisualBasic.ApplicationServices.Development
 Imports Microsoft.VisualBasic.ApplicationServices.Development.VisualStudio
+Imports Microsoft.VisualBasic.ApplicationServices.Development.VisualStudio.CodeSign
 Imports Microsoft.VisualBasic.ApplicationServices.Development.VisualStudio.IL
 Imports Microsoft.VisualBasic.ApplicationServices.Development.VisualStudio.SourceMap
 Imports Microsoft.VisualBasic.ApplicationServices.Development.VisualStudio.VersionControl
@@ -85,8 +86,15 @@ Module devkit
         Return Assembly.UnsafeLoadFrom(dllfile.GetFullPath).FromAssembly
     End Function
 
-    Public Function writeCodeBanner(file As String) As codestatics
+    <ExportAPI("writeCodeBanner")>
+    Public Function writeCodeBanner(file As String, banner As LicenseInfo, Optional rootDir As String = Nothing) As CodeStatics
+        If rootDir.StringEmpty Then
+            rootDir = file.ParentPath
+        End If
 
+        Dim stat As CodeStatics = Nothing
+        Call LicenseMgr.Insert(file.ReadAllText, banner, rootDir, stat)
+        Return stat
     End Function
 
     <ExportAPI("svn.log")>
--- studio/code_banner.py
+++ studio/code_banner.py
@@ -1,1 +1,2 @@
-import 
+from Rstudio import gtk
+
