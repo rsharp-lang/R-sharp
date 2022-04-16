d98a4096ba36f8a31f4cd710b76931bc276164ea
xieguigang <xie.guigang@gcmodeller.org>
2022-04-16 12:49:10 +0800

    vector assert is not working?

--- Library/base/utils/utils.vb
+++ Library/base/utils/utils.vb
@@ -1,54 +1,54 @@
 ﻿#Region "Microsoft.VisualBasic::06f7fc5994584cd253d1322cd5c393be, R-sharp\Library\R.base\utils\utils.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 375
-    '    Code Lines: 259
-    ' Comment Lines: 82
-    '   Blank Lines: 34
-    '     File Size: 16.28 KB
+' Summaries:
 
 
-    ' Module utils
-    ' 
-    '     Function: DataFrameRows, ensureRowNames, MeasureGenericType, printRawTable, read_csv
-    '               saveGeneric, setRowNames, write_csv
-    ' 
-    '     Sub: Main
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 375
+'    Code Lines: 259
+' Comment Lines: 82
+'   Blank Lines: 34
+'     File Size: 16.28 KB
+
+
+' Module utils
+' 
+'     Function: DataFrameRows, ensureRowNames, MeasureGenericType, printRawTable, read_csv
+'               saveGeneric, setRowNames, write_csv
+' 
+'     Sub: Main
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -357,7 +357,19 @@ Public Module utils
         ElseIf type.IsArray OrElse type Is GetType(vector) Then
             Return saveGeneric(x, type, file, encoding.CodePage, env)
         Else
-            Return Message.InCompatibleType(GetType(File), type, env)
+            Dim stream As pipeline = pipeline.TryCreatePipeline(Of EntityObject)(x, env)
+
+            If stream.isError Then
+                stream = pipeline.TryCreatePipeline(Of DataSet)(x, env)
+
+                If stream.isError Then
+                    Return Message.InCompatibleType(GetType(File), type, env)
+                Else
+                    Return stream.populates(Of DataSet)(env).SaveTo(path:=file, encoding:=encoding.CodePage, silent:=True, metaBlank:=0)
+                End If
+            Else
+                Return stream.populates(Of EntityObject)(env).SaveTo(path:=file, encoding:=encoding.CodePage, silent:=True)
+            End If
         End If
     End Function
 
