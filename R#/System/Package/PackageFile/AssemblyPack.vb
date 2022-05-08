165cde9465d7d36d20c93bc7e014d11767a4e994
xieguigang <xie.guigang@gcmodeller.org>
2022-05-08 16:20:49 +0800

    try to makes bugs fixed for copy assembly module files when build a new package

--- R#/System/Package/PackageFile/AssemblyPack.vb
+++ R#/System/Package/PackageFile/AssemblyPack.vb
@@ -1,54 +1,54 @@
 ﻿#Region "Microsoft.VisualBasic::a74dfc2d2406f1cb92880ba5708abcb8, R-sharp\R#\System\Package\PackageFile\AssemblyPack.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 37
-    '    Code Lines: 24
-    ' Comment Lines: 7
-    '   Blank Lines: 6
-    '     File Size: 1.30 KB
+' Summaries:
 
 
-    '     Class AssemblyPack
-    ' 
-    '         Properties: assembly, directory, framework
-    ' 
-    '         Function: GenericEnumerator, GetEnumerator, GetFileContents
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 37
+'    Code Lines: 24
+' Comment Lines: 7
+'   Blank Lines: 6
+'     File Size: 1.30 KB
+
+
+'     Class AssemblyPack
+' 
+'         Properties: assembly, directory, framework
+' 
+'         Function: GenericEnumerator, GetEnumerator, GetFileContents
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -64,11 +64,39 @@ Namespace Development.Package.File
         ''' </summary>
         ''' <returns>get filtered dll file modules</returns>
         ''' <remarks>
-        ''' 程序会将几乎所有的文件都打包进去
+        ''' 程序会将几乎所有的文件都打包进去: <see cref="GetAllPackageContentFiles()"/>
         ''' </remarks>
         Public Property assembly As String()
         Public Property directory As String
 
+        ''' <summary>
+        ''' enumerates all package contents files, includes:
+        ''' 
+        ''' 1. dll modules
+        ''' 2. deps.json files
+        ''' 3. app config files
+        ''' </summary>
+        ''' <returns></returns>
+        Public Iterator Function GetAllPackageContentFiles() As IEnumerable(Of String)
+            For Each dll As String In assembly
+                Dim depsJson As String = $"{dll.ParentPath}/{dll.BaseName}.deps.json"
+                Dim appConfig As String = $"{dll.ParentPath}/{dll.FileName}.config"
+
+                If depsJson.FileExists Then
+                    Yield depsJson.GetFullPath
+                End If
+                If appConfig.FileExists Then
+                    Yield appConfig.GetFullPath
+                End If
+
+                Yield dll
+            Next
+        End Function
+
+        ''' <summary>
+        ''' enumerates all dll files
+        ''' </summary>
+        ''' <returns></returns>
         Public Iterator Function GenericEnumerator() As IEnumerator(Of String) Implements Enumeration(Of String).GenericEnumerator
             For Each dll As String In assembly
                 Yield dll
