a9ba2f1a5015941c39f904badbace71a6b38468c
guigang xie <gg.xie@bionovogene.com>
2022-05-19 16:25:35 +0800

    split of the source function

---
+++ studio/Rsharp_kit/devkit/documents/RDefinition.vb
@@ -0,0 +1,10 @@
+﻿Imports System.IO
+Imports SMRUCC.Rsharp.Interpreter.ExecuteEngine.ExpressionSymbols.Closure
+
+Public Class RDefinition
+
+    Public Sub WriteRDefinition(R As DeclareNewFunction, file As TextWriter)
+
+    End Sub
+
+End Class
---
+++ studio/Rsharp_kit/roxygenNet/Rd/RDoc/ContentModel.vb
@@ -0,0 +1,58 @@
+﻿
+#Region "Doc content model"
+
+Public Class Doc
+
+    Public Property Fragments As DocFragment()
+
+    Public ReadOnly Property PlainText As String
+        Get
+            Return ToString()
+        End Get
+    End Property
+
+    Public Function GetMarkdown() As String
+        Throw New NotImplementedException
+    End Function
+
+    Public Function GetHtml() As String
+        Throw New NotImplementedException
+    End Function
+
+    Public Overrides Function ToString() As String
+        Return Fragments.Select(Function(frag) frag.ToString).JoinBy(" ")
+    End Function
+End Class
+
+Public MustInherit Class DocFragment
+
+End Class
+
+Public Class PlainText : Inherits DocFragment
+
+    Public Property text As String
+
+    Public Overrides Function ToString() As String
+        Return text
+    End Function
+End Class
+
+Public Class Code : Inherits DocFragment
+
+    Public Property content As DocFragment
+
+    Public Overrides Function ToString() As String
+        Return content.ToString
+    End Function
+End Class
+
+Public Class Link : Inherits DocFragment
+
+    Public Property target As String
+
+    Public Overrides Function ToString() As String
+        Return target.ToString
+    End Function
+End Class
+
+#End Region
