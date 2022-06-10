a663f53db19b909e4e40862d855039e984527d4f
xieguigang <xie.guigang@gcmodeller.org>
2022-06-10 13:22:55 +0800

    test removes html tags

--- test/unitTest/splitParagraph.R
+++ test/unitTest/splitParagraph.R
@@ -1,4 +1,9 @@
+imports "Html" from "webKit";
+
 labels = "1,2-dipalmitoyl-3'-<i>O</i>-(L-lysyl)-phosphatid... [M-H2O+NH4]+ 851.634";
-labels = splitParagraph(labels, 16);
+
+print(labels);
+
+labels = splitParagraph(Html::plainText(labels), 24);
 
 print(labels);
