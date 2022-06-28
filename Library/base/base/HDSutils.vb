7a33230f9d70f6d70632d7cd916ca68dbc4fe20a
guigang xie <gg.xie@bionovogene.com>
2022-06-28 10:01:49 +0800

    fix of the package namespace

--- Library/base/base/HDSutils.vb
+++ Library/base/base/HDSutils.vb
@@ -8,7 +8,7 @@ Imports SMRUCC.Rsharp.Runtime
 Imports SMRUCC.Rsharp.Runtime.Components
 Imports SMRUCC.Rsharp.Runtime.Interop
 
-<Package("HDF5.utils")>
+<Package("HDS")>
 Module HDSutils
 
     Sub New()
