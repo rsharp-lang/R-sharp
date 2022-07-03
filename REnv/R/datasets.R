35ffdef08e6db5301eae61c85181286246e6ec61
xieguigang <xie.guigang@gcmodeller.org>
2022-07-03 15:50:32 +0800

    Create datasets.R

---
+++ REnv/R/datasets.R
@@ -0,0 +1,26 @@
+
+#' http://www.sph.emory.edu/~dkleinb/datasets/cancer.dat
+#' 
+const classify_cancer = function() {
+    const file as string = system.file("data/cancer.dat", package = "REnv");
+    const data = readLines(file) |> skip(1) |> lapply(function(str) {
+        strsplit(str, "\s+", fixed = FALSE, perl = TRUE);
+    });
+    const tags = sapply(data, x -> x[1]);
+    const labels = sapply(data, x -> x[length(x)]);
+    const i as integer = 2:(length(data) - 1);
+    const matrix = lapply(data, r -> i[i]);
+    const dataset = list();
+
+    for(i in 1:length(i)) {
+        dataset[[`V${i}`]] = matrix 
+        |> sapply(r -> r[i]) 
+        |> as.numeric()
+        ;
+    }
+
+    let exports = data.frame(dataset);
+    exports = cbind(exports, labels);
+    rownames(exports) = tags;
+    exports;
+}
