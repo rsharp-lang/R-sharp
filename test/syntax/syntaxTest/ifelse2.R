076ce033f1009ecc0e4de40cd95f15d83ffbc765
guigang xie <gg.xie@bionovogene.com>
2022-07-11 09:56:09 +0800

    make improvements of the if else branch test

--- test/syntax/syntaxTest/ifelse2.R
+++ test/syntax/syntaxTest/ifelse2.R
@@ -1,13 +1,21 @@
 options(strict = FALSE);    
 
-header = "123";
+# header = "123";
 # header = "97.5%-tile:";
 # header = "2.5%-tile:";
+for(header in ["123", "97.5%-tile:", "2.5%-tile:", "xxx"]) {
 
+	print(header);
+	
 	if (header == "2.5%-tile:") {
-                print(4);
-            } else if (header == "97.5%-tile:") {
-                print(400);
-            } else {
-				stop("test");
-			}
+		print(4);
+	} else if (header == "97.5%-tile:") {
+		print(400);
+	} else if (header == $"\d+") {
+		print("is a number");
+	} else {
+		stop("test");
+	}
+}
+
+
