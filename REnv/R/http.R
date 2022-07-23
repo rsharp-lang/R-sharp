05a717d4c61105b4d923ec0cdfe378defe25c09a
xieguigang <xie.guigang@gcmodeller.org>
2022-07-23 23:56:10 +0800

    add dev notes

--- REnv/R/http.R
+++ REnv/R/http.R
@@ -107,6 +107,18 @@ const getHtml = function(url, interval = 3, filetype = "html") {
   ;
 }
 
+#' get json web request
+#' 
+#' @param raw_text this parameter determine that the 
+#'    function return value is the raw json character 
+#'    text data or ``R#`` object value.
+#' @param url the web resource of the target json 
+#'    resource object.
+#' 
+#' @details this function works on the ``http_get`` function,
+#'    so that the options for ``http_get``: http.cache_dir and 
+#'    http.debug is required as well.
+#' 
 const getJSON = function(url, interval = 3, raw_text = FALSE) {
   const json_text as string = getHtml(url, interval, filetype = "json");
 
