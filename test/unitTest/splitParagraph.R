6f9e1cd3ea05f227f347dedb03c7e11876e5604c
xieguigang <xie.guigang@gcmodeller.org>
2022-07-25 10:50:51 +0800

    Update splitParagraph.R

--- test/unitTest/splitParagraph.R
+++ test/unitTest/splitParagraph.R
@@ -1,9 +1,17 @@
 imports "Html" from "webKit";
 
-labels = "1,2-dipalmitoyl-3'-<i>O</i>-(L-lysyl)-phosphatid... [M-H2O+NH4]+ 851.634";
+# labels = "1,2-dipalmitoyl-3'-<i>O</i>-(L-lysyl)-phosphatid... [M-H2O+NH4]+ 851.634";
 
-print(labels);
+# print(labels);
 
+# labels = splitParagraph(Html::plainText(labels), 24);
+
+# print(labels);
+
+labels = "The coverage cutoff value for measure the RGB ion combination score. if there is no ms-imaging result in the triple ions 
+        ms-imaging folder, then it means this parameter cutoff value 
+        may be too high for filter ions list, try to make this threshold cutoff value smaller.";
+		
 labels = splitParagraph(Html::plainText(labels), 24);
 
 print(labels);
