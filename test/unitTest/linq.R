6c1bbdd7270192bfcfa34d613c893b34907c3560
guigang xie <gg.xie@bionovogene.com>
2022-08-05 13:35:42 +0800

    list subset by which

--- test/unitTest/linq.R
+++ test/unitTest/linq.R
@@ -1,18 +1,26 @@
-li = list(
-    a1 = list(x=1,y="A"),
-    a2 = list(x=3,y="B"),
-    a3 = list(x=5,y="B"),
-    a4 = list(x=7,y="A"),
-    a5 = list(x=88,y="A"),
-    a6 = list(x=10,y="A"),
-    a7 = list(x=-1,y="C"),
-    a8 = list(x=-101,y="C")
-);
+li = {
+    a1: list(x=1,y="A"),
+    a2: list(x=30,y="B"),
+    a3: list(x=5,y="B"),
+    a4: list(x=7,y="A"),
+    a5: list(x=88,y="A"),
+    a6: list(x=10,y="A"),
+    a7: list(x=-1,y="C"),
+    a8: list(x=-101,y="C")
+};
 
+print("raw list");
 str(li);
 
 # order by x
+print("list order by x:");
 str(li |> orderBy(i -> i$x));
 
+# filter by x
+print("list filter by x greater than zero!");
+print("and then order by x desc:");
+str(li |> which(i -> i$x > 0) |> orderBy(i -> i$x, desc = TRUE));
+
 # group by y
+print("list group by y:");
 str(li |> groupBy(i -> i$y));
