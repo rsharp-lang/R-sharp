a4c083a8685abd8505ae6bb9dda35278dd654d30
xieguigang <xie.guigang@gcmodeller.org>
2022-08-18 17:44:31 +0800

    fix of the missing json literal syntax on code analysis

--- R#/System/Document/ShellScript/ShellScript.vb
+++ R#/System/Document/ShellScript/ShellScript.vb
@@ -1,64 +1,65 @@
 ﻿#Region "Microsoft.VisualBasic::376c340638c90e1efbe5504b8c33f439, R-sharp\R#\System\Document\ShellScript\ShellScript.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 462
-    '    Code Lines: 365
-    ' Comment Lines: 13
-    '   Blank Lines: 84
-    '     File Size: 20.04 KB
+' Summaries:
 
 
-    '     Class ShellScript
-    ' 
-    '         Properties: argumentList, message
-    ' 
-    '         Constructor: (+1 Overloads) Sub New
-    ' 
-    '         Function: AnalysisAllCommands, GetCommandArgument, loadMetaLines, parseDefault, parseMetaData
-    ' 
-    '         Sub: AddArgumentValue, (+26 Overloads) analysisTree, AnalysisTree, PrintUsage
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 462
+'    Code Lines: 365
+' Comment Lines: 13
+'   Blank Lines: 84
+'     File Size: 20.04 KB
+
+
+'     Class ShellScript
+' 
+'         Properties: argumentList, message
+' 
+'         Constructor: (+1 Overloads) Sub New
+' 
+'         Function: AnalysisAllCommands, GetCommandArgument, loadMetaLines, parseDefault, parseMetaData
+' 
+'         Sub: AddArgumentValue, (+26 Overloads) analysisTree, AnalysisTree, PrintUsage
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.IO
 Imports System.Runtime.CompilerServices
 Imports System.Text.RegularExpressions
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Language.Default
 Imports Microsoft.VisualBasic.Linq
@@ -288,13 +289,19 @@ Namespace Development.CommandLine
                 Case GetType(UsingClosure) : Call analysisTree(DirectCast(expr, UsingClosure), attrs)
                 Case GetType(ByRefFunctionCall) : Call analysisTree(DirectCast(expr, ByRefFunctionCall), attrs)
                 Case GetType(UnaryNumeric) : Call AnalysisTree(DirectCast(expr, UnaryNumeric).numeric, attrs)
-
+                Case GetType(JSONLiteral) : Call analysisTree(DirectCast(expr, JSONLiteral), attrs)
                 Case GetType(ArgumentValue)
                     ' do nothing 
 
                 Case Else
                     Throw New NotImplementedException(expr.GetType.FullName)
             End Select
+        End Sub
+
+        Private Sub analysisTree(expr As JSONLiteral, attrs As ArgumentInfo)
+            For Each member As NamedValue(Of Expression) In expr.members
+                Call AnalysisTree(member.Value, attrs)
+            Next
         End Sub
 
         Private Sub analysisTree(expr As ByRefFunctionCall, attrs As ArgumentInfo)
