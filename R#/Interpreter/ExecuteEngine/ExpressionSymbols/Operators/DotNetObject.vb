a1b42576040251bcee843ad7d78c26d5e1457f85
guigang xie <xie.guigang@gcmodeller.org>
2022-08-20 01:31:56 +0800

    vector shadows

--- R#/Interpreter/ExecuteEngine/ExpressionSymbols/Operators/DotNetObject.vb
+++ R#/Interpreter/ExecuteEngine/ExpressionSymbols/Operators/DotNetObject.vb
@@ -1,61 +1,62 @@
 ﻿#Region "Microsoft.VisualBasic::dfce19f96847ae4ceae0dc2dc0b89d9c, R-sharp\R#\Interpreter\ExecuteEngine\ExpressionSymbols\Operators\DotNetObject.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 76
-    '    Code Lines: 61
-    ' Comment Lines: 3
-    '   Blank Lines: 12
-    '     File Size: 3.02 KB
+' Summaries:
 
 
-    '     Class DotNetObject
-    ' 
-    '         Properties: [object], expressionName, member, type
-    ' 
-    '         Constructor: (+1 Overloads) Sub New
-    '         Function: Evaluate, ToString
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 76
+'    Code Lines: 61
+' Comment Lines: 3
+'   Blank Lines: 12
+'     File Size: 3.02 KB
+
+
+'     Class DotNetObject
+' 
+'         Properties: [object], expressionName, member, type
+' 
+'         Constructor: (+1 Overloads) Sub New
+'         Function: Evaluate, ToString
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.Reflection
 Imports Microsoft.VisualBasic.ComponentModel.Collection
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports Microsoft.VisualBasic.Linq
 Imports SMRUCC.Rsharp.Development.Package.File
 Imports SMRUCC.Rsharp.Runtime
 Imports SMRUCC.Rsharp.Runtime.Components
@@ -81,6 +82,11 @@ Namespace Interpreter.ExecuteEngine.ExpressionSymbols.Operators
         End Property
 
         Public ReadOnly Property [object] As Expression
+
+        ''' <summary>
+        ''' the vector shadow reader
+        ''' </summary>
+        ''' <returns></returns>
         Public ReadOnly Property member As Expression
 
         Sub New(obj As Expression, member As Expression)
@@ -94,6 +100,8 @@ Namespace Interpreter.ExecuteEngine.ExpressionSymbols.Operators
 
             If Program.isException(objVal) Then
                 Return objVal
+            ElseIf objVal Is Nothing Then
+                Return Nothing
             ElseIf TypeOf objVal Is vbObject Then
                 objVal = DirectCast(objVal, vbObject).target
             End If
@@ -106,21 +114,55 @@ Namespace Interpreter.ExecuteEngine.ExpressionSymbols.Operators
 
             If TypeOf objVal Is list Then
                 Return DirectCast(objVal, list).getByName(memberName)
+            ElseIf objVal.GetType.IsArray Then
+                Return readVectorShadows(TryCastGenericArray(objVal, envir), memberName, envir)
             Else
-                Static schema As New Dictionary(Of Type, Dictionary(Of String, PropertyInfo))
+                Return readSingle(objVal, memberName, envir)
+            End If
+        End Function
 
-                Dim schemaTable = schema.ComputeIfAbsent(objVal.GetType, Function(cache) DataFramework.Schema(cache, PropertyAccess.Readable, nonIndex:=True))
-                Dim reader As PropertyInfo = schemaTable.TryGetValue(memberName)
+        Private Shared Function getReader(type As Type, memberName As String) As PropertyInfo
+            Static schema As New Dictionary(Of Type, Dictionary(Of String, PropertyInfo))
 
-                If reader Is Nothing Then
-                    If envir.globalEnvironment.options.strict Then
-                        Return Internal.debug.stop($"can not found member symbol {memberName} in [{objVal.GetType.FullName}].", envir)
-                    Else
-                        Return Nothing
-                    End If
+            Dim schemaTable = schema.ComputeIfAbsent(type, Function(cache) DataFramework.Schema(cache, PropertyAccess.Readable, nonIndex:=True))
+            Dim reader As PropertyInfo = schemaTable.TryGetValue(memberName)
+
+            Return reader
+        End Function
+
+        Private Function readVectorShadows(array As Array, memberName As String, envir As Environment) As Object
+            Dim type As Type = array.GetType
+            Dim reader As PropertyInfo = getReader(type.GetElementType, memberName)
+
+            If reader Is Nothing Then
+                If envir.globalEnvironment.options.strict Then
+                    Return Internal.debug.stop($"can not found member symbol {memberName} in [{type.GetElementType.FullName}].", envir)
                 Else
-                    Return reader.GetValue(objVal, Nothing)
+                    Return Nothing
                 End If
+            Else
+                array = array _
+                    .AsObjectEnumerator _
+                    .Select(Function(a)
+                                Return reader.GetValue(a, Nothing)
+                            End Function) _
+                    .ToArray
+
+                Return TryCastGenericArray(array, env:=envir)
+            End If
+        End Function
+
+        Private Function readSingle(objVal As Object, memberName As String, envir As Environment) As Object
+            Dim reader As PropertyInfo = getReader(objVal.GetType, memberName)
+
+            If reader Is Nothing Then
+                If envir.globalEnvironment.options.strict Then
+                    Return Internal.debug.stop($"can not found member symbol {memberName} in [{objVal.GetType.FullName}].", envir)
+                Else
+                    Return Nothing
+                End If
+            Else
+                Return reader.GetValue(objVal, Nothing)
             End If
         End Function
 
