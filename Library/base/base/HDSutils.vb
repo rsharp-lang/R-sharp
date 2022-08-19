dbce86652b3743f17d0f2fe9d2b8e6bfa71234eb
xieguigang <xie.guigang@gcmodeller.org>
2022-08-19 16:00:32 +0800

    add option for make the stream pack data readonly

--- Library/base/base/HDSutils.vb
+++ Library/base/base/HDSutils.vb
@@ -1,53 +1,53 @@
 ﻿#Region "Microsoft.VisualBasic::73b74923199a2780b4bdda2047a30b02, R-sharp\Library\base\base\HDSutils.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 109
-    '    Code Lines: 91
-    ' Comment Lines: 0
-    '   Blank Lines: 18
-    '     File Size: 3.79 KB
+' Summaries:
 
 
-    ' Module HDSutils
-    ' 
-    '     Constructor: (+1 Overloads) Sub New
-    '     Function: createStream, listFiles, openStream, readText, saveFile
-    '               Tree
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 109
+'    Code Lines: 91
+' Comment Lines: 0
+'   Blank Lines: 18
+'     File Size: 3.79 KB
+
+
+' Module HDSutils
+' 
+'     Constructor: (+1 Overloads) Sub New
+'     Function: createStream, listFiles, openStream, readText, saveFile
+'               Tree
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -79,7 +79,10 @@ Module HDSutils
 
     <ExportAPI("openStream")>
     <RApiReturn(GetType(StreamPack))>
-    Public Function openStream(<RRawVectorArgument> file As Object, Optional env As Environment = Nothing) As Object
+    Public Function openStream(<RRawVectorArgument> file As Object,
+                               Optional [readonly] As Boolean = False,
+                               Optional env As Environment = Nothing) As Object
+
         If file Is Nothing Then
             Return Internal.debug.stop("file for open can not be nothing!", env)
         ElseIf TypeOf file Is vector Then
@@ -97,12 +100,12 @@ Module HDSutils
 
         If TypeOf file Is String Then
             If DirectCast(file, String).FileExists Then
-                Return New StreamPack(DirectCast(file, String))
+                Return New StreamPack(DirectCast(file, String), [readonly]:=[readonly])
             Else
                 Return Internal.debug.stop("the given file is not found on your filesystem!", env)
             End If
         ElseIf TypeOf file Is Stream Then
-            Return New StreamPack(DirectCast(file, Stream))
+            Return New StreamPack(DirectCast(file, Stream), [readonly]:=[readonly])
         Else
             Return Internal.debug.stop(Message.InCompatibleType(GetType(String), file.GetType, env), env)
         End If
