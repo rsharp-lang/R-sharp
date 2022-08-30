52478d692ce2ab70db2d7c1ae437ec961972d3a6
xieguigang <xie.guigang@gcmodeller.org>
2022-08-30 20:04:17 +0800

    add timespan api

---
+++ R#/Runtime/Internal/internalInvokes/Math/time.vb
@@ -0,0 +1,17 @@
+﻿Imports Microsoft.VisualBasic.CommandLine.Reflection
+
+Namespace Runtime.Internal.Invokes
+
+    Module time
+
+        <ExportAPI("hours")>
+        Public Function hours(h As Double) As TimeSpan
+            Return TimeSpan.FromHours(h)
+        End Function
+
+        <ExportAPI("minutes")>
+        Public Function minutes(m As Double) As TimeSpan
+            Return TimeSpan.FromMinutes(m)
+        End Function
+    End Module
+End Namespace
