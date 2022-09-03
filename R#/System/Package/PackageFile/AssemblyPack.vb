a569f0ccb755dadb47344e41bf7e1a2f665b6710
xieguigang <xie.guigang@gcmodeller.org>
2022-09-03 22:55:24 +0800

    try to handling of the native library

--- R#/System/Package/PackageFile/AssemblyPack.vb
+++ R#/System/Package/PackageFile/AssemblyPack.vb
@@ -1,57 +1,58 @@
 ﻿#Region "Microsoft.VisualBasic::cd23473c7f7dfa2190da23fcacce4e75, R-sharp\R#\System\Package\PackageFile\AssemblyPack.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 56
-    '    Code Lines: 29
-    ' Comment Lines: 19
-    '   Blank Lines: 8
-    '     File Size: 2.04 KB
+' Summaries:
 
 
-    '     Class AssemblyPack
-    ' 
-    '         Properties: assembly, directory, framework
-    ' 
-    '         Function: GenericEnumerator, GetAllPackageContentFiles, GetEnumerator
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 56
+'    Code Lines: 29
+' Comment Lines: 19
+'   Blank Lines: 8
+'     File Size: 2.04 KB
+
+
+'     Class AssemblyPack
+' 
+'         Properties: assembly, directory, framework
+' 
+'         Function: GenericEnumerator, GetAllPackageContentFiles, GetEnumerator
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
+Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.Linq
 
 Namespace Development.Package.File
@@ -68,6 +69,11 @@ Namespace Development.Package.File
         ''' </remarks>
         Public Property assembly As String()
         Public Property directory As String
+
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        Public Function getRelativePath(fullname As String) As String
+            Return PathExtensions.RelativePath(directory, fullname)
+        End Function
 
         ''' <summary>
         ''' enumerates all package contents files, includes:
