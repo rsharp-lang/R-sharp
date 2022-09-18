f78969d340b36eda013f6f0e01353a3c8a0ca1f7
xieguigang <xie.guigang@gcmodeller.org>
2022-09-18 23:02:34 +0800

    vector loop

---
+++ R#/Interpreter/ExecuteEngine/ExpressionSymbols/DataSet/SymbolIndexer/VectorLoop.vb
@@ -0,0 +1,57 @@
+﻿Imports SMRUCC.Rsharp.Development.Package.File
+Imports SMRUCC.Rsharp.Runtime
+Imports SMRUCC.Rsharp.Runtime.Components
+Imports SMRUCC.Rsharp.Runtime.Internal.Object
+Imports any = Microsoft.VisualBasic.Scripting
+Imports REnv = SMRUCC.Rsharp.Runtime
+
+Namespace Interpreter.ExecuteEngine.ExpressionSymbols.DataSets
+
+    ''' <summary>
+    ''' create a new vector data which is introduced via 
+    ''' the perl syntax:
+    ''' 
+    ''' ``list@name`` means ``sapply(list, x -> x$name);``
+    ''' 
+    ''' </summary>
+    Public Class VectorLoop : Inherits Expression
+
+        Public Overrides ReadOnly Property type As TypeCodes
+
+        Friend ReadOnly index As Expression
+        Friend symbol As Expression
+
+        Public Overrides ReadOnly Property expressionName As ExpressionTypes
+            Get
+                Return ExpressionTypes.VectorLoop
+            End Get
+        End Property
+
+        Public Overrides Function Evaluate(envir As Environment) As Object
+            Dim data As Object = symbol.Evaluate(envir)
+            Dim member As Object = index.Evaluate(envir)
+
+            If Program.isException(data) Then
+                Return data
+            ElseIf Program.isException(member) Then
+                Return member
+            End If
+
+            Dim memberName As String = any.ToString(REnv.single(member))
+
+            If Not TypeOf data Is list Then
+                Return Message.InCompatibleType(GetType(list), data.GetType, envir)
+            End If
+
+            Dim datalist As list = DirectCast(data, list)
+            Dim vec As Object() = New Object(datalist.length - 1) {}
+            Dim source As Object() = datalist.data.ToArray
+
+            For i As Integer = 0 To vec.Length - 1
+                vec(i) = DirectCast(source(i), list).getByName(memberName)
+            Next
+
+            Return REnv.TryCastGenericArray(vec, env:=envir)
+        End Function
+    End Class
+End Namespace
--- R#/Language/Syntax/SyntaxImplements/SymbolIndexer.vb
+++ R#/Language/Syntax/SyntaxImplements/SymbolIndexer.vb
@@ -58,7 +58,6 @@ Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Linq
 Imports SMRUCC.Rsharp.Interpreter.ExecuteEngine
 Imports SMRUCC.Rsharp.Interpreter.ExecuteEngine.ExpressionSymbols.DataSets
-Imports SMRUCC.Rsharp.Language
 Imports SMRUCC.Rsharp.Language.TokenIcer
 
 Namespace Language.Syntax.SyntaxParser.SyntaxImplements
