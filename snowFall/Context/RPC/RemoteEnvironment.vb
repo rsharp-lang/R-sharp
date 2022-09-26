32dfb84d0d65fd3cc75f26392bc0ee534efa8218
xieguigang <xie.guigang@gcmodeller.org>
2022-09-26 10:21:06 +0800

    make bugs fixed of the remote environmnet query

--- snowFall/Context/RPC/RemoteEnvironment.vb
+++ snowFall/Context/RPC/RemoteEnvironment.vb
@@ -1,57 +1,58 @@
 ﻿#Region "Microsoft.VisualBasic::3980a5130d070ff2686ae1e257ebe199, R-sharp\snowFall\Context\RPC\RemoteEnvironment.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 83
-    '    Code Lines: 50
-    ' Comment Lines: 20
-    '   Blank Lines: 13
-    '     File Size: 3.19 KB
+' Summaries:
 
 
-    '     Class RemoteEnvironment
-    ' 
-    '         Constructor: (+1 Overloads) Sub New
-    '         Function: FindSymbol, getRemoteSymbol
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 83
+'    Code Lines: 50
+' Comment Lines: 20
+'   Blank Lines: 13
+'     File Size: 3.19 KB
+
+
+'     Class RemoteEnvironment
+' 
+'         Constructor: (+1 Overloads) Sub New
+'         Function: FindSymbol, getRemoteSymbol
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.IO
+Imports Microsoft.VisualBasic.ComponentModel.Collection
 Imports Microsoft.VisualBasic.Net
 Imports Microsoft.VisualBasic.Net.Tcp
 Imports Microsoft.VisualBasic.Parallel
@@ -75,6 +76,10 @@ Namespace Context.RPC
 
         ReadOnly master As IPEndPoint
         ReadOnly uuid As Integer
+        ''' <summary>
+        ''' symbols list that which is missing from the master node
+        ''' </summary>
+        ReadOnly missing404 As New Index(Of String)
 
         Sub New(uuid As Integer, master As IPEndPoint, parent As Environment)
             Call MyBase.New(
@@ -93,13 +98,20 @@ Namespace Context.RPC
         ''' </summary>
         ''' <param name="name"></param>
         ''' <param name="[inherits]"></param>
-        ''' <returns></returns>
+        ''' <returns>
+        ''' returns nothing if symbol not found on both local and master node environment
+        ''' </returns>
         Public Overrides Function FindSymbol(name As String, Optional [inherits] As Boolean = True) As Symbol
             Dim local As Symbol = MyBase.FindSymbol(name, [inherits])
+            Dim missingOnMaster As Boolean
+
+            SyncLock missing404
+                missingOnMaster = name Like missing404
+            End SyncLock
 
             Call Console.WriteLine($"try find symbol: {name}")
 
-            If local Is Nothing Then
+            If local Is Nothing AndAlso Not missingOnMaster Then
                 Call Console.WriteLine($"but not exists on local or cache...")
                 Call Console.WriteLine($"request symbol '{name}' from master!")
 
@@ -119,6 +131,13 @@ Namespace Context.RPC
             Using buffer As New MemoryStream(resp.ChunkBuffer)
                 If buffer.Length = 0 OrElse resp.Protocol = 404 Then
                     Call Console.WriteLine($"[404/NOT FOUND] target symbol is also not find on master environment!")
+
+                    ' this symbol will not query in the master
+                    ' environment any more
+                    SyncLock missing404
+                        Call missing404.Add(name)
+                    End SyncLock
+
                     ' symbol not found
                     Return Nothing
                 Else
