b01f5ee98c978b0ea0fc5bdf92792c7c7a1779c9
xieguigang <xie.guigang@gcmodeller.org>
2022-10-04 00:54:09 +0800

    try to handling of the missing post parameters

--- studio/Rserver/Rweb/RProcessor.vb
+++ studio/Rserver/Rweb/RProcessor.vb
@@ -1,55 +1,55 @@
 ﻿#Region "Microsoft.VisualBasic::492f6f417f1840e8655b767b1ffdaf5c, R-sharp\studio\Rserver\Rweb\RProcessor.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 203
-    '    Code Lines: 161
-    ' Comment Lines: 12
-    '   Blank Lines: 30
-    '     File Size: 7.92 KB
+' Summaries:
 
 
-    ' Class RProcessor
-    ' 
-    '     Constructor: (+1 Overloads) Sub New
-    ' 
-    '     Function: RscriptRouter, WithStartups
-    ' 
-    '     Sub: pushBackResult, RscriptHttpGet, RscriptHttpPost, runRweb, SaveResponse
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 203
+'    Code Lines: 161
+' Comment Lines: 12
+'   Blank Lines: 30
+'     File Size: 7.92 KB
+
+
+' Class RProcessor
+' 
+'     Constructor: (+1 Overloads) Sub New
+' 
+'     Function: RscriptRouter, WithStartups
+' 
+'     Sub: pushBackResult, RscriptHttpGet, RscriptHttpPost, runRweb, SaveResponse
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -67,6 +67,7 @@ Imports Microsoft.VisualBasic.Parallel
 Imports Microsoft.VisualBasic.Serialization.JSON
 Imports SMRUCC.Rsharp.Runtime
 Imports SMRUCC.Rsharp.Runtime.Serialize
+Imports REnv = SMRUCC.Rsharp.Runtime
 
 Public Class RProcessor
 
@@ -116,6 +117,12 @@ Public Class RProcessor
         For Each formKey In form.AllKeys
             args(formKey) = form.GetValues(formKey)
         Next
+
+        If Not request.POSTData.Objects.IsNullOrEmpty Then
+            For Each obj In request.POSTData.Objects
+                args(obj.Key) = REnv.asVector(Of String)(obj.Value)
+            Next
+        End If
 
         If is_background Then
             Call RunTask(Sub() Call runRweb(Rscript, request_id, args, response, is_background))
