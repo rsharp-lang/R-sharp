a4277a875afeea3687d0cc2cfe184961428b55e9
guigang xie <xie.guigang@gcmodeller.org>
2022-10-12 11:29:31 +0800

    handling of the debug info

--- R#/Runtime/Interop/RsharpOperator/ROperatorInvoke.vb
+++ R#/Runtime/Interop/RsharpOperator/ROperatorInvoke.vb
@@ -1,57 +1,58 @@
 ﻿#Region "Microsoft.VisualBasic::c95ee30912b0d343bafa27c63624efcf, R-sharp\R#\Runtime\Interop\RsharpOperator\ROperatorInvoke.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 57
-    '    Code Lines: 45
-    ' Comment Lines: 1
-    '   Blank Lines: 11
-    '     File Size: 1.85 KB
+' Summaries:
 
 
-    '     Class ROperatorInvoke
-    ' 
-    '         Constructor: (+1 Overloads) Sub New
-    '         Function: GetInvoke, Invoke2, Invoke3
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 57
+'    Code Lines: 45
+' Comment Lines: 1
+'   Blank Lines: 11
+'     File Size: 1.85 KB
+
+
+'     Class ROperatorInvoke
+' 
+'         Constructor: (+1 Overloads) Sub New
+'         Function: GetInvoke, Invoke2, Invoke3
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.Reflection
+Imports SMRUCC.Rsharp.Development
 Imports SMRUCC.Rsharp.Runtime.Components
 Imports SMRUCC.Rsharp.Runtime.Internal.Object.Converts
 Imports REnv = SMRUCC.Rsharp.Runtime.Internal
@@ -96,7 +97,11 @@ Namespace Runtime.Interop.Operator
             ElseIf TypeOf y Is Message Then
                 Return y
             Else
-                Return method.Invoke(Nothing, {x, y})
+                Try
+                    Return method.Invoke(Nothing, {x, y})
+                Catch ex As Exception
+                    Return REnv.debug.stop(New RuntimeError(ToString, ex), internal)
+                End Try
             End If
         End Function
 
@@ -112,7 +117,7 @@ Namespace Runtime.Interop.Operator
                 Try
                     Return method.Invoke(Nothing, {x, y, internal})
                 Catch ex As Exception
-                    Return REnv.debug.stop(New Exception(ToString, ex), internal)
+                    Return REnv.debug.stop(New RuntimeError(ToString, ex), internal)
                 End Try
             End If
         End Function
