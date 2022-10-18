d80c5baddd8440844c8baf7cfe19e3bfae6dfeef
guigang xie <xie.guigang@gcmodeller.org>
2022-10-18 21:23:36 +0800

    Update viewHDS.R

--- test/demo/buffer/viewHDS.R
+++ test/demo/buffer/viewHDS.R
@@ -1,5 +1,5 @@
 require(HDS);
 
-data = HDS::openStream("F:\5-livers.mzPack");
+data = HDS::openStream("T:\project\2022 KD\MALDI Da 50-1000\analysis\H39572-CHCA-1.mzPack");
 
 print(HDS::tree(data));
