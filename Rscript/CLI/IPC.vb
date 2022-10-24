e4413ef358a7c12811faec32584f6c230c46f9be
xieguigang <xie.guigang@gcmodeller.org>
2022-10-24 23:12:56 +0800

    Update IPC.vb
    config default entry function name

--- Rscript/CLI/IPC.vb
+++ Rscript/CLI/IPC.vb
@@ -1,51 +1,51 @@
 ﻿#Region "Microsoft.VisualBasic::efd4fca0127b16b9d8b20bb8a2fade96, R-sharp\Rscript\CLI\IPC.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 240
-    '    Code Lines: 203
-    ' Comment Lines: 7
-    '   Blank Lines: 30
-    '     File Size: 10.59 KB
+' Summaries:
 
 
-    ' Module CLI
-    ' 
-    '     Function: postResult, slaveMode, tryHandleJSON
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 240
+'    Code Lines: 203
+' Comment Lines: 7
+'   Blank Lines: 30
+'     File Size: 10.59 KB
+
+
+' Module CLI
+' 
+'     Function: postResult, slaveMode, tryHandleJSON
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -99,7 +99,7 @@ Partial Module CLI
     ''' <param name="args"></param>
     ''' <returns></returns>
     <ExportAPI("--slave")>
-    <Usage("--slave /exec <script.R> /args <json_base64> /request-id <request_id> /PORT=<port_number> [--debug /timeout=<timeout in ms, default=1000> /retry=<retry_times, default=5> /MASTER=<ip, default=localhost> --startups <packageNames, default=""""> /entry=<function_name, default=NULL> --attach <debug_pkg_dir>]")>
+    <Usage("--slave /exec <script.R> /args <json_base64> /request-id <request_id> /PORT=<port_number> [--debug /timeout=<timeout in ms, default=1000> /retry=<retry_times, default=5> /MASTER=<ip, default=localhost> --startups <packageNames, default=""""> /entry=<function_name, default=run> --attach <debug_pkg_dir>]")>
     <Description("Create a R# cluster node for run background or parallel task. This IPC command will run a R# script file that specified by the ``/exec`` argument, and then post back the result data json to the specific master listener.")>
     <Argument("/exec", False, CLITypes.File, AcceptTypes:={GetType(String)}, Extensions:="*.R", Description:="a specific R# script for run as background task.")>
     <Argument("/args", False, CLITypes.Base64, PipelineTypes.std_in,
@@ -130,7 +130,7 @@ Partial Module CLI
             .tryHandleJSON
         Dim port As Integer = CInt(Val(args <= "/PORT"))
         Dim master As String = args("/MASTER") Or "localhost"
-        Dim entry As String = args <= "/entry"
+        Dim entry As String = args("/entry") Or "run"
         Dim request_id As String = args <= "/request-id"
         Dim retryTimes As Integer = args("/retry") Or 5
         Dim timeout As Double = args("/timeout") Or 1000
