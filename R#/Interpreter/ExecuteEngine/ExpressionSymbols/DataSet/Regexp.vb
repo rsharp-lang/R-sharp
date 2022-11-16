b749c67d4f753817bd98c74596a5a7fd7e7b94bb
xieguigang <xie.guigang@gcmodeller.org>
2022-11-16 16:05:29 +0800

    show debug info

--- R#/Interpreter/ExecuteEngine/ExpressionSymbols/DataSet/Regexp.vb
+++ R#/Interpreter/ExecuteEngine/ExpressionSymbols/DataSet/Regexp.vb
@@ -1,65 +1,68 @@
 ﻿#Region "Microsoft.VisualBasic::c0e26f44f450af6adc33625de14a4cc9, R-sharp\R#\Interpreter\ExecuteEngine\ExpressionSymbols\DataSet\Regexp.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 68
-    '    Code Lines: 53
-    ' Comment Lines: 3
-    '   Blank Lines: 12
-    '     File Size: 2.33 KB
+' Summaries:
 
 
-    '     Class Regexp
-    ' 
-    '         Properties: expressionName, pattern, type
-    ' 
-    '         Constructor: (+1 Overloads) Sub New
-    '         Function: Evaluate, Matches, ToString
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 68
+'    Code Lines: 53
+' Comment Lines: 3
+'   Blank Lines: 12
+'     File Size: 2.33 KB
+
+
+'     Class Regexp
+' 
+'         Properties: expressionName, pattern, type
+' 
+'         Constructor: (+1 Overloads) Sub New
+'         Function: Evaluate, Matches, ToString
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.Text.RegularExpressions
+Imports Microsoft.VisualBasic.Emit.Delegates
 Imports Microsoft.VisualBasic.Linq
+Imports SMRUCC.Rsharp.Development.Package.File
 Imports SMRUCC.Rsharp.Runtime
 Imports SMRUCC.Rsharp.Runtime.Components
+Imports SMRUCC.Rsharp.Runtime.Components.Interface
 Imports SMRUCC.Rsharp.Runtime.Internal.Object
-Imports SMRUCC.Rsharp.Development.Package.File
 Imports r = System.Text.RegularExpressions.Regex
+Imports REnv = SMRUCC.Rsharp.Runtime
 
 Namespace Interpreter.ExecuteEngine.ExpressionSymbols.DataSets
 
@@ -86,6 +89,11 @@ Namespace Interpreter.ExecuteEngine.ExpressionSymbols.DataSets
             pattern = regexp
         End Sub
 
+        ''' <summary>
+        ''' just create a new regexp object for run text match
+        ''' </summary>
+        ''' <param name="envir"></param>
+        ''' <returns></returns>
         Public Overrides Function Evaluate(envir As Environment) As Object
             Return New r(pattern, RegexICSng)
         End Function
@@ -94,6 +102,13 @@ Namespace Interpreter.ExecuteEngine.ExpressionSymbols.DataSets
             Return $"/{pattern}/"
         End Function
 
+        ''' <summary>
+        ''' do regular expression pattern matches
+        ''' </summary>
+        ''' <param name="r"></param>
+        ''' <param name="text"></param>
+        ''' <param name="env"></param>
+        ''' <returns></returns>
         Public Shared Function Matches(r As Regex, text As Expression, env As Environment) As Object
             Dim strData As Object = text.Evaluate(env)
 
@@ -101,9 +116,14 @@ Namespace Interpreter.ExecuteEngine.ExpressionSymbols.DataSets
                 Return strData
             ElseIf strData Is Nothing Then
                 Return Nothing
+            ElseIf strData.GetType.ImplementInterface(Of RFunction) Then
+                Return Internal.debug.stop({
+                    $"the given symbols is a function symbol, can not be apply for regular expression pattern matches!",
+                    $"symbol expression: {text.ToString}"
+                }, env)
             End If
 
-            Dim inputs As String() = Runtime.asVector(Of String)(strData)
+            Dim inputs As String() = REnv.asVector(Of String)(strData)
 
             If inputs.Length = 1 Then
                 Return r.Matches(inputs(Scan0)).ToArray
