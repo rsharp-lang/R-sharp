d30eeb71fb1e5168c2c2fd8f7abcc4ce4f01c778
xieguigang <xie.guigang@gcmodeller.org>
2022-11-22 21:00:53 +0800

    Create httpHelp.vb

---
+++ studio/Rstudio/httpHelp.vb
@@ -0,0 +1,24 @@
+﻿Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.Rsharp.Runtime
+
+''' <summary>
+''' backend for the http help documents
+''' </summary>
+<Package("help")>
+Public Module httpHelp
+
+    ''' <summary>
+    ''' generate html page for keyword query
+    ''' </summary>
+    ''' <param name="term"></param>
+    ''' <param name="env"></param>
+    ''' <returns></returns>
+    <ExportAPI("search")>
+    Public Function handleSearch(term As String, Optional env As Environment = Nothing) As Object
+        Dim libpath As String = env.globalEnvironment.options.lib_loc
+
+        Return libpath
+    End Function
+
+End Module
