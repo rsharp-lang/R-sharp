10189681ce7dda0ada774eca0c9b956f12c0e463
guigang xie <xie.guigang@gcmodeller.org>
2022-11-29 13:33:13 +0800

    try to handling of the invalid syntax when parse the json syntax

--- R#/Language/Syntax/SyntaxTree/ExpressionBuilder.vb
+++ R#/Language/Syntax/SyntaxTree/ExpressionBuilder.vb
@@ -1,56 +1,57 @@
 ﻿#Region "Microsoft.VisualBasic::7522fdd495ebb95dadee368e6bd8134e, R-sharp\R#\Language\Syntax\SyntaxTree\ExpressionBuilder.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 507
-    '    Code Lines: 398
-    ' Comment Lines: 40
-    '   Blank Lines: 69
-    '     File Size: 23.05 KB
+' Summaries:
 
 
-    '     Module ExpressionBuilder
-    ' 
-    '         Function: getTupleSymbols, getValueAssign, keywordExpressionHandler, ParseExpression, parseInvoke
-    '                   parseSymbolIndex
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 507
+'    Code Lines: 398
+' Comment Lines: 40
+'   Blank Lines: 69
+'     File Size: 23.05 KB
+
+
+'     Module ExpressionBuilder
+' 
+'         Function: getTupleSymbols, getValueAssign, keywordExpressionHandler, ParseExpression, parseInvoke
+'                   parseSymbolIndex
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
+Imports System.Diagnostics.SymbolStore
 Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.ApplicationServices.Debugging.Diagnostics
 Imports Microsoft.VisualBasic.ComponentModel.Collection
@@ -500,15 +501,19 @@ Binary:
                     End If
                 End If
             ElseIf target.Length >= 3 Then
-                ' the exception is always the last one
-                With target.Skip(1) _
-                           .Take(code(Scan0).Length - 2) _
-                           .DoCall(Function(tokens)
-                                       Return getTupleSymbols(tokens, opts)
-                                   End Function) _
-                           .ToArray
+                target = target.Skip(1) _
+                    .Take(code(Scan0).Length - 2) _
+                    .ToArray
 
-                    If .Last.isException Then
+                If target.Length = 1 AndAlso target(Scan0).name = TokenType.comma Then
+                    Return New SyntaxResult(SyntaxError.CreateError(opts, New Exception("invalid syntax!")))
+                End If
+
+                ' the exception is always the last one
+                With getTupleSymbols(target, opts).ToArray
+                    If .IsNullOrEmpty Then
+                        symbol = {}
+                    ElseIf .Last.isException Then
                         Return .Last
                     Else
                         symbol = .Select(Function(e) e.expression) _
