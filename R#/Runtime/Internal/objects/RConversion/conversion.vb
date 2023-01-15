56bc37b3a37aff3c6212f1cca2f8d87285ddf6c2
xieguigang <xie.guigang@gcmodeller.org>
2023-01-15 23:40:52 +0800

    cast size to list

--- R#/Runtime/Internal/objects/RConversion/conversion.vb
+++ R#/Runtime/Internal/objects/RConversion/conversion.vb
@@ -1,57 +1,57 @@
 ﻿#Region "Microsoft.VisualBasic::7653dd74e04ca3e96691c7bb7c9534d4, R-sharp\R#\Runtime\Internal\objects\RConversion\conversion.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 1109
-    '    Code Lines: 646
-    ' Comment Lines: 351
-    '   Blank Lines: 112
-    '     File Size: 48.84 KB
+' Summaries:
 
 
-    '     Module RConversion
-    ' 
-    '         Function: asCharacters, asDataframe, asDate, asDate2, asDouble
-    '                   asInteger, asList, asLogicals, asNumeric, asObject
-    '                   asPipeline, asRaw, asVector, castArrayOfGeneric, castArrayOfObject
-    '                   castListRowsToDataframe, castListToDataframe, castType, handleListFeatureProjections, handleUnsure
-    '                   isCharacter, isDateTime, isLogical, populateNumeric, tryUnlistArray
-    '                   unlist, unlistOfRList, unlistRecursive
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 1109
+'    Code Lines: 646
+' Comment Lines: 351
+'   Blank Lines: 112
+'     File Size: 48.84 KB
+
+
+'     Module RConversion
+' 
+'         Function: asCharacters, asDataframe, asDate, asDate2, asDouble
+'                   asInteger, asList, asLogicals, asNumeric, asObject
+'                   asPipeline, asRaw, asVector, castArrayOfGeneric, castArrayOfObject
+'                   castListRowsToDataframe, castListToDataframe, castType, handleListFeatureProjections, handleUnsure
+'                   isCharacter, isDateTime, isLogical, populateNumeric, tryUnlistArray
+'                   unlist, unlistOfRList, unlistRecursive
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -64,6 +64,7 @@ Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.ComponentModel.Collection
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports Microsoft.VisualBasic.Emit.Delegates
+Imports Microsoft.VisualBasic.Imaging
 Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.Serialization
@@ -780,6 +781,17 @@ RE0:
                 }
             ElseIf obj.GetType.ImplementInterface(Of IDictionary) Then
                 Return DirectCast(obj, IDictionary).dictionaryToRList
+            ElseIf TypeOf obj Is Size OrElse TypeOf obj Is SizeF Then
+                Dim size As SizeF = If(TypeOf obj Is Size, DirectCast(obj, Size).SizeF, DirectCast(obj, SizeF))
+                Dim listdata As New list With {
+                    .elementType = RType.GetRSharpType(GetType(Double)),
+                    .slots = New Dictionary(Of String, Object) From {
+                        {"w", size.Width},
+                        {"h", size.Height}
+                    }
+                }
+
+                Return listdata
             Else
                 Return listInternal(obj, base.Rlist(args, env), env)
             End If
