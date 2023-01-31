dcb096a97705b057c5bc3142fe1bb75338fd82af
guigang xie <xie.guigang@gcmodeller.org>
2023-01-31 14:32:37 +0800

    fix of the missing parameter for invoke read.csv function from the data function

--- R#/System/Package/PackageFile/CreatePackage.vb
+++ R#/System/Package/PackageFile/CreatePackage.vb
@@ -538,7 +538,7 @@ Namespace Development.Package.File
 
         Private Function getFileReader(path As String) As String
             Select Case path.ExtensionSuffix.ToLower
-                Case "csv" : Return "read.csv,%s,1%,TRUE,TRUE,utf8,#,FALSE,$"
+                Case "csv" : Return "read.csv,%s,1%,TRUE,TRUE,utf8,#,FALSE,-1,$"
                 Case "txt" : Return "readLines,%s,NULL"
                 Case "rda" : Return "load,%s,$,FALSE"
                 Case "rds" : Return "readRDS,%s,NULL,$"
