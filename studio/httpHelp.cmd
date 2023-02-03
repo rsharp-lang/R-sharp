50c05ce6cfe0afd9f63d37e26181d84921e63bf8
guigang xie <xie.guigang@gcmodeller.org>
2023-02-03 14:48:37 +0800

    Update httpHelp.cmd

--- studio/httpHelp.cmd
+++ studio/httpHelp.cmd
@@ -1,7 +1,14 @@
 @echo off
 
-SET RENV=../App/net6.0-windows/R#.exe
+SET R_HELP=../App/help/R#.exe
+SET R_ENV=../App/net6.0-windows/R#.exe
 
-"%RENV%" ./httpHelp.R
+start http://127.0.0.1/
 
-pause
+if exist %R_HELP% (
+    "%R_HELP%"  ./httpHelp.R
+) else (
+    "%R_ENV%"   ./httpHelp.R
+)
+
+PAUSE
