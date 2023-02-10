feb1d4e615c948e391b3b3791b99ea4d8c459126
guigang xie <xie.guigang@gcmodeller.org>
2023-02-10 14:24:13 +0800

    add values

--- R#/Language/TokenIcer/Scanner.vb
+++ R#/Language/TokenIcer/Scanner.vb
@@ -544,7 +544,7 @@ Namespace Language.TokenIcer
             Select Case text
                 'Case RInterpreter.lastVariableName
                 '    Return New Token With {.name = TokenType.identifier, .text = text}
-                Case "|>", ":>", "+", "-", "*", "=", "/", ">", "<", "~", "<=", ">=", "!", "<-", "&&", "&", "||", "$", "@"
+                Case "|>", ":>", "+", "-", "*", "=", "/", ">", "<", "~", "<=", ">=", "!", "<-", "&&", "&", "||", "$", "@", "→"
                     Return New Token With {.name = TokenType.operator, .text = text}
                 Case ":"
                     Return New Token With {.name = TokenType.sequence, .text = text}
--- R#/Language/TokenIcer/Token.vb
+++ R#/Language/TokenIcer/Token.vb
@@ -1,58 +1,59 @@
 ﻿#Region "Microsoft.VisualBasic::2574cf7e175ca0df7d572cb45a94f780, R-sharp\R#\Language\TokenIcer\Token.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 65
-    '    Code Lines: 44
-    ' Comment Lines: 11
-    '   Blank Lines: 10
-    '     File Size: 2.19 KB
+' Summaries:
 
 
-    '     Class Token
-    ' 
-    '         Properties: isLiteral, literal
-    ' 
-    '         Constructor: (+2 Overloads) Sub New
-    '         Operators: <>, =
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 65
+'    Code Lines: 44
+' Comment Lines: 11
+'   Blank Lines: 10
+'     File Size: 2.19 KB
+
+
+'     Class Token
+' 
+'         Properties: isLiteral, literal
+' 
+'         Constructor: (+2 Overloads) Sub New
+'         Operators: <>, =
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
+Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.Scripting.TokenIcer
 
 Namespace Language.TokenIcer
@@ -67,6 +68,7 @@ Namespace Language.TokenIcer
         ''' </summary>
         ''' <returns></returns>
         Public ReadOnly Property literal As Object
+            <MethodImpl(MethodImplOptions.AggressiveInlining)>
             Get
                 Select Case name
                     Case TokenType.stringLiteral
@@ -89,6 +91,7 @@ Namespace Language.TokenIcer
         ''' </summary>
         ''' <returns></returns>
         Public ReadOnly Property isLiteral As Boolean
+            <MethodImpl(MethodImplOptions.AggressiveInlining)>
             Get
                 Return name = TokenType.booleanLiteral OrElse
                        name = TokenType.integerLiteral OrElse
@@ -98,21 +101,27 @@ Namespace Language.TokenIcer
             End Get
         End Property
 
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        <DebuggerStepThrough>
         Sub New()
         End Sub
 
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
         Public Sub New(name As TokenType, Optional value$ = Nothing)
             Call MyBase.New(name, value)
         End Sub
 
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
         Public Shared Widening Operator CType(type As TokenType) As Token
             Return New Token(type)
         End Operator
 
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
         Public Overloads Shared Operator =(token As Token, type As TokenType) As Boolean
             Return token.name = type
         End Operator
 
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
         Public Overloads Shared Operator <>(token As Token, type As TokenType) As Boolean
             Return Not token = type
         End Operator
