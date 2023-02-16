9909bab4e6da5c1b29ba23bcd781b7d4917f1525
xieguigang <xie.guigang@gcmodeller.org>
2023-02-16 20:21:32 +0800

    Create update_banner.cmd

---
+++ .visualstudio/update_banner.cmd
@@ -0,0 +1,12 @@
+@echo off
+
+SET R_HOME=../App\net6.0-windows
+SET REnv="%R_HOME%/R#.exe"
+SET updater=../studio/code_banner.R
+
+%REnv% %updater% --banner-xml ./gpl3.xml --proj-folder ../
+
+git add -A
+git commit -m "update source file banner headers!"
+
+pause
