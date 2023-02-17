a14bd1f4490a22847431a7abf31ee8f8585dc80d
xieguigang <xie.guigang@gcmodeller.org>
2023-02-17 09:40:18 +0800

    summary report table

--- R#/Runtime/Internal/internalInvokes/set.vb
+++ R#/Runtime/Internal/internalInvokes/set.vb
@@ -1,54 +1,54 @@
 ﻿#Region "Microsoft.VisualBasic::ea6ab69321978387d1ae1564e52a32d9, R-sharp\R#\Runtime\Internal\internalInvokes\set.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 466
-    '    Code Lines: 256
-    ' Comment Lines: 156
-    '   Blank Lines: 54
-    '     File Size: 20.46 KB
+' Summaries:
 
 
-    '     Module [set]
-    ' 
-    '         Function: combn, count, createLoop, crossing, duplicated
-    '                   getObjectSet, indexOf, intersect, jaccard, rev
-    '                   setdiff, union
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 466
+'    Code Lines: 256
+' Comment Lines: 156
+'   Blank Lines: 54
+'     File Size: 20.46 KB
+
+
+'     Module [set]
+' 
+'         Function: combn, count, createLoop, crossing, duplicated
+'                   getObjectSet, indexOf, intersect, jaccard, rev
+'                   setdiff, union
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -71,6 +71,19 @@ Namespace Runtime.Internal.Invokes
     ''' Set Operations
     ''' </summary>
     Module [set]
+
+        <ExportAPI("table")>
+        Public Function table(x As String()) As Object
+            Dim factors = x.GroupBy(Function(str) If(str, "")).ToArray
+            Dim summary As New dataframe With {
+                .columns = New Dictionary(Of String, Array) From {
+                    {"factors", factors.Select(Function(c) c.Key).ToArray},
+                    {"size", factors.Select(Function(c) c.Count).ToArray}
+                }
+            }
+
+            Return summary
+        End Function
 
         ''' <summary>
         ''' ### setdiff: Set Difference of Subsets
