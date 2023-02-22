a922d70550e4850d1df85b03f31c1e864232dbee
guigang xie <xie.guigang@gcmodeller.org>
2023-02-22 14:46:54 +0800

    add xml dev notes

--- R#/Runtime/Interop/RInteropAttributes/RInteropAttribute.vb
+++ R#/Runtime/Interop/RInteropAttributes/RInteropAttribute.vb
@@ -1,82 +1,83 @@
 ﻿#Region "Microsoft.VisualBasic::7fa462658c3ffbba9f6d752ea56099da, D:/GCModeller/src/R-sharp/R#//Runtime/Interop/RInteropAttributes/RInteropAttribute.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 70
-    '    Code Lines: 39
-    ' Comment Lines: 15
-    '   Blank Lines: 16
-    '     File Size: 2.50 KB
+' Summaries:
 
 
-    '     Class RInteropAttribute
-    ' 
-    ' 
-    ' 
-    '     Class RInitializeAttribute
-    ' 
-    ' 
-    ' 
-    '     Class RByRefValueAssignAttribute
-    ' 
-    ' 
-    ' 
-    '     Class RNameAliasAttribute
-    ' 
-    '         Properties: [alias]
-    ' 
-    '         Constructor: (+1 Overloads) Sub New
-    '         Function: ToString
-    ' 
-    '     Class RBasePackageAttribute
-    ' 
-    '         Constructor: (+1 Overloads) Sub New
-    ' 
-    '     Class RTypeExportAttribute
-    ' 
-    '         Properties: model, name
-    ' 
-    '         Constructor: (+1 Overloads) Sub New
-    '         Function: ToString
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 70
+'    Code Lines: 39
+' Comment Lines: 15
+'   Blank Lines: 16
+'     File Size: 2.50 KB
+
+
+'     Class RInteropAttribute
+' 
+' 
+' 
+'     Class RInitializeAttribute
+' 
+' 
+' 
+'     Class RByRefValueAssignAttribute
+' 
+' 
+' 
+'     Class RNameAliasAttribute
+' 
+'         Properties: [alias]
+' 
+'         Constructor: (+1 Overloads) Sub New
+'         Function: ToString
+' 
+'     Class RBasePackageAttribute
+' 
+'         Constructor: (+1 Overloads) Sub New
+' 
+'     Class RTypeExportAttribute
+' 
+'         Properties: model, name
+' 
+'         Constructor: (+1 Overloads) Sub New
+'         Function: ToString
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.Rsharp.Development.Package
 
 Namespace Runtime.Interop
 
@@ -125,6 +126,13 @@ Namespace Runtime.Interop
     ''' <summary>
     ''' export data type to ``R#`` runtime environment
     ''' </summary>
+    ''' <remarks>
+    ''' type information will be imports into current runtime 
+    ''' context environment when do package module imports at 
+    ''' function invoke 
+    ''' <see cref="ImportsPackage.ImportsStaticInternalImpl"/>
+    ''' </remarks>
+    ''' 
     <AttributeUsage(AttributeTargets.Class, AllowMultiple:=True, Inherited:=True)>
     Public Class RTypeExportAttribute : Inherits RInteropAttribute
 
