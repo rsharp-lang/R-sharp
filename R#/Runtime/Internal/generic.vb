79c8567adda345eaf34f151c757350cee676b662
xieguigang <xie.guigang@gcmodeller.org>
2023-03-23 20:29:19 +0800

    try to handling the bug about invoke generic

--- R#/Runtime/Internal/generic.vb
+++ R#/Runtime/Internal/generic.vb
@@ -1,58 +1,58 @@
 ﻿#Region "Microsoft.VisualBasic::ffd9b166addd9d0caa134fc2fe9af603, D:/GCModeller/src/R-sharp/R#//Runtime/Internal/generic.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 163
-    '    Code Lines: 103
-    ' Comment Lines: 37
-    '   Blank Lines: 23
-    '     File Size: 6.41 KB
+' Summaries:
 
 
-    '     Delegate Function
-    ' 
-    ' 
-    '     Module generic
-    ' 
-    '         Function: exists, (+3 Overloads) invokeGeneric, missingGenericSymbol, parseGeneric
-    ' 
-    '         Sub: add
-    ' 
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 163
+'    Code Lines: 103
+' Comment Lines: 37
+'   Blank Lines: 23
+'     File Size: 6.41 KB
+
+
+'     Delegate Function
+' 
+' 
+'     Module generic
+' 
+'         Function: exists, (+3 Overloads) invokeGeneric, missingGenericSymbol, parseGeneric
+' 
+'         Sub: add
+' 
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -97,6 +97,7 @@ Namespace Runtime.Internal
             generics(name)(x) = [overloads]
         End Sub
 
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
         Public Function exists(funcName As String) As Boolean
             Return generics.ContainsKey(funcName)
         End Function
@@ -189,13 +190,13 @@ Namespace Runtime.Internal
                 type = x.GetType
             End If
 
+            If x IsNot Nothing AndAlso x.GetType.IsArray Then
+                x = UnsafeTryCastGenericArray(x)
+            End If
+
             If Not generics(funcName).ContainsKey(type) Then
                 If TypeOf x Is Object() Then
-                    x = MeasureRealElementType(DirectCast(x, Array)) _
-                        .DoCall(Function(constrain)
-                                    Return New vector(constrain, DirectCast(x, Array), env)
-                                End Function) _
-                        .data
+                    x = UnsafeTryCastGenericArray(DirectCast(x, Array))
                     type = x.GetType
 
                     If generics(funcName).ContainsKey(type) Then
