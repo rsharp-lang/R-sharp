31a82c8202b8086d1fe1a984601ac72e0deb07ed
xieguigang <xie.guigang@gcmodeller.org>
2023-03-25 02:20:18 +0800

    test rust string parameter

--- REnv/src/delaunay/src/lib.rs
+++ REnv/src/delaunay/src/lib.rs
@@ -1,5 +1,10 @@
 #[no_mangle]
-pub extern fn demo_rust_func(x: i32) -> i32 {
+pub extern "C" fn demo_rust_func(x: i32) -> i32 {
     println!("Hello from Rust");
     return 42 + x;
+}
+
+#[no_mangle]
+pub extern "C" fn rust_string_test(str: &str) {
+    println!("Hello, {}!", str);
 }
