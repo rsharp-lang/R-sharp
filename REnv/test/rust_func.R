1db322e383f868f9b18c428df49e255e3cc3a059
xieguigang <xie.guigang@gcmodeller.org>
2023-03-25 13:36:14 +0800

    get string test success

--- REnv/test/rust_func.R
+++ REnv/test/rust_func.R
@@ -1,9 +1,5 @@
 dyn.load("E:\GCModeller\src\R-sharp\REnv\src\delaunay\target\release\delaunay.dll");
-
-# a = .Call("demo_rust_func", "delaunay", x = i32(66), return = "i32");
-
-# print(a);
-
+a = .Call("demo_rust_func", "delaunay", x = i32(66), return = "i32");
+print(a);
 .Call("rust_string_test", "delaunay", x = string("value from R# language"));
-
 str(.Call("get_rust_str", "delaunay", str = string("AAABBBCCCDDD"), return = "string"));
