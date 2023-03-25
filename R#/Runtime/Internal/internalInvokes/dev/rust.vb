9240d9b537884b7d026b7e1e6a524b690e268b12
xieguigang <xie.guigang@gcmodeller.org>
2023-03-25 14:52:58 +0800

    make improvements for find path

--- R#/Runtime/Internal/internalInvokes/dev/rust.vb
+++ R#/Runtime/Internal/internalInvokes/dev/rust.vb
@@ -9,6 +9,37 @@ Namespace Runtime.Internal.Invokes
     ''' </summary>
     Public Module rust
 
+        Private Function SolveCLibrary(x As String, env As Environment) As String
+            If x.FileExists Then
+                Return x
+            ElseIf TypeOf env Is PackageEnvironment Then
+                Dim pkg As PackageEnvironment = env
+                Dim dir As String = $"{pkg.libpath}/lib_c/"
+                Dim fileName As String = $"{dir}/{x}"
+
+                If fileName.FileExists Then
+                    Return fileName
+                Else
+                    GoTo FIND_RENV
+                End If
+            Else
+FIND_RENV:
+                For Each dir As String In New String() {
+                    App.HOME,
+                    $"{App.HOME}/lib/",
+                    $"{App.HOME}/lib_c/",
+                    $"{App.HOME}/../lib/",
+                    $"{App.HOME}/../lib_c/"
+                }
+                    If $"{dir}/{x}".FileExists Then
+                        Return $"{dir}/{x}"
+                    End If
+                Next
+
+                Return x
+            End If
+        End Function
+
         ''' <summary>
         ''' ### Foreign Function Interface
         ''' 
@@ -22,12 +53,14 @@ Namespace Runtime.Internal.Invokes
         ''' </param>
         <ExportAPI("dyn.load")>
         Public Function dynLoad(x As String, Optional env As Environment = Nothing) As Object
-            Dim dll As New UnmanagedDll(dllName:=x)
+            Dim dll As New UnmanagedDll(dllName:=SolveCLibrary(x, env))
             Dim globalEnv = env.globalEnvironment
+            Dim libc_key As String = x.BaseName
 
-            Call globalEnv.nativeLibraries.Add(x.BaseName, dll)
+            ' hook current library to runtime environment
+            globalEnv.nativeLibraries(libc_key) = dll
 
-            Return Nothing
+            Return dll
         End Function
 
         ''' <summary>
