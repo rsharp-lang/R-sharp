d65654d8909c581fd86a8a04ab518b8401d211b1
guigang xie <xie.guigang@gcmodeller.org>
2023-03-29 14:22:51 +0800

    Update tryCatch.R

--- test/syntax/closures/tryCatch.R
+++ test/syntax/closures/tryCatch.R
@@ -1,6 +1,7 @@
 # catch error
 const catch = try(ex -> stop(123)) {
-	print(ex);
+	print([ex]::error);
+	print([ex]::stackframe);
 	
 	# returns a default value
 	# if an error has been catched
@@ -12,4 +13,9 @@ print(catch);
 # get try-error
 const tryErr = try(stop(123));
 
+
+print("get try catch error:");
+
 print(tryErr);
+
+print(111222333);
