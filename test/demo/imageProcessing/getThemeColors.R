bdfb51acd153969bddf7be81fea10f3436b0b4c2
xieguigang <xie.guigang@gcmodeller.org>
2023-04-08 00:54:40 +0800

    not working as expected?

--- test/demo/imageProcessing/getThemeColors.R
+++ test/demo/imageProcessing/getThemeColors.R
@@ -2,7 +2,12 @@ setwd(@dir);
 
 img = readImage("./906338a89185d86bf93650eab1df27bffb34ee06.jpg");
 
-theme_colors = colors(img, n = 8);
+theme_colors = colors(img, n = 8,character  = TRUE);
 
 print(theme_colors);
 
+rect = `<div style="width: 50px; height: 50px; background-color: ${theme_colors}"></div>`;
+
+writeLines(rect, con = "./theme_colors.html");
+
+# pause();
