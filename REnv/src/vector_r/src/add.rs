272f8e26d9c9662df58e6d2118dc86f04f6ff579
guigang xie <xie.guigang@gcmodeller.org>
2023-04-14 17:52:44 +0800

    unsrue how to deal with the array type

--- REnv/src/vector_r/src/add.rs
+++
@@ -1,6 +0,0 @@
-use std::simd::f32x4;
-use std::simd::i32x4;
-
-pub fn add(x: &[i32], y: &[i32], len: usize) -> &[i32] {
-    
-}
--- REnv/src/vector_r/src/lib.rs
+++ REnv/src/vector_r/src/lib.rs
@@ -1,21 +1,7 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
+mod LdaGibbsSampler;
 
 #[no_mangle]
 pub extern "C" add(left: *const usize, right: *const usize) -> *mut usize {
 
 } 
 
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
