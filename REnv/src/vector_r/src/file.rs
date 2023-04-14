cc0357baba3d314b0c5b916016c61189b09f08a3
xieguigang <xie.guigang@gcmodeller.org>
2023-04-15 01:27:38 +0800

    fix for open file and write text data

--- REnv/src/vector_r/src/file.rs
+++ REnv/src/vector_r/src/file.rs
@@ -1,8 +1,7 @@
-use std::fs;
+use std::fs::File;
 use std::fs::OpenOptions;
 use std::io::Write;
 use std::path::Path;
-use std::result::Result;
 
 pub enum FileModes {
     OpenOrCreate,
@@ -10,43 +9,49 @@ pub enum FileModes {
     Truncate,
 }
 
-pub fn Open(path: *const char, mode: FileModes) {
+pub fn Open(path: &str, mode: FileModes) -> Result<File, &str> {
     match mode {
         FileModes::OpenOrCreate => {
             if !FileExists(path) {
                 // create new
-                return std::fs::File::create(path).expect("create file error!");
+                return Ok(std::fs::File::create(path).expect("create file error!"));
             } else {
                 // open
-                return OpenOptions::new().open(path).expect("can not open file!");
+                return Ok(OpenOptions::new().open(path).expect("can not open file!"));
             }
         }
         FileModes::Append => {
             if !FileExists(path) {
                 return Err("file missing!");
             } else {
-                return OpenOptions::new()
+                return Ok(OpenOptions::new()
                     .append(true)
                     .open(path)
-                    .expect("can not open file!");
+                    .expect("can not open file!"));
             }
         }
         FileModes::Truncate => {
             if !FileExists(path) {
                 // just create new
-                return std::fs::File::create(path).expect("create file error!");
+                return Ok(std::fs::File::create(path).expect("create file error!"));
             } else {
-                return OpenOptions::new()
+                return Ok(OpenOptions::new()
                     .write(true)
                     .truncate(true)
                     .open(path)
-                    .expect("open file error!");
+                    .expect("open file error!"));
             }
         }
         _ => return Err("Unknow options!"),
     }
 }
 
-pub fn FileExists(path: *const char) -> bool {
+pub fn FileExists(path: &str) -> bool {
     return Path::new(path).exists();
+}
+
+pub fn WriteLines(x: Vec<&str>, mut con: File) {
+    for val in x.iter() {
+        con.write_all(val.chars().as_str().as_bytes());
+    }
 }
