25243871a90eb3640558ab52e001a93171f4800d
guigang xie <xie.guigang@gcmodeller.org>
2023-04-17 10:33:58 +0800

    parse syntax text

--- studio/Rsharp_IL/nts/InternalParser.vb
+++ studio/Rsharp_IL/nts/InternalParser.vb
@@ -1,5 +1,7 @@
 ﻿Imports System.Runtime.CompilerServices
 Imports SMRUCC.Rsharp.Interpreter
+Imports SMRUCC.Rsharp.Language.Syntax.SyntaxParser
+Imports SMRUCC.Rsharp.Language.TokenIcer
 Imports SMRUCC.Rsharp.Runtime.Components
 
 Public Module InternalParser
@@ -7,5 +9,9 @@ Public Module InternalParser
     <Extension>
     Public Function ParseTsScript(script As Rscript, Optional debug As Boolean = False) As Program
         Return New SyntaxTree(script, debug).ParseTsScript()
+    End Function
+
+    Public Function ParseTypeScriptLine(tokens As IEnumerable(Of Token), opts As SyntaxBuilderOptions) As SyntaxResult
+
     End Function
 End Module
--- studio/Rsharp_IL/nts/SyntaxTree.vb
+++ studio/Rsharp_IL/nts/SyntaxTree.vb
@@ -1,11 +1,24 @@
 ﻿Imports System.Runtime.CompilerServices
 Imports SMRUCC.Rsharp.Interpreter
+Imports SMRUCC.Rsharp.Language.Syntax.SyntaxParser
 Imports SMRUCC.Rsharp.Runtime.Components
 
 Public Class SyntaxTree
 
-    Sub New(script As RScript, Optional debug As Boolean = False)
+    ReadOnly script As Rscript
+    ReadOnly debug As Boolean = False
+    ReadOnly scanner As TsScanner
+    ReadOnly opts As SyntaxBuilderOptions
 
+    Sub New(script As Rscript, Optional debug As Boolean = False)
+        Me.debug = debug
+        Me.script = script
+        Me.scanner = New TsScanner(script.script)
+        Me.opts = New SyntaxBuilderOptions(AddressOf ParseTypeScriptLine, Function(c, s) New TsScanner(c, s)) With {
+            .source = script,
+            .debug = debug,
+            .pipelineSymbols = {"."}
+        }
     End Sub
 
     Friend Function ParseTsScript() As Program
