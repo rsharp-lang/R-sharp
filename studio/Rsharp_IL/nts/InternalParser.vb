6a97e18a04d6ac8bfd5037752401c201629259be
guigang xie <xie.guigang@gcmodeller.org>
2023-04-17 10:17:54 +0800

    Update InternalParser.vb

--- studio/Rsharp_IL/nts/InternalParser.vb
+++ studio/Rsharp_IL/nts/InternalParser.vb
@@ -6,6 +6,6 @@ Public Module InternalParser
 
     <Extension>
     Public Function ParsePyScript(script As Rscript, Optional debug As Boolean = False) As Program
-        Return New SyntaxTree(script, debug).ParseTsScript()
+        ' Return New SyntaxTree(script, debug).ParseTsScript()
     End Function
 End Module
