0143e7ce55105b7d54d33a80bafc69d22f8a1012
xieguigang <xie.guigang@gcmodeller.org>
2023-04-18 23:52:23 +0800

    run js test success

--- test/jsTest/run_test.js
+++ test/jsTest/run_test.js
@@ -5,7 +5,20 @@ console.log({
 	suffix: "!"
 })
 
+
+
+console.log([
+	{title: "aaaa", dat: "vvvvv"},
+	{title:"bbbbbbb", dat:"xxxxxxx"}
+])
+
 console.table([
 	{title: "aaaa", dat: "vvvvv"},
 	{title:"bbbbbbb", dat:"xxxxxxx"}
 ])
+
+
+var x = 5.1;
+
+console.log(x)
+console.log(x * 2)
