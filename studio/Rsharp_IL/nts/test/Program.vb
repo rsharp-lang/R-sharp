668846259ea76d757f93d7afd6da2e4d932c016c
xieguigang <xie.guigang@gcmodeller.org>
2023-04-18 20:05:08 +0800

    configs for run test

--- studio/Rsharp_IL/nts/test/Program.vb
+++ studio/Rsharp_IL/nts/test/Program.vb
@@ -4,7 +4,8 @@ Imports SMRUCC.TypeScript
 Module Program
     Sub Main(args As String())
         Dim ts = New TypeScriptLoader
-        Dim script = ts.ParseScript("D:\GCModeller\src\R-sharp\test\jsTest\test3.js", GlobalEnvironment.defaultEmpty)
+        Dim script1 = ts.ParseScript("/GCModeller\src\R-sharp\test\jsTest\test1.js", GlobalEnvironment.defaultEmpty)
+        Dim script3 = ts.ParseScript("/GCModeller\src\R-sharp\test\jsTest\test3.js", GlobalEnvironment.defaultEmpty)
 
         Console.WriteLine("Hello World!")
     End Sub
