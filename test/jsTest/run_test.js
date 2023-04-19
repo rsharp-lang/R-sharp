e9efb324201d12081ccbdb75508a001b4d0434b5
xieguigang <xie.guigang@gcmodeller.org>
2023-04-19 23:41:54 +0800

    Update run_test.js

--- test/jsTest/run_test.js
+++ test/jsTest/run_test.js
@@ -1,24 +1,26 @@
 console.log("hello world!")
+console.log("\n")
+console.log("print object json string:")
 console.log({
 	name: "hello",
 	data: "world",
 	suffix: "!"
 })
-
-
-
 console.log([
-	{title: "aaaa", dat: "vvvvv"},
-	{title:"bbbbbbb", dat:"xxxxxxx"}
+	{ title: "aaaa", dat: "vvvvv" },
+	{ title: "bbbbbbb", dat: "xxxxxxx" }
 ])
-
+console.log("\n")
+console.log("print object in table:")
+console.log("\n")
 console.table([
-	{title: "aaaa", dat: "vvvvv"},
-	{title:"bbbbbbb", dat:"xxxxxxx"}
+	{ title: "aaaa", dat: "vvvvv" },
+	{ title: "bbbbbbb", dat: "xxxxxxx" }
 ])
-
 
 var x = 5.1;
 
+// console.log("\n")
+console.log("print math value:")
 console.log(x)
 console.log(x * 2)
