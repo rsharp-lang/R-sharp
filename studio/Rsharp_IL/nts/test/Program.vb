e8d0e956828b438639e9f938f5996dedc1311843
xieguigang <gg.xie@bionovogene.com>
2023-04-19 13:58:06 +0800

    configs for run js parser test debug

--- studio/Rsharp_IL/nts/test/Program.vb
+++ studio/Rsharp_IL/nts/test/Program.vb
@@ -6,11 +6,11 @@ Module Program
     Sub Main(args As String())
         Dim ts = New TypeScriptLoader
         ' Dim script1 As RProgram = ts.ParseScript("/GCModeller\src\R-sharp\test\jsTest\test1.js", GlobalEnvironment.defaultEmpty)
-        ' Dim script3 As RProgram = ts.ParseScript("/GCModeller\src\R-sharp\test\jsTest\test3.js", GlobalEnvironment.defaultEmpty)
+        Dim script3 As RProgram = ts.ParseScript("/GCModeller\src\R-sharp\test\jsTest\test3.js", GlobalEnvironment.defaultEmpty)
         ' Dim script4 As RProgram = ts.ParseScript("/GCModeller\src\R-sharp\test\jsTest\test4.js", GlobalEnvironment.defaultEmpty)
         ' Dim script5 As RProgram = ts.ParseScript("/GCModeller\src\R-sharp\test\jsTest\test5.js", GlobalEnvironment.defaultEmpty)
         ' Dim script6 As RProgram = ts.ParseScript("/GCModeller\src\R-sharp\test\jsTest\test6.js", GlobalEnvironment.defaultEmpty)
-        Dim script7 As RProgram = ts.ParseScript("/GCModeller\src\R-sharp\test\jsTest\run_test.js", GlobalEnvironment.defaultEmpty)
+        ' Dim script7 As RProgram = ts.ParseScript("/GCModeller\src\R-sharp\test\jsTest\run_test.js", GlobalEnvironment.defaultEmpty)
 
         Console.WriteLine("Hello World!")
     End Sub
