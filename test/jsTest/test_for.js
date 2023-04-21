e43091e553495ed8f1264765a18182352bb9d045
xieguigang <xie.guigang@gcmodeller.org>
2023-04-21 23:02:18 +0800

    vector test success

--- test/jsTest/test_for.js
+++ test/jsTest/test_for.js
@@ -5,3 +5,12 @@ for(let x of [1,3,5,7,9]) {
 
 // json test
 console.log( JSON.stringify( {a5: [1,3,5,7,9,"dddddd ######"]}) );
+
+// vector test
+
+var v1 = [1,3,5,7,9];
+var f = v1 * 5
+
+console.log(v1);
+console.log(f);
+console.log(f / 2.3257);
