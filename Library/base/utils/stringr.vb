7eb6dddcbc33b42758b6745f10c4e93b8bcc16eb
guigang xie <xie.guigang@gcmodeller.org>
2023-04-26 15:51:54 +0800

    config MSA module

--- Library/base/utils/stringr.vb
+++ Library/base/utils/stringr.vb
@@ -55,6 +55,7 @@ Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.ComponentModel.Algorithm.DynamicProgramming
 Imports Microsoft.VisualBasic.ComponentModel.Algorithm.DynamicProgramming.Levenshtein
+Imports Microsoft.VisualBasic.DataMining.DynamicProgramming
 Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.MIME.application.xml
 Imports Microsoft.VisualBasic.Scripting.MetaData
@@ -206,4 +207,31 @@ Module stringr
 
         Return scs_str
     End Function
+
+    <ExportAPI("multiple_text_alignment")>
+    <RApiReturn("cost", "alignments")>
+    Public Function multipleTextAlignment(<RRawVectorArgument> x As Object, Optional env As Environment = Nothing) As Object
+        Dim centerStar As New CenterStar(CLRVector.asCharacter(x))
+        Dim alignments As String() = Nothing
+        Dim cost As Double = centerStar.Compute(New CharCompare, alignments)
+        Dim output As New list With {
+            .slots = New Dictionary(Of String, Object) From {
+                {"cost", cost},
+                {"alignments", alignments}
+            }
+        }
+
+        Return output
+    End Function
+
+    Private Structure CharCompare : Implements IScore(Of Char)
+
+        Public Function GetSimilarityScore(a As Char, b As Char) As Double Implements IScore(Of Char).GetSimilarityScore
+            If a = b Then
+                Return 0
+            Else
+                Return 1
+            End If
+        End Function
+    End Structure
 End Module
