9a57b04a25c5ba6e3fd9bd793aa8d9f53e75f8c0
guigang xie <xie.guigang@gcmodeller.org>
2023-05-05 12:22:01 +0800

    parse the literal string value

--- R#/System/Document/ShellScript/CommandLineArgument.vb
+++ R#/System/Document/ShellScript/CommandLineArgument.vb
@@ -1,57 +1,59 @@
 ﻿#Region "Microsoft.VisualBasic::738af6d73eb6a34e2103ed7b249c4df0, D:/GCModeller/src/R-sharp/R#//System/Document/ShellScript/CommandLineArgument.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 24
-    '    Code Lines: 19
-    ' Comment Lines: 0
-    '   Blank Lines: 5
-    '     File Size: 700 B
+' Summaries:
 
 
-    '     Class CommandLineArgument
-    ' 
-    '         Properties: defaultValue, description, isLiteral, isNumeric, name
-    '                     type
-    ' 
-    '         Function: ToString
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 24
+'    Code Lines: 19
+' Comment Lines: 0
+'   Blank Lines: 5
+'     File Size: 700 B
+
+
+'     Class CommandLineArgument
+' 
+'         Properties: defaultValue, description, isLiteral, isNumeric, name
+'                     type
+' 
+'         Function: ToString
+' 
+' 
+' /********************************************************************************/
 
 #End Region
+
+Imports SMRUCC.Rsharp.Runtime.Vectorization
 
 Namespace Development.CommandLine
 
@@ -70,6 +72,17 @@ Namespace Development.CommandLine
                     type = "double"
             End Get
         End Property
+
+        Public Function GetLiteralValue() As Object
+            Select Case type
+                Case "integer" : Return CLRVector.asInteger(defaultValue)
+                Case "numeric", "double" : Return CLRVector.asNumeric(defaultValue)
+                Case "boolean" : Return CLRVector.asLogical(defaultValue)
+                Case "string" : Return CLRVector.asCharacter(defaultValue)
+                Case Else
+                    Return defaultValue
+            End Select
+        End Function
 
         Public Overrides Function ToString() As String
             Return $"{name}: {description}"
