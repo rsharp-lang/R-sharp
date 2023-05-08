a5fefc0abde58e74e479ef0c9a6656c57c7e731a
xieguigang <xie.guigang@gcmodeller.org>
2023-05-08 14:37:08 +0800

    add language interop environment

---
+++ R#/Runtime/Environment/PolyglotInteropEnvironment.vb
@@ -0,0 +1,16 @@
+﻿Namespace Runtime
+
+    ''' <summary>
+    ''' symbols solver for javascript/python reference to R# object
+    ''' </summary>
+    Public Class PolyglotInteropEnvironment : Inherits Environment
+
+        Sub New(_global As GlobalEnvironment)
+            Call MyBase.New(_global)
+        End Sub
+
+        Public Sub AddInteropSymbol(symbol As String, value As Object)
+
+        End Sub
+    End Class
+End Namespace
