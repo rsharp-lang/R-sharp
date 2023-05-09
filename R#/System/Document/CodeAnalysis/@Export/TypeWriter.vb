3c89fcabbd96e43da91862acfab869ea8cf31f70
guigang xie <xie.guigang@gcmodeller.org>
2023-05-09 16:55:10 +0800

    move to export folder

---
+++ R#/System/Document/CodeAnalysis/@Export/TypeWriter.vb
@@ -0,0 +1,18 @@
+﻿Namespace Development.CodeAnalysis
+
+    Public Class TypeWriter
+
+        ReadOnly indent As Integer
+        ReadOnly symbol As SymbolTypeDefine
+
+        Sub New(indent As Integer, symbol As SymbolTypeDefine)
+            Me.indent = indent
+            Me.symbol = symbol
+        End Sub
+
+        Public Overrides Function ToString() As String
+            Return symbol.ToString
+        End Function
+
+    End Class
+End Namespace
