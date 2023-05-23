9f16914be91d40b6a3b49e73f363d2802294aecf
xieguigang <xie.guigang@gcmodeller.org>
2023-05-23 16:54:03 +0800

    convert gdi+ size to a numeric vector

--- R#/Runtime/Internal/objects/RConversion/conversion.vb
+++ R#/Runtime/Internal/objects/RConversion/conversion.vb
@@ -1,57 +1,57 @@
 ﻿#Region "Microsoft.VisualBasic::4c2ee237cafc07dfcd69092715d6dc9c, D:/GCModeller/src/R-sharp/R#//Runtime/Internal/objects/RConversion/conversion.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 1167
-    '    Code Lines: 693
-    ' Comment Lines: 354
-    '   Blank Lines: 120
-    '     File Size: 52.57 KB
+' Summaries:
 
 
-    '     Module RConversion
-    ' 
-    '         Function: asCharacters, asDataframe, asDate, asDate2, asDouble
-    '                   asInteger, asList, asLogicals, asNumeric, asObject
-    '                   asPipeline, asRaw, asVector, castArrayOfGeneric, castArrayOfObject
-    '                   castListRowsToDataframe, castListToDataframe, castType, handleListFeatureProjections, handleUnsure
-    '                   isCharacter, isDateTime, isLogical, populateNumeric, tryUnlistArray
-    '                   unlist, unlistOfRList, unlistRecursive
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 1167
+'    Code Lines: 693
+' Comment Lines: 354
+'   Blank Lines: 120
+'     File Size: 52.57 KB
+
+
+'     Module RConversion
+' 
+'         Function: asCharacters, asDataframe, asDate, asDate2, asDouble
+'                   asInteger, asList, asLogicals, asNumeric, asObject
+'                   asPipeline, asRaw, asVector, castArrayOfGeneric, castArrayOfObject
+'                   castListRowsToDataframe, castListToDataframe, castType, handleListFeatureProjections, handleUnsure
+'                   isCharacter, isDateTime, isLogical, populateNumeric, tryUnlistArray
+'                   unlist, unlistOfRList, unlistRecursive
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -1012,6 +1012,16 @@ RE0:
                     Yield DirectCast(item, TimeSpan).TotalMilliseconds
                 ElseIf TypeOf item Is Date Then
                     Yield DirectCast(item, Date).UnixTimeStamp
+                ElseIf TypeOf item Is Size Then
+                    With DirectCast(item, Size)
+                        Yield .Width
+                        Yield .Height
+                    End With
+                ElseIf TypeOf item Is SizeF Then
+                    With DirectCast(item, SizeF)
+                        Yield .Width
+                        Yield .Height
+                    End With
                 Else
                     Yield RCType.CTypeDynamic(item, GetType(Double), env)
                 End If
