d34d3802e008076a50b7cd1c07507b6164182fa2
xieguigang <xie.guigang@gcmodeller.org>
2023-05-30 10:07:26 +0800

    config for test the vector constructor

---
+++ R#/test/test_vector_constructor.vb
@@ -0,0 +1,8 @@
+﻿Module test_vector_constructor
+
+    Sub Main()
+        Dim a As Object() = {"65", 144, 1567643245564664456&, 1.231, 5.456, 4.5, 4, 5.64, 56.4, 5.6, 45.6, 4.56, 4, 56}
+
+        Pause()
+    End Sub
+End Module
