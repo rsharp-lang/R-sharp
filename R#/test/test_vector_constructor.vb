9a109f5d05f6c9fb5f7319553373da5a27dacbf2
xieguigang <xie.guigang@gcmodeller.org>
2023-05-30 10:08:34 +0800

    add test for the vector constuctor

--- R#/test/test_vector_constructor.vb
+++ R#/test/test_vector_constructor.vb
@@ -1,8 +1,11 @@
-﻿Module test_vector_constructor
+﻿Imports SMRUCC.Rsharp.Runtime.Internal.[Object]
+Imports SMRUCC.Rsharp.Runtime.Interop
+
+Module test_vector_constructor
 
     Sub Main()
         Dim a As Object() = {"65", 144, 1567643245564664456&, 1.231, 5.456, 4.5, 4, 5.64, 56.4, 5.6, 45.6, 4.56, 4, 56}
-
+        Dim v As New vector(a, RType.GetRSharpType(GetType(Double)))
         Pause()
     End Sub
 End Module
