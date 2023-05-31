a5336ea563a825a420bc4cc3762781e7ea4a112b
xieguigang <xie.guigang@gcmodeller.org>
2023-05-31 09:04:19 +0800

    test success

--- test/unitTest/pinyin_api_test.R
+++ test/unitTest/pinyin_api_test.R
@@ -1,6 +1,6 @@
 imports "clr" from "devkit";
 
 const lib = clr::open("Microsoft.VisualBasic.Text.GB2312");
-const pinyin = clr::call_clr(lib, "TranscriptPinYin", args = list(str = "哈喽，大家好呀", sep = " | "));
+const pinyin = clr::call_clr(lib, "TranscriptPinYin", str = "哈喽，大家好呀", sep = " | ");
 
 print(pinyin);
