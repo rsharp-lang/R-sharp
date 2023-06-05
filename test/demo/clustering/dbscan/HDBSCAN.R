0ab4b29d8a1d1263e14e6b3456866e1b2369deda
guigang xie <xie.guigang@gcmodeller.org>
2023-06-05 16:59:54 +0800

    configs for run test of hdbscan

--- test/demo/clustering/dbscan/HDBSCAN.R
+++ test/demo/clustering/dbscan/HDBSCAN.R
@@ -3,7 +3,8 @@ imports "clustering" from "MLkit";
 require(charts);
 
 const filepath as string = ?"--data" || stop("a data table is required for run clustering!");
-const eps as double = ?"--eps" || 0.15;
+const min_points as integer = ?"--min_points" || 6;
+const min_clusters as integer = ?"--min_clusters" || 5;
 
 let raw = read.csv(filepath, row.names = NULL, check.names = FALSE)[, ["x","y"]];
 let export_dir = dirname(filepath);
@@ -11,14 +12,15 @@ let export_dir = dirname(filepath);
 print("previews of the raw input data:");
 print(head(raw));
 
-let result = as.object(dbscan(raw, eps))$cluster;
+let result = hdbscan(raw, min_points = min_points, min_clusters = min_clusters);
 
-str(summary(result));
+str((result));
 
-let table = as.data.frame(result);
+let table = as.data.frame(raw);
+table[, "Cluster"] = sapply(rownames(table), r -> result[[r]]);
 
 table |> head |> print;
-table |> write.csv(file = `${export_dir}/${basename(filepath)}_dbscan.csv`, row_names = FALSE);
+table |> write.csv(file = `${export_dir}/${basename(filepath)}_hdbscan.csv`, row_names = FALSE);
 
 let cluster = table[, "Cluster"];
 let unique_clusters = unique(cluster);
@@ -33,7 +35,7 @@ for(id in unique_clusters) {
 	points = c(points, serial(x,y, name = id, color = colorSet(), ptSize = 30, alpha = 230));
 }
 
-bitmap(file = `${export_dir}/${basename(filepath)}_dbscan.png`) {
+bitmap(file = `${export_dir}/${basename(filepath)}_hdbscan.png`) {
 	plot(points, 
 		size = [1920,1440], 
 		padding = "padding: 125px 200px 200px 200px;", 
