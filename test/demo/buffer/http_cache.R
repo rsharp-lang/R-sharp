b7d96591d0f31e5486214143b289a6bd7d231096
guigang xie <xie.guigang@gcmodeller.org>
2023-06-15 14:04:49 +0800

    make improvements for the web query cache

--- test/demo/buffer/http_cache.R
+++ test/demo/buffer/http_cache.R
@@ -1,5 +1,6 @@
 imports "http" from "webKit";
 
 require(HDS);
+setwd(@dir);
 
-let local = http::http.cache(fs = )
+let local = http::http.cache(fs = HDS::openStream(file = "./cache.dat", meta_size = 16*1024*1024));
