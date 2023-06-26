cc6f41a68c7240af0ba6ecf9ee6126ac9e61e356
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-06-26 19:39:20 +0800

    the local function is test success???

--- test/unitTest/function_test/local_func.R
+++ test/unitTest/function_test/local_func.R
@@ -9,6 +9,31 @@ const run = function() {
     print(basename(!script));
 }
 
-run();
+const load = function() {
+    return(function(aaa) {
+        print(aaa);
+        print("ok");
+    });
+}
+
+const run2 = function() {
+    print(basename(!script));
+
+    let basename = load();
+
+    print(basename(!script));
+}
+
+const run3 = function() {
+    str(description('REnv'));
+
+    let description = function(x) {
+        x;
+    }
+
+    str(description('REnv'));
+}
+
+run3();
 
 
