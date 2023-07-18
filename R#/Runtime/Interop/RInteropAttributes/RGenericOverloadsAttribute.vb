f65e36f6d9c60691b5e56153d27e27ba8c189619
guigang xie <xie.guigang@gcmodeller.org>
2023-07-18 13:11:53 +0800

    handling of the overloads by attribute?

---
+++ R#/Runtime/Interop/RInteropAttributes/RGenericOverloadsAttribute.vb
@@ -0,0 +1,22 @@
+﻿Namespace Runtime.Interop
+
+    <AttributeUsage(AttributeTargets.Method, AllowMultiple:=True, Inherited:=True)>
+    Public Class RGenericOverloadsAttribute : Inherits RInteropAttribute
+
+        Public ReadOnly Property FunctionName As String
+
+        ''' <summary>
+        ''' 
+        ''' </summary>
+        ''' <param name="func">
+        ''' The name of the target function for overloads
+        ''' </param>
+        Sub New(func As String)
+            FunctionName = func
+        End Sub
+
+        Public Overrides Function ToString() As String
+            Return $"{FunctionName}(...)"
+        End Function
+    End Class
+End Namespace
