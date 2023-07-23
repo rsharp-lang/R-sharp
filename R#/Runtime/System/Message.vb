7bad4438b9b0510f70726ddfde8a09bae17acf7e
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-07-23 15:07:26 +0800

    make improvements for handling http error

--- R#/Runtime/System/Message.vb
+++ R#/Runtime/System/Message.vb
@@ -1,61 +1,63 @@
 ﻿#Region "Microsoft.VisualBasic::4044955e4980ef17ba66a09d6970c75a, D:/GCModeller/src/R-sharp/R#//Runtime/System/Message.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 142
-    '    Code Lines: 85
-    ' Comment Lines: 38
-    '   Blank Lines: 19
-    '     File Size: 5.55 KB
+' Summaries:
 
 
-    '     Class Message
-    ' 
-    '         Properties: environmentStack, level, message, source, trace
-    ' 
-    '         Function: [Error], GenericEnumerator, GetEnumerator, InCompatibleType, SymbolNotFound
-    '                   SyntaxNotImplemented, ToException, ToString
-    ' 
-    '         Sub: ThrowCLRError
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 142
+'    Code Lines: 85
+' Comment Lines: 38
+'   Blank Lines: 19
+'     File Size: 5.55 KB
+
+
+'     Class Message
+' 
+'         Properties: environmentStack, level, message, source, trace
+' 
+'         Function: [Error], GenericEnumerator, GetEnumerator, InCompatibleType, SymbolNotFound
+'                   SyntaxNotImplemented, ToException, ToString
+' 
+'         Sub: ThrowCLRError
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
+Imports System.IO
 Imports System.Runtime.CompilerServices
+Imports System.Text
 Imports System.Xml.Serialization
 Imports Microsoft.VisualBasic.ApplicationServices.Debugging.Diagnostics
 Imports Microsoft.VisualBasic.ApplicationServices.Debugging.Logging
@@ -180,6 +182,13 @@ Namespace Runtime.Components
 
                          Return Internal.debug.stop(msg, envir, suppress)
                      End Function)
+        End Function
+
+        Public Function ToErrorText() As String
+            Dim sb As New StringBuilder
+            Dim str As TextWriter = New StringWriter(sb)
+            Call Internal.debug.writeErrMessage(Me, str)
+            Return sb.ToString
         End Function
 
         Public Overrides Function ToString() As String
