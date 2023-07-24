a49e2771ff983a4712f9a93bb00ba0265d82ea65
谢桂纲 <gg.xie@bionovogene.com>
2023-07-24 11:46:18 +0800

    empty header text?

--- studio/Rsharp_kit/webKit/URL.vb
+++ studio/Rsharp_kit/webKit/URL.vb
@@ -1,53 +1,53 @@
 ﻿#Region "Microsoft.VisualBasic::1e5f702df59c769e7c5c9bab0fc3589d, G:/GCModeller/src/R-sharp/studio/Rsharp_kit/webKit//URL.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 454
-    '    Code Lines: 317
-    ' Comment Lines: 90
-    '   Blank Lines: 47
-    '     File Size: 18.68 KB
+' Summaries:
 
 
-    ' Module URL
-    ' 
-    '     Function: [get], content, encodeTokenPart, getPostPayload, httpCache
-    '               HttpClientPost, HttpCookies, is_http_error, post, runHttpRequest
-    '               upload, urlcomponent, urlencode
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 454
+'    Code Lines: 317
+' Comment Lines: 90
+'   Blank Lines: 47
+'     File Size: 18.68 KB
+
+
+' Module URL
+' 
+'     Function: [get], content, encodeTokenPart, getPostPayload, httpCache
+'               HttpClientPost, HttpCookies, is_http_error, post, runHttpRequest
+'               upload, urlcomponent, urlencode
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -259,7 +259,13 @@ Public Module URL
             End If
 
             Dim code As String = ex.Message.Match("\d+")
-            Dim code_i32 As Long = Long.Parse(code)
+            Dim code_i32 As Long
+
+            If code.StringEmpty Then
+                code_i32 = 500
+            Else
+                code_i32 = Long.Parse(code)
+            End If
 
             If code_i32 > Integer.MaxValue OrElse code_i32 < Integer.MinValue Then
                 code_i32 = Integer.MaxValue
