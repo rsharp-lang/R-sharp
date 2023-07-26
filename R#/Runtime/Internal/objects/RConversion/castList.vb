c99abdb9e541308a604cf6a2a1236337ff83505b
xieguigang <xie.guigang@gcmodeller.org>
2023-07-26 14:45:33 +0800

    deal with the possible null object reference

--- R#/Runtime/Internal/objects/RConversion/castList.vb
+++ R#/Runtime/Internal/objects/RConversion/castList.vb
@@ -1,53 +1,53 @@
 ﻿#Region "Microsoft.VisualBasic::a84d60ec1f9694180f6faca62504f747, G:/GCModeller/src/R-sharp/R#//Runtime/Internal/objects/RConversion/castList.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 280
-    '    Code Lines: 220
-    ' Comment Lines: 14
-    '   Blank Lines: 46
-    '     File Size: 11.29 KB
+' Summaries:
 
 
-    '     Module castList
-    ' 
-    '         Function: CTypeList, dataframe_castList, dictionaryToRList, isEnumType, listElementNames
-    '                   listInternal, objCastList, vector_castList
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 280
+'    Code Lines: 220
+' Comment Lines: 14
+'   Blank Lines: 46
+'     File Size: 11.29 KB
+
+
+'     Module castList
+' 
+'         Function: CTypeList, dataframe_castList, dictionaryToRList, isEnumType, listElementNames
+'                   listInternal, objCastList, vector_castList
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -137,7 +137,13 @@ Namespace Runtime.Internal.Object.Converts
         End Function
 
         Friend Function listInternal(obj As Object, args As list, env As Environment) As Object
-            Dim type As Type = obj.GetType
+            Dim type As Type
+
+            If obj Is Nothing Then
+                Return Nothing
+            Else
+                type = obj.GetType
+            End If
 
             If TypeOf obj Is vector Then
                 Return vector_castList(DirectCast(obj, vector).data, args, env)
