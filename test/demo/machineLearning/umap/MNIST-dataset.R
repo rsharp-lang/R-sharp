d104bf67b78faee5e747e60378acf8a85ecbb351
guigang xie <xie.guigang@gcmodeller.org>
2023-08-01 15:41:35 +0800

    MNIST dataset test success

--- test/demo/machineLearning/umap/MNIST-dataset.R
+++ test/demo/machineLearning/umap/MNIST-dataset.R
@@ -1,6 +1,6 @@
 imports "dataset" from "MLkit";
 
-let raw = read.MNIST.labelledvector(`${@dir}/MNIST-LabelledVectorArray-60000x100.msgpack`, takes = 10);
+let raw = read.MNIST.labelledvector(`${@dir}/MNIST-LabelledVectorArray-60000x100.msgpack`);
 
 raw[, "num"] = as.numeric(rownames(raw));
 
