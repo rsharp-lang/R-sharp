a1b0f717b183e53d4cf44a42cf8bdb7e90010104
guigang xie <xie.guigang@gcmodeller.org>
2023-08-02 11:37:59 +0800

    fix of the data type cast error

--- studio/Rsharp_kit/MLkit/GA/GeneralChromosome.vb
+++ studio/Rsharp_kit/MLkit/GA/GeneralChromosome.vb
@@ -1,68 +1,63 @@
 ﻿#Region "Microsoft.VisualBasic::a91cc7c722a375782a5ad13217a78e28, D:/GCModeller/src/R-sharp/studio/Rsharp_kit/MLkit//GA/GeneralChromosome.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 50
-    '    Code Lines: 41
-    ' Comment Lines: 0
-    '   Blank Lines: 9
-    '     File Size: 2.11 KB
+' Summaries:
 
 
-    ' Class GeneralChromosome
-    ' 
-    '     Properties: MutationRate, UniqueHashKey
-    ' 
-    '     Function: Crossover, Mutate, ToString
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 50
+'    Code Lines: 41
+' Comment Lines: 0
+'   Blank Lines: 9
+'     File Size: 2.11 KB
+
+
+' Class GeneralChromosome
+' 
+'     Properties: MutationRate, UniqueHashKey
+' 
+'     Function: Crossover, Mutate, ToString
+' 
+' /********************************************************************************/
 
 #End Region
 
-Imports Microsoft.VisualBasic.CommandLine.Reflection
-Imports Microsoft.VisualBasic.MachineLearning.Darwinism.GAF
-Imports Microsoft.VisualBasic.MachineLearning.Darwinism.GAF.Helper
 Imports Microsoft.VisualBasic.MachineLearning.Darwinism.Models
-Imports Microsoft.VisualBasic.Scripting.MetaData
-Imports SMRUCC.Rsharp.Runtime.Components.Interface
 
 Public Class GeneralChromosome : Implements Chromosome(Of GeneralChromosome)
 
     Public Property MutationRate As Double Implements Chromosome(Of GeneralChromosome).MutationRate
 
-    Public ReadOnly Property UniqueHashKey As String Implements Chromosome(Of GeneralChromosome).UniqueHashKey
+    Public ReadOnly Property UniqueHashKey As String Implements Chromosome(Of GeneralChromosome).Identity
 
     Friend crossoverGeneric As Func(Of Object, Object, Object)
     Friend mutateGeneric As Func(Of Object, Double, Object)
