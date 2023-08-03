c769577a40632bfcb38fa4892908a2147081bd98
guigang xie <xie.guigang@gcmodeller.org>
2023-08-03 13:38:21 +0800

    count nlp tokens

--- studio/Rsharp_kit/MLkit/NLP.vb
+++ studio/Rsharp_kit/MLkit/NLP.vb
@@ -52,9 +52,14 @@
 Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Data.NLP
 Imports Microsoft.VisualBasic.Data.NLP.Model
+Imports Microsoft.VisualBasic.DataStorage.netCDF.Components
+Imports Microsoft.VisualBasic.Linq
+Imports Microsoft.VisualBasic.Math.Logical.FuzzyLogic.Models
 Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports Microsoft.VisualBasic.Serialization.JSON
 Imports SMRUCC.Rsharp
 Imports SMRUCC.Rsharp.Runtime
+Imports SMRUCC.Rsharp.Runtime.Internal.[Object]
 Imports SMRUCC.Rsharp.Runtime.Interop
 
 ''' <summary>
@@ -70,8 +75,88 @@ Module NLP
     ''' <returns></returns>
     <ExportAPI("segmentation")>
     <RApiReturn(GetType(Paragraph))>
-    Public Function Tokenice(<RRawVectorArgument> text As Object, Optional env As Environment = Nothing) As Object
-        Return env.EvaluateFramework(Of String, Paragraph())(text, eval:=Function(si) Paragraph.Segmentation(si).ToArray)
+    Public Function Tokenice(<RRawVectorArgument> text As Object, Optional delimiter As String = ".?!", Optional env As Environment = Nothing) As Object
+        Return env.EvaluateFramework(Of String, Paragraph())(text, eval:=Function(si) Paragraph.Segmentation(si, delimiter).ToArray)
+    End Function
+
+    ''' <summary>
+    ''' count tokens distribution
+    ''' </summary>
+    ''' <param name="x"></param>
+    ''' <param name="env"></param>
+    ''' <returns></returns>
+    <ExportAPI("count")>
+    Public Function counts(<RRawVectorArgument> x As Object, Optional env As Environment = Nothing) As Object
+        Dim df As New dataframe With {.columns = New Dictionary(Of String, Array)}
+
+        ' token
+        ' total
+        ' paragraph
+        ' sentences
+        ' source
+        If x Is Nothing Then
+            Return Nothing
+        ElseIf TypeOf x Is Paragraph Then
+            Dim tokens As Counter() = Counter.Count(x).ToArray
+
+            df.add("token", tokens.Select(Function(a) a.token))
+            df.add("total", tokens.Select(Function(a) a.total))
+            df.add("paragraph", {1})
+            df.add("sentences", tokens.Select(Function(a) a.sentences))
+        ElseIf TypeOf x Is Paragraph() Then
+            df = countMultipleParagraph(DirectCast(x, Paragraph()), df)
+        ElseIf TypeOf x Is list Then
+            Dim list = DirectCast(x, list).AsGeneric(Of Paragraph)(env)
+            Dim tokens As New List(Of (src As String, tokens As Counter()))
+
+            For Each par In list
+                tokens.Add((par.Key, Counter.Count(par.Value).ToArray))
+            Next
+
+            Dim tokenSet = tokens _
+                .Select(Function(a) a.tokens.Select(Function(i) (i, a.src))) _
+                .IteratesALL _
+                .GroupBy(Function(t) t.i.token.ToLower) _
+                .Select(Function(ts)
+                            Dim union As New Counter With {
+                                .paragraph = ts.Sum(Function(t) t.i.paragraph),
+                                .sentences = ts.Sum(Function(t) t.i.sentences),
+                                .token = ts.Key,
+                                .total = ts.Sum(Function(t) t.i.total)
+                            }
+                            Dim sources As String() = ts.Select(Function(i) i.src).Distinct.ToArray
+
+                            Return (union, sources)
+                        End Function) _
+                .ToArray
+
+            df.add("token", tokenSet.Select(Function(a) a.union.token))
+            df.add("total", tokenSet.Select(Function(a) a.union.total))
+            df.add("paragraph", tokenSet.Select(Function(a) a.union.paragraph))
+            df.add("sentences", tokenSet.Select(Function(a) a.union.sentences))
+            df.add("source", tokenSet.Select(Function(a) a.sources.Where(Function(si) Not si.StartsWith("NULL")).JoinBy("; ")))
+        Else
+            Dim list = pipeline.TryCreatePipeline(Of Paragraph)(x, env)
+
+            If list.isError Then
+                Return list.getError
+            Else
+                df = countMultipleParagraph(list.populates(Of Paragraph)(env), df)
+            End If
+        End If
+
+        Return df
+    End Function
+
+    Private Function countMultipleParagraph(x As IEnumerable(Of Paragraph), df As dataframe) As dataframe
+        Dim tokens As Counter() = Counter.Count(x).ToArray
+
+        df.add("token", tokens.Select(Function(a) a.token))
+        df.add("total", tokens.Select(Function(a) a.total))
+        df.add("paragraph", tokens.Select(Function(a) a.paragraph))
+        df.add("sentences", tokens.Select(Function(a) a.sentences))
+
+        Return df
     End Function
 
     <ExportAPI("article")>
