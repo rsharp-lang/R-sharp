1b6dd6a0f9fb285ae0558476db31e9881c6a6806
guigang xie <xie.guigang@gcmodeller.org>
2023-08-08 15:45:36 +0800

    add script for test dataset

---
+++ test/demo/machineLearning/umap/MNIST-raw.R
@@ -0,0 +1,15 @@
+# script demo for read mnist dataset in raw
+
+imports "dataset" from "MLkit";
+
+setwd(@dir);
+
+let raw = read.MNIST.labelledvector("mnist_dataset/train-images-idx3-ubyte", 
+format = "mnist", 
+dataset = "vector", 
+labelfile = "mnist_dataset/train-labels-idx1-ubyte",
+subset = 10
+);
+
+str(raw);
+
