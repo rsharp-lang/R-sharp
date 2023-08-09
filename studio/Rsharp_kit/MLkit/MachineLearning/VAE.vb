71797fc8860e32e6b0ac4c77634a712007a365ce
guigang xie <xie.guigang@gcmodeller.org>
2023-08-09 11:55:32 +0800

    train VAE by its self

---
+++ studio/Rsharp_kit/MLkit/MachineLearning/VAE.vb
@@ -0,0 +1,34 @@
+﻿Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.MachineLearning.ComponentModel.StoreProcedure
+Imports Microsoft.VisualBasic.MachineLearning.VariationalAutoencoder
+Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports Microsoft.VisualBasic.Scripting.Runtime
+Imports SMRUCC.Rsharp.Runtime
+Imports SMRUCC.Rsharp.Runtime.Interop
+
+<Package("VAE")>
+Module VAE
+
+    ''' <summary>
+    ''' create vae training algorithm
+    ''' </summary>
+    ''' <returns></returns>
+    <ExportAPI("vae")>
+    <RApiReturn(GetType(Trainer))>
+    Public Function Trainer(<RRawVectorArgument> dims As Object, Optional env As Environment = Nothing) As Object
+        Dim imgDims = InteropArgumentHelper.getSize(dims, env, [default]:=Nothing)
+
+        If imgDims.StringEmpty Then
+            Return Internal.debug.stop("the required image dimension size must be specificed!", env)
+        End If
+
+        With imgDims.SizeParser
+            Return New Trainer(.Width, .Height)
+        End With
+    End Function
+
+    <ExportAPI("train")>
+    Public Function train(vae As Trainer, ds As DataSet) As Object
+
+    End Function
+End Module
