c86e5fce90061235d8cbacad88027cf8963ea21b
guigang xie <xie.guigang@gcmodeller.org>
2023-08-09 17:35:21 +0800

    apply the code reference to the new VAE engine

--- studio/Rsharp_kit/MLkit/MachineLearning/VAE.vb
+++ studio/Rsharp_kit/MLkit/MachineLearning/VAE.vb
@@ -8,6 +8,7 @@ Imports SMRUCC.Rsharp.Runtime
 Imports SMRUCC.Rsharp.Runtime.Internal.[Object]
 Imports SMRUCC.Rsharp.Runtime.Interop
 Imports SMRUCC.Rsharp.Runtime.Vectorization
+Imports stdvec = Microsoft.VisualBasic.Math.LinearAlgebra.Vector
 
 <Package("VAE")>
 Module VAE
@@ -18,7 +19,11 @@ Module VAE
     ''' <returns></returns>
     <ExportAPI("vae")>
     <RApiReturn(GetType(Trainer))>
-    Public Function Trainer(<RRawVectorArgument> dims As Object, Optional env As Environment = Nothing) As Object
+    Public Function Trainer(<RRawVectorArgument>
+                            dims As Object,
+                            Optional latent_dims As Integer = 100,
+                            Optional env As Environment = Nothing) As Object
+
         Dim imgDims = InteropArgumentHelper.getSize(dims, env, [default]:=Nothing)
 
         If imgDims.StringEmpty Then
@@ -26,7 +31,7 @@ Module VAE
         End If
 
         With imgDims.SizeParser
-            Return New Trainer(.Width, .Height)
+            Return New Trainer(.Width, .Height, latent_dims)
         End With
     End Function
 
@@ -35,27 +40,27 @@ Module VAE
     Public Function train(vae As Trainer,
                           <RRawVectorArgument>
                           ds As Object,
-                          Optional steps As Integer = Integer.MaxValue,
+                          Optional steps As Integer = 100,
                           Optional env As Environment = Nothing) As Object
 
-        Dim dataset_input As Double()()
+        Dim dataset_input As stdvec()
 
         If TypeOf ds Is list Then
             dataset_input = DirectCast(ds, list).slots _
                 .Values _
-                .Select(Function(o) CLRVector.asNumeric(o)) _
+                .Select(Function(o) New stdvec(CLRVector.asNumeric(o))) _
                 .ToArray
         ElseIf TypeOf ds Is DataSet Then
             dataset_input = DirectCast(ds, DataSet).DataSamples _
                 .AsEnumerable _
-                .Select(Function(si) si.vector) _
+                .Select(Function(si) New stdvec(si.vector)) _
                 .ToArray
         Else
             Throw New NotImplementedException
         End If
 
-        Call vae.train_vae(steps, dataset_input)
+        Call vae.train(dataset_input, steps)
 
-        Return vae.Vae
+        Return vae.autoencoder
     End Function
 End Module
