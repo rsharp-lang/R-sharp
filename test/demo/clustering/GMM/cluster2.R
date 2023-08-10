d2b4750fa4b5975e17a616f3da1f96b5bebd0e33
guigang xie <xie.guigang@gcmodeller.org>
2023-08-10 17:00:14 +0800

    test clustering of 2d data

--- test/demo/clustering/GMM/cluster2.R
+++ test/demo/clustering/GMM/cluster2.R
@@ -4,10 +4,12 @@ setwd(@dir);
 
 let x = read.csv("../feature_regions.csv", row.names = 1, check.names = FALSE);
 
-x$Cluster = NULL;
+x[,"Cluster"] = NULL;
 
 print(x);
 
 let gmm = gmm(x, 5);
 
 print(gmm.predict_proba(gmm));
+
+write.csv(gmm.predict_proba(gmm), file = "./cluster2.csv", row.names = TRUE);
