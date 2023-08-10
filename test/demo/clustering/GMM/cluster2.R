924401ff2f25b34b8139bbda19d67cb3b86ef516
guigang xie <xie.guigang@gcmodeller.org>
2023-08-10 16:54:43 +0800

    config for test 2d clustering via gmm

---
+++ test/demo/clustering/GMM/cluster2.R
@@ -0,0 +1,13 @@
+imports "clustering" from "MLkit";
+
+setwd(@dir);
+
+let x = read.csv("../feature_regions.csv", row.names = 1, check.names = FALSE);
+
+x$Cluster = NULL;
+
+print(x);
+
+let gmm = gmm(x, 5);
+
+print(gmm.predict_proba(gmm));
