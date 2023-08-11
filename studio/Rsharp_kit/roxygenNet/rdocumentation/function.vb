d352c6a511d7b6078517c780489ccc616bfdde9a
guigang xie <xie.guigang@gcmodeller.org>
2023-08-11 17:43:26 +0800

    Update function.vb

--- studio/Rsharp_kit/roxygenNet/rdocumentation/function.vb
+++ studio/Rsharp_kit/roxygenNet/rdocumentation/function.vb
@@ -227,7 +227,7 @@ Public Class [function]
             !version = assembly.AssemblyVersion
             !copyright = assembly.AssemblyCopyright
             !show_details = If(docs.details.StringEmpty, "none", "block")
-            !examples = docs.examples
+            !examples = Strings.Trim(docs.examples).LineTokens.JoinBy("<br />")
 
             If docs.examples.StringEmpty Then
                 !show_examples = "none"
