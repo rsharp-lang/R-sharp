2e17de7050de8ce8a5f546e2aeac29a2bf432db2
guigang xie <xie.guigang@gcmodeller.org>
2023-08-16 15:47:08 +0800

    save the winform ui configs

--- studio/Rsharp_kit/MLkit/MachineLearning/VAE.vb
+++ studio/Rsharp_kit/MLkit/MachineLearning/VAE.vb
@@ -18,49 +18,48 @@ Module VAE
     ''' </summary>
     ''' <returns></returns>
     <ExportAPI("vae")>
-    <RApiReturn(GetType(Trainer))>
     Public Function Trainer(<RRawVectorArgument>
                             dims As Object,
                             Optional latent_dims As Integer = 100,
                             Optional env As Environment = Nothing) As Object
 
-        Dim imgDims = InteropArgumentHelper.getSize(dims, env, [default]:=Nothing)
+        'Dim imgDims = InteropArgumentHelper.getSize(dims, env, [default]:=Nothing)
 
-        If imgDims.StringEmpty Then
-            Return Internal.debug.stop("the required image dimension size must be specificed!", env)
-        End If
+        'If imgDims.StringEmpty Then
+        '    Return Internal.debug.stop("the required image dimension size must be specificed!", env)
+        'End If
 
-        With imgDims.SizeParser
-            Return New Trainer(.Width, .Height, latent_dims)
-        End With
+        'With imgDims.SizeParser
+        '    Return New Trainer(.Width, .Height, latent_dims)
+        'End With
     End Function
 
-    <ExportAPI("train")>
-    <RApiReturn(GetType(VAE))>
-    Public Function train(vae As Trainer,
-                          <RRawVectorArgument>
-                          ds As Object,
-                          Optional steps As Integer = 100,
-                          Optional env As Environment = Nothing) As Object
+    '<ExportAPI("train")>
+    '<RApiReturn(GetType(VAE))>
+    'Public Function train(vae As Trainer,
+    '                      <RRawVectorArgument>
+    '                      ds As Object,
+    '                      Optional steps As Integer = 100,
+    '                      Optional env As Environment = Nothing) As Object
 
-        Dim dataset_input As stdvec()
+    '    Dim dataset_input As stdvec()
 
-        If TypeOf ds Is list Then
-            dataset_input = DirectCast(ds, list).slots _
-                .Values _
-                .Select(Function(o) New stdvec(CLRVector.asNumeric(o))) _
-                .ToArray
-        ElseIf TypeOf ds Is DataSet Then
-            dataset_input = DirectCast(ds, DataSet).DataSamples _
-                .AsEnumerable _
-                .Select(Function(si) New stdvec(si.vector)) _
-                .ToArray
-        Else
-            Throw New NotImplementedException
-        End If
+    '    If TypeOf ds Is list Then
+    '        dataset_input = DirectCast(ds, list).slots _
+    '            .Values _
+    '            .Select(Function(o) New stdvec(CLRVector.asNumeric(o))) _
+    '            .ToArray
+    '    ElseIf TypeOf ds Is DataSet Then
+    '        dataset_input = DirectCast(ds, DataSet).DataSamples _
+    '            .AsEnumerable _
+    '            .Select(Function(si) New stdvec(si.vector)) _
+    '            .ToArray
+    '    Else
+    '        Throw New NotImplementedException
+    '    End If
 
-        Call vae.train(dataset_input, steps)
+    '    Call vae.train(dataset_input, steps)
 
-        Return vae.autoencoder
-    End Function
+    '    Return vae.autoencoder
+    'End Function
 End Module
