8e771245f753d94c1681b11594f0fe27750bfa5a
guigang xie <xie.guigang@gcmodeller.org>
2023-08-22 08:40:42 +0800

    make bugs fixed for get value from rds file

--- studio/RData/Convertor/ConvertToR.vb
+++ studio/RData/Convertor/ConvertToR.vb
@@ -1,53 +1,53 @@
 ﻿#Region "Microsoft.VisualBasic::c5533c3a7297fd3afacb75462680aead, D:/GCModeller/src/R-sharp/studio/RData//Convertor/ConvertToR.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 278
-    '    Code Lines: 200
-    ' Comment Lines: 34
-    '   Blank Lines: 44
-    '     File Size: 10.36 KB
+' Summaries:
 
 
-    '     Module ConvertToR
-    ' 
-    '         Function: CreateFactor, CreatePairList, CreateRTable, CreateRVector, PullRawData
-    '                   (+2 Overloads) PullRObject, readColumnNames, readRowNames, ToRObject
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 278
+'    Code Lines: 200
+' Comment Lines: 34
+'   Blank Lines: 44
+'     File Size: 10.36 KB
+
+
+'     Module ConvertToR
+' 
+'         Function: CreateFactor, CreatePairList, CreateRTable, CreateRVector, PullRawData
+'                   (+2 Overloads) PullRObject, readColumnNames, readRowNames, ToRObject
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -131,11 +131,17 @@ Namespace Convertor
             Dim pullAll As New list With {
                 .slots = New Dictionary(Of String, Object)
             }
-
             ' Pull all R# object from the RData linked list
-            Call rdata.PullRObject(pullAll.slots)
+            Dim pull = rdata.PullRObject(pullAll.slots)
 
-            Return pullAll
+            ' rda: get value from pullAll symbol
+            ' rds: get value from pull symbol
+
+            If pullAll.length = 0 Then
+                Return pull
+            Else
+                Return pullAll
+            End If
         End Function
 
         Public Function PullRObject(rdata As RObject) As Object
