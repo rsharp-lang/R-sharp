e6222f24456a68f285ecf08ce0de646182ca6710
xieguigang <xie.guigang@gcmodeller.org>
2023-08-25 10:21:02 +0800

    cast vector

---
+++ R#/Runtime/Interop/CType/ICTypeVector.vb
@@ -0,0 +1,12 @@
+﻿Namespace Runtime.Interop.CType
+
+    Public Interface ICTypeVector
+
+        Function ToNumeric() As Double()
+        Function ToFloat() As Single()
+        Function ToFactors() As String()
+        Function ToInteger() As Integer()
+        Function ToLong() As Long()
+
+    End Interface
+End Namespace
