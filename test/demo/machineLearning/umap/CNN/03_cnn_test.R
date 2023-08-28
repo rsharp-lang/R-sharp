e00f10530c206a908bb942b7d36386fcf191f2b6
guigang xie <xie.guigang@gcmodeller.org>
2023-08-28 15:26:42 +0800

    evaluate the multiple feature outputs

--- test/demo/machineLearning/umap/CNN/03_cnn_test.R
+++ test/demo/machineLearning/umap/CNN/03_cnn_test.R
@@ -12,7 +12,7 @@ cnn = cnn + input_layer([56, 56])
 + pool_layer([2, 2])
 + conv_layer(12, [5, 5])
 + pool_layer([2, 2])
-+ output_layer(class.num = [10,10,10,10])
++ output_layer(class.num = 10, w = 4)
 ;
 
 cnn = CNN::training(cnn, dataset = ds, max.loops = 6, batch.size = 50);
