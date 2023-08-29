d40f9fe718b7a48d8dea33b96244449e3a594009
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-08-29 23:26:57 +0800

    configs for run generative CNN test

--- test/demo/machineLearning/umap/CNN/03_cnn_test.R
+++ test/demo/machineLearning/umap/CNN/03_cnn_test.R
@@ -5,14 +5,18 @@ setwd(@dir);
 let ds = read.ML_model(file = "./images_x4.pack");
 ds = as.sampleSet(ds);
 
+print(CNN::n_threads());
+
 let cnn = cnn();
 
-cnn = cnn + input_layer([56, 56])
-+ conv_layer(6, [5, 5])
-+ pool_layer([2, 2])
-+ conv_layer(12, [5, 5])
-+ pool_layer([2, 2])
-+ output_layer(class.num = 10, w = 4)
+cnn = cnn + input_layer([28, 28])
++ conv_layer(5, 32, 1, 2)
++ pool_layer(2, 2, 0)
++ conv_layer(5, 64, 1, 2)
++ pool_layer(2, 2, 0)
++ full_connected_layer(4)
++ relu_layer()
 ;
 
-cnn = CNN::training(cnn, dataset = ds, max.loops = 6, batch.size = 50);
+CNN::n_threads(8);
+cnn = CNN::training(cnn, dataset = ds, max.loops = 6, trainer = CNN::ada_grad(batch.size = 50));
