70154c9e2f483215465d5392c57a10b4973bf6b1
guigang xie <xie.guigang@gcmodeller.org>
2023-08-29 15:04:35 +0800

    fix of the type cast mis-matched data types

--- test/demo/machineLearning/umap/CNN/cnn_mnist.R
+++ test/demo/machineLearning/umap/CNN/cnn_mnist.R
@@ -10,7 +10,7 @@ const raw = images_set
 format = "mnist", 
 dataset = "dataframe", 
 labelfile = "../mnist_dataset/train-labels-idx1-ubyte",
-subset = 30
+subset = 20
 );
 
 str(raw);
@@ -41,7 +41,7 @@ cnn = cnn + input_layer([28, 28])
 
 let ds = sample_dataset(dataset = raw, labels = as.numeric(labels));
 
-cnn = CNN::training(cnn, ds, max.loops = 6, batch.size = 10);
+cnn = CNN::training(cnn, ds, max.loops = 1, batch.size = 5);
 
 
 raw[, "label"] = NULL;
