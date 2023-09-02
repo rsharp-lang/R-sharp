203865e06745f35e76572b1d814e222a1b316e7c
guigang xie <xie.guigang@gcmodeller.org>
2023-09-02 18:25:09 +0800

    add attribute mask for the default function

---
+++ R#/Runtime/Interop/RInteropAttributes/RDefaultFunctionAttribute.vb
@@ -0,0 +1,7 @@
+﻿Namespace Runtime.Interop
+
+    <AttributeUsage(AttributeTargets.Method, AllowMultiple:=False, Inherited:=True)>
+    Public Class RDefaultFunctionAttribute : Inherits Attribute
+
+    End Class
+End Namespace
