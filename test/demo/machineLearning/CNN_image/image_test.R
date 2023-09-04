bea267e6795d2a4bec40d612a20588fd5e2e81c3
guigang xie <xie.guigang@gcmodeller.org>
2023-09-04 11:30:38 +0800

    add test script for raster image processing

---
+++ test/demo/machineLearning/CNN_image/image_test.R
@@ -0,0 +1,10 @@
+require(graphics);
+
+setwd(@dir);
+
+const img_src = "../../1537192287563.jpg";
+const raster = as.raster(img = readImage(img_src));
+const data = raster_convolution(raster);
+
+print(data);
+
