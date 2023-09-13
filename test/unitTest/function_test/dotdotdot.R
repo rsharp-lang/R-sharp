0fd0836b71e2cc8ce5f2ac1d2b860d2b240c3e0e
guigang xie <xie.guigang@gcmodeller.org>
2023-09-13 15:45:21 +0800

    ... dotdotdot parameter test success

--- test/unitTest/function_test/dotdotdot.R
+++ test/unitTest/function_test/dotdotdot.R
@@ -1,6 +1,7 @@
 let f1 <- function(x, ...) {
   f2(...);
   str(names(list(...)));
+  str(list(...));
 }
 
 let f2 <- function(y) {
@@ -8,4 +9,8 @@ let f2 <- function(y) {
 }
 
 f1(55, z = 1, y = 2);
-# [1] 2
+# [1]     2
+# chr [1:2] "z" "y"
+# List of 2
+#  $ z : int 1
+#  $ y : int 2
