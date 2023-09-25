4f0be34b0afb639d31b7b8542a4b8eb53754bb82
xieguigang <xie.guigang@gcmodeller.org>
2023-09-25 10:44:28 +0800

    add .NET Implement of Real-time Contrast Preserving Decolorization R# function exports

--- Library/graphics/ImageFilters.vb
+++ Library/graphics/ImageFilters.vb
@@ -1,52 +1,52 @@
 ﻿#Region "Microsoft.VisualBasic::9d517633ae64eab44811621c24deb2a3, D:/GCModeller/src/R-sharp/Library/graphics//ImageFilters.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 101
-    '    Code Lines: 85
-    ' Comment Lines: 0
-    '   Blank Lines: 16
-    '     File Size: 3.98 KB
+' Summaries:
 
 
-    ' Module ImageFilters
-    ' 
-    '     Function: Diffusion, Emboss, gaussBlurEffect, HqxScalesF, Pencil
-    '               Sharp, Soften, WoodCarving
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 101
+'    Code Lines: 85
+' Comment Lines: 0
+'   Blank Lines: 16
+'     File Size: 3.98 KB
+
+
+' Module ImageFilters
+' 
+'     Function: Diffusion, Emboss, gaussBlurEffect, HqxScalesF, Pencil
+'               Sharp, Soften, WoodCarving
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -149,5 +149,18 @@ Module ImageFilters
     <MethodImpl(MethodImplOptions.AggressiveInlining)>
     Public Function HqxScalesF(image As Image, Optional scale As HqxScales = HqxScales.Hqx_2x) As Object
         Return New RasterScaler(New Bitmap(image)).Scale(scale)
+    End Function
+
+    ''' <summary>
+    ''' .NET Implement of Real-time Contrast Preserving Decolorization
+    ''' </summary>
+    ''' <param name="img"></param>
+    ''' <returns></returns>
+    ''' <remarks>
+    ''' https://github.com/IntPtrZero/RTCPRGB2Gray/tree/master
+    ''' </remarks>
+    <ExportAPI("RTCP_gray")>
+    Public Function RTCPGray_func(img As Image) As Bitmap
+        Return RTCP.RTCPGray(New Bitmap(img))
     End Function
 End Module
