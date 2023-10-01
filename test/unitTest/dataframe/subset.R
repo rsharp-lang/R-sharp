c1c71e58ed7de100ee273964d8cc48d3c26466bf
xieguigang <xie.guigang@gcmodeller.org>
2023-10-01 11:38:19 +0800

    Update subset.R

--- test/unitTest/dataframe/subset.R
+++ test/unitTest/dataframe/subset.R
@@ -1,6 +1,10 @@
-a = data.frame(i = `#${1:3}`, A = ['a','c','z']);
+a = data.frame(i = `#${1:5}`, A = ['a','c','z','b',"a"], row.names = "a":"e");
 
 print(a);
+
+print(a[a$A == 'a', ]);
+print(a["b":"d", ]);
+
 
 print(a[c(1,2,3, 3,1, 5), ]);
 print(a[c(TRUE,TRUE,FALSE), ]);
