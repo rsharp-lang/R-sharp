d89c7ca0e2e48cfac67f8a8a909c18df38263e32
xieguigang <xie.guigang@gcmodeller.org>
2023-10-07 14:00:05 +0800

    try to handling of thge possible empty line

--- studio/Rsharp_kit/roxygenNet/RoxygenDocument.vb
+++ studio/Rsharp_kit/roxygenNet/RoxygenDocument.vb
@@ -1,51 +1,51 @@
 ﻿#Region "Microsoft.VisualBasic::887e596ec30ac5e4416a98bfe43b7f76, D:/GCModeller/src/R-sharp/studio/Rsharp_kit/roxygenNet//RoxygenDocument.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 137
-    '    Code Lines: 115
-    ' Comment Lines: 2
-    '   Blank Lines: 20
-    '     File Size: 5.32 KB
+' Summaries:
 
 
-    ' Class RoxygenDocument
-    ' 
-    '     Function: continuteLines, ParseDocument, ParseDocuments, SplitBlocks
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 137
+'    Code Lines: 115
+' Comment Lines: 2
+'   Blank Lines: 20
+'     File Size: 5.32 KB
+
+
+' Class RoxygenDocument
+' 
+'     Function: continuteLines, ParseDocument, ParseDocuments, SplitBlocks
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -102,7 +102,9 @@ Public Class RoxygenDocument
         Dim buffer As New List(Of String)
 
         For Each line As String In scriptText.LineIterators
-            If line.StartsWith("#'") Then
+            If Strings.Trim(line).StringEmpty Then
+                Continue For
+            ElseIf line.StartsWith("#'") Then
                 buffer.Add(line.Substring(2).Trim)
             ElseIf buffer > 0 Then
                 Dim name As String() = line.Trim _
