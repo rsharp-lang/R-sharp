7857fb31fd656587e072f2e4d4476e4c738bf240
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-10-08 13:44:12 +0800

    implements the syntax

--- R#/Interpreter/ExecuteEngine/ExpressionSymbols/DataSet/Literal/VectorLiteral.vb
+++ R#/Interpreter/ExecuteEngine/ExpressionSymbols/DataSet/Literal/VectorLiteral.vb
@@ -1,56 +1,56 @@
 ﻿#Region "Microsoft.VisualBasic::8b98dbe5da8aba62bd6a1f3eb275551c, D:/GCModeller/src/R-sharp/R#//Interpreter/ExecuteEngine/ExpressionSymbols/DataSet/Literal/VectorLiteral.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 92
-    '    Code Lines: 69
-    ' Comment Lines: 5
-    '   Blank Lines: 18
-    '     File Size: 3.13 KB
+' Summaries:
 
 
-    '     Class VectorLiteral
-    ' 
-    '         Properties: expressionName, length, type
-    ' 
-    '         Constructor: (+2 Overloads) Sub New
-    '         Function: Evaluate, FromArray, GetEnumerator, IEnumerable_GetEnumerator, ToArray
-    '                   ToString
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 92
+'    Code Lines: 69
+' Comment Lines: 5
+'   Blank Lines: 18
+'     File Size: 3.13 KB
+
+
+'     Class VectorLiteral
+' 
+'         Properties: expressionName, length, type
+' 
+'         Constructor: (+2 Overloads) Sub New
+'         Function: Evaluate, FromArray, GetEnumerator, IEnumerable_GetEnumerator, ToArray
+'                   ToString
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -65,8 +65,16 @@ Imports REnv = SMRUCC.Rsharp.Runtime
 Namespace Interpreter.ExecuteEngine.ExpressionSymbols.DataSets
 
     ''' <summary>
+    ''' 1. vector literal
+    ''' 
     ''' ```
     ''' [x,y,z,...]
+    ''' ```
+    ''' 
+    ''' 2. vector append
+    ''' 
+    ''' ```
+    ''' [x, ...y, ...z]
     ''' ```
     ''' </summary>
     Public Class VectorLiteral : Inherits Expression
@@ -99,27 +107,52 @@ Namespace Interpreter.ExecuteEngine.ExpressionSymbols.DataSets
         End Sub
 
         Public Overrides Function Evaluate(envir As Environment) As Object
-            Dim vector As Array = Array.CreateInstance(GetType(Object), values.Length)
+            Dim vector As New List(Of Object)
             Dim val As Object
             Dim expr As Expression
 
             For i As Integer = 0 To values.Length - 1
                 expr = values(i)
-                val = REnv.single(expr.Evaluate(envir))
 
-                If Program.isException(val) Then
-                    Return val
+                If TypeOf expr Is SymbolReference AndAlso DirectCast(expr, SymbolReference).symbol.StartsWith("...") Then
+                    Dim [try] = expr.Evaluate(envir)
+
+                    If TypeOf [try] Is Message Then
+                        ' is vector append syntax
+                        Dim srcSymbol As String = Mid(DirectCast(expr, SymbolReference).symbol, 4)
+
+                        [try] = SymbolReference.GetReferenceObject(srcSymbol, env:=envir)
+
+                        If TypeOf [try] Is Message Then
+                            Return [try]
+                        Else
+                            Dim data As Array = REnv.asVector(Of Object)([try])
+
+                            For index As Integer = 0 To data.Length - 1
+                                Call vector.Add(data(index))
+                            Next
+                        End If
+                    Else
+                        ' isnot, add to current vector
+                        vector.Add(REnv.single([try]))
+                    End If
                 Else
-                    vector(i) = val
+                    val = REnv.single(expr.Evaluate(envir))
+
+                    If Program.isException(val) Then
+                        Return val
+                    Else
+                        vector.Add(val)
+                    End If
                 End If
             Next
 
-            Dim type As Type = MeasureRealElementType(vector)
+            Dim type As Type = MeasureRealElementType(vector.ToArray)
 
             If Not type Is GetType(Void) AndAlso Not type Is GetType(Object) Then
                 Return New vector(type, vector, envir)
             Else
-                Return Environment.asRVector(Me.type, vector)
+                Return Environment.asRVector(Me.type, vector.ToArray)
             End If
         End Function
 
