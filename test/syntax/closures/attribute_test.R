fad8f2fd2d17bba0b67ae502b7a3b7332c180106
谢桂纲 <gg.xie@bionovogene.com>
2023-10-17 13:06:53 +0800

    get function attribute test success

--- test/syntax/closures/attribute_test.R
+++ test/syntax/closures/attribute_test.R
@@ -1,6 +1,10 @@
 [@check 'list(a = "integer")']
+[@check 111111]
 [@app "test_function"]
 [@name "attribute value text for name"]
 const f = function(x) {
     return(x);
 }
+
+
+str(attributes(f));
