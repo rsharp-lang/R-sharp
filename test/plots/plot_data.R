cd8ee64fe0888d983c95a9ca52b318c4beef34ff
guigang xie <xie.guigang@gcmodeller.org>
2023-10-18 09:21:11 +0800

    make bugs fixed of the legend shape flag parser

---
+++ test/plots/plot_data.R
@@ -0,0 +1,42 @@
+require(JSON);
+require(charts);
+
+setwd(@dir);
+
+let scatter = "./region_spots.json"
+|> readText()
+|> JSON::json_decode()
+;
+
+str(scatter);
+
+let x = [];
+let y = [];
+let class = [];
+let xy = [];
+
+for(tag in names(scatter)) {
+    xy = scatter[[tag]];
+    class = append(class, rep(tag, length(xy)));
+    xy = strsplit(xy, ",");
+    x = append(x, sapply(xy, i -> i[1]));
+    y = append(y, sapply(xy, i -> i[2]));
+
+    str(tag);
+}
+
+print(x);
+print(y);
+print(class);
+
+bitmap(file = "./Rplot.png", size = [2700, 3600]) {
+plot(as.numeric(x), y= as.numeric(y), class = class, point.size = 9, 
+shape = "rect",reverse = TRUE, colorSet = "paper", grid.fill = "white" ,
+xlim = 256, ylim = 373, absolute_scale = TRUE);
+}
+
+
+
+
+
+
