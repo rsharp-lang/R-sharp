0ec035fa2ad80f11cb27efe11b5b1eddd4ddd7b8
guigang xie <xie.guigang@gcmodeller.org>
2023-10-25 12:53:50 +0800

    add suffix for the number literal

--- R#/Language/TokenIcer/Scanner.vb
+++ R#/Language/TokenIcer/Scanner.vb
@@ -1,61 +1,61 @@
 ﻿#Region "Microsoft.VisualBasic::c042aa61ed79b8323726ca0ce41a0092, D:/GCModeller/src/R-sharp/R#//Language/TokenIcer/Scanner.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 562
-    '    Code Lines: 400
-    ' Comment Lines: 90
-    '   Blank Lines: 72
-    '     File Size: 23.67 KB
+' Summaries:
 
 
-    '     Class Scanner
-    ' 
-    '         Properties: lastCharIsEscapeSplash
-    ' 
-    '         Constructor: (+1 Overloads) Sub New
-    '         Function: finalizeToken, GetRKeywords, GetTokens, isLINQKeyword, MeasureToken
-    '                   populateToken, walkChar
-    '         Class Escapes
-    ' 
-    '             Function: ToString
-    ' 
-    ' 
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 562
+'    Code Lines: 400
+' Comment Lines: 90
+'   Blank Lines: 72
+'     File Size: 23.67 KB
+
+
+'     Class Scanner
+' 
+'         Properties: lastCharIsEscapeSplash
+' 
+'         Constructor: (+1 Overloads) Sub New
+'         Function: finalizeToken, GetRKeywords, GetTokens, isLINQKeyword, MeasureToken
+'                   populateToken, walkChar
+'         Class Escapes
+' 
+'             Function: ToString
+' 
+' 
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -605,6 +605,12 @@ Namespace Language.TokenIcer
                         Return New Token With {.name = TokenType.integerLiteral, .text = text}
                     ElseIf Double.TryParse(text, Nothing) Then
                         Return New Token With {.name = TokenType.numberLiteral, .text = text}
+                    ElseIf text.IsPattern("\d+L") Then
+                        ' integer literal
+                        Return New Token With {.name = TokenType.integerLiteral, .text = CInt(Val(text)).ToString}
+                    ElseIf text.IsPattern(NumericPattern & "F") Then
+                        ' float literal
+                        Return New Token With {.name = TokenType.numberLiteral, .text = Val(text).ToString}
                     ElseIf text.IsPattern(RSymbol) Then
                         Return New Token With {.name = TokenType.identifier, .text = text}
                     End If
