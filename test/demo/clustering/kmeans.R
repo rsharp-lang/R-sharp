5f9d4fbb0db7cb436bf4043adedfb3d5b0567516
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-11-20 11:29:28 +0800

    run test success

--- test/demo/clustering/kmeans.R
+++ test/demo/clustering/kmeans.R
@@ -1,5 +1,7 @@
 imports "clustering" from "MLkit";
 
+require(charting);
+
 setwd(@dir);
 
 let rawdata = read.csv("./feature_regions.csv", row.names = 1, check.names = FALSE);
@@ -15,3 +17,26 @@ let result2 = kmeans(rawdata, centers = 6, bisecting = TRUE);
 
 write.csv(result, file = "./kmeans.csv");
 write.csv(result2, file = "./bisecting_kmeans.csv");
+
+result = as.data.frame(result);
+result2 = as.data.frame(result2);
+
+print(result, max.print = 6);
+
+bitmap(file = "./kmeans1.png") {
+	plot(as.numeric(result[, "x"]), as.numeric(result[, "y"]), 
+		class     = result$Cluster, 
+		grid.fill = "white",
+		padding   = "padding: 125px 300px 200px 200px;",
+		colorSet  = "paper"
+	);
+}
+
+bitmap(file = "./kmeans2.png") {
+	plot(as.numeric(result2[, "x"]), as.numeric(result2[, "y"]), 
+		class     = result2$Cluster, 
+		grid.fill = "white",
+		padding   = "padding: 125px 300px 200px 200px;",
+		colorSet  = "paper"
+	);
+}
