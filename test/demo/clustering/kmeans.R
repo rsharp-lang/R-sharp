a12d4c15251e3ad51a9fa10fabe1306dc2125057
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-11-20 11:48:38 +0800

    run lloyds but not working?

--- test/demo/clustering/kmeans.R
+++ test/demo/clustering/kmeans.R
@@ -1,6 +1,6 @@
 imports "clustering" from "MLkit";
 
-require(charting);
+# require(charting);
 
 setwd(@dir);
 
@@ -12,16 +12,28 @@ rawdata[, "y"] = as.numeric(rawdata$y);
 
 print(rawdata, max.print = 13);
 
+let lloyds_result = lloyds(rawdata, k = 9);
 let result = kmeans(rawdata, centers = 9, bisecting = FALSE);
 let result2 = kmeans(rawdata, centers = 9, bisecting = TRUE);
 
 write.csv(result, file = "./kmeans.csv");
 write.csv(result2, file = "./bisecting_kmeans.csv");
+write.csv(lloyds_result, file = "./lloyds_result");
 
+lloyds_result = as.data.frame(lloyds_result);
 result = as.data.frame(result);
 result2 = as.data.frame(result2);
 
 print(result, max.print = 6);
+
+bitmap(file = "./lloyds_result.png") {
+	plot(as.numeric(lloyds_result[, "x"]), as.numeric(lloyds_result[, "y"]), 
+		class     = lloyds_result$Cluster, 
+		grid.fill = "white",
+		padding   = "padding: 125px 300px 200px 200px;",
+		colorSet  = "paper"
+	);
+}
 
 bitmap(file = "./kmeans1.png") {
 	plot(as.numeric(result[, "x"]), as.numeric(result[, "y"]), 
