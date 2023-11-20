6ccee3f41d0ac59eed956327be6184bd4cd0342d
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-11-20 11:32:30 +0800

    run kmeans test success

--- test/demo/clustering/kmeans.R
+++ test/demo/clustering/kmeans.R
@@ -12,8 +12,8 @@ rawdata[, "y"] = as.numeric(rawdata$y);
 
 print(rawdata, max.print = 13);
 
-let result = kmeans(rawdata, centers = 6, bisecting = FALSE);
-let result2 = kmeans(rawdata, centers = 6, bisecting = TRUE);
+let result = kmeans(rawdata, centers = 9, bisecting = FALSE);
+let result2 = kmeans(rawdata, centers = 9, bisecting = TRUE);
 
 write.csv(result, file = "./kmeans.csv");
 write.csv(result2, file = "./bisecting_kmeans.csv");
