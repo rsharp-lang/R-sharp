cd3d26df04e5571dfa87423daf866cb11be8ea22
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-11-20 11:12:18 +0800

    fix of the data type cast error

--- test/demo/clustering/kmeans.R
+++ test/demo/clustering/kmeans.R
@@ -4,5 +4,14 @@ setwd(@dir);
 
 let rawdata = read.csv("./feature_regions.csv", row.names = 1, check.names = FALSE);
 
+rawdata[, "Cluster"] = NULL;
+rawdata[, "x"] = as.numeric(rawdata$x);
+rawdata[, "y"] = as.numeric(rawdata$y);
+
 print(rawdata, max.print = 13);
 
+let result = kmeans(rawdata, centers = 6, bisecting = FALSE);
+let result2 = kmeans(rawdata, centers = 6, bisecting = TRUE);
+
+write.csv(result, file = "./kmeans.csv");
+write.csv(result2, file = "./bisecting_kmeans.csv");
