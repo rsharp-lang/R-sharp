ff46a35c6b6bcae9d67e4082bb7dc360b3395bdb
guigang xie <xie.guigang@gcmodeller.org>
2023-12-15 13:53:09 +0800

    try to fix of the document indent format bug of the code example

--- studio/Rsharp_kit/roxygenNet/rdocumentation/function.vb
+++ studio/Rsharp_kit/roxygenNet/rdocumentation/function.vb
@@ -114,7 +114,7 @@ Public Class [function]
                 .ToArray
             docs.returns = markdown.Transform(xml.Returns)
             docs.details = markdown.Transform(xml.Remarks)
-            docs.examples = xml.example
+            docs.examples = " " & xml.example
         End If
 
         If docs.returns.StringEmpty Then
@@ -227,7 +227,7 @@ Public Class [function]
             !copyright = assembly.AssemblyCopyright
             !show_details = If(docs.details.StringEmpty, "none", "block")
 
-            If docs.examples.StringEmpty Then
+            If Strings.Trim(docs.examples).StringEmpty Then
                 !show_examples = "none"
                 !examples = ""
             Else
