61c64eaa9c9d59b266dac26772f188c450147781
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-12-17 12:26:00 +0800

    cast the size data

---
+++ Library/shares/R_graphic.interop/cast.vb
@@ -0,0 +1,24 @@
+﻿Imports System.Drawing
+Imports System.Runtime.CompilerServices
+Imports SMRUCC.Rsharp.Runtime.Internal.Object
+
+Module cast
+
+    ''' <summary>
+    ''' cast the gdi size data to R# tuple list object
+    ''' </summary>
+    ''' <param name="size"></param>
+    ''' <returns>
+    ''' a tuple list obejct that contains two slot elements: w for width and h for height
+    ''' </returns>
+    <Extension>
+    Public Function size_toList(size As Size) As list
+        Return New list With {
+            .slots = New Dictionary(Of String, Object) From {
+                {"w", size.Width},
+                {"h", size.Height}
+            }
+        }
+    End Function
+
+End Module
