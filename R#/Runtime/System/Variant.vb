7e4165083f1fa49bb4aadb6c792f304957b1740f
guigang xie <xie.guigang@gcmodeller.org>
2023-12-19 09:08:55 +0800

    how to implements the variant type parameter?

---
+++ R#/Runtime/System/Variant.vb
@@ -0,0 +1,25 @@
+﻿Imports Microsoft.VisualBasic.Emit.Delegates
+Imports Microsoft.VisualBasic.Language
+
+Namespace Runtime.Components
+
+    Public Class [Variant](Of T) : Inherits Value(Of Object)
+
+        Public ReadOnly Property CanbeCastTo As Boolean
+            Get
+                Return GetUnderlyingType() Is GetType(T) OrElse
+                    GetUnderlyingType.IsInheritsFrom(GetType(T)) OrElse
+                    (GetType(T).IsInterface AndAlso GetUnderlyingType.ImplementInterface(GetType(T)))
+            End Get
+        End Property
+
+        Public Function [TryCast]() As T
+            If CanbeCastTo Then
+                Return DirectCast(Value, T)
+            Else
+                Return Nothing
+            End If
+        End Function
+
+    End Class
+End Namespace
