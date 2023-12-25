041c404ea75ecba52003559cb9183c586df44c93
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-12-25 16:35:30 +0800

    order by weight desc

--- test/demo/math/signals/gauss.R
+++ test/demo/math/signals/gauss.R
@@ -4,7 +4,7 @@ setwd(@dir);
 
 let signals_data = read.csv("./linear_sampler.csv", 
     row.names = NULL, check.names = FALSE);
-let peaks = gaussian_fit(signals_data,  max_peaks = 3);
+let peaks = gaussian_fit(signals_data,  max_peaks = 6);
 
 print(signals_data, max.print = 13);
 print(peaks);
