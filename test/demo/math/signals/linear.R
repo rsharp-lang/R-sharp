5eff347ce031faf347825b5ddcd3a07b0558d595
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-12-25 15:28:34 +0800

    source file renames

--- test/demo/math/signals/linear.R
+++ test/demo/math/signals/linear.R
@@ -22,6 +22,10 @@ print("x axis for do signal data re-sampling:");
 print(x_axis);
 
 let signal = as.signal(raw_signal$Time, raw_signal$Intensity);
-ley linear = resampler(signal, max.dx = mean(x_diff) * 10);
+let linear = resampler(signal, max.dx = mean(x_diff) * 10);
 
+let interpolate = data.frame(time = x_axis, intensity = linear(x_axis));
 
+print(interpolate, max.print = 6);
+
+write.csv(interpolate, file = "./linear_sampler.csv", row.names = FALSE);
