a0e3416aa029f08c643ce48463a4b500d2b07171
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-12-28 17:23:36 +0800

    configs for run traceback debug test

---
+++ test/demo/clustering/traceback_demo.R
@@ -0,0 +1,23 @@
+require(clustering);
+require(JSON);
+
+setwd(@dir);
+
+let rawdata = read.csv("./feature_regions.csv", row.names = 1, check.names = FALSE);
+
+rawdata[, "Cluster"] = NULL;
+rawdata[, "x"] = as.numeric(rawdata$x);
+rawdata[, "y"] = as.numeric(rawdata$y);
+
+print(rawdata, max.print = 13);
+
+let result2 = kmeans(rawdata, centers = 18, bisecting = TRUE, traceback = TRUE);
+let debug = getTraceback();
+
+print("kmeans clustering algorithm traceback:");
+str(debug);
+
+debug 
+|> JSON::json_encode()
+|> writeLines(con = "./traceback.json")
+;
