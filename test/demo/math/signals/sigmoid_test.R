fd4077950e670941b021d20cecedcf4271d413ee
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-01-02 17:07:18 +0800

    run test failure

--- test/demo/math/signals/sigmoid_test.R
+++ test/demo/math/signals/sigmoid_test.R
@@ -7,7 +7,7 @@ let y = f(x, 20, 1 , 5) + runif(length(x), -1.5, 1.5);
 
 x = x + runif(length(x), -1.5, 1.5);
 
-let pars = curve_fit(f, x, y, A = 1, B = 1, C = 1);
+let pars = curve_fit(f, x, y, A = 0.5, B = 0.5, C = 0.5);
 
 str(pars);
 setwd(@dir);
@@ -16,5 +16,6 @@ setwd(@dir);
 
 bitmap(file = "./sigmoid_fit.png", fill = "white", padding = [100, 200, 100, 200]) {
     plot(x, y, line = FALSE, color = "blue",
-         fit1 = list(x = x, y = f(x, pars$A, pars$B, pars$C), color = "red"));
+         fit1 = list(x = x, y = f(x, pars$A, pars$B, pars$C), color = "red"),
+         real = list(x = x, y = f(x, 20,1,5), color = "green"));
 }
