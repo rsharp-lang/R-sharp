6b2ad0774cd04e1c01d67335ebd757aa9c28d470
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-01-03 14:47:26 +0800

    truncate the range

--- test/demo/math/signals/gauss_fit_test.R
+++ test/demo/math/signals/gauss_fit_test.R
@@ -1,9 +1,9 @@
 imports "signalProcessing" from "signalKit";
 
 # create test signal_data
-let peaks = gaussian_peak( center = [1.6, 4.9, 2.3, 1.944, 5.5],
-                                 height = [1 2 6 0.5 0.3 1.1114] ,
-                                width = [0.12 0.5 1.6 0.999 2.1112]) ;
+let peaks = gaussian_peak( center = [3.6, 4.9, 3.3, 3.944, 6.5],
+                                 height = [20000 23333 64444 52222 30000 11114] ,
+                                width = [0.12 0.5 0.6 0.399 0.71112]) ;
 
 setwd(@dir);
 
@@ -11,13 +11,19 @@ bitmap(file = "./view_data.png") {
     plot(peaks, x = 1:10 step 0.5, sin = FALSE);
 }
 
-let signal = as.signal(1:10 step 0.25, peaks);
+let signal = as.signal(1:10 step 0.5, peaks);
+let sig_df = as.data.frame(signal);
 
-print(as.data.frame(signal));
+print(sig_df, max.print = 6);
 
-let peaks_guess = gaussian_fit(signal,  max_peaks = 3, 
-  gauss_clr = TRUE, sine_kernel = TRUE);
+print(sig_df$x);
+print(sig_df$y);
+
+let peaks_guess = gaussian_fit(signal,  max_peaks = 5, 
+  gauss_clr = TRUE, sine_kernel = FALSE);
+
+print(as.data.frame(peaks_guess));
 
 bitmap(file = "./decompose_peaks.png") {
-    plot(peaks_guess, x = 1:10 step 0.5, sin = TRUE);
+    plot(peaks_guess, x = 1:10 step 0.1, sin = FALSE);
 }
