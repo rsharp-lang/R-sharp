20f5e1e53aa5cbd61b0e8ad6b73c543e5436d9e4
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-01-03 12:56:07 +0800

    plot gauss signal data compositions

--- test/demo/math/signals/gauss_fit_test.R
+++ test/demo/math/signals/gauss_fit_test.R
@@ -1,3 +1,16 @@
 imports "signalProcessing" from "signalKit";
 
 # create test signal_data
+let peaks = gaussian_peak( center = [1.6, 4.9, 2.3, 1.944, 5.5],
+                                 height = [1 2 6 0.5 0.3 1.1114] ,
+                                width = [0.12 0.5 1.6 0.999 2.1112]) ;
+
+setwd(@dir);
+
+bitmap(file = "./view_data.png") {
+    plot(peaks, x = 1:10 step 0.5);
+}
+
+let signal = as.signal(1:10 step 0.25, peaks);
+
+print(signal);
