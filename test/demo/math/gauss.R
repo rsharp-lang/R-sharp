0d1c3fe35bf7058ec4bc3becf69e09a033ee4968
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-01-12 09:31:10 +0800

    try to implements the gauss solver function

--- test/demo/math/gauss.R
+++ test/demo/math/gauss.R
@@ -1,9 +1,23 @@
-let problem = [
-    2 * x + y - z = 8,
-    -3 * x-y+2*z = -11,
-    -2* x +y+2 *z = -3
-]
+require(Matrix);
+
+let problem = ~[
+     2*x + y -   z =  8,
+    -3*x - y + 2*z = -11,
+    -2*x + y + 2*z = -3
+];
 
 let x = gauss_solve(problem);
 
 print(x);
+
+let problem_mat = data.frame(
+    x = [ 2, -3, -2],
+    y = [ 1, -1,  1],
+    z = [-1,  2,  2]
+);
+
+print(problem_mat);
+
+let x1 = gauss_solve(problem_mat, [8, -11, -3]);
+
+print(x1);
