961da08aa5d5f68f3fd9195859b436036b4ad65f
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-01-23 16:02:51 +0800

    canopy

--- test/demo/clustering/canopy.R
+++ test/demo/clustering/canopy.R
@@ -10,6 +10,8 @@ rawdata[, "y"] = as.numeric(rawdata$y);
 print(rawdata, max.print = 13);
 
 let k0 = canopy(rawdata);
+let k = kmeans(rawdata, centers = length(k0),
+                         bisecting = TRUE);
 let kdf = as.data.frame(k0);
 
 print(kdf, max.print = 6);
@@ -19,6 +21,22 @@ write.csv(kdf, file = "./canopy.csv");
 bitmap(file = `./canopy.png`) {
     plot(kdf$V1, kdf$V2, 
         class     = kdf$clusters, 
+        grid.fill = "white",
+        padding   = "padding: 125px 300px 200px 200px;",
+        colorSet  = "paper"
+    );
+}
+
+
+k = as.data.frame(k);
+
+print(k, max.print = 6);
+
+write.csv(k, file = "./canopy-kmeans.csv");
+
+bitmap(file = `./canopy-kmeans.png`) {
+    plot(as.numeric(k$x), as.numeric(k$y), 
+        class     = k$Cluster, 
         grid.fill = "white",
         padding   = "padding: 125px 300px 200px 200px;",
         colorSet  = "paper"
