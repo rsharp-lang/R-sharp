ac7196125e52197082afe0d19a3496965a319428
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-01-23 15:57:50 +0800

    run cluster test success

--- test/demo/clustering/canopy.R
+++ test/demo/clustering/canopy.R
@@ -10,4 +10,17 @@ rawdata[, "y"] = as.numeric(rawdata$y);
 print(rawdata, max.print = 13);
 
 let k0 = canopy(rawdata);
+let kdf = as.data.frame(k0);
 
+print(kdf, max.print = 6);
+
+write.csv(kdf, file = "./canopy.csv");
+
+bitmap(file = `./canopy.png`) {
+    plot(kdf$V1, kdf$V2, 
+        class     = kdf$clusters, 
+        grid.fill = "white",
+        padding   = "padding: 125px 300px 200px 200px;",
+        colorSet  = "paper"
+    );
+}
