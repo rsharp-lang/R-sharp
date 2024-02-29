7c370168bb485e6a22344dc38e1e9646602a6190
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-02-29 22:27:46 +0800

    unsure for the path bug?

--- test/unitTest/environment/dir.R
+++ test/unitTest/environment/dir.R
@@ -1,6 +1,17 @@
-lib_dir =  "/mnt/254/项目以外内容/2023/血清血浆物质列表/lipidomics_test/blood/output-20240301-3/tmp/mzVault/";
+lib_dir =  ["/mnt/254/项目以外内容/2023/血清血浆物质列表/lipidomics_test/blood/output-20240301-3/tmp/mzVault/"
 
-print(lib_dir);
+"./test/app/dir"
+"./test/app/dir/"
+"C:/demo/dir/file"
+"\\192.168.1.1\aaa\test\dirname\file.txt"
+"\\192.168.1.1\aaa\test\dirname"
+
+"/opt/aaa/file.dat"
+
+
+];
+
+# print(lib_dir);
 
 print(dirname(lib_dir));
-print(basename(lib_dir));
+# print(basename(lib_dir));
