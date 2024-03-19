8e320927d4ca90f41baf62214594ffd1ae15f148
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-03-19 17:02:59 +0800

    test not working as expected

---
+++ test/demo/clustering/spectral_cluster/plot.R
@@ -0,0 +1,5 @@
+bitmap(file = file.path(@dir, "plot_clusters.png")) {
+    let data = read.csv(file.path(@dir, "spirals_out.csv"), row.names = NULL);
+
+    plot(as.numeric(data$x),as.numeric(data$y),class = data$cluster_id);
+}
