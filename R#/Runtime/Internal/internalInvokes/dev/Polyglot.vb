255c2a557810e0de6e5cc0c0495ba994a61a430e
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-03-21 18:58:02 +0800

    how to register the language module in runtime?

---
+++ R#/Runtime/Internal/internalInvokes/dev/Polyglot.vb
@@ -0,0 +1,20 @@
+﻿Imports System.Reflection
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports SMRUCC.Rsharp.Runtime.Interop
+
+Namespace Runtime.Internal.Invokes
+
+    Module Polyglot
+
+        <ExportAPI("register")>
+        Public Function register(<RRawVectorArgument> file_types As Object, assembly As Assembly, Optional env As Environment = Nothing) As Object
+
+        End Function
+
+        <ExportAPI("assembly")>
+        <RApiReturn(GetType(Assembly))>
+        Public Function assembly(filename As String, Optional env As Environment = Nothing) As Object
+
+        End Function
+    End Module
+End Namespace
