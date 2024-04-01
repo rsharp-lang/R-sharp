b9f867b7bb2e9330e695dd74d83b60e03b5b2edd
谢桂纲 <gg.xie@bionovogene.com>
2024-04-01 13:19:17 +0800

    run metachar decode test success

--- test/unitTest/json_test_zhcn.R
+++ test/unitTest/json_test_zhcn.R
@@ -1,5 +1,5 @@
 require(JSON);
 
-let zhcn = "['/mnt/smb3/\u9879\u76ee\u4ee5\u5916\u5185\u5bb9/2024/中文字符名称']";
+let zhcn = "['/mnt/smb3/\u9879\u76ee\t\u4ee5\u5916\u5185\u5bb9/2024/中文字符名称']";
 
 print(JSON::json_decode(zhcn));
