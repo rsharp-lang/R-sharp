2ca06d322e8a0776bdbad7c3d88ee3de081fe676
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-04-23 19:52:03 +0800

    test susccess

--- test/unitTest/dataframe/dataframe_json.R
+++ test/unitTest/dataframe/dataframe_json.R
@@ -3,4 +3,4 @@ require(JSON);
 let df = data.frame(x = 1:10, y = 5, z = ((1:10) ^ 2) / 5, row.names = 'a':'j');
 
 print(df);
-print(JSON::json_encode(df, maskReadonly = TRUE));
+print(JSON::json_encode(df, maskReadonly = TRUE, row.names = TRUE, full.vector = TRUE));
