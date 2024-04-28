acf47d88fb4fc5e29762e1c73ffb159189ada757
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-04-28 22:20:55 +0800

    try to fix of the object type cast erropr

--- R#/Runtime/Internal/objects/RConversion/conversion.vb
+++ R#/Runtime/Internal/objects/RConversion/conversion.vb
@@ -1,58 +1,58 @@
 ﻿#Region "Microsoft.VisualBasic::c6f06fd0a5ff8371122e856555e57313, E:/GCModeller/src/R-sharp/R#//Runtime/Internal/objects/RConversion/conversion.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 1394
-    '    Code Lines: 825
-    ' Comment Lines: 433
-    '   Blank Lines: 136
-    '     File Size: 63.13 KB
+' Summaries:
 
 
-    '     Module RConversion
-    ' 
-    '         Function: asCharacters, asDataframe, asDate, asDate2, asDouble
-    '                   asInteger, asList, asLogicals, asNumeric, asObject
-    '                   asPipeline, asRaw, asVector, castArrayOfGeneric, castArrayOfObject
-    '                   castListMatrix, castListRows, castListRowsToDataframe, castListToDataframe, castType
-    '                   checkList, checkNames, handleListFeatureProjections, handleUnsure, isCharacter
-    '                   isDateTime, isLogical, populateNumeric, tryUnlistArray, unlist
-    '                   unlistOfRList, unlistRecursive
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 1394
+'    Code Lines: 825
+' Comment Lines: 433
+'   Blank Lines: 136
+'     File Size: 63.13 KB
+
+
+'     Module RConversion
+' 
+'         Function: asCharacters, asDataframe, asDate, asDate2, asDouble
+'                   asInteger, asList, asLogicals, asNumeric, asObject
+'                   asPipeline, asRaw, asVector, castArrayOfGeneric, castArrayOfObject
+'                   castListMatrix, castListRows, castListRowsToDataframe, castListToDataframe, castType
+'                   checkList, checkNames, handleListFeatureProjections, handleUnsure, isCharacter
+'                   isDateTime, isLogical, populateNumeric, tryUnlistArray, unlist
+'                   unlistOfRList, unlistRecursive
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -372,7 +372,15 @@ Namespace Runtime.Internal.Object.Converts
                 Return clone
             ElseIf x.GetType.IsArray AndAlso DirectCast(x, Array).Length = 0 Then
                 Return Nothing
-            ElseIf TypeOf x Is list Then
+            End If
+
+            args = base.Rlist(args, env)
+
+            If Program.isException(args) Then
+                Return args
+            End If
+
+            If TypeOf x Is list Then
                 Return DirectCast(x, list).slots.castListToDataframe(args, env)
 
             ElseIf TypeOf x Is list() OrElse (TypeOf x Is Object() AndAlso DirectCast(x, Object()).All(Function(xi) TypeOf xi Is list)) Then
@@ -383,11 +391,7 @@ Namespace Runtime.Internal.Object.Converts
                     x = DirectCast(x, vbObject).target
                 End If
 
-                args = base.Rlist(args, env)
-
-                If Program.isException(args) Then
-                    Return args
-                ElseIf DirectCast(args, list).hasName("features") Then
+                If DirectCast(args, list).hasName("features") Then
                     If TypeOf x Is list Then
                         x = DirectCast(x, list).data.ToArray
                     End If
