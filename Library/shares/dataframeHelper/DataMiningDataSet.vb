003dd05970f786a0b252a9b21c3d0da7c61443e7
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-04-28 19:50:56 +0800

    fix for the class label data

--- Library/shares/dataframeHelper/DataMiningDataSet.vb
+++ Library/shares/dataframeHelper/DataMiningDataSet.vb
@@ -1,5 +1,6 @@
 ﻿#If DATAMINING_DATASET Then
 
+Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.ComponentModel.Collection
 Imports Microsoft.VisualBasic.Data.csv.IO
 Imports Microsoft.VisualBasic.DataMining.KMeans
@@ -7,12 +8,13 @@ Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Linq
 Imports SMRUCC.Rsharp.Runtime
 Imports SMRUCC.Rsharp.Runtime.Components
+Imports SMRUCC.Rsharp.Runtime.Vectorization
 Imports Rdataframe = SMRUCC.Rsharp.Runtime.Internal.Object.dataframe
 Imports REnv = SMRUCC.Rsharp.Runtime
 
 Module DataMiningDataSet
 
-    Public Function getDataModel(x As Object, env As Environment) As [Variant](Of Message, EntityClusterModel())
+    Public Function getDataModel(x As Object, check_class As Boolean, env As Environment) As [Variant](Of Message, EntityClusterModel())
         Dim model As EntityClusterModel()
 
         If x Is Nothing Then
@@ -31,13 +33,36 @@ Module DataMiningDataSet
             End Select
 #Enable Warning
         ElseIf TypeOf x Is Rdataframe Then
-            Dim colNames As String() = DirectCast(x, Rdataframe).columns _
+            Dim df = DirectCast(x, Rdataframe)
+            Dim colNames As String() = df.columns _
                 .Keys _
                 .ToArray
+            Dim has_class As Boolean = False
+            Dim class_col As String = Nothing
+
+            If check_class Then
+                For Each col As String In {"class", "cluster", "group", "type", "Class", "Cluster", "Group", "Type"}
+                    If df.checkClassLabels(col) Then
+                        class_col = col
+                        Exit For
+                    End If
+                Next
+            End If
+
+            Dim class_labels As String() = Nothing
+
+            If Not class_col.StringEmpty Then
+                class_labels = CLRVector.asCharacter(df(class_col))
+                df = New Rdataframe With {
+                    .columns = New Dictionary(Of String, Array)(df.columns),
+                    .rownames = df.rownames
+                }
+                df.columns.Remove(class_col)
+            End If
 
             model = DirectCast(x, Rdataframe) _
                 .forEachRow _
-                .Select(Function(r)
+                .Select(Function(r, index)
                             Return New EntityClusterModel With {
                                 .ID = r.name,
                                 .Properties = colNames _
@@ -45,7 +70,8 @@ Module DataMiningDataSet
                                     .ToDictionary(Function(c) c.value,
                                                   Function(i)
                                                       Return CType(r(i), Double)
-                                                  End Function)
+                                                  End Function),
+                                .Cluster = class_labels.ElementAtOrNull(index)
                             }
                         End Function) _
                 .ToArray
@@ -54,6 +80,15 @@ Module DataMiningDataSet
         End If
 
         Return model
+    End Function
+
+    <Extension>
+    Private Function checkClassLabels(df As Rdataframe, col As String) As Boolean
+        Return df.hasName(col) AndAlso CLRVector _
+            .asCharacter(df(col)) _
+            .All(Function(str)
+                     Return str.IsPattern("((class)|(cluster)|(group)).*\d+")
+                 End Function)
     End Function
 End Module
 #End If
