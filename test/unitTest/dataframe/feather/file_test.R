97137e01379863d33c4e502672888a8c4607ec1a
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-05-08 13:48:33 +0800

    unsure for the file size growth

--- test/unitTest/dataframe/feather/file_test.R
+++ test/unitTest/dataframe/feather/file_test.R
@@ -2,4 +2,4 @@ let df = read.feather(file = file.path(@dir, "r-feather-test-nullable.feather"))
 
 print(df);
 
-write.feather(df, file = file.path(@dir, "demo_write.fbs"));
+write.feather(df, file = file.path(@dir, "demo_write.fbs"), row.names = FALSE);
