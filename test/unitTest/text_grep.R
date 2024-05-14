4f095a0c2a6a58b0e69908626e9645fe232251d2
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-05-14 10:54:01 +0800

    config test

--- test/unitTest/text_grep.R
+++ test/unitTest/text_grep.R
@@ -1,4 +1,4 @@
-let names = ["258_Herniarin_[M+H2O+H]+" "993_Geranyl acetate_[M+H2O+H]+" "3229_Glycerol_[M+NH4]+" "398587_Cytidine_[M+H]+" "398007_Adenosine_[M+H]+"];
+let names = ["258_Herniarin_[M+H2O+H]+" "993_Geranyl acetate_[M+H2O+H]+" "3229_Glycerol_[M+NH4]+" "398587_Cytidine_[M+H]+" "398007_Adenosine_[M+H]+" "398007_Adenosine_[2M+H]2+"];
 let grep = "$(\d+)_$(\[\d*M.*\]\d*[+-])";
 
 print(text_grep(grep, names));
