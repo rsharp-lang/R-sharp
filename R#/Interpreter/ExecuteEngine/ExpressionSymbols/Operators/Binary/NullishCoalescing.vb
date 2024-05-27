174d90525a82790ddd697d873fc0f198c04a397c
谢桂纲 <gg.xie@bionovogene.com>
2024-05-27 14:04:22 +0800

    NullishCoalescing

--- R#/Interpreter/ExecuteEngine/ExpressionSymbols/Operators/Binary/NullishCoalescing.vb
+++ R#/Interpreter/ExecuteEngine/ExpressionSymbols/Operators/Binary/NullishCoalescing.vb
@@ -1,6 +1,58 @@
-﻿Namespace Interpreter.ExecuteEngine.ExpressionSymbols.Operators
+﻿Imports SMRUCC.Rsharp.Development.Package.File
+Imports SMRUCC.Rsharp.Runtime
+Imports SMRUCC.Rsharp.Runtime.Components
 
-    Public Class NullishCoalescing
+Namespace Interpreter.ExecuteEngine.ExpressionSymbols.Operators
 
+    ''' <summary>
+    ''' ```
+    ''' x ?? default
+    ''' ```
+    ''' </summary>
+    Public Class NullishCoalescing : Inherits Expression
+        Implements IBinaryExpression
+
+        Public Overrides ReadOnly Property type As TypeCodes
+            Get
+                Return TypeCodes.generic
+            End Get
+        End Property
+
+        Public Overrides ReadOnly Property expressionName As ExpressionTypes
+            Get
+                Return ExpressionTypes.Binary
+            End Get
+        End Property
+
+        Public ReadOnly Property value As Expression Implements IBinaryExpression.left
+        Public ReadOnly Property [default] As Expression Implements IBinaryExpression.right
+        Public ReadOnly Property [operator] As String Implements IBinaryExpression.operator
+            Get
+                Return "??"
+            End Get
+        End Property
+
+        Sub New(a As Expression, b As Expression)
+            value = a
+            [default] = b
+        End Sub
+
+        Public Overrides Function Evaluate(envir As Environment) As Object
+            ' 20191216
+            ' fix for
+            ' value || stop(xxxx)
+            Dim a As Object = value.Evaluate(envir)
+
+            ' let arg as string = ?"--opt" || default;
+            If Internal.Invokes.base.isEmpty(a) Then
+                Return [default].Evaluate(envir)
+            Else
+                Return a
+            End If
+        End Function
+
+        Public Overrides Function ToString() As String
+            Return $"({value} || {[default]})"
+        End Function
     End Class
 End Namespace
