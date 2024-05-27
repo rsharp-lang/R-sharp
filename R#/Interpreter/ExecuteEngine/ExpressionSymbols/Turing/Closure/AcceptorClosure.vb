9bf5dcbef865d9ba24c779bd9b28cfaf966ff5c5
谢桂纲 <gg.xie@bionovogene.com>
2024-05-27 15:35:57 +0800

    try to handling of the stack error

--- R#/Interpreter/ExecuteEngine/ExpressionSymbols/Turing/Closure/AcceptorClosure.vb
+++ R#/Interpreter/ExecuteEngine/ExpressionSymbols/Turing/Closure/AcceptorClosure.vb
@@ -1,57 +1,57 @@
 ﻿#Region "Microsoft.VisualBasic::b1e655ebf182a8e883f82d968ce3fe6b, R#\Interpreter\ExecuteEngine\ExpressionSymbols\Turing\Closure\AcceptorClosure.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 48
-    '    Code Lines: 40 (83.33%)
-    ' Comment Lines: 0 (0.00%)
-    '    - Xml Docs: 0.00%
-    ' 
-    '   Blank Lines: 8 (16.67%)
-    '     File Size: 1.88 KB
+' Summaries:
 
 
-    '     Class AcceptorClosure
-    ' 
-    '         Properties: expressionName
-    ' 
-    '         Constructor: (+1 Overloads) Sub New
-    '         Function: Evaluate
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 48
+'    Code Lines: 40 (83.33%)
+' Comment Lines: 0 (0.00%)
+'    - Xml Docs: 0.00%
+' 
+'   Blank Lines: 8 (16.67%)
+'     File Size: 1.88 KB
+
+
+'     Class AcceptorClosure
+' 
+'         Properties: expressionName
+' 
+'         Constructor: (+1 Overloads) Sub New
+'         Function: Evaluate
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -59,6 +59,7 @@ Imports Microsoft.VisualBasic.Linq
 Imports SMRUCC.Rsharp.Development.Package.File
 Imports SMRUCC.Rsharp.Interpreter.ExecuteEngine.ExpressionSymbols.Operators
 Imports SMRUCC.Rsharp.Runtime
+Imports SMRUCC.Rsharp.Runtime.Components
 Imports SMRUCC.Rsharp.Runtime.Interop
 
 Namespace Interpreter.ExecuteEngine.ExpressionSymbols.Closure
@@ -91,29 +92,45 @@ Namespace Interpreter.ExecuteEngine.ExpressionSymbols.Closure
 
         Public Overrides Function Evaluate(envir As Environment) As Object
             If TypeOf program.Last Is FunctionInvoke Then
-                Dim arguments = envir.acceptorArguments
-                Dim newProgram As New Program(program.Take(program.lines - 1))
-                Dim lastFunc As FunctionInvoke = program.Last
-                Dim value As Object = newProgram.Execute(envir)
-
-                If Program.isException(value) Then
-                    Return value
-                Else
-                    Dim lastClosureCopy As New FunctionInvoke(lastFunc)
-
-                    lastClosureCopy.parameters = lastClosureCopy.parameters _
-                        .JoinIterates(arguments _
-                            .Select(Function(arg)
-                                        Return New ValueAssignExpression(arg.Key, New RuntimeValueLiteral(arg.Value))
-                                    End Function)) _
-                        .ToArray
-                    value = lastClosureCopy.Evaluate(envir)
-                End If
-
-                Return value
+                Return evaluateAcceptor(envir)
             Else
                 Return MyBase.Evaluate(envir)
             End If
+        End Function
+
+        Private Function evaluateAcceptor(envir As Environment) As Object
+            Dim arguments As Dictionary(Of String, Object) = envir.acceptorArguments
+            Dim newProgram As New Program(program.Take(program.lines - 1))
+            Dim lastFunc As FunctionInvoke = program.Last
+            Dim value As Object = newProgram.Execute(envir)
+
+            If Program.isException(value) Then
+                Return value
+            End If
+
+            Dim lastClosureCopy As New FunctionInvoke(lastFunc)
+            Dim accepts As New List(Of Expression)
+            Dim valExpr As Object
+
+            For Each arg In arguments
+                valExpr = DirectCast(arg.Value, Expression).Evaluate(envir)
+
+                If TypeOf valExpr Is Message Then
+                    Return valExpr
+                Else
+                    Call accepts.Add(New ValueAssignExpression(
+                        symbol:=arg.Key,
+                        value:=New RuntimeValueLiteral(valExpr))
+                    )
+                End If
+            Next
+
+            lastClosureCopy.parameters = lastClosureCopy.parameters _
+                .JoinIterates(accepts) _
+                .ToArray
+            value = lastClosureCopy.Evaluate(envir)
+
+            Return value
         End Function
     End Class
 End Namespace
