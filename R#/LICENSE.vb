bf430a466cb41eb5ff34ff59815bc3411186c369
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-05-30 10:12:55 +0800

    Update LICENSE.vb

--- R#/LICENSE.vb
+++ R#/LICENSE.vb
@@ -1,59 +1,63 @@
 ﻿#Region "Microsoft.VisualBasic::4606b8530e6810cf84938f7a43cb73c9, R#\LICENSE.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 41
-    '    Code Lines: 14 (34.15%)
-    ' Comment Lines: 23 (56.10%)
-    '    - Xml Docs: 60.87%
-    ' 
-    '   Blank Lines: 4 (9.76%)
-    '     File Size: 1.42 KB
+' Summaries:
 
 
-    ' Module LICENSE
-    ' 
-    '     Properties: GPL3
-    ' 
-    '     Sub: GithubRepository
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 41
+'    Code Lines: 14 (34.15%)
+' Comment Lines: 23 (56.10%)
+'    - Xml Docs: 60.87%
+' 
+'   Blank Lines: 4 (9.76%)
+'     File Size: 1.42 KB
+
+
+' Module LICENSE
+' 
+'     Properties: GPL3
+' 
+'     Sub: GithubRepository
+' 
+' /********************************************************************************/
 
 #End Region
 
+Imports System.Diagnostics.CodeAnalysis
 Imports System.Runtime.CompilerServices
+
+' ignores of the gdi+ warning message
+<Assembly: SuppressMessage("", "CA1416")>
 
 ''' <summary>
 ''' Information about the ``R#`` interpreter code module.
