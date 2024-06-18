4b1f5b1349646b54efa93aaf0ab0aba7f423e038
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-06-18 14:55:44 +0800

    make bugs fixed for the collection set operator: where the collection has been modified

--- test/unitTest/operators/set_jaccard_test.R
+++ test/unitTest/operators/set_jaccard_test.R
@@ -4,4 +4,13 @@ let b = as.set([1 2 3 4 5 6], mode = "integer");
 print(a == b);
 print(a - b);
 
+print(length(a - b));
+print(length(a));
 
+let c = as.set([3 4 5 6 7 8 9 0], mode = "integer");
+
+print(a == c);
+print(c == c);
+print(a - c);
+print(a + c);
+print(a & c);
