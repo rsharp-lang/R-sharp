64cf91315549ac627bd9cb9736a2c08445230773
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-06-18 14:17:29 +0800

    try to make improvements of the data set api

--- R#/Runtime/Internal/objects/RConversion/conversion.vb
+++ R#/Runtime/Internal/objects/RConversion/conversion.vb
@@ -1,60 +1,60 @@
 ﻿#Region "Microsoft.VisualBasic::258868e46e7e5366c46ccc2da0b86e36, R#\Runtime\Internal\objects\RConversion\conversion.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 1404
-    '    Code Lines: 833 (59.33%)
-    ' Comment Lines: 433 (30.84%)
-    '    - Xml Docs: 78.06%
-    ' 
-    '   Blank Lines: 138 (9.83%)
-    '     File Size: 63.58 KB
+' Summaries:
 
 
-    '     Module RConversion
-    ' 
-    '         Function: asCharacters, asDataframe, asDate, asDate2, asDouble
-    '                   asInteger, asList, asLogicals, asNumeric, asObject
-    '                   asPipeline, asRaw, asVector, castArrayOfGeneric, castArrayOfObject
-    '                   castListMatrix, castListRows, castListRowsToDataframe, castListToDataframe, castType
-    '                   checkList, checkNames, handleListFeatureProjections, handleUnsure, isCharacter
-    '                   isDateTime, isLogical, populateNumeric, tryUnlistArray, unlist
-    '                   unlistOfRList, unlistRecursive
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 1404
+'    Code Lines: 833 (59.33%)
+' Comment Lines: 433 (30.84%)
+'    - Xml Docs: 78.06%
+' 
+'   Blank Lines: 138 (9.83%)
+'     File Size: 63.58 KB
+
+
+'     Module RConversion
+' 
+'         Function: asCharacters, asDataframe, asDate, asDate2, asDouble
+'                   asInteger, asList, asLogicals, asNumeric, asObject
+'                   asPipeline, asRaw, asVector, castArrayOfGeneric, castArrayOfObject
+'                   castListMatrix, castListRows, castListRowsToDataframe, castListToDataframe, castType
+'                   checkList, checkNames, handleListFeatureProjections, handleUnsure, isCharacter
+'                   isDateTime, isLogical, populateNumeric, tryUnlistArray, unlist
+'                   unlistOfRList, unlistRecursive
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -66,6 +66,7 @@ Imports Microsoft.VisualBasic.ApplicationServices.Debugging.Logging
 Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.ComponentModel.Collection
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports Microsoft.VisualBasic.ComponentModel.DataStructures
 Imports Microsoft.VisualBasic.Emit.Delegates
 Imports Microsoft.VisualBasic.Imaging
 Imports Microsoft.VisualBasic.Language
