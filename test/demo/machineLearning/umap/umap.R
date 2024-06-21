0d696fc45fc6e99284258db25390e054e09bf49c
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-06-22 00:41:53 +0800

    make improvements of the progress displauy

--- test/demo/machineLearning/umap/umap.R
+++ test/demo/machineLearning/umap/umap.R
@@ -5,17 +5,34 @@ require(visualizer);
 
 setwd(@dir);
 
-const MNIST_LabelledVectorArray as string = "\GCModeller\src\runtime\sciBASIC#\Data_science\DataMining\data\umap\MNIST-LabelledVectorArray-60000x100.msgpack";
+const MNIST_LabelledVectorArray as string = "./MNIST-LabelledVectorArray-60000x100.msgpack";
 
 let manifold = MNIST_LabelledVectorArray 
-|> read.MNIST(subset = 30000, dataset = "dataframe")
-:> umap(
-	dimension         = 2, 
-	numberOfNeighbors = 15,
+|> read.MNIST(subset = 60000, dataset = "dataframe")
+|> umap(
+	dimension         = 3, 
+	numberOfNeighbors = 32,
     localConnectivity = 1,
-    KnnIter           = 32,
+    KnnIter           = 64,
     bandwidth         = 1
 )
 ;
 
 writeBin(manifold$umap, con = "./umap_mnist.dat", labels = manifold$labels);
+
+manifold = as.data.frame(manifold$umap, 
+    labels = manifold$labels, 
+    dimension = ["x", "y", "z"]);
+
+print(manifold, max.print = 13);
+
+bitmap(file = `./plot_mnist.png`, size = [6000,4000]) {
+	plot(manifold$x, manifold$y,
+		class       = rownames(manifold), 
+		labels      = rownames(manifold),
+		show_bubble = FALSE,
+		point_size  = 50,
+		legendlabel = "font-style: normal; font-size: 24; font-family: Bookman Old Style;",
+		padding     = "padding:150px 150px 350px 350px;"
+	);
+}
