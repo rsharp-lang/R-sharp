701aa657ae8226cb2b371cbd35361639e7658410
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-07-03 09:45:13 +0800

    make improvements of the CNN function calls

--- studio/Rsharp_kit/MLkit/MachineLearning/VAE.vb
+++ studio/Rsharp_kit/MLkit/MachineLearning/VAE.vb
@@ -1,58 +1,59 @@
 ﻿#Region "Microsoft.VisualBasic::c5277b79488fd1ed776d0193deafb9a6, studio\Rsharp_kit\MLkit\MachineLearning\VAE.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 65
-    '    Code Lines: 20 (30.77%)
-    ' Comment Lines: 35 (53.85%)
-    '    - Xml Docs: 11.43%
-    ' 
-    '   Blank Lines: 10 (15.38%)
-    '     File Size: 2.39 KB
+' Summaries:
 
 
-    ' Module VAE
-    ' 
-    '     Function: Trainer
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 65
+'    Code Lines: 20 (30.77%)
+' Comment Lines: 35 (53.85%)
+'    - Xml Docs: 11.43%
+' 
+'   Blank Lines: 10 (15.38%)
+'     File Size: 2.39 KB
+
+
+' Module VAE
+' 
+'     Function: Trainer
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Linq
+Imports Microsoft.VisualBasic.MachineLearning.CNN
 Imports Microsoft.VisualBasic.MachineLearning.ComponentModel.StoreProcedure
 Imports Microsoft.VisualBasic.MachineLearning.VariationalAutoencoder
 Imports Microsoft.VisualBasic.Scripting.MetaData
@@ -62,7 +63,11 @@ Imports SMRUCC.Rsharp.Runtime.Internal.[Object]
 Imports SMRUCC.Rsharp.Runtime.Interop
 Imports SMRUCC.Rsharp.Runtime.Vectorization
 Imports stdvec = Microsoft.VisualBasic.Math.LinearAlgebra.Vector
+Imports Microsoft.VisualBasic.MachineLearning.CNN.trainers
 
+''' <summary>
+''' VAE embedding method implements
+''' </summary>
 <Package("VAE")>
 Module VAE
 
@@ -115,4 +120,32 @@ Module VAE
 
     '    Return vae.autoencoder
     'End Function
+
+    ''' <summary>
+    ''' make matrix data embedding via the VAE method
+    ''' </summary>
+    ''' <param name="x"></param>
+    ''' <param name="dims"></param>
+    ''' <param name="batch_size"></param>
+    ''' <param name="max_iteration"></param>
+    ''' <param name="env"></param>
+    ''' <returns>
+    ''' a dataframe object contains the embedding result.
+    ''' </returns>
+    <ExportAPI("embedding")>
+    Public Function embedding(<RRawVectorArgument> x As Object, model As ConvolutionalNN,
+                              Optional dims As Integer = 9,
+                              Optional batch_size As Integer = 100,
+                              Optional max_iteration As Integer = 1000,
+                              Optional verbose As Boolean? = Nothing,
+                              Optional env As Environment = Nothing) As Object
+
+        Dim alg As New AdaGradTrainer(batch_size, 0.001F)
+        Dim cnn_val = New Trainer(alg:=alg.SetKernel(model),
+                              log:=Sub(s) base.print(s,, env),
+                              verbose:=If(verbose Is Nothing, env.verboseOption(False), CBool(verbose))) _
+            .train(cnn_val, DataSet, max_loops)
+
+        Return New CNNFunction With {.cnn = cnn_val}
+    End Function
 End Module
