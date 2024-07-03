3419c7eb878ec2b40cbd9a96219a582fd73cbc38
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-07-03 12:23:51 +0800

    handling zero

---
+++ test/demo/machineLearning/umap/Vae/mnf_test.R
@@ -0,0 +1,25 @@
+imports "dataset" from "MLkit";
+imports "VAE" from "MLkit";
+
+require(Matrix);
+require(graphics);
+
+options(SIMD = "legacy");
+
+setwd(@dir);
+
+const images_set = "../mnist_dataset/train-images-idx3-ubyte";
+const raw = images_set 
+|> read.MNIST( 
+format = "mnist", 
+dataset = "vector", 
+labelfile = "../mnist_dataset/train-labels-idx1-ubyte",
+subset = 100
+);
+
+const embedding = nmf(raw, rank = 10);  # VAE::embedding(raw,max_iteration= 2, batch_size = 150);
+
+print(embedding);
+
+# write.csv(embedding, file = "./embedding.csv", row.names = TRUE);
+
