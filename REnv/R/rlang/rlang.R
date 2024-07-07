f5a7065371e5b83ed021743bee1acf3e3f3ae57b
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-07-07 22:29:55 +0800

    Update rlang.R

--- REnv/R/rlang/rlang.R
+++ REnv/R/rlang/rlang.R
@@ -8,14 +8,14 @@ const rlang_interop = function(code, source = NULL, debug = FALSE) {
     let code_save = tempfile(fileext = ".R");
     let load_deps = __sourcescript(source);
     let program   = translate_to_rlang(code);
-
-    code <- paste([load_deps, program], sep = "\n\n");
+    let script_code <- paste([load_deps, "# --------end of load deps----------", program], sep = "\n\n");
 
     if (debug) {
-        cat(code);
-        cat("\n");
+        print("translate R# code to rlang script:");
+        cat(script_code);
+        cat("\n\n");
     } else {
-        writeLines(code, con = code_save);
+        writeLines(script_code, con = code_save);
         system(`Rscript "${code_save}"`);
     }
 }
