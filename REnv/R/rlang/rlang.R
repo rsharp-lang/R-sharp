37dc6062e0b239e7fb4c86c6684f9e42eee69d1c
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-07-07 16:52:46 +0800

    Create rlang.R

---
+++ REnv/R/rlang/rlang.R
@@ -0,0 +1,43 @@
+#' helper function for R# interop with R language
+#' 
+#' @param code a closure expresion for invoke R function
+#' @param source the source file paths for provides the 
+#'    runtime environment for the specific input code. 
+#' 
+const rlang_interop = function(code, source = NULL) {
+    let code_save = tempfile(fileext = ".R");
+
+    code <- paste([
+        __sourcescript(source), 
+        __codescript(code)
+    ], sep = "\n\n");
+
+    writeLines(code, con = code_save);
+
+    system(`Rscript "${code_save}"`);
+}
+
+const __codescript = function(code) {
+
+}
+
+const __sourcescript = function(source) {
+    let rscripts = [];
+
+    for(let handle in source) {
+        if (!file.exists(handle)) {
+            handle <- list.files(handle, pattern = ["*.R" "*.r"], 
+                recursive = TRUE);
+        } else {
+            handle <- normalizePath(handle);
+        }
+
+        rscripts <- append(rscripts, handle);
+    }
+
+    if (length(rscripts) == 0) {
+        "# no dependecy rscript was required!";
+    } else {
+        paste(`source("${rscripts}");`, sep = "\n");
+    }
+}
