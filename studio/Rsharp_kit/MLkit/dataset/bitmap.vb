622342a8cdff2a6e73b0c548812898d21858c6b8
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-07-08 13:41:55 +0800

    test data reader

--- studio/Rsharp_kit/MLkit/dataset/bitmap.vb
+++ studio/Rsharp_kit/MLkit/dataset/bitmap.vb
@@ -1,8 +1,10 @@
 ﻿
+Imports System.Drawing
 Imports System.IO
 Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Imaging.BitmapImage
 Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports Microsoft.VisualBasic.Scripting.Runtime
 Imports SMRUCC.Rsharp.Runtime
 Imports SMRUCC.Rsharp.Runtime.Components
 Imports SMRUCC.Rsharp.Runtime.Interop
@@ -15,6 +17,7 @@ Imports SMRUCC.Rsharp.Runtime.Interop
 Module bitmap_func
 
     <ExportAPI("open")>
+    <RApiReturn(GetType(BitmapReader))>
     Public Function open(<RRawVectorArgument> file As Object, Optional env As Environment = Nothing) As Object
         Dim buf = SMRUCC.Rsharp.GetFileStream(file, FileAccess.Read, env)
 
@@ -34,10 +37,34 @@ Module bitmap_func
     ''' <param name="env"></param>
     ''' <returns></returns>
     <ExportAPI("corp_rectangle")>
+    <RApiReturn(GetType(Bitmap))>
     Public Function corp_rectangle(bmp As BitmapReader,
                                    <RRawVectorArgument> pos As Object,
                                    <RRawVectorArgument> size As Object,
                                    Optional env As Environment = Nothing) As Object
 
+        Dim loc_str = InteropArgumentHelper.getSize(pos, env, Nothing)
+        Dim size_str = InteropArgumentHelper.getSize(size, env, Nothing)
+
+        If loc_str.StringEmpty(, True) OrElse loc_str = "0,0" Then
+            Return Internal.debug.stop("the required location should not be empty!", env)
+        End If
+        If size_str.StringEmpty(, True) OrElse size_str = "0,0" Then
+            Return Internal.debug.stop("the required rectangle size for corp should not be empty!", env)
+        End If
+
+        Dim loc As Point = Casting.PointParser(loc_str)
+        Dim sizeVal As Size = size_str.SizeParser
+        Dim copy As New Bitmap(sizeVal.Width, sizeVal.Height)
+        Dim c As Color
+
+        For x As Integer = loc.X To loc.X + sizeVal.Width
+            For y As Integer = loc.Y To loc.Y + sizeVal.Height
+                c = bmp.GetPixelColor(y, x)
+                copy.SetPixel(x - loc.X, y - loc.Y, c)
+            Next
+        Next
+
+        Return copy
     End Function
 End Module
--- studio/Rsharp_kit/MLkit/zzz.vb
+++ studio/Rsharp_kit/MLkit/zzz.vb
@@ -1,61 +1,68 @@
 ﻿#Region "Microsoft.VisualBasic::8203702a039cd9e50ea65c9ec110586e, studio\Rsharp_kit\MLkit\zzz.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 22
-    '    Code Lines: 17 (77.27%)
-    ' Comment Lines: 0 (0.00%)
-    '    - Xml Docs: 0.00%
-    ' 
-    '   Blank Lines: 5 (22.73%)
-    '     File Size: 599 B
+' Summaries:
 
 
-    ' Class zzz
-    ' 
-    '     Sub: onLoad
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 22
+'    Code Lines: 17 (77.27%)
+' Comment Lines: 0 (0.00%)
+'    - Xml Docs: 0.00%
+' 
+'   Blank Lines: 5 (22.73%)
+'     File Size: 599 B
+
+
+' Class zzz
+' 
+'     Sub: onLoad
+' 
+' /********************************************************************************/
 
 #End Region
 
+Imports System.Diagnostics.CodeAnalysis
 Imports Microsoft.VisualBasic.ApplicationServices.Development
 Imports Microsoft.VisualBasic.CommandLine
 Imports SMRUCC.Rsharp.Runtime.Interop
 
 <Assembly: RPackageModule>
+
+<Assembly: SuppressMessage("", "CA1416")>
+<Assembly: SuppressMessage("", "SYSLIB0021")>
+<Assembly: SuppressMessage("", "SYSLIB0022")>
+<Assembly: SuppressMessage("", "SYSLIB0003")>
+<Assembly: SuppressMessage("", "SYSLIB0004")>
 
 Public Class zzz
 
