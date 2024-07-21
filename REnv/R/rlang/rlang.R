14472c6312e0aa7c2d2d04ea6df4c675fadace9c
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-07-21 16:34:53 +0800

    set log file for the rlang interop

--- REnv/R/rlang/rlang.R
+++ REnv/R/rlang/rlang.R
@@ -4,19 +4,41 @@
 #' @param source the source file paths for provides the 
 #'    runtime environment for the specific input code. 
 #' 
-const rlang_interop = function(code, source = NULL, debug = FALSE) {
+const rlang_interop = function(code, source = NULL, debug = FALSE, workdir = NULL) {
     let code_save = tempfile(fileext = ".R");
     let load_deps = __sourcescript(source);
     let program   = translate_to_rlang(code);
-    let script_code <- paste([load_deps, "# --------end of load deps----------", program], sep = "\n\n");
+    let script_code <- paste([
+        "sink(file = 'rlang_interop.log');",
+        load_deps, 
+        "# --------end of load deps----------", 
+        program,
+        "sink();"
+    ], sep = "\n\n");
 
     if (debug) {
         print("translate R# code to rlang script:");
         cat(script_code);
         cat("\n\n");
     } else {
+        let current_wd = getwd();
+        let change_wd = nchar(workdir) > 0;
+        
+        if (change_wd) {
+            setwd(workdir);
+        }
+
+        print("run script at workspace:");
+        print(getwd());
+
         writeLines(script_code, con = code_save);
         system(`Rscript "${code_save}"`);
+
+        if (change_wd) {
+            setwd(current_wd);
+        }
+
+        print("end of rlang_interop~~~");
     }
 }
 
