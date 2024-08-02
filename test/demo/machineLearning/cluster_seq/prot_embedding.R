3021cfc3e729b19e7eae1b468716875c34b2e43d
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-08-03 01:27:50 +0800

    export embedding vector data

--- test/demo/machineLearning/cluster_seq/prot_embedding.R
+++ test/demo/machineLearning/cluster_seq/prot_embedding.R
@@ -10,9 +10,29 @@ for(let file in ["./cluster1.txt" "./cluster2.txt" "./cluster3.txt"]) {
     fa = as.data.frame(fa);
     print(fa);
     rownames(fa) = `${basename(file)}-${fa$id}`;
+    fa[,"label"] = basename(file);
     seqs = rbind(seqs, fa);
 }
 
+print(seqs);
+
 write.csv(seqs, file = file.path(@dir, "seqs.csv"));
 
-let sgt = SGT(alphabets = );
+let sgt = SGT(alphabets = bioseq.fasta::chars("Protein"));
+
+seqs[,"name"] = rownames(seqs);
+
+let vec = list();
+
+for(let seq in as.list( seqs, byrow = TRUE)) {
+    vec[[seq$name]] = as.list(fit(sgt, sequence = seq$seq));
+}
+
+# str(vec);
+
+vec = as.data.frame(vec, t= TRUE);
+rownames(vec) = seqs$name;
+
+print(vec);
+
+write.csv(vec, file = file.path(@dir, "vector.csv"));
