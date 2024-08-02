bbd3b3efe2404b6b46399be1b177e87a69ea773c
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-08-02 11:45:36 +0800

    run package msbuild

--- test/demo/machineLearning/cluster_seq/prot_embedding.R
+++ test/demo/machineLearning/cluster_seq/prot_embedding.R
@@ -3,11 +3,16 @@ require(GCModeller);
 imports "dataset" from "MLkit";
 imports "bioseq.fasta" from "seqtoolkit";
 
-let seqs = [];
+let seqs = NULL;
 
 for(let file in ["./cluster1.txt" "./cluster2.txt" "./cluster3.txt"]) {
     let fa = read.fasta(file);
-    
+    fa = as.data.frame(fa);
+    print(fa);
+    rownames(fa) = `${basename(file)}-${fa$id}`;
+    seqs = rbind(seqs, fa);
 }
 
+write.csv(seqs, file = file.path(@dir, "seqs.csv"));
 
+let sgt = SGT();
