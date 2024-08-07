f790e7f0cbd4d7a7d3762d54d69bd05fa8fbf6d8
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-08-07 14:25:32 +0800

    configs for the missing nuget, save xlsx test success

--- test/demo/excel/xlsx.R
+++ test/demo/excel/xlsx.R
@@ -11,11 +11,15 @@ write.xlsx(data.frame(
 	sheetName = "hello_world"
 );
 
-write.xlsx(data.frame(
+write.xlsx( list(aaaaa = data.frame(
 	col1 = [1,2,3,4,5], 
 	text = "hello", 
 	target = ["world", "word", "package", "ABCDEFG", "this is a test string!"
-]), 
+]), sheet333333 = data.frame(
+	col1 = 1:10,
+	col2 = TRUE,
+	col5 = ["sfa","s","dad","ad","ee","ee","eesd","asdas","da","sd"]
+)), 
 	file = "./test.xlsx",
-	sheetName = "hello_world"
+	row.names = FALSE
 );
