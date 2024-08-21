a271ad95395675bfb5f735f1b4fc660c5d7d8eef
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-08-21 13:21:00 +0800

    Update knn_iris.R

--- test/demo/clustering/knn_iris.R
+++ test/demo/clustering/knn_iris.R
@@ -9,6 +9,10 @@ set.seed(123);
 
 let [training, test] = split_training_test(bezdekIris, ratio = 0.7);
 let predictions = knn(train = training[, -"class"], 
-    test = test[, -"class"], cl = training$class, k = 3); 
+    test = test[, -"class"], cl = training$class, k = 16, prob = TRUE); 
 
 print(predictions);
+print(test$class);
+
+print(predictions == test$class);
+print(attr(predictions,"prob"));
