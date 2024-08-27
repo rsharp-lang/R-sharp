aec7f0ee971817af63ef5d65af8bf39a440133c7
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-08-27 17:23:16 +0800

    matrix save/load test success

--- test/unitTest/matrix_test/matrix_save.R
+++ test/unitTest/matrix_test/matrix_save.R
@@ -7,11 +7,29 @@ setwd(@dir);
 print(rawdata);
 
 let m = as.matrix(rawdata);
+# let [W,H] = nmf(m, rank = 2);
 
-print(m);
+# W = as.data.frame(W);
+# W[,"x1"] = as.integer(W$x1);
+# W[,"x2"] = as.integer(W$x2);
+
+# print(W);
+
+print(sum(as.data.frame(m)[,1]));
 
 writeBin(m, con = "./wine.dat");
 
+m=NULL;
 m = readBin("./wine.dat", what = "LA_mat");
 
-print(m);
+# W=NULL;
+# H=NULL;
+
+# [W,H] = nmf(m, rank = 2);
+
+# W = as.data.frame(W);
+# W[,"x1"] = as.integer(W$x1);
+# W[,"x2"] = as.integer(W$x2);
+
+# print(W);
+print(sum(as.data.frame(m)[,1]));
