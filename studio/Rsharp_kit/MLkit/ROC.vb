0ac3732c3402623ee07f87ea896a3092daa80ad1
谢桂纲 <gg.xie@bionovogene.com>
2024-10-12 13:42:24 +0800

    make bugs fixed of the possible NA value in AUC evaluation

--- studio/Rsharp_kit/MLkit/ROC.vb
+++ studio/Rsharp_kit/MLkit/ROC.vb
@@ -86,11 +86,17 @@ Public Class ROC : Inherits JavaScriptObject
         Get
             Dim TPR = sensibility
             Dim FPR = Me.FPR
+            ' 20241012
+            ' make both data be sorted via the TPR
+            Dim sort = TPR.Zip(FPR) _
+                .Where(Function(z)
+                           Return Not (z.First.IsNaNImaginary OrElse z.Second.IsNaNImaginary)
+                       End Function) _
+                .OrderBy(Function(z) z.First) _
+                .ToArray
 
-            With TPR.Select(Function(x, i) (x, i)).OrderBy(Function(t) t.x).ToArray
-                TPR = .Select(Function(t) t.x).ToArray
-                FPR = .Select(Function(t) FPR(t.i)).ToArray
-            End With
+            TPR = sort.Select(Function(a) a.First).ToArray
+            FPR = sort.Select(Function(a) a.Second).ToArray
 
             Return (100 - Evaluation.SimpleAUC(TPR, FPR)) / 100
         End Get
