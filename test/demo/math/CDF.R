ec7557765166c95a9a80de516e86b41da60d4e6d
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-10-16 19:32:14 +0800

    fix of the type cast error

--- test/demo/math/CDF.R
+++ test/demo/math/CDF.R
@@ -5,11 +5,11 @@ x <- seq(-10, 10, by = .1);
 # Choose the mean as 2.5 and standard deviation as 0.5.
 y <- dnorm(x, mean = 2.5, sd = 0.5);
 
-bitmap(file = `${@dir}/dnorm.png`) {
+pdf(file = `${@dir}/dnorm.pdf`) {
 	plot(x, y, background = "white", grid.fill = "white", color = "steelblue", point_size = 16);
 }
 
-bitmap(file = `${@dir}/CDF.png`) {
+pdf(file = `${@dir}/CDF.pdf`) {
 	ecdf = CDF(x -> dnorm(x, mean = 2.5, sd = 2), [min(x), max(x)], resolution = 120);
 	
 	str(ecdf);
