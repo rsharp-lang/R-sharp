966cb0268eb67be62b714766aaec7aeb8111cd17
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-10-31 14:44:51 +0800

    parse the yaml documents

--- studio/Rsharp_kit/devkit/yamlTool.vb
+++ studio/Rsharp_kit/devkit/yamlTool.vb
@@ -1,7 +1,27 @@
 ﻿
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.MIME.text.yaml.Grammar
+Imports Microsoft.VisualBasic.MIME.text.yaml.Syntax
 Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.Rsharp.Runtime
+Imports SMRUCC.Rsharp.Runtime.Interop
+Imports RInternal = SMRUCC.Rsharp.Runtime.Internal
 
 <Package("yaml")>
 Module yamlTool
 
+    <ExportAPI("parse")>
+    <RApiReturn(GetType(YamlStream))>
+    Public Function parse(yaml As String, Optional env As Environment = Nothing) As Object
+        Dim input As New TextInput(yaml)
+        Dim success As Boolean = Nothing
+        Dim parser As New YamlParser()
+        Dim yamlStream As YamlStream = parser.ParseYamlStream(input, success)
+
+        If success Then
+            Return yamlStream
+        End If
+
+        Return RInternal.debug.stop(parser.BuildErrorMessages.ToArray, env)
+    End Function
 End Module
