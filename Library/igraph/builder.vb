4941bc52257323941ea936edb3fe166d8a04e8c4
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-11-23 22:09:23 +0800

    make improvements of the debug view

--- Library/igraph/builder.vb
+++ Library/igraph/builder.vb
@@ -1,60 +1,62 @@
 ﻿#Region "Microsoft.VisualBasic::79cb9c89fe5d6687a645cdcba9b4c9eb, Library\igraph\builder.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 131
-    '    Code Lines: 83 (63.36%)
-    ' Comment Lines: 29 (22.14%)
-    '    - Xml Docs: 86.21%
-    ' 
-    '   Blank Lines: 19 (14.50%)
-    '     File Size: 5.19 KB
+' Summaries:
 
 
-    ' Module builder
-    ' 
-    '     Function: fromCorDataframe, FromCorrelations, SimilarityGraph
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 131
+'    Code Lines: 83 (63.36%)
+' Comment Lines: 29 (22.14%)
+'    - Xml Docs: 86.21%
+' 
+'   Blank Lines: 19 (14.50%)
+'     File Size: 5.19 KB
+
+
+' Module builder
+' 
+'     Function: fromCorDataframe, FromCorrelations, SimilarityGraph
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Data.GraphTheory
 Imports Microsoft.VisualBasic.Data.visualize
 Imports Microsoft.VisualBasic.Data.visualize.Network.FileStream.Generic
 Imports Microsoft.VisualBasic.Data.visualize.Network.Graph
+Imports Microsoft.VisualBasic.Emit.Delegates
 Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Math.DataFrame
 Imports Microsoft.VisualBasic.Scripting.MetaData
@@ -180,5 +182,16 @@ Module builder
     <RApiReturn(GetType(NetworkGraph))>
     Public Function SimilarityGraph(x As DistanceMatrix, Optional cutoff As Double = 0.6) As Object
         Return x.BuildNetwork(cutoff)
+    End Function
+
+    <ExportAPI("spare_graph")>
+    Public Function clone_sparegraph(g As Object, Optional env As Environment = Nothing) As Object
+        If g Is Nothing Then
+            Return Nothing
+        End If
+
+        If g.GetType.ImplementInterface(Of SparseGraph.ISparseGraph) Then
+
+        End If
     End Function
 End Module
