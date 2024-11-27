b6749fd2559bd2e87572fd5ab2bbfaf4b0dd6f3a
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-11-27 17:07:56 +0800

    Update color_test.R

--- REnv/test/color_test.R
+++ REnv/test/color_test.R
@@ -3,9 +3,10 @@ require(utils);
 let data = readRData(system.file("data/paletteer/palettes_d.rda", package = "REnv"));
 
 print(names(data$palettes_d));
+print(names(data$palettes_d$ggthemes));
 
 # stop();
 
-str(data);
+# str(data);
 
-print(paletteer_c("ggthemes::Blue-Green Sequential", 30));
+print(paletteer_c("ggthemes::excel_Office_Theme", 30));
