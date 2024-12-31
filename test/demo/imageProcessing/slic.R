68676133f6ebc1c7a6356598b4277fc91d9329e9
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-12-31 17:49:29 +0800

    fix of the cluster source errir

--- test/demo/imageProcessing/slic.R
+++ test/demo/imageProcessing/slic.R
@@ -1,14 +1,14 @@
 imports "bitmap" from "MLkit";
 
 let data = readImage(file.path(@dir, "lena.jpg" ));
-let regions = bitmap::slic(data,region_size = 0.05,
-                         iterations = 2);
+let regions = bitmap::slic(data,region_size = 0.1,
+                         iterations = 1);
 
 regions = as.data.frame(regions);
 
 setwd(@dir);
 
 bitmap(file = "./lena.png") {
-    plot(regions$x, regions$y, class = "cluster"  );
+    plot(regions$x, regions$y, class = regions$cluster  );
 };
 
