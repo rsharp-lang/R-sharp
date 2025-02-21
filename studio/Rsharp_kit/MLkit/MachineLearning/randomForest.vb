c6e60b9b069cae5dc16a745d5cbbca6060279277
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-02-21 17:33:10 +0800

    needs a randomForest tool

---
+++ studio/Rsharp_kit/MLkit/MachineLearning/randomForest.vb
@@ -0,0 +1,21 @@
+﻿
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Scripting.MetaData
+
+<Package("randomForest")>
+Public Module randomForest
+
+    <ExportAPI("randomForest")>
+    Public Function randomForest() As Object
+
+    End Function
+
+    Public Function importance() As Object
+
+    End Function
+
+    Public Function varImpPlot()
+
+    End Function
+
+End Module
