1952249883b9c43c75d4b2c5d81d1250da7ff46a
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-02-22 12:26:29 +0800

    create randomForest

--- studio/Rsharp_kit/MLkit/MachineLearning/randomForest.vb
+++ studio/Rsharp_kit/MLkit/MachineLearning/randomForest.vb
@@ -1,13 +1,21 @@
 ﻿
 Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.MachineLearning.ComponentModel.StoreProcedure
+Imports Microsoft.VisualBasic.MachineLearning.RandomForests
 Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.Rsharp.Runtime
+Imports SMRUCC.Rsharp.Runtime.Interop
 
 <Package("randomForest")>
 Public Module randomForest
 
     <ExportAPI("randomForest")>
-    Public Function randomForest() As Object
-
+    <RApiReturn(GetType(Result))>
+    Public Function randomForest(x As MLDataFrame, Optional env As Environment = Nothing) As Object
+        Dim data As New Data(x)
+        Dim tree As New RanFog
+        Dim result As Result = tree.Run(data)
+        Return result
     End Function
 
     Public Function importance() As Object
