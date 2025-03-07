96e21ef134e5adbbee59f2a219daf1a7191d1ee4
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-03-07 13:22:16 +0800

    implementns of the matrix interfacve

--- test/demo/clustering/MDS.R
+++ test/demo/clustering/MDS.R
@@ -5,6 +5,12 @@ print(bezdekIris);
 let class = bezdekIris$class;
 bezdekIris[,"class"] =NULL;
 
+rownames(bezdekIris) = unique.names(class);
+
 let dist_scale = dist(bezdekIris);
 
 print(dist_scale);
+
+let mds_result = cmdscale(dist_scale, k = 2);
+
+print(mds_result );
