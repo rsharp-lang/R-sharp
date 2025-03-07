568e972ea534367d6228346e182c68c26054e2b4
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-03-07 13:41:18 +0800

    run test success

--- test/demo/clustering/MDS.R
+++ test/demo/clustering/MDS.R
@@ -14,3 +14,7 @@ print(dist_scale);
 let mds_result = cmdscale(dist_scale, k = 2);
 
 print(mds_result );
+
+bitmap(file = file.path(@dir,"bezdekIris_mds.png"), bg = "white") {
+    plot(mds_result$D1, mds_result$D2, class = class, grid.fill = "white");
+}
