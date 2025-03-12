b1baf3e60315340c34aa44c6a7ae7799fb1df2a1
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-03-12 13:08:19 +0800

    how to create a generator model

---
+++ R#/Runtime/Internal/objects/dataset/generator.vb
@@ -0,0 +1,22 @@
+﻿Imports SMRUCC.Rsharp.Runtime.Components
+Imports SMRUCC.Rsharp.Runtime.Components.[Interface]
+
+Namespace Runtime.Internal.Object
+
+    ''' <summary>
+    ''' A generator function wrapper
+    ''' </summary>
+    Public Class generator : Inherits RsharpDataObject
+
+        Public Property fun As RFunction
+        Public ReadOnly Property current As Object
+
+        Dim generator As IEnumerator
+
+        Public Function moveNext() As Boolean
+            _current = generator.Current
+            Return generator.MoveNext
+        End Function
+
+    End Class
+End Namespace
