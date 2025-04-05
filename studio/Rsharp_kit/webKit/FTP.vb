e4f9bed6ab4e7613c192e4f84826caa6b3fdf3aa
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-04-05 16:40:25 +0800

    ftp context data

--- studio/Rsharp_kit/webKit/FTP.vb
+++ studio/Rsharp_kit/webKit/FTP.vb
@@ -162,27 +162,4 @@ Module FTP
     End Function
 End Module
 
-Public Class FtpContext
 
-    Public Property username As String
-    Public Property password As String
-    Public Property server As String
-
-    Public Function CreateRequest(dir As String) As FtpWebRequest
-        Dim ftpContext As String = $"ftp://{server}/{dir}"
-#Disable Warning SYSLIB0014 ' 类型或成员已过时
-        Dim request As FtpWebRequest = DirectCast(WebRequest.Create(ftpContext), FtpWebRequest)
-#Enable Warning SYSLIB0014 ' 类型或成员已过时
-
-        If Not (username.StringEmpty OrElse password.StringEmpty) Then
-            request.Credentials = New NetworkCredential(username, password)
-        End If
-
-        Return request
-    End Function
-
-    Public Overrides Function ToString() As String
-        Return $"{username Or "anonymous".AsDefault}@ftp://{server}"
-    End Function
-
-End Class
