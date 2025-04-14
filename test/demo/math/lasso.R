d552a4d96f93d8824f6faf5b0daca16b65939a17
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-04-14 09:13:23 +0800

    create xml dev notes

---
+++ test/demo/math/lasso.R
@@ -0,0 +1,9 @@
+let data = read.csv("G:\GCModeller\src\R-sharp\REnv\data\diabetes.data", row.names = NULL, check.names = FALSE, tsv = TRUE);
+
+# print(data);
+print(data[,-11]);
+print(data[,11]);
+
+let fit = lasso(data[,-11], data[,11]);
+
+str(as.list(fit));
