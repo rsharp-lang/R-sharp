19c1e05417f20fe1b53ef4eb2a24d04103e6a79e
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-08-10 13:36:29 +0800

    try to handling of the ipc error

--- snowFall/Context/RPC/MasterContext.vb
+++ snowFall/Context/RPC/MasterContext.vb
@@ -1,69 +1,71 @@
 ﻿#Region "Microsoft.VisualBasic::06b4b7b0e256ce2f3539aab659213459, snowFall\Context\RPC\MasterContext.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 249
-    '    Code Lines: 158 (63.45%)
-    ' Comment Lines: 51 (20.48%)
-    '    - Xml Docs: 62.75%
-    ' 
-    '   Blank Lines: 40 (16.06%)
-    '     File Size: 9.47 KB
+' Summaries:
 
 
-    '     Class MasterContext
-    ' 
-    '         Properties: port, Protocol
-    ' 
-    '         Constructor: (+1 Overloads) Sub New
-    ' 
-    '         Function: GetSymbol, pop, popLoopSymbol, (+2 Overloads) popSymbol, PostResult
-    '                   SetSymbolLogTempDir, ToString
-    ' 
-    '         Sub: (+2 Overloads) Dispose, push, Register, Run
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 249
+'    Code Lines: 158 (63.45%)
+' Comment Lines: 51 (20.48%)
+'    - Xml Docs: 62.75%
+' 
+'   Blank Lines: 40 (16.06%)
+'     File Size: 9.47 KB
+
+
+'     Class MasterContext
+' 
+'         Properties: port, Protocol
+' 
+'         Constructor: (+1 Overloads) Sub New
+' 
+'         Function: GetSymbol, pop, popLoopSymbol, (+2 Overloads) popSymbol, PostResult
+'                   SetSymbolLogTempDir, ToString
+' 
+'         Sub: (+2 Overloads) Dispose, push, Register, Run
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
+Imports System.IO
 Imports System.Runtime.CompilerServices
 Imports Darwinism.HPC.Parallel
 Imports Darwinism.IPC.Networking.HTTP
 Imports Darwinism.IPC.Networking.Protocols.Reflection
 Imports Darwinism.IPC.Networking.Tcp
+Imports Microsoft.VisualBasic.Net.Http
 Imports Microsoft.VisualBasic.Parallel
 Imports SMRUCC.Rsharp.Runtime
 Imports SMRUCC.Rsharp.Runtime.Components
@@ -261,7 +263,7 @@ Namespace Context.RPC
 
                 Return New DataPipe(NetResponse.RFC_NOT_FOUND)
             Else
-                Return New DataPipe(data)
+                Return New DataPipe(New MemoryStream(data).Zip)
             End If
         End Function
 
--- snowFall/Context/RPC/RemoteEnvironment.vb
+++ snowFall/Context/RPC/RemoteEnvironment.vb
@@ -1,55 +1,55 @@
 ﻿#Region "Microsoft.VisualBasic::029f2df7866a617f7cbef85ab5aaab70, snowFall\Context\RPC\RemoteEnvironment.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 164
-    '    Code Lines: 106 (64.63%)
-    ' Comment Lines: 33 (20.12%)
-    '    - Xml Docs: 84.85%
-    ' 
-    '   Blank Lines: 25 (15.24%)
-    '     File Size: 6.15 KB
+' Summaries:
 
 
-    '     Class RemoteEnvironment
-    ' 
-    '         Constructor: (+1 Overloads) Sub New
-    '         Function: buffer404, FindSymbol, getRemoteSymbol, loadRemoteSymbol
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 164
+'    Code Lines: 106 (64.63%)
+' Comment Lines: 33 (20.12%)
+'    - Xml Docs: 84.85%
+' 
+'   Blank Lines: 25 (15.24%)
+'     File Size: 6.15 KB
+
+
+'     Class RemoteEnvironment
+' 
+'         Constructor: (+1 Overloads) Sub New
+'         Function: buffer404, FindSymbol, getRemoteSymbol, loadRemoteSymbol
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -145,13 +145,13 @@ Namespace Context.RPC
                 Thread.Sleep(10)
 
                 Try
-                    Using buffer As New MemoryStream(resp.ChunkBuffer)
-                        If resp.Protocol = HTTP_RFC.RFC_NOT_FOUND Then
-                            Return buffer404(name)
-                        Else
+                    If resp.Protocol = HTTP_RFC.RFC_NOT_FOUND Then
+                        Return buffer404(name)
+                    Else
+                        Using buffer As MemoryStream = resp.ChunkBuffer.UnZipStream
                             Return loadRemoteSymbol(name, buffer, symbols, verbose)
-                        End If
-                    End Using
+                        End Using
+                    End If
                 Catch ex As Exception
                     [error] = ex
 
