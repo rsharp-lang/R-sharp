ff75afdc44c1ab17142dad2e43ae3109e5a1f5c0
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-10-24 14:36:11 +0800

    run test success

--- test/demo/contourTracing/alpha_shapes.R
+++ test/demo/contourTracing/alpha_shapes.R
@@ -6,8 +6,11 @@ setwd(@dir);
 let outline = function(data) {
 	data.frame(x = data$X, y= data$Y) |> concaveHull(as.polygon = TRUE);
 }
-let a = read.csv("./region_11.csv", row.names = NULL) |> outline;
+
+const a = read.csv("./region_11.csv", row.names = NULL) |> outline;
+const b = read.csv("./region_2.csv", row.names = NULL) |> outline;
+const c = read.csv("./region_9.csv", row.names = NULL) |> outline;
 
 bitmap(file = "./region_alpha_shapes.png") {
-	charts::fillPolygon(a, reverse = TRUE);
+	charts::fillPolygon(list(A = a, B = b, C = c), reverse = TRUE);
 }
