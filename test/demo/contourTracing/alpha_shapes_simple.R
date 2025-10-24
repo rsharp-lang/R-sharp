8560d168f9509698bd895c80edf6841276a7ccfd
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-10-24 14:45:42 +0800

    not working for random points

--- test/demo/contourTracing/alpha_shapes_simple.R
+++ test/demo/contourTracing/alpha_shapes_simple.R
@@ -1,5 +1,5 @@
-let x = runif(100,20 , 900);
-let y = runif(100,20, 900);
+let x = runif(500,20 , 900);
+let y = runif(500,20, 900);
 let outline = data.frame(x, y) |> concaveHull(r = 5, as.polygon = TRUE);
 
 print(outline);
