9f1cd004395a685fae0fb369775b481477976ef8
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-11-15 02:22:42 +0800

    fix of the vector cast error

--- R#/Runtime/Vectorization/CLRVector.vb
+++ R#/Runtime/Vectorization/CLRVector.vb
@@ -1,57 +1,57 @@
 ﻿#Region "Microsoft.VisualBasic::e19fc0b4420b289360687f6c9b8ff107, R#\Runtime\Vectorization\CLRVector.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 685
-    '    Code Lines: 534 (77.96%)
-    ' Comment Lines: 82 (11.97%)
-    '    - Xml Docs: 67.07%
-    ' 
-    '   Blank Lines: 69 (10.07%)
-    '     File Size: 27.91 KB
+' Summaries:
 
 
-    '     Module CLRVector
-    ' 
-    '         Function: asCharacter, asDate, asFloat, asInteger, (+2 Overloads) asLogical
-    '                   asLong, asNumeric, asObject, asRawByte, asScalarCharacter
-    '                   asScalarLogical, castVector, fromObjectCollection, parseString, safeCharacters
-    '                   testNull
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 685
+'    Code Lines: 534 (77.96%)
+' Comment Lines: 82 (11.97%)
+'    - Xml Docs: 67.07%
+' 
+'   Blank Lines: 69 (10.07%)
+'     File Size: 27.91 KB
+
+
+'     Module CLRVector
+' 
+'         Function: asCharacter, asDate, asFloat, asInteger, (+2 Overloads) asLogical
+'                   asLong, asNumeric, asObject, asRawByte, asScalarCharacter
+'                   asScalarLogical, castVector, fromObjectCollection, parseString, safeCharacters
+'                   testNull
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -155,7 +155,7 @@ Namespace Runtime.Vectorization
                                 If s = "" Then
                                     Return 0
                                 ElseIf s = "NaN" OrElse s = "NA" Then
-                                    Call "NA or NaN value was found when cast input as integer".Warning
+                                    Call "NA or NaN value was found when cast input as integer".warning
                                     Return 0
                                 ElseIf s.IndexOf("."c) > -1 Then
                                     Return CLng(Val(s))
@@ -285,6 +285,8 @@ Namespace Runtime.Vectorization
                 Else
                     typeof_x = x.GetType
                 End If
+            ElseIf x.GetType.ImplementInterface(Of ICollection) AndAlso DirectCast(x, ICollection).Count = 1 Then
+                Return asCharacter(DirectCast(x, ICollection)(0))
             End If
 
             If TypeOf x Is String Then
