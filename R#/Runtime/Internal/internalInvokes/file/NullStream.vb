a0dc43c8c88f3dcfbdcb87a0176e8461abf87bb9
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-11-27 23:21:58 +0800

    nullfile

---
+++ R#/Runtime/Internal/internalInvokes/file/NullStream.vb
@@ -0,0 +1,62 @@
+﻿Imports System.IO
+
+Namespace Runtime.Internal.Invokes
+
+    ''' <summary>
+    ''' 提供一个不存储任何数据的 Stream。所有写入的数据都被丢弃，所有读取操作都返回0。
+    ''' 这是对类 Unix 系统上的 /dev/null 或 Windows 上的 NUL 设备的 .NET 模拟。
+    ''' </summary>
+    Friend NotInheritable Class NullStream
+        Inherits Stream
+
+        ' 单例模式，因为 NullStream 不需要任何状态，一个实例就足够了
+        Public Shared ReadOnly Instance As New NullStream()
+
+        Private Sub New()
+            ' 私有构造函数以强制使用单例
+        End Sub
+
+        ' 重写核心属性
+        Public Overrides ReadOnly Property CanRead As Boolean = True
+        Public Overrides ReadOnly Property CanSeek As Boolean = False
+        Public Overrides ReadOnly Property CanWrite As Boolean = True
+        Public Overrides ReadOnly Property Length As Long = 0
+
+        Public Overrides Property Position As Long
+            Get
+                Return 0
+            End Get
+            Set(value As Long)
+
+            End Set
+        End Property
+
+        ' 重写核心方法
+        Public Overrides Sub Flush()
+            ' 什么都不做，因为没有数据需要刷新
+        End Sub
+
+        Public Overrides Function Read(buffer() As Byte, offset As Integer, count As Integer) As Integer
+            ' 立即返回0，表示已到达流的末尾
+            Return 0
+        End Function
+
+        Public Overrides Function Seek(offset As Long, origin As SeekOrigin) As Long
+            Return 0
+        End Function
+
+        Public Overrides Sub SetLength(value As Long)
+        End Sub
+
+        Public Overrides Sub Write(buffer() As Byte, offset As Integer, count As Integer)
+            ' 什么都不做，数据被丢弃
+            ' 为了符合 Stream 的约定，我们不需要抛出异常
+            ' 如果需要，可以在这里记录被丢弃的字节数，但通常没有必要
+        End Sub
+
+        Protected Overrides Sub Dispose(disposing As Boolean)
+            ' 没有非托管资源需要释放，所以什么都不做
+            ' 但仍然需要提供这个方法以符合基类契约
+        End Sub
+    End Class
+End Namespace
---
+++ R#/Runtime/Internal/internalInvokes/file/stddev.vb
@@ -0,0 +1,142 @@
+﻿Imports System.IO
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+
+Namespace Runtime.Internal.Invokes
+
+    Module stddev
+
+        ''' <summary>
+        ''' ### Display Connections
+        ''' 
+        ''' Display aspects of connections.
+        ''' </summary>
+        ''' <returns>
+        ''' stdin(), stdout() and stderr() return connection objects.
+        ''' showConnections returns a character matrix of information with a row for each connection,
+        ''' by default only for open non-standard connections.
+        ''' getConnection returns a connection object, or NULL.
+        ''' </returns>
+        ''' <remarks>
+        ''' stdin(), stdout() and stderr() are standard connections corresponding to input, output and 
+        ''' error on the console respectively (and not necessarily to file streams). They are text-mode 
+        ''' connections of class "terminal" which cannot be opened or closed, and are read-only, write-only 
+        ''' and write-only respectively. The stdout() and stderr() connections can be re-directed by sink 
+        ''' (and in some circumstances the output from stdout() can be split: see the help page).
+        ''' The encoding for stdin() when redirected can be set by the command-line flag --encoding.
+        ''' nullfile() returns filename of the null device ("/dev/null" on Unix, "nul:" on Windows).
+        ''' showConnections returns a matrix of information. If a connection object has been lost or forgotten,
+        ''' getConnection will take a row number from the table and return a connection object for that connection,
+        ''' which can be used to close the connection, for example. However, if there is no R level object 
+        ''' referring to the connection it will be closed automatically at the next garbage collection 
+        ''' (except for gzcon connections).
+        ''' closeAllConnections closes (and destroys) all user connections, restoring all sink diversions as it does so.
+        ''' isatty returns true if the connection is one of the class "terminal" connections and it is apparently 
+        ''' connected to a terminal, otherwise false. This may not be reliable in embedded applications, 
+        ''' including GUI consoles.
+        ''' getAllConnections returns a sequence of integer connection descriptors for use with getConnection,
+        ''' corresponding to the row names of the table returned by showConnections(all = TRUE).
+        ''' 
+        ''' stdin() refers to the ‘console’ and not to the C-level ‘stdin’ of the process. The distinction matters 
+        ''' in GUI consoles (which may not have an active ‘stdin’, and if they do it may not be connected to 
+        ''' console input), and also in embedded applications. If you want access to the C-level file stream 
+        ''' ‘stdin’, use file("stdin").
+        ''' When R is reading a script from a file, the file is the ‘console’: this is traditional usage to allow 
+        ''' in-line data (see ‘An Introduction to R’ for an example).
+        ''' </remarks>
+        <ExportAPI("stderr")>
+        Public Function stderr() As Stream
+            Return Console.OpenStandardError
+        End Function
+
+        ''' <summary>
+        ''' ### Display Connections
+        ''' 
+        ''' Display aspects of connections.
+        ''' </summary>
+        ''' <returns>
+        ''' stdin(), stdout() and stderr() return connection objects.
+        ''' showConnections returns a character matrix of information with a row for each connection,
+        ''' by default only for open non-standard connections.
+        ''' getConnection returns a connection object, or NULL.
+        ''' </returns>
+        ''' <remarks>
+        ''' stdin(), stdout() and stderr() are standard connections corresponding to input, output and 
+        ''' error on the console respectively (and not necessarily to file streams). They are text-mode 
+        ''' connections of class "terminal" which cannot be opened or closed, and are read-only, write-only 
+        ''' and write-only respectively. The stdout() and stderr() connections can be re-directed by sink 
+        ''' (and in some circumstances the output from stdout() can be split: see the help page).
+        ''' The encoding for stdin() when redirected can be set by the command-line flag --encoding.
+        ''' nullfile() returns filename of the null device ("/dev/null" on Unix, "nul:" on Windows).
+        ''' showConnections returns a matrix of information. If a connection object has been lost or forgotten,
+        ''' getConnection will take a row number from the table and return a connection object for that connection,
+        ''' which can be used to close the connection, for example. However, if there is no R level object 
+        ''' referring to the connection it will be closed automatically at the next garbage collection 
+        ''' (except for gzcon connections).
+        ''' closeAllConnections closes (and destroys) all user connections, restoring all sink diversions as it does so.
+        ''' isatty returns true if the connection is one of the class "terminal" connections and it is apparently 
+        ''' connected to a terminal, otherwise false. This may not be reliable in embedded applications, 
+        ''' including GUI consoles.
+        ''' getAllConnections returns a sequence of integer connection descriptors for use with getConnection,
+        ''' corresponding to the row names of the table returned by showConnections(all = TRUE).
+        ''' 
+        ''' stdin() refers to the ‘console’ and not to the C-level ‘stdin’ of the process. The distinction matters 
+        ''' in GUI consoles (which may not have an active ‘stdin’, and if they do it may not be connected to 
+        ''' console input), and also in embedded applications. If you want access to the C-level file stream 
+        ''' ‘stdin’, use file("stdin").
+        ''' When R is reading a script from a file, the file is the ‘console’: this is traditional usage to allow 
+        ''' in-line data (see ‘An Introduction to R’ for an example).
+        ''' </remarks>
+        <ExportAPI("stdout")>
+        Public Function stdout() As Stream
+            Return Console.OpenStandardOutput
+        End Function
+
+        ''' <summary>
+        ''' ### Display Connections
+        ''' 
+        ''' Display aspects of connections.
+        ''' </summary>
+        ''' <returns>
+        ''' stdin(), stdout() and stderr() return connection objects.
+        ''' showConnections returns a character matrix of information with a row for each connection,
+        ''' by default only for open non-standard connections.
+        ''' getConnection returns a connection object, or NULL.
+        ''' </returns>
+        ''' <remarks>
+        ''' stdin(), stdout() and stderr() are standard connections corresponding to input, output and 
+        ''' error on the console respectively (and not necessarily to file streams). They are text-mode 
+        ''' connections of class "terminal" which cannot be opened or closed, and are read-only, write-only 
+        ''' and write-only respectively. The stdout() and stderr() connections can be re-directed by sink 
+        ''' (and in some circumstances the output from stdout() can be split: see the help page).
+        ''' The encoding for stdin() when redirected can be set by the command-line flag --encoding.
+        ''' nullfile() returns filename of the null device ("/dev/null" on Unix, "nul:" on Windows).
+        ''' showConnections returns a matrix of information. If a connection object has been lost or forgotten,
+        ''' getConnection will take a row number from the table and return a connection object for that connection,
+        ''' which can be used to close the connection, for example. However, if there is no R level object 
+        ''' referring to the connection it will be closed automatically at the next garbage collection 
+        ''' (except for gzcon connections).
+        ''' closeAllConnections closes (and destroys) all user connections, restoring all sink diversions as it does so.
+        ''' isatty returns true if the connection is one of the class "terminal" connections and it is apparently 
+        ''' connected to a terminal, otherwise false. This may not be reliable in embedded applications, 
+        ''' including GUI consoles.
+        ''' getAllConnections returns a sequence of integer connection descriptors for use with getConnection,
+        ''' corresponding to the row names of the table returned by showConnections(all = TRUE).
+        ''' 
+        ''' stdin() refers to the ‘console’ and not to the C-level ‘stdin’ of the process. The distinction matters 
+        ''' in GUI consoles (which may not have an active ‘stdin’, and if they do it may not be connected to 
+        ''' console input), and also in embedded applications. If you want access to the C-level file stream 
+        ''' ‘stdin’, use file("stdin").
+        ''' When R is reading a script from a file, the file is the ‘console’: this is traditional usage to allow 
+        ''' in-line data (see ‘An Introduction to R’ for an example).
+        ''' </remarks>
+        <ExportAPI("stdin")>
+        Public Function stdin() As Stream
+            Return Console.OpenStandardInput
+        End Function
+
+        <ExportAPI("nullfile")>
+        Public Function nullfile() As Stream
+            Return NullStream.Instance
+        End Function
+    End Module
+End Namespace
