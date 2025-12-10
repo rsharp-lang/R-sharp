6ce10ff95c5348fdc22dff08a39bd073a7c4d295
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-12-11 03:26:20 +0800

    returns the error message

--- R#/Interpreter/ExecuteEngine/ExpressionSymbols/Operators/ValueAssignOperator/ValueAssignExpression.vb
+++ R#/Interpreter/ExecuteEngine/ExpressionSymbols/Operators/ValueAssignOperator/ValueAssignExpression.vb
@@ -1,60 +1,60 @@
 ﻿#Region "Microsoft.VisualBasic::3e6505c26acc644f17037c4f715951fa, R#\Interpreter\ExecuteEngine\ExpressionSymbols\Operators\ValueAssignOperator\ValueAssignExpression.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 389
-    '    Code Lines: 271 (69.67%)
-    ' Comment Lines: 62 (15.94%)
-    '    - Xml Docs: 69.35%
-    ' 
-    '   Blank Lines: 56 (14.40%)
-    '     File Size: 16.30 KB
+' Summaries:
 
 
-    '     Class ValueAssignExpression
-    ' 
-    '         Properties: expressionName, isByRef, symbolSize, targetSymbols, type
-    '                     value
-    ' 
-    '         Constructor: (+3 Overloads) Sub New
-    '         Function: assignSymbol, assignTuples, doValueAssign, DoValueAssign, Evaluate
-    '                   EvaluateValue, GetSymbol, getSymbols, setFromDataFrame, setFromObjectList
-    '                   setFromVector, ToString
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 389
+'    Code Lines: 271 (69.67%)
+' Comment Lines: 62 (15.94%)
+'    - Xml Docs: 69.35%
+' 
+'   Blank Lines: 56 (14.40%)
+'     File Size: 16.30 KB
+
+
+'     Class ValueAssignExpression
+' 
+'         Properties: expressionName, isByRef, symbolSize, targetSymbols, type
+'                     value
+' 
+'         Constructor: (+3 Overloads) Sub New
+'         Function: assignSymbol, assignTuples, doValueAssign, DoValueAssign, Evaluate
+'                   EvaluateValue, GetSymbol, getSymbols, setFromDataFrame, setFromObjectList
+'                   setFromVector, ToString
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -431,9 +431,21 @@ Namespace Interpreter.ExecuteEngine.ExpressionSymbols.Operators
                         If slotName.IsNullOrEmpty Then
                             Return RInternal.debug.stop("the slot member name to set value should not be nothing!", envir)
                         ElseIf slotName.Length = 1 Then
-                            Return DirectCast(obj, s4Reflector).setByName(slotName(0), value, envir)
+                            Dim err = DirectCast(obj, s4Reflector).setByName(slotName(0), value, envir)
+
+                            If TypeOf err Is Message Then
+                                Return err
+                            Else
+                                Return Nothing
+                            End If
                         Else
-                            Return DirectCast(obj, s4Reflector).setByName(slotName, value, envir)
+                            Dim err = DirectCast(obj, s4Reflector).setByName(slotName, value, envir)
+
+                            If TypeOf err Is Message Then
+                                Return err
+                            Else
+                                Return Nothing
+                            End If
                         End If
                     End If
                 Case Else
