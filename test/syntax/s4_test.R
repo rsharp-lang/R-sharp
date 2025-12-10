ae4f4677d1b63b40d0fcfebd66cf632512e4f492
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-12-11 03:27:25 +0800

    test s4 class object slot success

--- test/syntax/s4_test.R
+++ test/syntax/s4_test.R
@@ -4,7 +4,13 @@ setClass("test_opts", slots = list(
 
 let obj = new("test_opts", aaa = "123");
 
+print("before:");
+print(obj@aaa);
+
 obj@aaa = "223";
 
+print("after:");
 print(obj@aaa);
+
+print("test missing error:");
 print(obj@missing_error);
