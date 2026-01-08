db4c3e53f52ae000b738a94115c7e2d6eaa644b7
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-01-08 17:08:54 +0800

    make script copy to root

--- studio/R-terminal/InstallRscript.vb
+++ studio/R-terminal/InstallRscript.vb
@@ -8,12 +8,14 @@ Imports Microsoft.VisualBasic.Text
 ''' </summary>
 Module InstallRscript
 
+    Const LinuxRoot As String = "/usr/local/bin/"
+
     Public Function Install(rscript As String, installLinuxRoot As Boolean) As Boolean
         ' XXXX.R -> XXXX
         '
         ' $ # alias of R# XXXX.R in linux bash
         ' $ XXXX
-        Dim bash$ = If(installLinuxRoot, "/usr/local/bin/", rscript.ParentPath) & "/" & rscript.BaseName
+        Dim bash$ = If(installLinuxRoot, LinuxRoot, rscript.ParentPath) & "/" & rscript.BaseName
         Dim utf8 As Encoding = Encodings.UTF8WithoutBOM.CodePage
         Dim dirHelper As String = UNIX.GetLocationHelper
         Dim script As String = dirHelper & vbLf & "
@@ -26,6 +28,13 @@ dotnet ""{rscript}"" ""$app"" $cli" _
             .Replace("{rscript}", $"{App.HOME}/R#.dll")
 
         script = script.LineTokens.JoinBy(vbLf)
+
+        If installLinuxRoot Then
+            ' make copy of the script file
+            Call rscript _
+                .ReadAllLines _
+                .FlushAllLines($"{LinuxRoot}/{rscript.FileName}")
+        End If
 
         Return script.SaveTo(bash, utf8)
     End Function
