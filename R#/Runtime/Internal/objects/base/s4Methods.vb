0e28d900875962253b3f6b18e45950199848bf30
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-01-21 12:13:17 +0800

    make cast of boolean value

--- R#/Runtime/Internal/objects/base/s4Methods.vb
+++ R#/Runtime/Internal/objects/base/s4Methods.vb
@@ -1,54 +1,54 @@
 ﻿#Region "Microsoft.VisualBasic::e5920091921b775b35d4634f2acec2d6, R#\Runtime\Internal\objects\base\s4Methods.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 102
-    '    Code Lines: 82 (80.39%)
-    ' Comment Lines: 2 (1.96%)
-    '    - Xml Docs: 0.00%
-    ' 
-    '   Blank Lines: 18 (17.65%)
-    '     File Size: 4.21 KB
+' Summaries:
 
 
-    '     Module s4Methods
-    ' 
-    '         Function: conversionValue, (+2 Overloads) createObject, defineObject
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 102
+'    Code Lines: 82 (80.39%)
+' Comment Lines: 2 (1.96%)
+'    - Xml Docs: 0.00%
+' 
+'   Blank Lines: 18 (17.65%)
+'     File Size: 4.21 KB
+
+
+'     Module s4Methods
+' 
+'         Function: conversionValue, (+2 Overloads) createObject, defineObject
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -142,11 +142,12 @@ Namespace Runtime.Internal.Object.baseOp
             Select Case Microsoft.VisualBasic.Strings.Trim(type).ToLower
                 Case "numeric", "double" : val = CLRVector.asNumeric(val)
                 Case "integer" : val = CLRVector.asInteger(val)
-                Case "character" : val = CLRVector.asCharacter(val)
-                Case "raw" : val = CLRVector.asRawByte(val)
+                Case "character", "string" : val = CLRVector.asCharacter(val)
+                Case "raw", "byte" : val = CLRVector.asRawByte(val)
                 Case "list" : val = base.Rlist(val, env)
                 Case "float" : val = CLRVector.asFloat(val)
                 Case "long" : val = CLRVector.asLong(val)
+                Case "logical", "boolean", "bool" : val = CLRVector.asLogical(val)
                 Case Else
                     ' no conversion?
             End Select
