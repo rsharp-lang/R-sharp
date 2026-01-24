93f1d98935be1367c18a7484309ccbcdbeeb75bd
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-01-24 17:13:04 +0800

    Update native_r.R

--- REnv/R/rlang/native_r.R
+++ REnv/R/rlang/native_r.R
@@ -1,3 +1,7 @@
-const native_r = function(f, args = list(), native_R = getOption("native_rexec")) {
+const native_r = function(f, args = list(), source = NULL, debug = FALSE, native_R = getOption("native_rexec")) {
+    # make translate of the managed R# closure expression
+    # to the native R code
+    let script_code = f |> transform_rlang_source(source, debug = debug);    
 
+    
 }
