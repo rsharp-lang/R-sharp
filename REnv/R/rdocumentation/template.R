90c9a5bb9e1864dadcef0a550c9c158c33cf42c7
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-02-06 01:20:30 +0800

    get external template source via the options

--- REnv/R/rdocumentation/template.R
+++ REnv/R/rdocumentation/template.R
@@ -7,20 +7,27 @@
 #'   + clr_template: the html document template for the clr object which is exported from the .NET dll assembly files.
 #' 
 const __template = function() {
-	const R_syntax_js = getOption("r_syntax.js", default = "../../_assets/R_syntax.js");
-    const css_ref = `${dirname(R_syntax_js, fullpath = FALSE)}/page.css`;
-    const R_highlights = `${dirname(R_syntax_js, fullpath = FALSE)}/highlights.js`;
+	let R_syntax_js = getOption("r_syntax.js", default = "../../_assets/R_syntax.js");
+    let css_ref = `${dirname(R_syntax_js, fullpath = FALSE)}/page.css`;
+    let R_highlights = `${dirname(R_syntax_js, fullpath = FALSE)}/highlights.js`;
 
-	const template = "templates/Rdocumentation.html" 
-	|> system.file(package = "REnv") 
+	let doc_template = getOption("doc_template");
+	let clr_template = getOption("clr_template");
+
+	let template = ifelse(nchar(doc_template) == 0, 
+		system.file("templates/Rdocumentation.html", package = "REnv"), 
+		doc_template
+	)
 	|> readText()
 	|> gsub("%r_syntax%", R_syntax_js)
     |> gsub("%r_css%", css_ref)
     |> gsub("%r_highlights%", R_highlights)
 	;	
 
-	const clr_template = "templates/clr_template.html"
-	|> system.file(package = "REnv") 
+	let clr_template = ifelse(nchar(clr_template) == 0, 
+		system.file("templates/clr_template.html", package = "REnv"), 
+		clr_template
+	) 
 	|> readText()
 	|> gsub("%r_syntax%", R_syntax_js)
     |> gsub("%r_css%", css_ref)
