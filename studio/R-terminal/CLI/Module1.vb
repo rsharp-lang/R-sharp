eadb112772070989c0aaf9445a32a75abcc6e714
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-02-09 19:14:54 +0800

    read top n rows

---
+++ studio/R-terminal/CLI/Module1.vb
@@ -0,0 +1,32 @@
+﻿Imports System.IO
+Imports Microsoft.VisualBasic.CommandLine
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Language
+
+Partial Module CLI
+
+    <ExportAPI("--head")>
+    <Usage("--head <data.csv> [-n <n rows, default=15>]")>
+    Public Function head(file As String, args As CommandLine) As Integer
+        Dim n As Integer = args("-n") Or 15
+        Dim rows As New List(Of String)
+
+        Using s As Stream = file.Open(FileMode.Open, doClear:=False, [readOnly]:=True)
+            Dim text As New StreamReader(s)
+            Dim line As Value(Of String) = text.ReadLine
+
+            Call rows.Add(CStr(line))
+
+            Do While (line = text.ReadLine) IsNot Nothing
+                Call rows.Add(CStr(line))
+
+                If rows.Count > n Then
+                    Exit Do
+                End If
+            Loop
+        End Using
+
+
+    End Function
+
+End Module
