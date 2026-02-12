899ea3982eed6eca385aff85ff036745477f078c
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-02-13 03:22:23 +0800

    try to make bugs fixed for test int type

--- R#/Interpreter/ExecuteEngine/ExpressionSymbols/DataSet/Literal/SequenceLiteral.vb
+++ R#/Interpreter/ExecuteEngine/ExpressionSymbols/DataSet/Literal/SequenceLiteral.vb
@@ -1,58 +1,58 @@
 ﻿#Region "Microsoft.VisualBasic::26e2a0d8e2f42264a8c5993e5e561fe4, R#\Interpreter\ExecuteEngine\ExpressionSymbols\DataSet\Literal\SequenceLiteral.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 218
-    '    Code Lines: 161 (73.85%)
-    ' Comment Lines: 26 (11.93%)
-    '    - Xml Docs: 73.08%
-    ' 
-    '   Blank Lines: 31 (14.22%)
-    '     File Size: 7.95 KB
+' Summaries:
 
 
-    '     Class SequenceLiteral
-    ' 
-    '         Properties: expressionName, stackFrame, type
-    ' 
-    '         Constructor: (+1 Overloads) Sub New
-    '         Function: characterSeq, Evaluate, getChar, integerSeq, measureSequence
-    '                   numericSeq, ToString
-    ' 
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 218
+'    Code Lines: 161 (73.85%)
+' Comment Lines: 26 (11.93%)
+'    - Xml Docs: 73.08%
+' 
+'   Blank Lines: 31 (14.22%)
+'     File Size: 7.95 KB
+
+
+'     Class SequenceLiteral
+' 
+'         Properties: expressionName, stackFrame, type
+' 
+'         Constructor: (+1 Overloads) Sub New
+'         Function: characterSeq, Evaluate, getChar, integerSeq, measureSequence
+'                   numericSeq, ToString
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -136,10 +136,17 @@ Namespace Interpreter.ExecuteEngine.ExpressionSymbols.DataSets
                             Dim rtype As RType = RType.TypeOf(num)
                             Dim ntype As Type = rtype.raw
 
-                            If ntype Like RType.floats Then
-                                Return "num"
-                            ElseIf ntype Like RType.integers Then
+                            If ntype Like RType.integers Then
                                 Return "int"
+                            ElseIf ntype Like RType.floats Then
+                                ' 20260213 fix for test integer value
+                                Dim dbl As Double = CLRVector.asNumeric(num)(0)
+
+                                If dbl = System.Math.Truncate(dbl) Then
+                                    Return "int"
+                                Else
+                                    Return "num"
+                                End If
                             ElseIf ntype Like RType.characters Then
                                 Return "chr"
                             Else
