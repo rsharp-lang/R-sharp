3dfcff3216e51d324a6737643c94c5f16ce8b6d2
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-02-14 17:13:40 +0800

    make bugs fixed for vector cast error

---
+++ R#/ListVector.vb
@@ -0,0 +1,20 @@
+﻿Imports System.Runtime.CompilerServices
+Imports SMRUCC.Rsharp.Runtime.Internal.[Object]
+Imports SMRUCC.Rsharp.Runtime.Vectorization
+
+Public Module ListVector
+
+    <Extension>
+    Public Function as_character(list As list) As Dictionary(Of String, String)
+        If list Is Nothing OrElse list.slots.IsNullOrEmpty Then
+            Return New Dictionary(Of String, String)
+        Else
+            Return list.slots _
+                .ToDictionary(Function(a) a.Key,
+                              Function(a)
+                                  Return CLRVector.asScalarCharacter(a.Value)
+                              End Function)
+        End If
+    End Function
+
+End Module
