3eaf1f6fc5d5b930557f7a5d3507b5d8c6d48560
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-05-02 03:56:26 +0800

    make screen of the type exports

--- test/syntax/data/rda/pickle_test.R
+++ test/syntax/data/rda/pickle_test.R
@@ -2,6 +2,6 @@ let data = {"name": "Alice", "scores": [95, 87, 92], "active": True};
 
 str(data);
 
-pickle.dumps(data, file = relative_work("data.pkl"));
+pickle.dump(data, file = relative_work("data.pkl"));
 
 str(pickle.load(relative_work("data.pkl")));
