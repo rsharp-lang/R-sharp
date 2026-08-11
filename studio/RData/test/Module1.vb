eaf6c710c5d189ccb7f3ac328477288aa9cbdb69
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-08-12 06:09:00 +0800

    Simplify RDS test to summarize loaded samples
    本次改动对 studio/RData/test/Module1.vb测试程序做了大幅精简与整理。
    
    清理方面：
    -删除了文件顶部自动生成的注释头（作者、版权、统计信息等 Region块）。
    -移除了 listIO、testRealExample、readmultiple、vector、list、table等旧的、零散且硬编码了具体文件路径的测试方法及其大量空行。
    
    功能调整方面：
    - loadAllSamples在读取每个样例后，不再调用 R.Inspect输出完整信息，而是改用新增的 summarize方法打印简短、便于 grep比对的摘要。
    -新增 summarize与 describe：对 list打印长度及每个槽位的名称与取值摘要；对 dataframe打印行列数与列名；其他对象则打印类型、长度及前几个元素（向量最多显示前3个元素）。
    
    影响：测试程序聚焦于批量加载并校验 RDS样例，输出更紧凑，便于与 GNU R的结果进行快速比对与回归检查，同时删除的旧方法不再引用已过时的测试数据路径。

--- studio/RData/test/Module1.vb
+++ studio/RData/test/Module1.vb
@@ -1,58 +1,4 @@
-﻿#Region "Microsoft.VisualBasic::dabf2b53501f95b6469004f951222940, studio\RData\test\Module1.vb"
-
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
-
-
-
-    ' /********************************************************************************/
-
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 240
-    '    Code Lines: 99 (41.25%)
-    ' Comment Lines: 9 (3.75%)
-    '    - Xml Docs: 0.00%
-    ' 
-    '   Blank Lines: 132 (55.00%)
-    '     File Size: 4.40 KB
-
-
-    ' Module Module1
-    ' 
-    '     Sub: list, listIO, Main, readmultiple, table
-    '          testRealExample, vector
-    ' 
-    ' /********************************************************************************/
-
-#End Region
-
-Imports System.IO
+﻿Imports System.IO
 Imports SMRUCC.Rsharp.Interpreter
 Imports SMRUCC.Rsharp.RDataSet
 Imports SMRUCC.Rsharp.RDataSet.Convertor
@@ -96,7 +42,7 @@ Module Module1
                     Dim obj = Reader.ParseData(stream, debug:=False)
                     Dim value = ConvertToR.ToRObject(obj.object)
 
-                    Call R.Inspect(value)
+                    Call summarize(value, file)
                 End Using
             Catch ex As Exception
                 Call Console.WriteLine($"!!! FAILED to read {file}: {ex.GetType.Name}: {ex.Message}")
@@ -105,219 +51,51 @@ Module Module1
         Next
     End Sub
 
-    Sub listIO()
-        Dim list As list
+    ''' <summary>
+    ''' Print a short, greppable summary of the read value.
+    ''' </summary>
+    Sub summarize(value As Object, file As String)
+        If TypeOf value Is list Then
+            Dim l As list = value
 
-        Using file = "test_list2.rda".Open
-            Dim obj = Reader.ParseData(file, debug:=True)
-            list = ConvertToR.ToRObject(obj.object)
+            Call Console.WriteLine($"  type=list, length={l.length}")
 
-            Call R.Inspect(list)
-        End Using
+            For Each name In l.getNames
+                Dim v = l(name)
 
-        Using save = "test_write.rda".Open(FileMode.OpenOrCreate, doClear:=True, [readOnly]:=False)
-            'list.add("save_table", New dataframe With {.columns = New Dictionary(Of String, Array) From {
-            '    {"col1", {3, 4, 5, 5, 5}},
-            '    {"xxx", {"qqq", "www", "eee", "rr", "r"}}
-            '}})
+                Call Console.WriteLine($"    ${name} -> {describe(v)}")
+            End If
+        ElseIf TypeOf value Is dataframe Then
+            Dim df As dataframe = value
 
-            Dim innerList = New list With {
-                .slots = New Dictionary(Of String, Object) From {
-                    {"aaa_x", New list With {
-                        .slots = New Dictionary(Of String, Object) From {
-                            {"bbx", 333}
-                        }
-                    }}
-                }
-            }
-
-            list.add("Test_inner", innerList)
-
-            Call Writer.Save(list, save)
-        End Using
-
-        Call Console.WriteLine(vbNewLine)
-
-        Using read = "test_write.rda".Open
-            Dim obj = Reader.ParseData(read, debug:=True)
-
-            list = ConvertToR.ToRObject(obj.object)
-
-            Call R.Inspect(list)
-        End Using
+            Call Console.WriteLine($"  type=dataframe, rows={df.nrows}, cols={df.ncols}")
+            Call Console.WriteLine($"    columns: {df.getColumns(True).JoinBy(", ")}")
+        Else
+            Call Console.WriteLine($"  {describe(value)}")
+        End If
     End Sub
 
-    Sub testRealExample()
-        Using file = "E:\biodeep\biodeep_pipeline\biodeepNPSearch\data\Flavonoid.rda".Open
-            Dim obj As RData = Reader.ParseData(file)
-            Dim vec = ConvertToR.ToRObject(obj.object)
-
-            Call R.Inspect(vec)
-        End Using
-    End Sub
-
-    Sub readmultiple()
-        Using file = "E:\GCModeller\src\R-sharp\studio\test\data\multiple_object.rda".Open
-            Dim obj As RData = Reader.ParseData(file)
-            Dim vec = ConvertToR.ToRObject(obj.object)
-
-            Call R.Print(vec)
-        End Using
-
-        Call Console.WriteLine()
-        Call Console.WriteLine()
-        Call Console.WriteLine("-------------------------------------------------------")
-    End Sub
-
-    Sub vector()
-        Using file = "test_vector.rda".Open
-            Dim obj As RData = Reader.ParseData(file)
-            Dim vec = ConvertToR.ToRObject(obj.object)
-
-            Call R.Print(vec)
-        End Using
-
-        Using file = "test_logical.rda".Open
-            Dim obj = Reader.ParseData(file)
-            Dim vec = ConvertToR.ToRObject(obj.object)
-
-            Call R.Print(vec)
-        End Using
-
-        Pause()
-    End Sub
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    Sub list()
-        Using file = "test_list.rda".Open
-            Dim obj As RData = Reader.ParseData(file)
-            Dim lst = ConvertToR.ToRObject(obj.object)
-
-            Call R.Inspect(lst)
-        End Using
-
-        Using file = "test_list2.rda".Open
-            Dim obj As RData = Reader.ParseData(file)
-            Dim lst = ConvertToR.ToRObject(obj.object)
-
-            Call R.Inspect(lst)
-        End Using
-    End Sub
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    Sub table()
-        Using file = "test_dataframe2.rda".Open
-            Dim obj = Reader.ParseData(file)
-            Dim tbl = ConvertToR.ToRObject(obj.object)
-
-            Call R.Print(tbl)
-        End Using
-
-        Using file = "test_dataframe_v3.rda".Open
-            Dim obj = Reader.ParseData(file)
-            Dim tbl = ConvertToR.ToRObject(obj.object)
-
-            Call R.Print(tbl)
-        End Using
-
-        Using file = "test_dataframe.rda".Open
-            Dim obj = Reader.ParseData(file)
-            Dim tbl = ConvertToR.ToRObject(obj.object)
-
-            Call R.Print(tbl)
-        End Using
-    End Sub
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    Function describe(v As Object) As String
+        If v Is Nothing Then
+            Return "NULL"
+        End If
+
+        If TypeOf v Is Array Then
+            Dim a As Array = v
+            Dim n As Integer = a.Length
+
+            If n = 0 Then
+                Return $"vector[0]"
+            End If
+
+            Dim head As String = $"[{a.GetValue(0)}"
+            If n > 1 Then head &= $", {a.GetValue(1)}"
+            If n > 2 Then head &= $", {a.GetValue(2)}"
+            head &= "...]"
+
+            Return $"vector[{n}] = {head}"
+        End If
+
+        Return v.GetType.Name & " = " & v.ToString
+    End Function
 End Module
