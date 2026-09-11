7298a604eabeac02c7a1f64805288e1d5777a2a2
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-09-12 04:27:20 +0800

    export cuda api

---
+++ studio/Rsharp_kit/MLkit/MachineLearning/CUDA.vb
@@ -0,0 +1,31 @@
+﻿Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports gpu = Microsoft.VisualBasic.Computing.ILCuda.GPUTensor
+Imports ILCudaRuntime = Microsoft.VisualBasic.Computing.ILCuda.Runtime
+Imports tfCompute = Microsoft.VisualBasic.MachineLearning.TensorFlow.Compute
+
+<Package("CUDA")>
+Module CUDATools
+
+    <ExportAPI("register_cuda_tensor")>
+    Public Function register_cuda_tensor() As Boolean
+        Dim opts As New ILCudaRuntime.EngineOptions()
+
+        If Not gpu.CudaTensor.Register(opts) Then
+            Console.WriteLine($"    注册失败: {gpu.CudaTensor.LastError}")
+
+            For Each line In opts.Diagnostics
+                Console.WriteLine($"      {line}")
+            Next
+
+            Return False
+        Else
+            Return True
+        End If
+    End Function
+
+    <ExportAPI("unregister_cuda")>
+    Public Sub unregister_cuda()
+        Call tfCompute.SIMDTensor.Register()
+    End Sub
+End Module
---
+++ studio/Rsharp_kit/MLkit/MachineLearning/tensorTools.vb
@@ -0,0 +1,9 @@
+﻿Imports Microsoft.VisualBasic.MachineLearning.TensorFlow
+Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.Rsharp.Runtime.Interop
+
+<Package("tensorflow")>
+<RTypeExport("tensor", GetType(Tensor))>
+Module tensorTools
+
+End Module
